142fab05e3d8279b1dae633f878c917f068832b3
Anthony Bronico <adbronico92@gmail.com>
2024-08-20 09:19:36 -0400

    Working on implementing DeathTrooper

---
+++ server/game/cardImplementations/01_SOR/DeathTrooper.ts
@@ -0,0 +1,38 @@
+import AbilityHelper from '../../AbilityHelper';
+import { AbilityContext } from '../../core/ability/AbilityContext';
+import Card from '../../core/card/Card';
+import { CardType, Location, RelativePlayer } from '../../core/Constants';
+
+export default class DeathTrooper extends Card {
+    protected override getImplementationId() {
+        return {
+            id: '6458912354',
+            internalName: 'death-trooper'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.whenPlayedAbility({
+            title: 'Deal 2 damage to a friendly ground unit and an enemy ground unit',
+            optional: false,
+            targetResolvers: {
+                myGroundUnit: {
+                    cardType: CardType.Unit,
+                    controller: RelativePlayer.Self,
+                    locationFilter: Location.GroundArena,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 2 })
+                },
+                theirGroundUnit: {
+                    cardType: CardType.Unit,
+                    controller: RelativePlayer.Opponent,
+                    locationFilter: Location.GroundArena,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 2 })
+                }
+            },
+            effect: 'deal 2 damage to {1} and {2}',
+            effectArgs: (context) => [context.target.myGroundUnit, context.target.theirGroundUnit]
+        });
+    }
+}
+
+DeathTrooper.implemented = false;
---
+++ test/server/cardImplementations/01_SOR/DeathTrooper.spec.js
@@ -0,0 +1,65 @@
+const exp = require('constants');
+
+describe('Death Trooper', function() {
+    integration(function() {
+        describe('Death Trooper\'s When Played ability', function() {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['death-trooper'],
+                        groundArena: ['pyke-sentinel'],
+                        spaceArena: ['cartel-spacer'],
+                        leader: ['director-krennic#aspiring-to-authority']
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        spaceArena: ['imperial-interceptor']
+                    }
+                });
+
+                this.deathTrooper = this.player1.findCardByName('death-trooper');
+                this.pykeSentinel = this.player1.findCardByName('pyke-sentinel');
+                this.cartepSpacer = this.player1.findCardByName('cartel-spacer');
+
+                this.wampa = this.player2.findCardByName('wampa');
+                this.interceptor = this.player2.findCardByName('imperial-interceptor');
+
+                this.p1Base = this.player1.base;
+                this.p2Base = this.player2.base;
+
+                this.noMoreActions();
+            });
+
+            it('death trooper when played cannot be passed', function () {
+                // Play Death Trooper
+                this.player1.clickCard(this.deathTrooper);
+                expect(this.player1).toHavePrompt('');
+                expect(this.deathTrooper.location).toBe('ground arena');
+                expect(this.player1).toBeAbleToSelectAllOf([this.pykeSentinel, this.deathTrooper]);
+                expect(this.player1).toBeAbleToSelectNoneOf([this.interceptor, this.cartepSpacer, this.wampa]);
+                expect(this.player1).toNotHavePassAbilityPrompt();
+            });
+
+            // it('death trooper can deal damage to itself', function () {
+            //     // Play Death Trooper
+            //     this.player1.clickCard(this.deathTrooper);
+            //     expect(this.deathTrooper.location).toBe('ground arena');
+
+
+            //     // Choose Friendly
+            //     expect(this.player1).toBeAbleToSelectAllOf([this.pykeSentinel, this.deathTrooper]);
+            //     expect(this.player1).toBeAbleToSelectNoneOf([this.interceptor, this.cartepSpacer, this.wampa]);
+            //     expect(this.player1).toNotHavePassAbilityPrompt();
+            //     this.player1.clickCard(this.deathTrooper);
+
+            //     // Choose Enemy
+            //     expect(this.deathTrooper.damage).toEqual(2);
+            //     expect(this.player1).toBeABleToSelectAllof([this.wampa]);
+            //     expect(this.player1).toBeABleToSelectNoneOf([this.pykeSentinel, this.deathTrooper, this.interceptor, this.cartepSpacer]);
+            //     this.player1.clickCard(this.wampa);
+            //     expect(this.wampa.damage).toEqual(2);
+            // });
+        });
+    });
+});
