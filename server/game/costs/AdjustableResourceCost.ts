166899301965e80a36c6ff527664de1457f43ad9
AMMayberry1 <ammayberry1@gmail.com>
2024-08-30 18:03:33 -0400

    Quick note

--- server/game/costs/AdjustableResourceCost.ts
+++ server/game/costs/AdjustableResourceCost.ts
@@ -3,6 +3,7 @@ import { EventName, Location, RelativePlayer } from '../core/Constants';
 import type { ICost, Result } from '../core/cost/ICost';
 import { GameEvent } from '../core/event/GameEvent';
 
+// TODO THIS PR: rename this to just "ResourceCost" and add a docstr explaining the adjustability
 export class AdjustableResourceCost implements ICost {
     public isPlayCost = true;
     public isPrintedResourceCost = true;
