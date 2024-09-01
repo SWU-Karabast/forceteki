d1072a8501a708a779f5a9a8c87016ed5ab6c03a
AMMayberry1 <ammayberry1@gmail.com>
2024-08-31 20:11:04 -0400

    Rename adjustableResourceCost -> resourceCost

--- server/game/costs/ResourceCost.ts
+++ server/game/costs/ResourceCost.ts
@@ -4,7 +4,7 @@ import type { ICost, Result } from '../core/cost/ICost';
 import { GameEvent } from '../core/event/GameEvent';
 
 // TODO THIS PR: rename this to just "ResourceCost" and add a docstr explaining the adjustability
-export class AdjustableResourceCost implements ICost {
+export class ResourceCost implements ICost {
     public isPlayCost = true;
     public isPrintedResourceCost = true;
 
