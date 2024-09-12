9510af13d6ce132ea16ea0e02161f6c39844efa6
AMMayberry1 <ammayberry1@gmail.com>
2024-09-12 18:17:58 -0400

    Add more pass tests (still some failing) and add Mando

--- test/server/cards/02_SHD/Headhunting.spec.js
+++ test/server/cards/02_SHD/Headhunting.spec.js
@@ -6,9 +6,9 @@ describe('Headhunting', function() {
                     phase: 'action',
                     player1: {
                         hand: ['headhunting'],
-                        groundArena: ['atst', 'reputable-hunter', 'discerning-veteran', 'wampa', { card: 'battlefield-marine', exhausted: true }],
+                        groundArena: ['atst', 'reputable-hunter', 'wampa', { card: 'battlefield-marine', exhausted: true }],
                         spaceArena: ['tieln-fighter'],
-                        leader: 'the-mandalorian#sworn-to-the-creed'
+                        leader: { card: 'the-mandalorian#sworn-to-the-creed', deployed: true }
                     },
                     player2: {
                         groundArena: ['bounty-guild-initiate', 'consular-security-force'],
@@ -18,12 +18,13 @@ describe('Headhunting', function() {
 
             it('should attack with three units', function () {
                 this.player1.clickCard(this.headhunting);
-                expect(this.player1).toBeAbleToSelectExactly([this.atst, this.reputableHunter, this.discerningVeteran, this.wampa]);
-                expect(this.player1).toHaveEnabledPromptButton('Pass ability'); // TODO THIS PR: fix this
 
                 // first attack, bounty hunter
+                expect(this.player1).toBeAbleToSelectExactly([this.atst, this.reputableHunter, this.theMandalorian, this.wampa]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability'); // TODO THIS PR: fix this
                 this.player1.clickCard(this.reputableHunter);
                 expect(this.player1).toBeAbleToSelectExactly([this.bountyGuildInitiate, this.consularSecurityForce]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability');
                 this.player1.clickCard(this.consularSecurityForce);
                 expect(this.reputableHunter.exhausted).toBe(true);
                 expect(this.consularSecurityForce.damage).toBe(5);
@@ -31,23 +32,27 @@ describe('Headhunting', function() {
 
                 // second attack, non-bounty-hunter
                 this.consularSecurityForce.damage = 0;
-                expect(this.player1).toBeAbleToSelectExactly([this.atst, this.discerningVeteran, this.wampa]);
+                expect(this.player1).toBeAbleToSelectExactly([this.atst, this.theMandalorian, this.wampa]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability'); // TODO THIS PR: fix this
                 this.player1.clickCard(this.atst);
                 expect(this.player1).toBeAbleToSelectExactly([this.bountyGuildInitiate, this.consularSecurityForce]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability');
                 this.player1.clickCard(this.consularSecurityForce);
                 expect(this.atst.exhausted).toBe(true);
                 expect(this.consularSecurityForce.damage).toBe(6);
                 expect(this.atst.damage).toBe(3);
 
-                // third attack, bounty hunter
+                // third attack, leader bounty hunter
                 this.consularSecurityForce.damage = 0;
-                expect(this.player1).toBeAbleToSelectExactly([this.discerningVeteran, this.wampa]);
-                this.player1.clickCard(this.discerningVeteran);
+                expect(this.player1).toBeAbleToSelectExactly([this.theMandalorian, this.wampa]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability'); // TODO THIS PR: fix this
+                this.player1.clickCard(this.theMandalorian);
                 expect(this.player1).toBeAbleToSelectExactly([this.bountyGuildInitiate, this.consularSecurityForce]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability');
                 this.player1.clickCard(this.consularSecurityForce);
-                expect(this.discerningVeteran.exhausted).toBe(true);
-                expect(this.consularSecurityForce.damage).toBe(5);
-                expect(this.discerningVeteran.damage).toBe(3);
+                expect(this.theMandalorian.exhausted).toBe(true);
+                expect(this.consularSecurityForce.damage).toBe(6);
+                expect(this.theMandalorian.damage).toBe(3);
 
                 expect(this.player2).toBeActivePlayer();
             });
