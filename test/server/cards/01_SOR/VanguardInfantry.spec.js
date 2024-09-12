41713144d1cabcd2f70a41c16a5d5b57280c29f0
AMMayberry1 <ammayberry1@gmail.com>
2024-09-12 14:11:05 -0400

    Finalize Vanguard Infantry tests

--- test/server/cards/01_SOR/VanguardInfantry.spec.js
+++ test/server/cards/01_SOR/VanguardInfantry.spec.js
@@ -24,6 +24,26 @@ describe('Vanguard Infantry', function() {
                 this.player1.clickCard(this.cartelSpacer);
                 expect(this.cartelSpacer).toHaveExactUpgradeNames(['experience']);
             });
+
+            it('should give an experience token to a unit when defeated by an ability', function () {
+                this.player1.clickCard(this.vanquish);
+                this.player1.clickCard(this.vanguardInfantry);
+                expect(this.player1).toBeAbleToSelectExactly([this.battlefieldMarine, this.wampa, this.cartelSpacer]);
+                expect(this.player1).toHaveEnabledPromptButton('Pass ability');
+
+                this.player1.clickCard(this.battlefieldMarine);
+                expect(this.battlefieldMarine).toHaveExactUpgradeNames(['experience']);
+            });
+
+            it('should be able to be passed', function () {
+                this.player1.clickCard(this.vanguardInfantry);
+                this.player1.clickCard(this.wampa);
+                this.player1.clickPrompt('Pass ability');
+
+                expect(this.battlefieldMarine).toHaveExactUpgradeNames([]);
+                expect(this.wampa).toHaveExactUpgradeNames([]);
+                expect(this.cartelSpacer).toHaveExactUpgradeNames([]);
+            });
         });
     });
 });
