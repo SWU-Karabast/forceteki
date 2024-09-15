a038fe3dedca47e5b3996a44c86a1605566965b8
Ennis0 <ennis0@protonmail.com>
2024-09-14 22:58:10 -0400

    Added more testing to Chewbacca to confirm his trigger can activate multiple times a phase. Put in a file for testing triggers triggering simultaneously. Right now it only has the case of Sabine attacking Chewbacca, which works as expected.

--- test/server/cards/01_SOR/ChewbaccaLoyalCompanion.spec.js
+++ test/server/cards/01_SOR/ChewbaccaLoyalCompanion.spec.js
@@ -5,7 +5,7 @@ describe('Chewbacca, Loyal Companion', function() {
                 this.setupTest({
                     phase: 'action',
                     player1: {
-                        groundArena: ['wampa']
+                        groundArena: ['specforce-soldier', 'alliance-dispatcher']
                     },
                     player2: {
                         groundArena: [{ card: 'chewbacca#loyal-companion', exhausted: true }]
@@ -14,8 +14,16 @@ describe('Chewbacca, Loyal Companion', function() {
             });
 
             it('readies Chewbacca when he is attacked', function () {
-                this.player1.clickCard(this.wampa);
+                expect(this.chewbacca.exhausted).toBe(true);
+                this.player1.clickCard(this.specforceSoldier);
                 //Don't need to click Chewbacca due to sentinel
+                expect(this.chewbacca.exhausted).toBe(false);
+
+                this.player2.clickCard(this.chewbacca);
+                this.player2.clickCard(this.p1Base);
+                expect(this.chewbacca.exhausted).toBe(true);
+
+                this.player1.clickCard(this.allianceDispatcher);
                 expect(this.chewbacca.exhausted).toBe(false);
             });
         });
