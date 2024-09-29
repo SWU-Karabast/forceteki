9c21ee2fa2151153d9e6663daabbecc41834cc65
Anthony Bronico <adbronico92@gmail.com>
2024-09-29 12:58:04 -0400

    Hotshot Blaster

--- test/server/cards/02_SHD/upgrades/HotshotDL44Blaster.spec.js
+++ test/server/cards/02_SHD/upgrades/HotshotDL44Blaster.spec.js
@@ -1,27 +1,27 @@
 describe('Hotshot DL-44 Blaster', function() {
     integration(function() {
-        // describe('Hotshot DL-44 Blaster\'s ability', function() {
-        //     beforeEach(function () {
-        //         this.setupTest({
-        //             phase: 'action',
-        //             player1: {
-        //                 groundArena: ['battlefield-marine'],
-        //                 spaceArena: ['collections-starhopper'],
-        //                 hand: ['hotshot-dl44-blaster']
-        //             },
-        //             base: 'tarkintown'
-        //         });
-        //     });
+        describe('Hotshot DL-44 Blaster\'s ability', function() {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['battlefield-marine'],
+                        spaceArena: ['collections-starhopper'],
+                        hand: ['hotshot-dl44-blaster']
+                    },
+                    base: 'tarkintown'
+                });
+            });
 
-        //     it('does not initiate an attack when played from hand', function () {
-        //         this.player1.clickCard(this.hotshotDl44Blaster);
-        //         expect(this.player2).toBeActivePlayer();
-        //         expect(this.hotshotDl44Blaster).toBeInLocation('ground arena');
-        //         expect(this.battlefieldMarine.exhausted).toBe(false);
-        //         expect(this.battlefieldMarine.getPower()).toBe(5);
-        //         expect(this.battlefieldMarine.getHp()).toBe(3);
-        //     });
-        // });
+            it('does not initiate an attack when played from hand', function () {
+                this.player1.clickCard(this.hotshotDl44Blaster);
+                expect(this.player2).toBeActivePlayer();
+                expect(this.hotshotDl44Blaster).toBeInLocation('ground arena');
+                expect(this.battlefieldMarine.exhausted).toBe(false);
+                expect(this.battlefieldMarine.getPower()).toBe(5);
+                expect(this.battlefieldMarine.getHp()).toBe(3);
+            });
+        });
 
         describe('Hotshot DL-44 Blaster\'s ability', function() {
             beforeEach(function () {
@@ -40,7 +40,6 @@ describe('Hotshot DL-44 Blaster', function() {
 
             it('initiates an attack with the upgraded unit when Smuggled', function () {
                 this.player1.clickCard(this.hotshotDl44Blaster);
-                expect(this.player1).toBeAbleToSelectExactly(this.p2Base);
                 expect(this.player2).toBeActivePlayer();
                 expect(this.hotshotDl44Blaster).toBeInLocation('ground arena');
                 expect(this.battlefieldMarine.upgrades).toContain(this.hotshotDl44Blaster);
