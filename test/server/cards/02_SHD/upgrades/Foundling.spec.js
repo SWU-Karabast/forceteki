4bb9b5a278203b0472de895dc79531df7eee28bf
Valentin Jubert <valentin.jubert@ideine.fr>
2024-10-03 21:30:18 +0200

    add test to foundling

--- test/server/cards/02_SHD/upgrades/Foundling.spec.js
+++ test/server/cards/02_SHD/upgrades/Foundling.spec.js
@@ -5,8 +5,8 @@ describe('Foundling', function () {
                 this.setupTest({
                     phase: 'action',
                     player1: {
-                        hand: ['foundling'],
-                        groundArena: ['battlefield-marine', 'pyke-sentinel'],
+                        hand: ['foundling', 'mandalorian-warrior'],
+                        groundArena: ['battlefield-marine', 'pyke-sentinel', 'protector-of-the-throne'],
                     },
                     player2: {
                         groundArena: ['snowspeeder']
@@ -20,12 +20,17 @@ describe('Foundling', function () {
 
                 // add battlefield marine foundling
                 this.player1.clickCard(this.foundling);
-                expect(this.player1).toBeAbleToSelectExactly([this.battlefieldMarine, this.snowspeeder, this.pykeSentinel]);
+                expect(this.player1).toBeAbleToSelectExactly([this.battlefieldMarine, this.snowspeeder, this.pykeSentinel, this.protectorOfTheThrone]);
                 this.player1.clickCard(this.battlefieldMarine);
 
                 // check players units traits
                 expect(this.battlefieldMarine.hasSomeTrait('mandalorian')).toBeTrue();
                 expect(this.pykeSentinel.hasSomeTrait('mandalorian')).toBeFalse();
+
+                this.player2.pass();
+                // mandalorian warrior should be able to add experience to battlefield marine
+                this.player1.clickCard(this.mandalorianWarrior);
+                expect(this.player1).toBeAbleToSelectExactly([this.battlefieldMarine, this.protectorOfTheThrone]);
             });
         });
     });
