f3e0fc4b8574c6ed8e69e622806e22b2df06cc01
Valentin Jubert <valentin.jubert@ideine.fr>
2024-10-03 17:01:10 +0200

    fix pr

--- test/server/cards/02_SHD/units/SupercommandoSquad.spec.js
+++ test/server/cards/02_SHD/units/SupercommandoSquad.spec.js
@@ -12,18 +12,25 @@ describe('Supercommando Squad', function() {
                     }
                 });
             });
-
             it('should give it sentinel only as long as it is upgraded', function () {
                 this.player1.pass();
+
                 this.player2.clickCard(this.wampa);
                 //Supercommando Squad automatically selected due to sentinel
+
                 expect(this.player1).toBeActivePlayer();
                 //no damage because of shield
                 expect(this.supercommandoSquad.damage).toBe(0);
+                expect(this.supercommandoSquad.isUpgraded()).toBe(false);
+                expect(this.wampa.damage).toBe(4);
+
                 this.player1.pass();
+
                 this.player2.clickCard(this.jedhaAgitator);
+
                 //player 2 should be able to select base and unit because supercommando squad is not sentinel anymore
                 expect(this.player2).toBeAbleToSelectExactly([this.supercommandoSquad, this.p1Base]);
+
                 this.player2.clickCard(this.p1Base);
             });
         });
