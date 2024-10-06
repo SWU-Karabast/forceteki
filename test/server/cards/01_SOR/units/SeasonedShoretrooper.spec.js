47ea33aa27b80057376b8d8c35719251479d7302
Valentin Jubert <valentin.jubert@ideine.fr>
2024-10-06 15:42:18 +0200

    militia + trooper

--- test/server/cards/01_SOR/units/SeasonedShoretrooper.spec.js
+++ test/server/cards/01_SOR/units/SeasonedShoretrooper.spec.js
@@ -5,8 +5,9 @@ describe('Seasoned Shoretrooper', function () {
                 this.setupTest({
                     phase: 'action',
                     player1: {
+                        hand: ['wampa', 'battlefield-marine'],
                         groundArena: ['seasoned-shoretrooper'],
-                        resources: ['distant-patroller', 'distant-patroller', 'distant-patroller', 'distant-patroller', 'distant-patroller']
+                        resources: 5
                     },
                     player2: {
                         groundArena: ['rugged-survivors']
@@ -19,7 +20,7 @@ describe('Seasoned Shoretrooper', function () {
                 expect(this.seasonedShoretrooper.getPower()).toBe(2);
 
                 // valball : pass to refresh state as I cheat a bit with resources
-                this.player1.resources.push('distant-patroller');
+                this.player1.moveCard(this.wampa, 'resource');
                 this.player1.pass();
 
                 // with 6 resources, power should be buff
@@ -28,10 +29,20 @@ describe('Seasoned Shoretrooper', function () {
                 expect(this.seasonedShoretrooper.getHp()).toBe(3);
 
                 // with 6 or more resources, power should be buff
-                this.player1.resources.push('distant-patroller');
-                this.player2.pass();
+                this.player1.moveCard(this.battlefieldMarine, 'resource');
+                // soft-pass
+                this.player2.clickCard(this.ruggedSurvivors);
+                this.player2.clickCard(this.p1Base);
                 expect(this.player1.resources.length).toBe(7);
                 expect(this.seasonedShoretrooper.getPower()).toBe(4);
+                expect(this.seasonedShoretrooper.getHp()).toBe(3);
+
+                // remove 2 resources : seasoned shoretrooper lost its buff
+                this.player1.moveCard(this.battlefieldMarine, 'hand', 'resource');
+                this.player1.moveCard(this.wampa, 'hand', 'resource');
+                this.player1.pass();
+                expect(this.player1.resources.length).toBe(5);
+                expect(this.seasonedShoretrooper.getPower()).toBe(2);
                 expect(this.seasonedShoretrooper.getHp()).toBe(3);
             });
         });
