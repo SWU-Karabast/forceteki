9c3da8b714db46efb85021e3c7b48c07ed58570f
Matevz <matevz.ogrinc@ijs.si>
2024-10-06 19:35:38 +0200

    #Maximum-fire-power  - made it somehow work.  - Need confirmation on the continued effect.

--- server/game/cards/01_SOR/events/MaximumFirePower.ts
+++ server/game/cards/01_SOR/events/MaximumFirePower.ts
@@ -1,8 +1,7 @@
 import {EventCard} from '../../../core/card/EventCard';
-import {RelativePlayer, Trait, WildcardCardType, WildcardLocation} from '../../../core/Constants';
+import {Location, RelativePlayer, Trait, WildcardCardType, WildcardLocation} from '../../../core/Constants';
 import AbilityHelper from '../../../AbilityHelper';
 import * as EnumHelpers from '../../../core/utils/EnumHelpers';
-
 export default class MaximumFirePower extends EventCard {
     protected override getImplementationId () {
         return {
@@ -13,40 +12,39 @@ export default class MaximumFirePower extends EventCard {
 
     public override setupCardAbilities () {
         this.setEventAbility({
-            title: 'A friendly Imperial unit deals damage equal to its power to a unit.  Then, another friendly Imperial unit deals damage equal to its power to the same unit.',
+            title: 'A friendly Imperial unit deals damage equal to its power to a unit.',
             targetResolvers: {
                 firstImperial: {
                     controller: RelativePlayer.Self,
                     locationFilter: WildcardLocation.AnyArena,
                     cardTypeFilter: WildcardCardType.Unit,
                     cardCondition: (card) => card.hasSomeTrait(Trait.Imperial),
-                    immediateEffect: AbilityHelper.immediateEffects.noAction(),
                 },
-                targetCard: {
+                selectedTarget: {
                     cardTypeFilter: WildcardCardType.Unit,
                     locationFilter: WildcardLocation.AnyArena,
                     dependsOn: 'firstImperial',
                     immediateEffect: AbilityHelper.immediateEffects.damage((context) => {
-                        return { target: context.targets.targetCard, amount: context.targets.firstImperial.getPower() };
-                    })
+                        return { target: context.targets.selectedTarget, amount: context.targets.firstImperial.getPower() };
+                    }),
                 },
-                secondImperial: {
+            },
+            then: (context) => ({
+                title: 'Then, another friendly Imperial unit deals damage equal to its power to the same unit.',
+                targetResolver: {
                     controller: RelativePlayer.Self,
-                    dependsOn: 'targetCard',
                     locationFilter: WildcardLocation.AnyArena,
                     cardTypeFilter: WildcardCardType.Unit,
-                    cardCondition: (card, context) => card.hasSomeTrait(Trait.Imperial) && context.target.firstImperial !== card,
-                    immediateEffect: AbilityHelper.immediateEffects.conditional({
-                        condition: (context) => EnumHelpers.isArena(context.targets.targetCard.Location),
-                        onTrue: AbilityHelper.immediateEffects.damage((context) => {
-                            return { target: context.targets.targetCard, amount: context.targets.secondImperial.getPower() };
-                        }),
+                    cardCondition: (card) => card.hasSomeTrait(Trait.Imperial) && card !== context.targets.firstImperial,
+                    /*immediateEffect: AbilityHelper.immediateEffects.damage((context2) =>
+                        ({ target: context.targets.selectedTarget, amount: context2.target.getPower() })),*/
+                    immediateEffect: AbilityHelper.immediateEffects.conditional((context2) => ({
+                        condition: context.targets.selectedTarget.location === Location.Discard,
+                        onTrue: AbilityHelper.immediateEffects.damage({ target: context.targets.selectedTarget, amount: context2.target.getPower() }),
                         onFalse: AbilityHelper.immediateEffects.noAction()
-                    })
+                    }))
                 }
-            },
-            effect: 'A {1} deals damage equal to its power to a {2}. Then, another {3} deals damage equal to its power to the same {2}.',
-            effectArgs: (context) => [context.targets.firstImperial, context.targets.targetCard, context.targets.secondImperial],
+            }),
         });
     }
 }
--- test/server/cards/01_SOR/events/MaximumFirePower.spec.js
+++ test/server/cards/01_SOR/events/MaximumFirePower.spec.js
@@ -7,7 +7,7 @@ describe('Maximum Firepower', function() {
                     player1: {
                         hand: ['maximum-firepower'],
                         groundArena: ['death-trooper'],
-                        spaceArena: ['tieln-fighter'],
+                        spaceArena: ['tieln-fighter', 'gladiator-star-destroyer'],
                     },
                     player2: {
                         groundArena: ['wampa']
@@ -18,7 +18,19 @@ describe('Maximum Firepower', function() {
             it('should deal 5 damage to wampa and friendly units aren\'t exhausted', function () {
                 // action play Maximum Firepower
                 this.player1.clickCard(this.maximumFirepower);
-                expect(this.player1).toBeAbleToSelectExactly([this.deathTrooper, this.tielnFighter]);
+                expect(this.player1).toBeAbleToSelectExactly([this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
+                this.player1.clickCard(this.deathTrooper);
+
+                // action check if wampa can be selected
+                expect(this.player1).toBeAbleToSelectExactly([this.wampa, this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
+                this.player1.clickCard(this.wampa);
+
+                // third action set
+                expect(this.wampa.location).toBe('ground arena');
+                // since there is nothing else to be selected it selects tieln-fighter and deals damage
+                expect(this.player1).toBeAbleToSelectExactly([this.tielnFighter, this.gladiatorStarDestroyer]);
+                this.player1.clickCard(this.tielnFighter);
+                // expect(this.deathTrooper).toHaveExactUpgradeNames(['shield']);
             });
         });
     });
