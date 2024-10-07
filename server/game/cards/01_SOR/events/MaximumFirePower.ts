2f10712fe573ba00415a2e8c6ea5a0db983d1a54
Matevz <matevz.ogrinc@ijs.si>
2024-10-07 07:39:47 +0200

    #Maximum-fire-power  - Implemented a possible way for maximum fire power.  - added tests for maximum fire power.

--- server/game/cards/01_SOR/events/MaximumFirePower.ts
+++ server/game/cards/01_SOR/events/MaximumFirePower.ts
@@ -1,5 +1,10 @@
-import {EventCard} from '../../../core/card/EventCard';
-import {Location, RelativePlayer, Trait, WildcardCardType, WildcardLocation} from '../../../core/Constants';
+import { EventCard } from '../../../core/card/EventCard';
+import {
+    RelativePlayer,
+    Trait,
+    WildcardCardType,
+    WildcardLocation
+} from '../../../core/Constants';
 import AbilityHelper from '../../../AbilityHelper';
 import * as EnumHelpers from '../../../core/utils/EnumHelpers';
 export default class MaximumFirePower extends EventCard {
@@ -21,13 +26,14 @@ export default class MaximumFirePower extends EventCard {
                     cardCondition: (card) => card.hasSomeTrait(Trait.Imperial),
                 },
                 selectedTarget: {
-                    cardTypeFilter: WildcardCardType.Unit,
-                    locationFilter: WildcardLocation.AnyArena,
                     dependsOn: 'firstImperial',
-                    immediateEffect: AbilityHelper.immediateEffects.damage((context) => {
-                        return { target: context.targets.selectedTarget, amount: context.targets.firstImperial.getPower() };
-                    }),
-                },
+                    locationFilter: WildcardLocation.AnyArena,
+                    cardTypeFilter: WildcardCardType.Unit,
+                    cardCondition: (_, context) => (context.player.getUnitsInPlay(WildcardLocation.AnyArena, (card) =>
+                        card.hasSomeTrait(Trait.Imperial)).length > 0),
+                    immediateEffect: AbilityHelper.immediateEffects.damage((context) =>
+                        ({ amount: context.targets.firstImperial.getPower() })),
+                }
             },
             then: (context) => ({
                 title: 'Then, another friendly Imperial unit deals damage equal to its power to the same unit.',
@@ -36,15 +42,14 @@ export default class MaximumFirePower extends EventCard {
                     locationFilter: WildcardLocation.AnyArena,
                     cardTypeFilter: WildcardCardType.Unit,
                     cardCondition: (card) => card.hasSomeTrait(Trait.Imperial) && card !== context.targets.firstImperial,
-                    /*immediateEffect: AbilityHelper.immediateEffects.damage((context2) =>
-                        ({ target: context.targets.selectedTarget, amount: context2.target.getPower() })),*/
                     immediateEffect: AbilityHelper.immediateEffects.conditional((context2) => ({
-                        condition: context.targets.selectedTarget.location === Location.Discard,
+                        condition: EnumHelpers.isArena(context.targets.selectedTarget.location),
                         onTrue: AbilityHelper.immediateEffects.damage({ target: context.targets.selectedTarget, amount: context2.target.getPower() }),
                         onFalse: AbilityHelper.immediateEffects.noAction()
                     }))
                 }
-            }),
+            })
+
         });
     }
 }
--- test/server/cards/01_SOR/events/MaximumFirePower.spec.js
+++ test/server/cards/01_SOR/events/MaximumFirePower.spec.js
@@ -15,22 +15,61 @@ describe('Maximum Firepower', function() {
                 });
             });
 
+            it('should defeat wampa when picking Gladiator Stardestroyer first', function() {
+                // action play Maximum Firepower with gladiator star destroyer
+                this.player1.clickCard(this.maximumFirepower);
+                expect(this.player1).toBeAbleToSelectExactly([this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
+                this.player1.clickCard(this.gladiatorStarDestroyer);
+
+                // action check for next unit selection
+                expect(this.player1).toBeAbleToSelectExactly([this.wampa, this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
+                this.player1.clickCard(this.wampa);
+
+                // check game state
+                expect(this.wampa.location).toBe('discard');
+                expect(this.player2).toBeActivePlayer();
+            });
+
             it('should deal 5 damage to wampa and friendly units aren\'t exhausted', function () {
                 // action play Maximum Firepower
                 this.player1.clickCard(this.maximumFirepower);
                 expect(this.player1).toBeAbleToSelectExactly([this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
                 this.player1.clickCard(this.deathTrooper);
 
-                // action check if wampa can be selected
+                // action check where we select wampa
                 expect(this.player1).toBeAbleToSelectExactly([this.wampa, this.deathTrooper, this.tielnFighter, this.gladiatorStarDestroyer]);
                 this.player1.clickCard(this.wampa);
 
-                // third action set
+                // third action set where we select tielnfighter and check if wampa received damage
+                expect(this.wampa.damage).toBe(3);
                 expect(this.wampa.location).toBe('ground arena');
-                // since there is nothing else to be selected it selects tieln-fighter and deals damage
                 expect(this.player1).toBeAbleToSelectExactly([this.tielnFighter, this.gladiatorStarDestroyer]);
                 this.player1.clickCard(this.tielnFighter);
-                // expect(this.deathTrooper).toHaveExactUpgradeNames(['shield']);
+
+                // end game stat
+                expect(this.wampa.location).toBe('discard');
+                expect(this.player2).toBeActivePlayer();
+            });
+        });
+        describe('Maximum Firepower\'s ability', function () {
+            beforeEach(function () {
+                this.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['maximum-firepower'],
+                        groundArena: ['jedha-agitator'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+            });
+
+            it('should not trigger', function() {
+                // action play Maximum Firepower without target
+                this.player1.clickCard(this.maximumFirepower);
+                expect(this.maximumFirepower).toBeInLocation('discard');
+                expect(this.player2).toBeActivePlayer();
             });
         });
     });
