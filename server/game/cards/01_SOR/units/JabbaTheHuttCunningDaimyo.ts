ef8e63771308f28c54a3438d72029efada6483f4
Valentin Jubert <val_jub@live.fr>
2024-10-27 21:48:39 +0100

    jabba 2

--- server/game/cards/01_SOR/units/JabbaTheHuttCunningDaimyo.ts
+++ server/game/cards/01_SOR/units/JabbaTheHuttCunningDaimyo.ts
@@ -1,4 +1,6 @@
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { CardType, Trait } from '../../../core/Constants';
 
 export default class JabbaTheHuttCunningDaimyo extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -9,9 +11,23 @@ export default class JabbaTheHuttCunningDaimyo extends NonLeaderUnitCard {
     }
 
     public override setupCardAbilities() {
-        this.addConstantAbility()
+        this.addConstantAbility({
+            title: 'Each Trick event you play costs 1 resource less',
+            ongoingEffect: AbilityHelper.ongoingEffects.decreaseCost({
+                amount: 1,
+                cardTypeFilter: CardType.Event,
+                match: (card) => card.hasSomeTrait(Trait.Trick)
+            })
+        });
 
-        this.addWhenPlayedAbility();
+        this.addWhenPlayedAbility({
+            title: 'Search the top 8 cards of your deck for a Trick event, reveal it, and draw it',
+            immediateEffect: AbilityHelper.immediateEffects.deckSearch({
+                searchCount: 8,
+                cardCondition: (card) => card.isEvent() && card.hasSomeTrait(Trait.Trick),
+                selectedCardsImmediateEffect: AbilityHelper.immediateEffects.drawSpecificCard()
+            })
+        });
     }
 }
 
