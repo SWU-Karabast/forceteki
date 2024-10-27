b7ff93d3b39621b5458e7f70fd3842f1fd4a9e08
Garrett Chesnut <garrettdc2@gmail.com>
2024-10-27 13:04:54 -0500

    Implement card Attack Pattern Delta (SOR 106)
    SWUDB: https://swudb.com/card/SOR/106/attack-pattern-delta

---
+++ test/server/cards/01_SOR/events/AttackPatternDelta.spec.ts
@@ -0,0 +1,78 @@
+describe('Attack Pattern Delta', function() {
+    integration(function(contextRef) {
+        describe('Attack Pattern Delta\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['attack-pattern-delta'],
+                        groundArena: ['battlefield-marine', 'wampa'],
+                        spaceArena: ['red-three#unstoppable']
+                    },
+                    player2: {
+                        leader: { card: 'luke-skywalker#faithful-friend', deployed: true },
+                    }
+                });
+            });
+
+            it(
+                'Give a friendly unit +3/+3 for this phase. Give another friendly unit +2/+2 for this phase. Give a third friendly unit +1/+1 for this phase.',
+                function () {
+                    const { context } = contextRef;
+
+                    // Case 1: The player can select any unit card.
+                    context.player1.clickCard(context.attackPatternDelta);
+
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.battlefieldMarine,
+                        context.wampa,
+                        context.redThree,
+                        context.lukeSkywalker,
+                    ]);
+
+                    // Case 2: The player selects Wampa. It gets +3/+3.
+                    context.player1.clickCard(context.wampa);
+
+                    expect(context.wampa.getPower()).toEqual(7);
+                    expect(context.wampa.getHp()).toEqual(8);
+
+                    // Case 3: The player can select any unit card except Wampa.
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.battlefieldMarine,
+                        context.redThree,
+                        context.lukeSkywalker,
+                    ]);
+
+                    // Case 4: The player selects Battlefield Marine. It gets +2/+2.
+                    context.player1.clickCard(context.battlefieldMarine);
+
+                    expect(context.battlefieldMarine.getPower()).toEqual(5);
+                    expect(context.battlefieldMarine.getHp()).toEqual(5);
+
+                    // Case 5: The player can select any unit card except Wampa or Battlefield Marine.
+                    expect(context.player1).toBeAbleToSelectExactly([
+                        context.redThree,
+                        context.lukeSkywalker,
+                    ]);
+
+                    // Case 6: The player selects Red Three. It gets +1/+1.
+                    context.player1.clickCard(context.redThree);
+
+                    expect(context.redThree.getPower()).toEqual(3);
+                    expect(context.redThree.getHp()).toEqual(4);
+
+                    // Case 7: The buffs wear off at the end of the phase.
+                    context.player2.passAction();
+                    context.player1.passAction();
+
+                    expect(context.wampa.getPower()).toEqual(4);
+                    expect(context.wampa.getHp()).toEqual(5);
+                    expect(context.battlefieldMarine.getPower()).toEqual(3);
+                    expect(context.battlefieldMarine.getHp()).toEqual(3);
+                    expect(context.redThree.getPower()).toEqual(2);
+                    expect(context.redThree.getHp()).toEqual(3);
+                }
+            );
+        });
+    });
+});
