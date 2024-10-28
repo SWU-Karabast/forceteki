6efbed4b2dda2db9986fca4bd162febcb185e424
Ennis0 <ennis0@protonmail.com>
2024-10-27 22:36:26 -0400

    Added `isAmbush` property to `class Attack`, `interface IAttackProperties`, and `interface IInitiateAttackProperties`. Implemented set 2 Enfys Nest to make sure it works.

--- server/game/core/gameSystem/GameSystem.ts
+++ server/game/core/gameSystem/GameSystem.ts
@@ -243,7 +243,7 @@ export abstract class GameSystem<TContext extends AbilityContext = AbilityContex
     }
 
     /**
-     * Resolves the effects of the effects of the system on game state by generating the necessary events and
+     * Resolves the effects of the system on game state by generating the necessary events and
      * opening a window to resolve them with {@link Game.openEventWindow}.
      */
     public resolve(
