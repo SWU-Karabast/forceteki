337f4e21237b434f1ae79a7eba3420ed726b2ae8
AMMayberry1 <ammayberry1@gmail.com>
2024-10-29 22:00:42 -0400

    Rename variable

--- test/server/cards/01_SOR/events/Resupply.spec.ts
+++ test/server/cards/01_SOR/events/Resupply.spec.ts
@@ -16,14 +16,14 @@ describe('Resupply', function() {
             it('should put Resupply into play as a resource', function () {
                 const { context } = contextRef;
 
-                const readyResourcesBeforePlay = context.player1.countSpendableResources();
+                const startingResources = context.player1.countSpendableResources();
 
                 context.player1.clickCard(context.resupply);
 
                 expect(context.resupply).toBeInLocation('resource');
                 expect(context.resupply.exhausted).toBe(true);
-                expect(context.player1.resources.length).toBe(readyResourcesBeforePlay + 1);
-                expect(context.player1.countSpendableResources()).toBe(readyResourcesBeforePlay - 3);
+                expect(context.player1.resources.length).toBe(startingResources + 1);
+                expect(context.player1.countSpendableResources()).toBe(startingResources - 3);
                 expect(context.player1.countExhaustedResources()).toBe(4);
 
                 expect(context.player2).toBeActivePlayer();
