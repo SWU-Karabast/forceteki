73a58924fbfe62bc9da1ffc49b26f4d9007e9903
AMMayberry1 <ammayberry1@gmail.com>
2024-10-29 21:58:36 -0400

    Add Resupply

---
+++ test/server/cards/01_SOR/events/Resupply.spec.ts
@@ -0,0 +1,33 @@
+describe('Resupply', function() {
+    integration(function(contextRef) {
+        describe('Resupply\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['resupply'],
+                        leader: 'leia-organa#alliance-general'
+                    },
+                    player2: {
+                    }
+                });
+            });
+
+            it('should put Resupply into play as a resource', function () {
+                const { context } = contextRef;
+
+                const readyResourcesBeforePlay = context.player1.countSpendableResources();
+
+                context.player1.clickCard(context.resupply);
+
+                expect(context.resupply).toBeInLocation('resource');
+                expect(context.resupply.exhausted).toBe(true);
+                expect(context.player1.resources.length).toBe(readyResourcesBeforePlay + 1);
+                expect(context.player1.countSpendableResources()).toBe(readyResourcesBeforePlay - 3);
+                expect(context.player1.countExhaustedResources()).toBe(4);
+
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
