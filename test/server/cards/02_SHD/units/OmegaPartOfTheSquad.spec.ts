89dae5a402edb47ff5b908dbb5989096d32f4e9c
Anthony Bronico <adbronico92@gmail.com>
2024-10-31 12:04:34 -0400

    Omega double test

--- test/server/cards/02_SHD/units/OmegaPartOfTheSquad.spec.ts
+++ test/server/cards/02_SHD/units/OmegaPartOfTheSquad.spec.ts
@@ -5,18 +5,22 @@ describe('Omega, Part of the Squad', function() {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
-                        hand: ['omega#part-of-the-squad'],
+                        hand: ['omega#part-of-the-squad', 'omega#part-of-the-squad'],
                         deck: ['crosshair#following-orders', 'pyke-sentinel', 'atst', 'cartel-spacer', 'battlefield-marine'],
                         base: 'echo-base',
                         leader: 'hera-syndulla#spectre-two'
                     }
                 });
+                const { context } = contextRef;
+                const p1Omegas = context.player1.findCardsByName('omega#part-of-the-squad');
+                context.firstOmega = p1Omegas[0];
+                context.secondOmega = p1Omegas[1];
             });
 
             it('can draw a clone', function () {
                 const { context } = contextRef;
 
-                context.player1.clickCard(context.omega);
+                context.player1.clickCard(context.firstOmega);
                 expect(context.player1).toHavePrompt('Select a card to reveal');
                 expect(context.player1).toHaveEnabledPromptButton(context.crosshair.title);
                 expect(context.player1).toHaveDisabledPromptButtons([context.atst.title, context.battlefieldMarine.title, context.cartelSpacer.title, context.pykeSentinel.title]);
@@ -32,6 +36,12 @@ describe('Omega, Part of the Squad', function() {
                 // Crosshair should cost 6 since Omega was the first Clone played this round
                 context.player1.clickCard(context.crosshair);
                 expect(context.player1.countExhaustedResources()).toBe(10);
+
+                context.moveToNextActionPhase();
+
+                expect(context.player1.countExhaustedResources()).toBe(0);
+                context.player1.clickCard(context.secondOmega);
+                expect(context.player1.countExhaustedResources()).toBe(2);
             });
         });
 
