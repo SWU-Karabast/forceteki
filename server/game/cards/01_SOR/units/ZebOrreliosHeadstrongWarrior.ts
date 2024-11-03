c6f21208bd5156bbd6fdef599273027f1fc17784
Valentin Jubert <val_jub@live.fr>
2024-11-03 22:59:10 +0100

    pr comments

--- server/game/cards/01_SOR/units/ZebOrreliosHeadstrongWarrior.ts
+++ server/game/cards/01_SOR/units/ZebOrreliosHeadstrongWarrior.ts
@@ -1,13 +1,21 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { Location, WildcardCardType } from '../../../core/Constants';
+import { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import { UnitsDefeatedThisPhaseWatcher } from '../../../stateWatchers/UnitsDefeatedThisPhaseWatcher';
 
 export default class ZebOrreliosHeadstrongWarrior extends NonLeaderUnitCard {
+    private unitsDefeatedThisPhaseWatcher: UnitsDefeatedThisPhaseWatcher;
+
     protected override getImplementationId() {
         return {
             id: '9647945674',
             internalName: 'zeb-orrelios#headstrong-warrior'
         };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar): void {
+        this.unitsDefeatedThisPhaseWatcher = AbilityHelper.stateWatchers.unitsDefeatedThisPhase(registrar, this);
     }
 
     public override setupCardAbilities() {
@@ -21,7 +29,7 @@ export default class ZebOrreliosHeadstrongWarrior extends NonLeaderUnitCard {
                 locationFilter: Location.GroundArena,
                 optional: true,
                 immediateEffect: AbilityHelper.immediateEffects.conditional({
-                    condition: (context) => context.event.attack.target.isUnit() && !context.event.attack.target.isInPlay(),
+                    condition: (context) => context.event.attack.target.isUnit() && this.unitsDefeatedThisPhaseWatcher.getDefeatedUnitsControlledByPlayer(context.source.controller.opponent).includes(context.event.attack.target),
                     onTrue: AbilityHelper.immediateEffects.damage({ amount: 4 }),
                     onFalse: AbilityHelper.immediateEffects.noAction()
                 })
--- server/game/cards/02_SHD/upgrades/VambraceFlamethrower.ts
+++ server/game/cards/02_SHD/upgrades/VambraceFlamethrower.ts
@@ -1,6 +1,7 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { UpgradeCard } from '../../../core/card/UpgradeCard';
-import { Location, RelativePlayer, WildcardCardType } from '../../../core/Constants';
+import { Location, RelativePlayer, Trait, WildcardCardType } from '../../../core/Constants';
+import { Card } from '../../../core/card/Card';
 
 export default class VambraceFlamethrower extends UpgradeCard {
     protected override getImplementationId() {
@@ -11,11 +12,13 @@ export default class VambraceFlamethrower extends UpgradeCard {
     }
 
     public override setupCardAbilities() {
+        this.setAttachCondition((card: Card) => !card.hasSomeTrait(Trait.Vehicle));
+
         this.addGainOnAttackAbilityTargetingAttached({
             title: 'Deal 3 damage divided as you choose among enemy ground units',
             immediateEffect: AbilityHelper.immediateEffects.distributeDamageAmong({
                 amountToDistribute: 3,
-                canChooseNoTargets: true,
+                canChooseNoTargets: false,
                 controller: RelativePlayer.Opponent,
                 cardTypeFilter: WildcardCardType.Unit,
                 locationFilter: Location.GroundArena,
