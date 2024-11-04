6a992191643d57066eb4d9cc4b8eaf5ccba9f43b
AMMayberry1 <ammayberry1@gmail.com>
2024-11-04 10:33:17 -0500

    Remove TODO

--- server/game/core/card/propertyMixins/UnitProperties.ts
+++ server/game/core/card/propertyMixins/UnitProperties.ts
@@ -302,7 +302,6 @@ export function WithUnitProperties<TBaseClass extends InPlayCardConstructor>(Bas
          * Checks if the unit currently has any keywords with a "when played" effect and registers them if so.
          * Also adds a listener to remove the registered abilities after the effect resolves.
          */
-        // TODO THIS PR: refactor this into a separate method so "checkRegisterWhenCaptured" will be simple
         public checkRegisterWhenDefeatedKeywordAbilities(event: GameEvent) {
             const bountyKeywords = this.getBountyAbilities();
             if (bountyKeywords.length === 0) {
