417d1fe3e3f9e0a42fda0586f5fa85707aefee87
Ennis0 <ennis0@protonmail.com>
2024-11-03 19:23:08 -0500

    cosmetic change to Enfys Nest card code

--- server/game/cards/02_SHD/units/EnfysNestMarauder.ts
+++ server/game/cards/02_SHD/units/EnfysNestMarauder.ts
@@ -14,7 +14,12 @@ export default class EnfysNestMarauder extends NonLeaderUnitCard {
         this.addConstantAbility({
             title: 'When a friendly unit is attacking using Ambush, the defender gets -3/-0',
             targetController: RelativePlayer.Opponent,
-            matchTarget: (card, context) => card.isUnit() && card.isInPlay() && card.isDefending() && card.activeAttack.attacker.controller === context.source.controller && card.activeAttack.isAmbush,
+            matchTarget: (card, context) =>
+                card.isUnit() &&
+                card.isInPlay() &&
+                card.isDefending() &&
+                card.activeAttack.attacker.controller === context.source.controller &&
+                card.activeAttack.isAmbush,
             ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: -3, hp: 0 })
         });
     }
