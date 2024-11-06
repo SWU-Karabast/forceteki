addaebd58f121694319753937be7aef3d59329e1
Jan Preisig <jan.preisig@intuitive-collaboration.com>
2024-11-06 17:41:26 +0100

    move testSetup inside the tests

--- test/server/cards/01_SOR/units/PiratedStarfighter.spec.ts
+++ test/server/cards/01_SOR/units/PiratedStarfighter.spec.ts
@@ -1,43 +1,33 @@
 describe('Pirated Starfighter', function () {
     integration(function (contextRef) {
-        describe('Pirated Starfighter\'s ability', function () {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        hand: ['pirated-starfighter'],
-                    }
-                });
+        it('should not prompt player if no units are available to return to hand', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['pirated-starfighter'],
+                }
             });
+            const { context } = contextRef;
 
-            it('should not prompt player if no units are available to return to hand', function () {
-                const { context } = contextRef;
-
-                context.player1.clickCard(context.piratedStarfighter);
-                expect(context.player2).toBeActivePlayer();
-            });
+            context.player1.clickCard(context.piratedStarfighter);
+            expect(context.player2).toBeActivePlayer();
         });
 
-        describe('Pirated Starfighter\'s ability', function () {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        hand: ['pirated-starfighter'],
-                        groundArena: ['pyke-sentinel'],
-                    }
-                });
+        it('should be able to return self or other unit to hand', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['pirated-starfighter'],
+                    groundArena: ['pyke-sentinel'],
+                }
             });
+            const { context } = contextRef;
 
-            it('should be able to return self or other unit to hand', function () {
-                const { context } = contextRef;
-
-                context.player1.clickCard(context.piratedStarfighter);
-                expect(context.player1).toBeAbleToSelectExactly([context.piratedStarfighter, context.pykeSentinel]);
-                context.player1.clickCard(context.pykeSentinel);
-                expect(context.pykeSentinel).toBeInLocation('hand');
-                expect(context.piratedStarfighter).toBeInLocation('space arena');
-            });
+            context.player1.clickCard(context.piratedStarfighter);
+            expect(context.player1).toBeAbleToSelectExactly([context.piratedStarfighter, context.pykeSentinel]);
+            context.player1.clickCard(context.pykeSentinel);
+            expect(context.pykeSentinel).toBeInLocation('hand');
+            expect(context.piratedStarfighter).toBeInLocation('space arena');
         });
     });
 });
