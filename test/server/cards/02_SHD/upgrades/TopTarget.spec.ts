94b738929755c3daa82eab1a404b77bef0d8d2c8
AMMayberry1 <ammayberry1@gmail.com>
2024-11-05 19:03:32 -0500

    Finish out Top Target tests

--- test/server/cards/02_SHD/upgrades/TopTarget.spec.ts
+++ test/server/cards/02_SHD/upgrades/TopTarget.spec.ts
@@ -1,7 +1,7 @@
 describe('Top Target', function() {
     integration(function(contextRef) {
         describe('Top Target\'s Bounty ability', function() {
-            it('should ready 2 resources', function () {
+            it('should heal 4 damage from a base if the attached unit is not unique', function () {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
@@ -22,6 +22,30 @@ describe('Top Target', function() {
                 expect(context.player2).toHavePassAbilityButton();
                 context.player2.clickCard(context.p2Base);
                 expect(context.p2Base.damage).toBe(6);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should heal 6 damage from a base if the attached unit is unique', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [{ card: 'colonel-yularen#isb-director', upgrades: ['top-target'] }],
+                        base: { card: 'jedha-city', damage: 10 }
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.colonelYularen);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                expect(context.player2).toHavePassAbilityButton();
+                context.player2.clickCard(context.p1Base);
+                expect(context.p1Base.damage).toBe(4);
                 expect(context.player2).toBeActivePlayer();
             });
         });
