3d66a1695e2fb8843834a5945f0ae1b057c6f04f
Tommaso Madonia <tommaso@madonia.me>
2024-11-07 16:41:13 +0000

    Add test scenario for dealing damage to shielded units while paying an action cost

---
+++ test/scenarios/event/ReplacementEffect.spec.ts
@@ -0,0 +1,29 @@
+describe('Replecement effect', function() {
+    integration(function(contextRef) {
+        describe('when a unit with one or more shields takes damage from a cost action', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [{ card: 'doctor-pershing#experimenting-with-life', upgrades: ['shield', 'shield'] }],
+                    },
+                    player2: {
+                        groundArena: ['vanguard-infantry'],
+                    }
+                });
+            });
+
+            it('one shield is removed', function () {
+                const { context } = contextRef;
+
+                expect(context.doctorPershing).toHaveExactUpgradeNames(['shield', 'shield']);
+
+                context.player1.clickCard(context.doctorPershing);
+                context.player1.clickPrompt('Draw a card');
+
+                expect(context.doctorPershing.damage).toBe(0);
+                expect(context.doctorPershing).toHaveExactUpgradeNames(['shield']);
+            });
+        });
+    });
+});
