d110490b592106a68a49a8239e759fc107357d5b
Tommaso Madonia <tommaso@madonia.me>
2024-11-11 09:02:57 +0000

    Use queueGenerateEventGameSteps instead of eventHandler

--- server/game/gameSystems/DiscardEntireHandSystem.ts
+++ server/game/gameSystems/DiscardEntireHandSystem.ts
@@ -3,6 +3,7 @@ import { AbilityContext } from '../core/ability/AbilityContext';
 import * as Helpers from '../core/utils/Helpers.js';
 import type Player from '../core/Player';
 import { IPlayerTargetSystemProperties, PlayerTargetSystem } from '../core/gameSystem/PlayerTargetSystem';
+import { DiscardSpecificCardSystem } from './DiscardSpecificCardSystem';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface IDiscardEntireHandSystemProperties extends IPlayerTargetSystemProperties {}
@@ -16,17 +17,31 @@ export class DiscardEntireHandSystem<TContext extends AbilityContext = AbilityCo
     public override readonly costDescription: string = 'discard hand';
     public override readonly effectDescription: string = 'discard hand';
 
-    public override eventHandler(event): void {
-        const players = Helpers.asArray(event.player) as Player[];
-
-        players.forEach((player) =>
-            player
-                .getCardPile(Location.Hand)
-                .forEach((card) => player.moveCard(card, Location.Discard))
-        );
+    public override eventHandler(): void {
+        // Do nothing
     }
 
     public override defaultTargets(context: TContext): Player[] {
         return [context.player];
+    }
+
+    public override queueGenerateEventGameSteps(events: any[], context: TContext, additionalProperties: Record<string, any> = {}): void {
+        const properties = this.generatePropertiesFromContext(context, additionalProperties);
+
+        for (const player of Helpers.asArray(properties.target) as Player[]) {
+            // Ensure that the player can be affected by this system
+            if (!this.canAffect(player, context, additionalProperties)) {
+                continue;
+            }
+
+            // Discard each card in the player's hand
+            for (const card of player.getCardPile(Location.Hand)) {
+                const discardCardEvent = new DiscardSpecificCardSystem({ target: card }).generateEvent(context);
+                events.push(discardCardEvent);
+            }
+
+            // Generate one final event to convey that the player's entire hand was discarded
+            events.push(this.generateRetargetedEvent(player, context, additionalProperties));
+        }
     }
 }
