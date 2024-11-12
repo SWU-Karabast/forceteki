5438792be2ca6c67884a4c70666517489b480143
Steve Townsend <steve.townsend88@gmail.com>
2024-11-12 22:40:01 +0000

    Updated code according to PR comments

--- test/server/cards/01_SOR/events/RallyingCry.spec.ts
+++ test/server/cards/01_SOR/events/RallyingCry.spec.ts
@@ -10,7 +10,6 @@ describe('Rallying Cry', function () {
                         spaceArena: ['imperial-interceptor']
                     },
                     player2: {
-                        groundArena: ['rugged-survivors'],
                         spaceArena: ['distant-patroller']
                     }
                 });
@@ -19,14 +18,22 @@ describe('Rallying Cry', function () {
             it('should give Raid 2 to friendly units', function () {
                 const { context } = contextRef;
 
+                // Playing Rallying Cry from hand
                 context.player1.clickCard(context.rallyingCry);
                 context.player2.passAction();
+
+                // Attacking with Death Trooper
                 context.player1.clickCard(context.deathTrooper);
-                expect(context.player1).toBeAbleToSelectExactly([context.p2Base, context.ruggedSurvivors]);
-                context.player1.clickCard(context.p2Base);
                 expect(context.p2Base.damage).toBe(5);
 
-                // should not give Raid 2 to non-friendly units
+                // Attacking with Imperial Interceptor
+                context.player2.passAction();
+                context.player1.clickCard(context.imperialInterceptor);
+                expect(context.player1).toBeAbleToSelectExactly([context.p2Base, context.distantPatroller]);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(10);
+
+                // Should not give Raid 2 to non-friendly units
                 context.player2.clickCard(context.distantPatroller);
                 expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.imperialInterceptor]);
                 context.player2.clickCard(context.p1Base);
@@ -35,9 +42,7 @@ describe('Rallying Cry', function () {
                 // Raid 2 should be gone by next phase
                 context.moveToNextActionPhase();
                 context.player1.clickCard(context.deathTrooper);
-                expect(context.player1).toBeAbleToSelectExactly([context.p2Base, context.ruggedSurvivors]);
-                context.player1.clickCard(context.p2Base);
-                expect(context.p2Base.damage).toBe(8);
+                expect(context.p2Base.damage).toBe(13);
             });
         });
     });
--- test/server/cards/02_SHD/units/GentleGiant.spec.ts
+++ test/server/cards/02_SHD/units/GentleGiant.spec.ts
@@ -8,52 +8,32 @@ describe('Gentle Giant', function() {
                         groundArena: [
                             { card: 'gentle-giant', damage: 1 },
                             { card: 'r2d2#ignoring-protocol', damage: 3 },
-                            { card: 'c3po#protocol-droid', damage: 1 }],
+                        ]
                     },
                     player2: {
-                        groundArena: [{ card: 'wampa', damage: 1 }]
+                        groundArena: [{ card: 'wampa' }]
                     }
                 });
             });
 
-            it('heal a target', function () {
+            it('can heal a target unit for the amount of damage it has', function () {
                 const { context } = contextRef;
-
-                const reset = (passAction = true) => {
-                    context.gentleGiant.exhausted = false;
-                    context.r2d2.damage = 3;
-                    if (passAction) {
-                        context.player2.passAction();
-                    }
-                };
 
                 // CASE 1: Heal a target
                 // Attack
                 context.player1.clickCard(context.gentleGiant);
-                expect(context.gentleGiant).toBeInLocation('ground arena');
-                expect(context.player1).toBeAbleToSelectExactly([context.p2Base, context.wampa]);
                 context.player1.clickCard(context.p2Base);
 
-                // Healing Target
-                expect(context.player1).toBeAbleToSelectExactly([context.r2d2, context.c3po, context.wampa]);
+                // Checking the you may part
+                expect(context.player1).toHavePassAbilityButton();
+
+                // Actually healing a unit
+                expect(context.player1).toBeAbleToSelectExactly([context.r2d2, context.wampa]);
                 context.player1.clickCard(context.r2d2);
 
                 // Confirm Results
                 expect(context.gentleGiant.exhausted).toBe(true);
                 expect(context.r2d2.damage).toBe(2);
-
-                reset();
-
-                // CASE 2: Should be able to pass
-                // Attack
-                expect(context.r2d2.damage).toBe(3);
-                context.player1.clickCard(context.gentleGiant);
-                context.player1.clickCard(context.p2Base);
-
-                // Passing prompt to heal
-                context.player1.clickPrompt('Pass ability');
-                expect(context.gentleGiant.exhausted).toBe(true);
-                expect(context.r2d2.damage).toBe(3);
             });
         });
     });
