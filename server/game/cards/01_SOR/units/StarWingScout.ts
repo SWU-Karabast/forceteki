7d31596f02134be2ab35f20fed5220ebcd9646cc
Steve Townsend <steve.townsend88@gmail.com>
2024-11-12 20:11:26 +0000

    Added Star Wing Scout

---
+++ server/game/cards/01_SOR/units/StarWingScout.ts
@@ -0,0 +1,25 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class StarWingScout extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7517208605',
+            internalName: 'star-wing-scout'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addWhenDefeatedAbility({
+            title: 'Draw 2 cards',
+            optional: false,
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.controller.hasInitiative(),
+                onTrue: AbilityHelper.immediateEffects.draw({ amount: 2 }),
+                onFalse: AbilityHelper.immediateEffects.noAction()
+            })
+        });
+    }
+}
+
+StarWingScout.implemented = true;
