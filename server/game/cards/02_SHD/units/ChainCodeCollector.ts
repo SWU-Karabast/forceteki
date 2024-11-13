475e227476fdd3444846cd18a8468299e284892c
Valentin Jubert <val_jub@live.fr>
2024-11-13 19:07:45 +0100

    chain code collector

--- server/game/cards/02_SHD/units/ChainCodeCollector.ts
+++ server/game/cards/02_SHD/units/ChainCodeCollector.ts
@@ -14,7 +14,10 @@ export default class ChainCodeCollector extends NonLeaderUnitCard {
         this.addOnAttackAbility({
             title: 'If the defender has a Bounty, it gets –4/–0 for this attack',
             immediateEffect: AbilityHelper.immediateEffects.conditional({
-                condition: (context) => context.event.attack.target.hasSomeKeyword(KeywordName.Bounty),
+                condition: (context) => {
+                    const target = context.event.attack.target;
+                    return target.isUnit() && target.hasSomeKeyword(KeywordName.Bounty);
+                },
                 onTrue: AbilityHelper.immediateEffects.cardLastingEffect((context) => ({
                     target: context.event.attack.target,
                     effect: AbilityHelper.ongoingEffects.modifyStats({ power: -4, hp: 0 }),
