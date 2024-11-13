c9334d7351944b5348253113d1ddef0ae1475873
Julio Gordijo <jgordijo@gmail.com>
2024-11-13 16:10:22 -0300

    test: add unit test for no bargain when opponent hand is empty

--- test/server/cards/02_SHD/events/NoBargain.spec.ts
+++ test/server/cards/02_SHD/events/NoBargain.spec.ts
@@ -31,5 +31,34 @@ describe('No Bargain', function() {
                 expect(context.player2).toBeActivePlayer();
             });
         });
+
+        describe('No Bargain\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['no-bargain', 'wampa'],
+                    },
+                    player2: {
+                        hand: [],
+                    }
+                });
+            });
+
+            it('should draw a card and skip discard if opponent has no cards in hand', function () {
+                const { context } = contextRef;
+
+                const activePlayerStartingHandSize = context.player1.handSize;
+                const opponentStartingHandSize = context.player2.handSize;
+
+                context.player1.clickCard(context.noBargain);
+
+                expect(context.player2.handSize).toBe(opponentStartingHandSize);
+                expect(context.player2.discard.length).toBe(0);
+                expect(context.player1.handSize).toBe(activePlayerStartingHandSize);
+
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
     });
 });
