0494c8774f6198c9e05e87086a89407c88a0ba24
Valentin Jubert <val_jub@live.fr>
2024-11-14 23:25:14 +0100

    wolffe

---
+++ server/game/cards/01_SOR/units/WolffeSuspiciousVeteran.ts
@@ -0,0 +1,29 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { AbilityRestriction, Duration } from '../../../core/Constants';
+
+export default class WolffeSuspiciousVeteran extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7533529264',
+            internalName: 'wolffe#suspicious-veteran'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Bases can\'t be healed',
+            when: {
+                onCardPlayed: (event, context) => event.card === context.source,
+                onAttackDeclared: (event, context) => event.attack.attacker === context.source,
+            },
+            immediateEffect: AbilityHelper.immediateEffects.cardLastingEffect((context) => ({
+                effect: AbilityHelper.ongoingEffects.cardCannot(AbilityRestriction.BeHealed),
+                target: [context.source.controller.base, context.source.controller.opponent.base],
+                duration: Duration.UntilEndOfPhase
+            }))
+        });
+    }
+}
+
+WolffeSuspiciousVeteran.implemented = true;
