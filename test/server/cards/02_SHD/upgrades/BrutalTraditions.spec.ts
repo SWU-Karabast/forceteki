b1f375539b12fd69995294a34d35d8185ce09503
matevz <matevz.ogrinc@ijs.si>
2024-11-14 18:35:23 +0100

    #KylosTieSilencer - intermezzo

--- test/server/cards/02_SHD/upgrades/BrutalTraditions.spec.ts
+++ test/server/cards/02_SHD/upgrades/BrutalTraditions.spec.ts
@@ -5,11 +5,11 @@ describe('Brutal Traditions', function() {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
-                        groundArena: [{ card: 'vigilant-honor-guards', upgrades: ['brutal-traditions'] }],
+                        groundArena: [{ card: 'vigilant-honor-guards', upgrades: ['brutal-traditions'] }, { card: 'moisture-farmer' }],
                     },
                     player2: {
                         groundArena: ['wampa'],
-                        hand: ['confiscate']
+                        hand: ['confiscate', 'vanquish']
                     }
                 });
             });
@@ -24,24 +24,28 @@ describe('Brutal Traditions', function() {
                 expect(context.wampa).toBeInLocation('discard');
 
                 context.player2.clickCard(context.confiscate);
-
                 expect(context.brutalTraditions).toBeInLocation('discard');
                 expect(context.player1).toBeActivePlayer();
                 expect(context.player1).toBeAbleToSelect(context.brutalTraditions);
 
                 context.player1.clickCard(context.brutalTraditions);
+                context.player1.clickCard(context.vigilantHonorGuards);
                 expect(context.vigilantHonorGuards.upgrades).toEqual([context.brutalTraditions]);
                 expect(context.vigilantHonorGuards.getPower()).toBe(5);
                 expect(context.vigilantHonorGuards.getHp()).toBe(8);
-            });
+                expect(context.vigilantHonorGuards.damage).toBe(4);
 
-            it('should not be able to play it from the discard pile.', function () {
-                const { context } = contextRef;
+                // Brutal tradition is again able to be played from the discard pile
+                context.player2.clickCard(context.vanquish);
+                expect(context.player2).toBeAbleToSelect(context.vigilantHonorGuards);
+                context.player2.clickCard(context.vigilantHonorGuards);
+                expect(context.vigilantHonorGuards).toBeInLocation('discard');
 
-                context.player1.passAction();
-                context.player2.clickCard(context.confiscate);
-                expect(context.player1).toBeActivePlayer();
-                expect(context.player1).not.toBeAbleToSelect(context.brutalTraditions);
+                expect(context.brutalTraditions).toBeInLocation('discard');
+                expect(context.player1).toBeAbleToSelect(context.brutalTraditions);
+
+                context.player1.clickCard(context.brutalTraditions);
+                expect(context.moistureFarmer.upgrades).toEqual([context.brutalTraditions]);
             });
         });
     });
