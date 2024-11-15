46a59dbcba0588f49abd771b2dda114e1abe3971
Grant Wasil <grantwasil1@gmail.com>
2024-11-14 17:21:33 -0700

    merged the test cases together

--- test/server/cards/02_SHD/units/FollowerOfTheWay.spec.ts
+++ test/server/cards/02_SHD/units/FollowerOfTheWay.spec.ts
@@ -1,10 +1,11 @@
 describe('Follower of the Way', function() {
     integration(function(contextRef) {
-        describe('Follower of the Way\'s ability', function() {
+        describe('Follower of the Way\'s ability', function () {
             beforeEach(function () {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
+                        hand: ['covert-strength'],
                         groundArena: [{ card: 'follower-of-the-way', upgrades: ['shield'] }]
                     },
                     player2: {
@@ -15,49 +16,23 @@ describe('Follower of the Way', function() {
 
             it('should give +1/+1 to itself when upgraded', function () {
                 const { context } = contextRef;
+
+                // validate buff with upgrade
                 expect(context.followerOfTheWay.getPower()).toBe(2);
                 expect(context.followerOfTheWay.getHp()).toBe(4);
-            });
-        });
 
-        describe('Follower of the Way\'s ability', function() {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        groundArena: [{ card: 'follower-of-the-way', upgrades: ['experience'] }]
-                    },
-                    player2: {
-                        groundArena: ['steadfast-battalion']
-                    }
-                });
-            });
-
-            it('should give +1/+1 to itself when upgraded', function () {
-                const { context } = contextRef;
-                expect(context.followerOfTheWay.getPower()).toBe(3);
-                expect(context.followerOfTheWay.getHp()).toBe(5);
-            });
-        });
-
-        describe('Follower of the Way\'s ability', function() {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        groundArena: ['follower-of-the-way']
-                    },
-                    player2: {
-                        groundArena: ['steadfast-battalion']
-                    }
-                });
-            });
-
-            it('should not give +1/+1 to itself when not upgraded', function () {
-                const { context } = contextRef;
-
+                // validate buff removed without upgrade
+                context.player1.clickCard(context.followerOfTheWay);
+                context.player1.clickCard(context.steadfastBattalion);
                 expect(context.followerOfTheWay.getPower()).toBe(1);
                 expect(context.followerOfTheWay.getHp()).toBe(3);
+
+                // validate correct buff with experience
+                context.player2.passAction();
+                context.player1.clickCard(context.covertStrength);
+                context.player1.clickCard(context.followerOfTheWay);
+                expect(context.followerOfTheWay.getPower()).toBe(3);
+                expect(context.followerOfTheWay.getHp()).toBe(5);
             });
         });
     });
