e1be36b8f95f4cbb7efac7e9234408eb7a44f295
Valentin Jubert <val_jub@live.fr>
2024-11-16 21:40:22 +0100

    jabba's rancor

---
+++ server/game/cards/02_SHD/units/JabbasRancorPateesa.ts
@@ -0,0 +1,47 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Location, RelativePlayer, WildcardCardType, WildcardLocation } from '../../../core/Constants';
+
+export default class JabbasRancorPateesa extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '8380936981',
+            internalName: 'jabbas-rancor#pateesa',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addDecreaseCostAbility({
+            title: 'If you control Jabba the Hutt, this unit costs 1 resource less to play',
+            condition: (context) =>
+                context.source.controller.leader.title === 'Jabba the Hutt' ||
+                context.source.controller.getUnitsInPlay(WildcardLocation.AnyArena, (card) => card.title === 'Jabba the Hutt').length > 0,
+            amount: 1
+        });
+
+        this.addTriggeredAbility({
+            title: 'Deal 3 damage to another friendly ground unit and 3 damage to an enemy ground unit',
+            when: {
+                onCardPlayed: (event, context) => event.card === context.source,
+                onAttackDeclared: (event, context) => event.attack.attacker === context.source,
+            },
+            targetResolvers: {
+                myGroundUnit: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    controller: RelativePlayer.Self,
+                    locationFilter: Location.GroundArena,
+                    cardCondition: (card, context) => card !== context.source,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 3 }),
+                },
+                theirGroundUnit: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    controller: RelativePlayer.Opponent,
+                    locationFilter: Location.GroundArena,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 3 }),
+                }
+            }
+        });
+    }
+}
+
+JabbasRancorPateesa.implemented = true;
