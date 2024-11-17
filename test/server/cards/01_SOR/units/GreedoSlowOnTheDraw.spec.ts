6b1b2e5ade23fe553948deaec441a2f6310add5a
Valentin Jubert <val_jub@live.fr>
2024-11-17 06:01:28 +0100

    fix greedo

--- test/server/cards/01_SOR/units/GreedoSlowOnTheDraw.spec.ts
+++ test/server/cards/01_SOR/units/GreedoSlowOnTheDraw.spec.ts
@@ -34,33 +34,56 @@ describe('Greedo, Slow on the Draw', function () {
                 expect(context.battlefieldMarine.damage).toBe(2);
             });
 
-            // it('should not deal 2 damage to a ground unit if the discarded card is a unit', function () {
-            //     const { context } = contextRef;
-            //     contextRef.setupTest({
-            //         phase: 'action',
-            //         player1: {
-            //             groundArena: ['wampa', 'battlefield-marine'],
-            //             spaceArena: ['green-squadron-awing'],
-            //         },
-            //         player2: {
-            //             groundArena: ['greedo#slow-on-the-draw', 'atst'],
-            //             deck: ['isb-agent']
-            //         }
-            //     });
-            //
-            //     // kill greedo
-            //     context.player1.clickCard(context.wampa);
-            //     context.player1.clickCard(context.greedo);
-            //     expect(context.greedo.location).toBe('discard');
-            //
-            //     // player2 should have prompt or pass
-            //     expect(context.player2).toHavePassAbilityPrompt(prompt);
-            //     context.player2.clickPrompt(prompt);
-            //
-            //     // top card is a unit, nothing happen
-            //     expect(context.isbAgent.location).toBe('discard');
-            //     expect(context.player2).toBeActivePlayer();
-            // });
+            it('should not deal 2 damage to a ground unit if the discarded card is a unit', function () {
+                const { context } = contextRef;
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['wampa', 'battlefield-marine'],
+                        spaceArena: ['green-squadron-awing'],
+                    },
+                    player2: {
+                        groundArena: ['greedo#slow-on-the-draw', 'atst'],
+                        deck: ['isb-agent']
+                    }
+                });
+
+                // kill greedo
+                context.player1.clickCard(context.wampa);
+                context.player1.clickCard(context.greedo);
+                expect(context.greedo.location).toBe('discard');
+
+                // player2 should have prompt or pass
+                expect(context.player2).toHavePassAbilityPrompt(prompt);
+                context.player2.clickPrompt(prompt);
+
+                // top card is a unit, nothing happen
+                expect(context.isbAgent.location).toBe('discard');
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not prompt if the deck is empty', function () {
+                const { context } = contextRef;
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['wampa', 'battlefield-marine'],
+                        spaceArena: ['green-squadron-awing'],
+                    },
+                    player2: {
+                        groundArena: ['greedo#slow-on-the-draw', 'atst'],
+                        deck: []
+                    }
+                });
+
+                // kill greedo
+                context.player1.clickCard(context.wampa);
+                context.player1.clickCard(context.greedo);
+                expect(context.greedo.location).toBe('discard');
+
+                // deck is empty, nothing to discard, no prompt
+                expect(context.player2).toBeActivePlayer();
+            });
         });
     });
 });
