6397d8fc2bf09e29c86f94fcb8222893c68ec76a
Tommaso Madonia <tommaso@madonia.me>
2024-11-18 08:26:08 +0000

    Simplify implementation of SmokeAndCinders

--- server/game/cards/01_SOR/events/SmokeAndCinders.ts
+++ server/game/cards/01_SOR/events/SmokeAndCinders.ts
@@ -12,14 +12,10 @@ export default class SmokeAndCinders extends EventCard {
     public override setupCardAbilities() {
         this.setEventAbility({
             title: 'Each player discards all but 2 cards from their hand',
-            immediateEffect: AbilityHelper.immediateEffects.simultaneous(
-                (context) => context.game.getPlayers().map((player) =>
-                    AbilityHelper.immediateEffects.discardCardsFromOwnHand({
-                        target: player,
-                        amount: (player) => Math.max(0, player.hand.length - 2)
-                    })
-                )
-            )
+            immediateEffect: AbilityHelper.immediateEffects.discardCardsFromOwnHand((context) => ({
+                target: context.game.getPlayers(),
+                amount: (player) => Math.max(0, player.hand.length - 2)
+            }))
         });
     }
 }
