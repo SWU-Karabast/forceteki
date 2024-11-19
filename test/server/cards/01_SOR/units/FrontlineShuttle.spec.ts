fd40edbf2ff68f11f6ac62e338eee5db0d0a1337
Julio Gordijo <jgordijo@gmail.com>
2024-11-19 13:53:16 -0300

    test: update scenarios for frontline shuttle

--- test/server/cards/01_SOR/units/FrontlineShuttle.spec.ts
+++ test/server/cards/01_SOR/units/FrontlineShuttle.spec.ts
@@ -6,7 +6,7 @@ describe('Frontline Shuttle', function() {
                     phase: 'action',
                     player1: {
                         spaceArena: ['frontline-shuttle'],
-                        groundArena: ['battlefield-marine', 'r2d2#ignoring-protocol', 'c3po#protocol-droid']
+                        groundArena: [{ card: 'battlefield-marine', exhausted: true }, 'consular-security-force', 'c3po#protocol-droid']
                     },
                     player2: {
                         groundArena: ['bail-organa#rebel-councilor', 'sundari-peacekeeper'],
@@ -17,12 +17,10 @@ describe('Frontline Shuttle', function() {
             it('should be able to activate action ability and attack with exhausted unit', function () {
                 const { context } = contextRef;
 
-                context.battlefieldMarine.exhausted = true;
-
                 context.player1.clickCard(context.frontlineShuttle);
 
                 context.player1.clickPrompt('Attack with a unit, even if it’s exhausted. It can’t attack bases for this attack.');
-                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.r2d2IgnoringProtocol, context.c3poProtocolDroid]);
+                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.consularSecurityForce, context.c3poProtocolDroid]);
                 context.player1.clickCard(context.battlefieldMarine);
 
                 expect(context.player1).toBeAbleToSelectExactly([context.bailOrganaRebelCouncilor, context.sundariPeacekeeper]);
@@ -43,15 +41,15 @@ describe('Frontline Shuttle', function() {
                 context.player1.clickCard(context.frontlineShuttle);
 
                 context.player1.clickPrompt('Attack with a unit, even if it’s exhausted. It can’t attack bases for this attack.');
-                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.r2d2IgnoringProtocol, context.c3poProtocolDroid]);
-                context.player1.clickCard(context.battlefieldMarine);
+                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.consularSecurityForce, context.c3poProtocolDroid]);
+                context.player1.clickCard(context.consularSecurityForce);
 
                 expect(context.player1).toBeAbleToSelectExactly([context.bailOrganaRebelCouncilor, context.sundariPeacekeeper]);
                 context.player1.clickCard(context.sundariPeacekeeper);
 
                 expect(context.sundariPeacekeeper.damage).toBe(3);
-                expect(context.battlefieldMarine.exhausted).toBe(true);
-                expect(context.battlefieldMarine.damage).toBe(1);
+                expect(context.consularSecurityForce.exhausted).toBe(true);
+                expect(context.consularSecurityForce.damage).toBe(1);
 
                 expect(context.frontlineShuttle).toBeInZone('discard');
 
