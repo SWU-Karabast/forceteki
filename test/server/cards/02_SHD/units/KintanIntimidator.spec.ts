6895f99761d13a3e9677a2362f5ff56a8229564e
deluxe <131956515+c0rrupt3d-d4t4@users.noreply.github.com>
2024-11-22 08:52:28 +0100

    Add files via upload

--- test/server/cards/02_SHD/units/KintanIntimidator.spec.ts
+++ test/server/cards/02_SHD/units/KintanIntimidator.spec.ts
@@ -16,11 +16,18 @@ describe('Kintan Intimidator', function () {
             it('should exhaust the defender when attacking a unit', function () {
                 const { context } = contextRef;
 
-                // Select Kintan Intimidator and attack the Marine
+                // When Kintan Intimidator attacks the enemy phase, nothing gets exhausted
+                context.player1.clickCard(context.kintanIntimidator);
+                context.player1.clickCard(context.p2Base);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine.exhausted).toBe(false);
+                context.player2.passAction();
+
+
+                // Select Kintan Intimidator and attack the Marine, then Marine gets exhausted
+                context.kintanIntimidator.exhausted = false;
                 context.player1.clickCard(context.kintanIntimidator);
                 context.player1.clickCard(context.battlefieldMarine);
-
-                // Verify that the Marine gets exhausted
                 expect(context.battlefieldMarine.exhausted).toBeTrue();
                 expect(context.player2).toBeActivePlayer();
             });
