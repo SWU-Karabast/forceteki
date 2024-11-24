c7503d81435ffa7ac8b2e942c7a92429e9215131
Scott McCulloch <scott.mcculloch@pia.ai>
2024-11-24 23:19:18 +1100

    initial implementation, reckless gunslinger

---
+++ test/server/cards/02_SHD/units/RecklessGunslinger.spec.ts
@@ -0,0 +1,22 @@
+describe('Reckless Gunslinger', function () {
+    integration(function (contextRef) {
+        describe('Reckless Gunslinger\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['reckless-gunslinger']
+                    },
+                    player2: {}
+                });
+            });
+
+            it('should deal 1 damage to each base.', function () {
+                const { context } = contextRef;
+                context.player1.clickCard(context.recklessGunslinger);
+                expect(context.p1Base.damage).toBe(1);
+                expect(context.p2Base.damage).toBe(1);
+            });
+        });
+    });
+});
