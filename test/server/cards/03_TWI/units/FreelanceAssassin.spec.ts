bc8adeb4dc60e745618992a5ba31381741d1f580
Valentin Jubert <val_jub@live.fr>
2024-12-01 09:59:40 +0100

    pr comments

--- test/server/cards/03_TWI/units/FreelanceAssassin.spec.ts
+++ test/server/cards/03_TWI/units/FreelanceAssassin.spec.ts
@@ -30,6 +30,32 @@ describe('Freelance Assassin', function () {
                 expect(context.battlefieldMarine.damage).toBe(2);
                 expect(context.player1.exhaustedResourceCount).toBe(5);
             });
+
+            it('should pass the ability', function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['freelance-assassin'],
+                        spaceArena: ['restored-arc170'],
+                        leader: 'lando-calrissian#with-impeccable-taste'
+                    },
+                    player2: {
+                        groundArena: ['battlefield-marine']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.freelanceAssassin);
+
+                // have a prompt to pay 2 resources
+                expect(context.player1).toHavePassAbilityPrompt('Pay 2 resources. If you do, deal 2 damage to a unit.');
+                context.player1.clickPrompt('Pass');
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine.damage).toBe(0);
+                expect(context.player1.exhaustedResourceCount).toBe(3);
+            });
         });
     });
 });
--- test/server/cards/03_TWI/units/MorganElsbethKeeperOfManySecrets.spec.ts
+++ test/server/cards/03_TWI/units/MorganElsbethKeeperOfManySecrets.spec.ts
@@ -23,9 +23,27 @@ describe('Morgan Elsbeth, Keeper of Many Secrets', function () {
                 expect(context.player1).toHavePassAbilityButton();
                 context.player1.clickCard(context.wampa);
 
-                // wampa should be defeated and we should had draw a card
+                // wampa should be defeated, and we should had draw a card
                 expect(context.player2).toBeActivePlayer();
                 expect(context.wampa).toBeInZone('discard');
+                expect(context.player1.hand.length).toBe(1);
+
+                context.morganElsbeth.exhausted = false;
+                context.player2.passAction();
+
+                context.player1.clickCard(context.morganElsbeth);
+                context.player1.clickCard(context.p2Base);
+
+                // choice between ability and restore 1
+                context.player1.clickPrompt('Defeat another friendly unit. If you do, draw a card.');
+
+                // can pass
+                expect(context.player1).toHavePassAbilityPrompt('Defeat another friendly unit. If you do, draw a card.');
+                context.player1.clickPrompt('Pass');
+
+                // we pass, atst should be alive, and we should not have draw
+                expect(context.player2).toBeActivePlayer();
+                expect(context.atst).toBeInZone('groundArena');
                 expect(context.player1.hand.length).toBe(1);
             });
         });
