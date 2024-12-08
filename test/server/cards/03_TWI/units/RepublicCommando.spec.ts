201943c14cb07ddc6a7c4333df371dbf1616524f
Travis Chase <travis@travischase.me>
2024-12-08 08:51:08 -0500

    Updates to address feedback

--- test/server/cards/03_TWI/units/RepublicCommando.spec.ts
+++ test/server/cards/03_TWI/units/RepublicCommando.spec.ts
@@ -5,28 +5,7 @@ describe('Republic Commando', function() {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
-                        groundArena: ['332nd-stalwart', 'republic-commando', 'clone-heavy-gunner']
-                    },
-                    player2: {
-                        groundArena: ['pyke-sentinel']
-                    }
-                });
-
-                const { context } = contextRef;
-
-                // Unit has Saboteur when Coordinate is active
-                context.player1.clickCard(context.republicCommando);
-                expect(context.player1).toBeAbleToSelectExactly([context.pykeSentinel, context.p2Base]);
-
-                context.player1.clickCard(context.p2Base);
-                expect(context.p2Base.damage).toBe(2);
-                expect(context.player2).toBeActivePlayer();
-            });
-
-            it('should not grant Saboteur when Coordinate is not active', function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
+                        hand: ['332nd-stalwart'],
                         groundArena: ['republic-commando', 'clone-heavy-gunner']
                     },
                     player2: {
@@ -40,6 +19,18 @@ describe('Republic Commando', function() {
                 context.player1.clickCard(context.republicCommando);
                 expect(context.pykeSentinel.damage).toBe(2);
                 expect(context.republicCommando.damage).toBe(2);
+                expect(context.player2).toBeActivePlayer();
+
+                context.moveToNextActionPhase();
+                context.player1.clickCard(context._332ndStalwart);
+                context.player2.passAction();
+
+                // Unit has Saboteur when Coordinate is active
+                context.player1.clickCard(context.republicCommando);
+                expect(context.player1).toBeAbleToSelectExactly([context.pykeSentinel, context.p2Base]);
+
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(2);
                 expect(context.player2).toBeActivePlayer();
             });
         });
