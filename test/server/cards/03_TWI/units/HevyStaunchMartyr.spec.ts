02d0d5cfb609fd3e49808ddd8d2c8cd7938d2f25
Stephane Soulier <stephane.soulier@alan.com>
2024-12-11 13:09:07 +0100

    TWI hevy staunch martyr

--- test/server/cards/03_TWI/units/HevyStaunchMartyr.spec.ts
+++ test/server/cards/03_TWI/units/HevyStaunchMartyr.spec.ts
@@ -8,22 +8,24 @@ describe('Hevy Staunch Martyr', function () {
                         groundArena: ['specforce-soldier', 'luke-skywalker#jedi-knight', 'hevy#staunch-martyr'],
                     },
                     player2: {
-                        groundArena: ['consular-security-force', 'wampa'],
+                        groundArena: ['consular-security-force', 'wampa', 'fleet-lieutenant'],
                     }
                 });
 
                 const { context } = contextRef;
 
                 context.player1.clickCard(context.hevy);
-                // context.player1.clickCard(context.consularSecurityForce);
-                expect(1).toBe(1);
-                // expect(context.consularSecurityForce.damage).toBe(6);
-                // expect(context.hevy.damage).toBe(3);
+                context.player1.clickCard(context.consularSecurityForce);
+                expect(context.consularSecurityForce.damage).toBe(6);
+                expect(context.hevy.damage).toBe(3);
 
-                // context.player2.clickCard(context.wampa);
-                // context.player2.clickCard(context.heavy);
-                // expect(context.wampa).toBeInZone('discard');
-                // expect(context.consularSecurityForce).toBeInZone('discard');
+                // When defeated, deal 1 damage to each enemy ground unit
+                context.player2.clickCard(context.wampa);
+                context.player2.clickCard(context.hevy);
+                expect(context.hevy).toBeInZone('discard');
+                expect(context.wampa).toBeInZone('discard');
+                expect(context.consularSecurityForce).toBeInZone('discard');
+                expect(context.fleetLieutenant.damage).toBe(1);
             });
         });
     });
