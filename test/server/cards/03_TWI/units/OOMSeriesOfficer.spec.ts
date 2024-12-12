0d9d1b91a287670a915bd1fb761f7af31a28077d
HugoHMZ <hugo.humez.p@gmail.com>
2024-12-12 15:25:34 +0900

    [+] OOM-Series Officer: Card Implementation

---
+++ test/server/cards/03_TWI/units/OOMSeriesOfficer.spec.ts
@@ -0,0 +1,49 @@
+describe('OOM-Series Officer', function () {
+    integration(function (contextRef) {
+        describe('OOM-Series Officer\'s ability', function () {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['oomseries-officer'],
+                        base: 'droid-manufactory'
+                    },
+                    player2: {
+                        groundArena: ['duchesss-champion'],
+                        base: 'sundari'
+                    }
+                });
+            });
+
+            it('Player 1 Base should take 2 damages when Officer is defeated', function () {
+                const { context } = contextRef;
+
+                // Attack
+                context.player1.clickCard(context.oomseriesOfficer);
+                expect(context.player1).toBeAbleToSelectExactly([context.duchesssChampion, context.p2Base]);
+                context.player1.clickCard(context.duchesssChampion);
+
+                // Check When Defeated trigger
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                context.player1.clickCard(context.p1Base);
+                expect(context.p1Base.damage).toBe(2);
+                expect(context.oomseriesOfficer).toBeInZone('discard');
+            });
+
+            it('Player 2 Base should take 2 damages when Officer is defeated', function () {
+                const { context } = contextRef;
+
+                // Attack
+                context.player1.clickCard(context.oomseriesOfficer);
+                expect(context.player1).toBeAbleToSelectExactly([context.duchesssChampion, context.p2Base]);
+                context.player1.clickCard(context.duchesssChampion);
+
+                // Check When Defeated trigger
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(2);
+                expect(context.oomseriesOfficer).toBeInZone('discard');
+            });
+        });
+    });
+});
