b13d2522bd5140f49d05e118c03aed5175969636
AMMayberry1 <ammayberry1@gmail.com>
2024-12-13 14:30:09 -0500

    Add On the Doorstep, 'enters ready' is working

---
+++ server/game/cards/03_TWI/events/OnTheDoorstep.ts
@@ -0,0 +1,22 @@
+import { EventCard } from '../../../core/card/EventCard';
+import AbilityHelper from '../../../AbilityHelper';
+
+export default class OnTheDoorstep extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '2483302291',
+            internalName: 'on-the-doorstep',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Create 3 Battle Droid tokens and ready them',
+            immediateEffect: AbilityHelper.immediateEffects.createBattleDroid(
+                (context) => ({ amount: 3, target: context.source.controller, entersReady: true })
+            )
+        });
+    }
+}
+
+OnTheDoorstep.implemented = true;
