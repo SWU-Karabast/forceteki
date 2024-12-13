4aff5b5d9575e9d8849ac12b06cca1774bad445b
Jeremy <jeremydunn29@gmail.com>
2024-12-13 11:30:32 -0600

    ESLint fixes

--- scripts/run-server.js
+++ scripts/run-server.js
@@ -1,22 +1,22 @@
 const fs = require('fs/promises');
-const childProcess = require('child_process')
+const childProcess = require('child_process');
 
-const envPath = '.env'
+const envPath = '.env';
 
 async function createEnvFile() {
     const defaultEnvFields = {
-        GAME_NODE_HOST: "localhost",
-        GAME_NODE_NAME: "test1",
-        GAME_NODE_SOCKET_IO_PORT: "9500",
-        ENVIRONMENT: "development",
-        SECRET: "verysecret"
-    }
+        GAME_NODE_HOST: 'localhost',
+        GAME_NODE_NAME: 'test1',
+        GAME_NODE_SOCKET_IO_PORT: '9500',
+        ENVIRONMENT: 'development',
+        SECRET: 'verysecret'
+    };
     return fs.writeFile(
         envPath,
         Object.entries(defaultEnvFields)
             .map(([key, value]) => `${key}="${value}"`)
-            .join("\n")
-    )
+            .join('\n')
+    );
 }
 
 async function fileExists(path) {
@@ -24,23 +24,22 @@ async function fileExists(path) {
         await fs.stat(path);
         return true;
     } catch (error) {
-        return false
+        return false;
     }
 }
 
 async function runCommand() {
-
-    const envFileExists = await fileExists(envPath)
+    const envFileExists = await fileExists(envPath);
 
     if (!envFileExists) {
-        console.log("Creating .env file.");
+        console.log('Creating .env file.');
         await createEnvFile();
     }
 
-    console.log("Building the server.");
+    console.log('Building the server.');
     childProcess.execSync('npm run build');
 
-    const serverProcess = childProcess.spawn('node', ['./build/server/gamenode'])
+    const serverProcess = childProcess.spawn('node', ['./build/server/gamenode']);
 
     // pipe the server output to the current shell
     // so the dev can see it.
@@ -48,8 +47,8 @@ async function runCommand() {
 
     // Disconnect the pipe on exit
     serverProcess.on('exit', () => {
-        serverProcess.stdout.unpipe(process.stdout)
-    })
+        serverProcess.stdout.unpipe(process.stdout);
+    });
 }
 
 runCommand();
