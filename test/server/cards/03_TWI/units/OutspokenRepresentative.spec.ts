e0c50ae90f3eb5279e1542c7d13f8b8a0b4085ec
HugoHMZ <hugo.humez.p@gmail.com>
2024-12-15 18:55:20 +0900

    [+] Outspoken Representative: adding test

--- test/server/cards/03_TWI/units/OutspokenRepresentative.spec.ts
+++ test/server/cards/03_TWI/units/OutspokenRepresentative.spec.ts
@@ -1,21 +1,45 @@
 describe('Outspoken Representative', function () {
     integration(function (contextRef) {
-        describe('ability', function () {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        groundArena: ['duchesss-champion']
-                    },
-                    player2: {
-                        groundArena: ['specforce-soldier'],
-                    },
-                });
+        it('should gain Sentinel', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['clone-heavy-gunner'],
+                    groundArena: ['outspoken-representative'],
+                },
+                player2: {
+                    groundArena: ['specforce-soldier', 'volunteer-soldier'],
+                    hasInitiative: true,
+                },
             });
+            const { context } = contextRef;
 
-            it('test Name', function () {
-                const { context } = contextRef;
+            // Check if sentinel is not yet active
+            context.player2.clickCard(context.specforceSoldier);
+            expect(context.player2).toBeAbleToSelectExactly([context.outspokenRepresentative, context.p1Base]);
+            context.player2.clickCard(context.outspokenRepresentative);
+
+            // Check if sentinel is now active
+            context.player1.clickCard(context.cloneHeavyGunner);
+            context.player2.clickCard(context.volunteerSoldier);
+            expect(context.player2).toBeAbleToSelectExactly([context.outspokenRepresentative]);
+            context.player2.clickCard(context.outspokenRepresentative);
+        });
+        it('should create a Clone Trooper token when defeated', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    groundArena: ['outspoken-representative'],
+                },
+                player2: {
+                    groundArena: ['battlefield-marine'],
+                },
             });
+            const { context } = contextRef;
+            context.player1.clickCard(context.outspokenRepresentative);
+            context.player1.clickCard(context.battlefieldMarine);
+            const cloneTroopers = context.player1.findCardsByName('clone-trooper');
+            expect(cloneTroopers.length).toBe(1);
         });
     });
 });
