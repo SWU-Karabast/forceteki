3752efbced49c0c0721b1e35c3a5ce6f44a55501
Valentin Jubert <val_jub@live.fr>
2024-12-17 23:25:51 +0100

    swoop down

--- test/server/cards/01_SOR/events/PrecisionFire.spec.ts
+++ test/server/cards/01_SOR/events/PrecisionFire.spec.ts
@@ -1,56 +1,54 @@
 describe('Precision Fire', function () {
     integration(function (contextRef) {
-        describe('Precision Fire\'s ability', function () {
-            beforeEach(function () {
-                contextRef.setupTest({
-                    phase: 'action',
-                    player1: {
-                        hand: ['precision-fire'],
-                        groundArena: ['battlefield-marine'],
-                        spaceArena: ['alliance-xwing']
-                    },
-                    player2: {
-                        groundArena: ['echo-base-defender'],
-                        spaceArena: ['corellian-freighter']
-                    }
-                });
+        it('Precision Fire\'s ability should initiate an attack with saboteur and +2/+0 if unit is trooper', function () {
+            contextRef.setupTest({
+                phase: 'action',
+                player1: {
+                    hand: ['precision-fire'],
+                    groundArena: ['battlefield-marine'],
+                    spaceArena: ['alliance-xwing']
+                },
+                player2: {
+                    groundArena: ['echo-base-defender'],
+                    spaceArena: ['corellian-freighter']
+                }
             });
 
-            it('should initiate 1 with saboteur and +2/+0 because unit is trooper', function () {
-                const { context } = contextRef;
+            const { context } = contextRef;
 
-                context.player1.clickCard(context.precisionFire);
-                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.allianceXwing]);
+            context.player1.clickCard(context.precisionFire);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.allianceXwing]);
 
-                // choose a trooper unit
-                context.player1.clickCard(context.battlefieldMarine);
+            // choose a trooper unit
+            context.player1.clickCard(context.battlefieldMarine);
 
-                // saboteur: we ignore sentinel
-                expect(context.player1).toBeAbleToSelectExactly([context.echoBaseDefender, context.p2Base]);
-                context.player1.clickCard(context.p2Base);
-                expect(context.p2Base.damage).toBe(5); // 3+2 (because battlefield marine is a trooper)
+            // saboteur: we ignore sentinel
+            expect(context.player1).toBeAbleToSelectExactly([context.echoBaseDefender, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(5); // 3+2 (because battlefield marine is a trooper)
 
-                expect(context.player2).toBeActivePlayer();
-                expect(context.battlefieldMarine.getPower()).toBe(3);
-            });
+            expect(context.player2).toBeActivePlayer();
+            expect(context.battlefieldMarine.getPower()).toBe(3);
 
-            it('should initiate 1 attack with saboteur and not +2/+0 because unit is not trooper', function () {
-                const { context } = contextRef;
+            // reset
+            context.battlefieldMarine.exhausted = false;
+            context.setDamage(context.p2Base, 0);
+            context.player1.moveCard(context.precisionFire, 'hand');
+            context.player2.passAction();
 
-                context.player1.clickCard(context.precisionFire);
-                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.allianceXwing]);
+            context.player1.clickCard(context.precisionFire);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.allianceXwing]);
 
-                // do not choose a trooper unit
-                context.player1.clickCard(context.allianceXwing);
+            // do not choose a trooper unit
+            context.player1.clickCard(context.allianceXwing);
 
-                // saboteur: we ignore sentinel
-                expect(context.player1).toBeAbleToSelectExactly([context.corellianFreighter, context.p2Base]);
-                context.player1.clickCard(context.p2Base);
-                expect(context.p2Base.damage).toBe(2);
+            // saboteur: we ignore sentinel
+            expect(context.player1).toBeAbleToSelectExactly([context.corellianFreighter, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
+            expect(context.p2Base.damage).toBe(2);
 
-                expect(context.player2).toBeActivePlayer();
-                expect(context.allianceXwing.getPower()).toBe(2);
-            });
+            expect(context.player2).toBeActivePlayer();
+            expect(context.allianceXwing.getPower()).toBe(2);
         });
     });
 });
