358d4c807a0677def76f53868a34101392563080
Travis Chase <travis@travischase.me>
2024-12-18 10:06:41 -0600

    Implementing Droid Cohort

---
+++ server/game/cards/03_TWI/upgrades/DroidCohort.ts
@@ -0,0 +1,23 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+
+export default class DroidCohort extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '7547538214',
+            internalName: 'droid-cohort',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addGainTriggeredAbilityTargetingAttached({
+            title: 'Create a Battle Droid token.',
+            when: {
+                onCardDefeated: (event, context) => event.card.parentCard === context.source
+            },
+            immediateEffect: AbilityHelper.immediateEffects.createBattleDroid()
+        });
+    }
+}
+
+DroidCohort.implemented = true;
