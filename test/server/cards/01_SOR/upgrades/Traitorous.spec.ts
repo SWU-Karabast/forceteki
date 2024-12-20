d12939313e829ab57338ac46c39d3f1504caf301
Tommaso Madonia <tommaso@madonia.me>
2024-12-20 21:07:47 +0000

    Add comments to describe test

--- test/server/cards/01_SOR/upgrades/Traitorous.spec.ts
+++ test/server/cards/01_SOR/upgrades/Traitorous.spec.ts
@@ -55,19 +55,24 @@ describe('Traitorous', function() {
             it('gives back control of a unit when unattached', function () {
                 const { context } = contextRef;
 
+                // Player 1 plays Traitorous to take control of the Superlaser Technician
                 context.player1.clickCard(context.traitorous);
                 expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.superlaserTechnician, context.cartelSpacer, context.wampa, context.lukeSkywalker, context.survivorsGauntlet]);
                 context.player1.clickCard(context.superlaserTechnician);
 
                 expect(context.superlaserTechnician).toBeInZone('groundArena', context.player1);
 
+                // Player 2 passes
                 context.player2.passAction();
 
+                // Player 1 plays Change of Heart to take control of the Wampa
                 context.player1.clickCard(context.changeOfHeart);
                 context.player1.clickCard(context.wampa);
 
                 expect(context.wampa).toBeInZone('groundArena', context.player1);
 
+                // Player 2 attacks with Survivors Gauntlet and move Traitorous to the Wampa,
+                // this causes the Superlaser Technician to go back under control of Player 2
                 context.player2.clickCard(context.survivorsGauntlet);
                 context.player2.clickCard(context.p1Base);
                 context.player2.clickCard(context.traitorous);
@@ -83,8 +88,12 @@ describe('Traitorous', function() {
                 expect(context.superlaserTechnician).toBeInZone('groundArena', context.player2);
                 expect(context.wampa).toBeInZone('groundArena', context.player1);
 
+                // Player 1 passes
                 context.player1.passAction();
 
+                // Player 2 plays Disabling Fang Fighter to defeat Traitorous,
+                // this causes the Wampa to go back under control of Player 2
+                // even if the Wampa costs more than 3
                 context.player2.clickCard(context.disablingFangFighter);
                 context.player2.clickCard(context.traitorous);
 
