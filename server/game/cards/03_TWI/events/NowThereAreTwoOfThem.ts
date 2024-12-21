b5df60338afde2259bd7dcf82387f05140093290
Stephane Soulier <stephane.soulier@alan.com>
2024-12-21 18:47:42 +0100

    improve formatting

--- server/game/cards/03_TWI/events/NowThereAreTwoOfThem.ts
+++ server/game/cards/03_TWI/events/NowThereAreTwoOfThem.ts
@@ -21,8 +21,12 @@ export default class NowThereAreTwoOfThem extends EventCard {
                     activePromptTitle: 'Play a non-Vehicle unit from your hand that shared a Trait with the unit you control. It costs 5 less.',
                     cardTypeFilter: CardType.BasicUnit,
                     zoneFilter: ZoneName.Hand,
-                    cardCondition: (card, context) => (!card.hasSomeTrait(Trait.Vehicle)) && (Array.from(context.source.controller.getUnitsInPlay()[0].traits).some((trait) => card.hasSomeTrait(trait))),
-                    innerSystem: AbilityHelper.immediateEffects.playCardFromHand({ adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 5 } })
+                    cardCondition: (card, context) =>
+                        !card.hasSomeTrait(Trait.Vehicle) &&
+                        Array.from(context.source.controller.getUnitsInPlay()[0].traits).some((trait) => card.hasSomeTrait(trait)),
+                    innerSystem: AbilityHelper.immediateEffects.playCardFromHand({
+                        adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 5 }
+                    })
                 }),
             })
         });
