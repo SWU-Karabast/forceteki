10909b64a7b952beeb34f0df582895d31260f151
Stephane Soulier <stephane.soulier@alan.com>
2024-12-26 10:50:51 +0100

    improve test

--- test/server/cards/01_SOR/events/SneakAttack.spec.ts
+++ test/server/cards/01_SOR/events/SneakAttack.spec.ts
@@ -1,30 +1,59 @@
 describe('Sneak Attack', function() {
     integration(function(contextRef) {
-        it('Sneak Attack ability\'s should play Sabine for 1 resource less and defeat it at the end.', function () {
-            contextRef.setupTest({
-                phase: 'action',
-                player1: {
-                    hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'obiwan-kenobi#following-fate'],
-                    groundArena: ['battlefield-marine'],
-                    spaceArena: ['cartel-spacer'],
-                    base: { card: 'administrators-tower', damage: 0 },
-                    leader: { card: 'luke-skywalker#faithful-friend', deployed: false },
-                    resources: 3
-                },
+        describe('Sneak Attack\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        hand: ['sneak-attack', 'sabine-wren#you-can-count-on-me', 'obiwan-kenobi#following-fate', 'recruit'],
+                        groundArena: ['battlefield-marine'],
+                        spaceArena: ['cartel-spacer'],
+                        base: 'administrators-tower',
+                        leader: 'luke-skywalker#faithful-friend',
+                        resources: 3
+                    },
+                    player2: {
+                        groundArena: ['atst'],
+                    }
+                });
             });
 
-            const { context } = contextRef;
+            it('should play Sabine for 1 resource less and defeat it at the end.', function () {
+                const { context } = contextRef;
 
-            context.player1.clickCard(context.sneakAttack);
-            expect(context.player1).toBeAbleToSelectExactly([context.sabineWren]);
-            context.player1.clickCard(context.sabineWren);
-            expect(context.sabineWren.exhausted).toBeFalse();
-            expect(context.player1.readyResourceCount).toBe(0);
+                context.player1.clickCard(context.sneakAttack);
+                expect(context.player1).toBeAbleToSelectExactly([context.sabineWren, context.obiwanKenobi]);
+                context.player1.clickCard(context.sabineWren);
+                expect(context.sabineWren.exhausted).toBeFalse();
+                expect(context.player1.readyResourceCount).toBe(0);
 
-            // Check that Sabine is defeated at the beginning of the regroup phase
-            context.moveToRegroupPhase();
-            expect(context.sabineWren).toBeInZone('discard');
-            expect(context.player1).toHavePrompt('Select between 0 and 1 cards to resource');
+                // Check that Sabine is defeated at the beginning of the regroup phase
+                context.moveToRegroupPhase();
+                expect(context.sabineWren).toBeInZone('discard');
+                expect(context.player1).toHavePrompt('Select between 0 and 1 cards to resource');
+            });
+
+            it('should not play Obi-Wan because not enough resource', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.sneakAttack);
+                expect(context.player1).toBeAbleToSelectExactly([context.sabineWren, context.obiwanKenobi]);
+                context.player1.clickCard(context.obiwanKenobi);
+                expect(context.obiwanKenobi).toBeInZone('hand');
+                expect(context.sabineWren).toBeInZone('hand');
+                expect(context.player1.readyResourceCount).toBe(1);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not bug if Sabine is defeated before the end of the phase', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.sneakAttack);
+                context.player1.clickCard(context.sabineWren);
+                context.player2.clickCard(context.atst);
+                context.player2.clickCard(context.sabineWren);
+                expect(context.sabineWren).toBeInZone('discard');
+            });
         });
     });
 });
