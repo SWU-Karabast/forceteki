73fa6fee0222fc773e0c275b7a9620bd4452322e
Nick Pelton <nickpelton@gmail.com>
2024-12-26 22:03:06 -0600

    linting

--- test/server/cards/03_TWI/units/SubjugatingStarfighter.spec.ts
+++ test/server/cards/03_TWI/units/SubjugatingStarfighter.spec.ts
@@ -1,7 +1,6 @@
 describe('Subjugating Starfighter', function() {
     integration(function(contextRef) {
         describe('Subjugating Starfighter\'s abilities', function() {
-            
             it('Should create a Battle Droid token if the player has the initiative', function () {
                 contextRef.setupTest({
                     phase: 'action',
@@ -15,10 +14,9 @@ describe('Subjugating Starfighter', function() {
                 });
 
                 const { context } = contextRef;
-                
-                // // Play the card.
+                // Play the card.
                 context.player1.clickCard(context.subjugatingStarfighter);
-                
+
                 // Check if the Battle Droid token was created if the player has initiative.'
                 context.player1.clickPrompt('Create a Battle Droid token.');
                 expect(context.player1.findCardsByName('battle-droid').length).toBe(1);
@@ -44,13 +42,13 @@ describe('Subjugating Starfighter', function() {
                 });
 
                 const { context } = contextRef;
-                
+
                 // Have player 2 claim initiative
                 context.player2.claimInitiative();
 
                 // Play the card.
                 context.player1.clickCard(context.subjugatingStarfighter);
-                
+
                 // Check that the Battle Droid token was not created.
                 expect(context.player1.findCardsByName('battle-droid').length).toBe(0);
 
@@ -59,8 +57,6 @@ describe('Subjugating Starfighter', function() {
                 context.player1.clickCard(context.hwk290Freighter);
                 expect(context.hwk290Freighter.damage).toBe(3);
             });
-
-        
         });
     });
 });
