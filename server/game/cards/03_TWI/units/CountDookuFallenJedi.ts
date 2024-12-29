ba7681d13bca64da2ce8067cd067ab605b08711a
AMMayberry1 <ammayberry1@gmail.com>
2024-12-29 17:24:04 -0500

    Fix Count Dooku optional and round out tests; all tests passing

--- server/game/cards/03_TWI/units/CountDookuFallenJedi.ts
+++ server/game/cards/03_TWI/units/CountDookuFallenJedi.ts
@@ -14,13 +14,13 @@ export default class CountDookuFallenJedi extends NonLeaderUnitCard {
     public override setupCardAbilities () {
         this.addWhenPlayedAbility({
             title: 'For each unit you exploited while playing this card, deal damage to an enemy unit equal to the power of the exploited unit',
-            optional: true,
             immediateEffect: AbilityHelper.immediateEffects.sequential(
                 // TODO: correct implementation of the rules for multiple instances of damage in the same ability
                 (context) => Helpers.asArray(context.event.costs.exploitedUnitsInformation).map((exploitedUnitInformation) =>
                     AbilityHelper.immediateEffects.selectCard({
                         cardTypeFilter: WildcardCardType.Unit,
                         controller: RelativePlayer.Opponent,
+                        optional: true,
                         innerSystem: AbilityHelper.immediateEffects.damage({ amount: exploitedUnitInformation.power }),
                     })
                 )
