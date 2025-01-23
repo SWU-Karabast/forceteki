bd3c32737f7a7e575f008b23a67bc87a5cba185a
Robbie Moyer <rmoyer08@gmail.com>
2025-01-23 14:49:58 -0500

    Add test cases for Second Chance

--- server/game/cards/02_SHD/upgrades/SecondChance.ts
+++ server/game/cards/02_SHD/upgrades/SecondChance.ts
@@ -10,16 +10,14 @@ export default class SecondChance extends UpgradeCard {
         };
     }
 
-    protected override setupCardAbilities() {
+    public override setupCardAbilities() {
         this.setAttachCondition((card) => card.isNonLeaderUnit());
         this.addGainWhenDefeatedAbilityTargetingAttached({
             title: 'For this phase, this unit\'s owner may play it from their discard pile for free.',
             immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect((context) => ({
                 effect: [
                     OngoingEffectLibrary.canPlayFromDiscard(),
-                    OngoingEffectLibrary.forFree({
-                        match: (card) => card === context.source
-                    })
+                    OngoingEffectLibrary.forFree({ match: (card) => card === context.source })
                 ]
             }))
         });
