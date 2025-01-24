f19f21a18f6bc7ab42a9e344135dcef124e04809
Kevin Ray <kevin.l.ray@gmail.com>
2025-01-24 11:41:57 -0600

    add more tests

--- test/server/cards/02_SHD/units/IG11ICannotBeCaptured.spec.ts
+++ test/server/cards/02_SHD/units/IG11ICannotBeCaptured.spec.ts
@@ -82,5 +82,113 @@ describe('IG-11, I Cannot Be Captured', function() {
                 expect(context.p2Base.damage).toBe(0);
             });
         });
+
+        describe('IG-11\'s on capture ability still triggers iden\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['rebel-pathfinder', 'bounty-hunter-crew'],
+                        spaceArena: ['cartel-spacer'],
+                        hand: ['relentless-pursuit'],
+                        leader: { card: 'iden-versio#inferno-squad-commander', deployed: true },
+                        base: { card: 'dagobah-swamp', damage: 5 }
+                    },
+                    player2: {
+                        groundArena: ['ig11#i-cannot-be-captured', 'wampa'],
+
+                    }
+                });
+            });
+
+            it('should deal to 3 damage to each enemeny ground unit', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.relentlessPursuit);
+                context.player1.clickCard(context.bountyHunterCrew);
+                context.player1.clickCard(context.ig11);
+                expect(context.ig11).toBeInZone('discard')
+                expect(context.wampa.damage).toBe(0);
+                expect(context.cartelSpacer.damage).toBe(0);
+                expect(context.rebelPathfinder).toBeInZone('discard')
+                expect(context.bountyHunterCrew.damage).toBe(3);
+                expect(context.idenVersio.damage).toBe(3); // Doesn't set Iden up with a shield
+                expect(context.p1Base.damage).toBe(4);
+            });
+        });
+
+        describe('IG-11\'s on capture ability still triggers when defeated ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['bounty-hunter-crew', 'oomseries-officer'],
+                        spaceArena: ['cartel-spacer'],
+                        hand: ['relentless-pursuit']
+                    },
+                    player2: {
+                        groundArena: ['ig11#i-cannot-be-captured', 'wampa'],
+
+                    }
+                });
+            });
+
+            it('should deal to 3 damage to each enemeny ground unit', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.relentlessPursuit);
+                context.player1.clickCard(context.bountyHunterCrew);
+                context.player1.clickCard(context.ig11);
+                expect(context.ig11).toBeInZone('discard')
+                expect(context.wampa.damage).toBe(0);
+                expect(context.cartelSpacer.damage).toBe(0);
+                expect(context.oomseriesOfficer).toBeInZone('discard')
+                expect(context.bountyHunterCrew.damage).toBe(3);
+                context.player1.clickCard(context.p2Base)
+                expect(context.p2Base.damage).toBe(2);
+            });
+        });
+
+        describe('IG-11\'s on capture ability still triggers when defeated ability and Iden\'s ability', function() {
+            beforeEach(function () {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['bounty-hunter-crew', 'oomseries-officer'],
+                        spaceArena: ['cartel-spacer'],
+                        hand: ['relentless-pursuit'],
+                        leader: { card: 'iden-versio#inferno-squad-commander', deployed: true }
+                    },
+                    player2: {
+                        groundArena: ['ig11#i-cannot-be-captured', 'wampa'],
+
+                    }
+                });
+            });
+
+            it('should deal to 3 damage to each enemeny ground unit', function () {
+                const { context } = contextRef;
+
+                context.setDamage(context.p1Base, 5)
+                context.player1.clickCard(context.relentlessPursuit);
+                context.player1.clickCard(context.bountyHunterCrew);
+                context.player1.clickCard(context.ig11);
+                
+                expect(context.player1).toHaveEnabledPromptButton('When an opponent\'s unit is defeated, heal 1 from base');
+                expect(context.player1).toHaveEnabledPromptButton('Deal 2 damage to a base.');
+                context.player1.clickPrompt('When an opponent\'s unit is defeated, heal 1 from base');
+                expect(context.p1Base.damage).toBe(4);
+
+                context.player1.clickCard(context.p2Base)
+                expect(context.ig11).toBeInZone('discard')
+                expect(context.wampa.damage).toBe(0);
+                expect(context.cartelSpacer.damage).toBe(0);
+                expect(context.oomseriesOfficer).toBeInZone('discard')
+                expect(context.bountyHunterCrew.damage).toBe(3);
+                expect(context.idenVersio.damage).toBe(3); // Doesn't set Iden up with a shield
+                expect(context.p2Base.damage).toBe(2);
+                
+            });
+        });
     });
 });
