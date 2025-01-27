90547169f7e2ed7943174089d3f3b5ad54b85a48
Kevin Ray <kevin.l.ray@gmail.com>
2025-01-26 23:08:31 -0600

    consolidate tests

--- test/server/cards/02_SHD/upgrades/BobaFettsArmor.spec.ts
+++ test/server/cards/02_SHD/upgrades/BobaFettsArmor.spec.ts
@@ -1,10 +1,12 @@
 describe('Boba Fett\'s Armor', function () {
     integration(function (contextRef) {
-        it('Boba Fett\'s Armor prevents 2 damage when attacked with > 2 power', function () {
+        it('Boba Fett\'s Armor prevents 2 damage', function () {
             contextRef.setupTest({
                 phase: 'action',
                 player1: {
-                    groundArena: ['battlefield-marine']
+                    groundArena: ['battlefield-marine', { card: 'sabine-wren#explosives-artist', upgrades: ['boba-fetts-armor'] }],
+                    leader: { card: 'boba-fett#daimyo', deployed: true },
+                    hand: ['bombing-run'],
                 },
                 player2: {
                     groundArena: [{ card: 'boba-fett#disintegrator', upgrades: ['boba-fetts-armor'] }]
@@ -13,89 +15,52 @@ describe('Boba Fett\'s Armor', function () {
 
             const { context } = contextRef;
 
-            // initial check
-            expect(context.bobaFett.getPower()).toBe(5);
-            expect(context.bobaFett.getHp()).toBe(7);
+            const resetBobaHealth = () => {
+                context.setDamage(context.bobaFettDisintegrator, 0);
+                context.setDamage(context.bobaFettDaimyo, 0);
+            };
 
-            // attack base and power up other unit
+            // CASE 1: Armor prevents 2 damage when attack damage > 2
             context.player1.clickCard(context.battlefieldMarine);
-            context.player1.clickCard(context.bobaFett);
-            expect(context.bobaFett.damage).toBe(1);
+            context.player1.clickCard(context.bobaFettDisintegrator);
+            expect(context.bobaFettDisintegrator.damage).toBe(1);
             expect(context.battlefieldMarine).toBeInZone('discard');
-        });
 
-        it('Boba Fett\'s Armor prevents 2 damage when attacked with 2 power and ping ability', function () {
-            contextRef.setupTest({
-                phase: 'action',
-                player1: {
-                    groundArena: ['sabine-wren#explosives-artist']
-                },
-                player2: {
-                    leader: { card: 'boba-fett#collecting-the-bounty', upgrades: ['boba-fetts-armor'], deployed: true },
-                }
-            });
+            resetBobaHealth();
 
-            const { context } = contextRef;
+            context.player2.passAction();
 
-            // initial check
-            expect(context.bobaFett.getPower()).toBe(6);
-            expect(context.bobaFett.getHp()).toBe(9);
-
-            // attack base and power up other unit
+            // CASE 2: Armor does not prevent damage to when not Boba Fett
+            //         and damage is reduced to 0 when <= 2.
             context.player1.clickCard(context.sabineWren);
-            context.player1.clickCard(context.bobaFett);
-            expect(context.player1).toBeAbleToSelectExactly([context.bobaFett, context.p1Base, context.p2Base]);
-            context.player1.clickCard(context.bobaFett);
-            expect(context.bobaFett.damage).toBe(0);
-        });
+            context.player1.clickCard(context.bobaFettDisintegrator);
+            context.player1.clickCard(context.bobaFettDisintegrator);
+            expect(context.bobaFettDisintegrator.damage).toBe(2);
+            expect(context.sabineWren).toBeInZone('discard');
 
-        it('Boba Fett\'s Armor prevents 2 damage from event', function () {
-            contextRef.setupTest({
-                phase: 'action',
-                player1: {
-                    groundArena: ['battlefield-marine'],
-                    hand: ['bombing-run'],
-                },
-                player2: {
-                    leader: { card: 'boba-fett#daimyo', upgrades: ['boba-fetts-armor'], deployed: true },
-                }
-            });
+            // Reset armor onto other Boba Fett for next tests
+            context.player1.moveCard(context.player1.discard[1], 'hand');
+            context.player2.passAction();
+            context.player1.clickCard(context.player1.hand[1]);
+            context.player1.clickCard(context.bobaFettDaimyo);
 
-            const { context } = contextRef;
+            resetBobaHealth();
 
-            // initial check
-            expect(context.bobaFett.getPower()).toBe(6);
-            expect(context.bobaFett.getHp()).toBe(9);
+            // CASE 3: Other Boba Fett also prevents damage
+            context.player2.clickCard(context.bobaFettDisintegrator);
+            context.player2.clickCard(context.bobaFettDaimyo);
+            context.player2.clickPrompt('You');
+            expect(context.bobaFettDisintegrator.damage).toBe(4);
+            expect(context.bobaFettDaimyo.damage).toBe(3);
 
-            // play bombing run
+            resetBobaHealth();
+
+            // CASE 4: Armor prevent 2 damage from an event
             context.player1.clickCard(context.bombingRun);
             context.player1.clickPrompt('Ground');
-            expect(context.bobaFett.damage).toBe(1);
-            expect(context.battlefieldMarine).toBeInZone('discard');
-        });
-
-        it('Boba Fett\'s Armor prevents 0 damage if not Boba Fett', function () {
-            contextRef.setupTest({
-                phase: 'action',
-                player1: {
-                    groundArena: ['battlefield-marine']
-                },
-                player2: {
-                    groundArena: [{ card: 'sabine-wren#explosives-artist', upgrades: ['boba-fetts-armor'] }]
-                }
-            });
-
-            const { context } = contextRef;
-
-            // initial check
-            expect(context.sabineWren.getPower()).toBe(4);
-            expect(context.sabineWren.getHp()).toBe(5);
-
-            // attack base and power up other unit
-            context.player1.clickCard(context.battlefieldMarine);
-            context.player1.clickCard(context.sabineWren);
-            expect(context.sabineWren.damage).toBe(3);
-            expect(context.battlefieldMarine).toBeInZone('discard');
+            context.player1.clickPrompt('Opponent');
+            expect(context.bobaFettDisintegrator.damage).toBe(1);
+            expect(context.bobaFettDaimyo.damage).toBe(1);
         });
     });
 });
