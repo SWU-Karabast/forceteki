3572376cdc39afeeb604c8841aa818288b5b824b
Anthony Bronico <adbronico92@gmail.com>
2025-01-29 13:40:36 -0500

    Updates to Han tests and Defeat Card system to allow defeating events in resources

--- test/server/cards/01_SOR/leaders/HanSoloAudaciousSmuggler.spec.ts
+++ test/server/cards/01_SOR/leaders/HanSoloAudaciousSmuggler.spec.ts
@@ -8,7 +8,7 @@ describe('Han Solo, Audacious Smuggler', function() {
                         leader: 'han-solo#audacious-smuggler',
                         hand: ['pyke-sentinel', 'wampa'],
                         deck: ['liberated-slaves'],
-                        resources: ['cunning', 'aggression', 'vigilance', 'command', 'atst'],
+                        resources: ['cunning', 'aggression', 'spark-of-rebellion', 'protector', 'atst'],
                     },
                     player2: {
                         groundArena: ['moisture-farmer'],
@@ -31,9 +31,9 @@ describe('Han Solo, Audacious Smuggler', function() {
                 context.player1.clickPrompt('Done');
 
                 expect(context.player1).toHavePrompt('Defeat a resource you control');
-                expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.cunning, context.aggression, context.vigilance, context.command, context.atst]);
-                context.player1.clickCard(context.wampa);
-                expect(context.wampa).toBeInZone('discard', context.player1);
+                expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.cunning, context.aggression, context.sparkOfRebellion, context.protector, context.atst]);
+                context.player1.clickCard(context.aggression);
+                expect(context.aggression).toBeInZone('discard', context.player1);
                 expect(context.player1.readyResourceCount).toBe(5);
             });
 
@@ -76,7 +76,7 @@ describe('Han Solo, Audacious Smuggler', function() {
         });
 
         describe('Han Solo\'s leader unit ability', function() {
-            beforeEach(function() {
+            it('should put the top card of his deck into play as a ready resource and then defeat a resource at the start of the next action phase', function() {
                 contextRef.setupTest({
                     phase: 'action',
                     player1: {
@@ -85,10 +85,9 @@ describe('Han Solo, Audacious Smuggler', function() {
                         resources: 6
                     }
                 });
-            });
 
-            it('should put the top card of his deck into play as a ready resource and then defeat a resource at the start of the next action phase', function() {
                 const { context } = contextRef;
+
                 expect(context.player1.readyResourceCount).toBe(6);
                 context.player1.clickCard(context.hanSolo);
                 context.player1.clickCard(context.p2Base);
@@ -104,6 +103,36 @@ describe('Han Solo, Audacious Smuggler', function() {
                 context.player1.clickCard(context.wampa);
                 expect(context.wampa).toBeInZone('discard', context.player1);
                 expect(context.player1.readyResourceCount).toBe(6);
+            });
+
+            it('should have to defeat a resource even if the deck was empty when Han attacked as a deployed leader', function() {
+                contextRef.setupTest({
+                    phase: 'action',
+                    player1: {
+                        leader: { card: 'han-solo#audacious-smuggler', deployed: true },
+                        deck: [],
+                        resources: ['wampa', 'cunning', 'aggression', 'spark-of-rebellion', 'protector', 'atst'],
+                    }
+                });
+
+                const { context } = contextRef;
+
+                expect(context.player1.readyResourceCount).toBe(6);
+                context.player1.clickCard(context.hanSolo);
+                context.player1.clickCard(context.p2Base);
+                expect(context.player1.readyResourceCount).toBe(6);
+                expect(context.wampa).toBeInZone('resource', context.player1);
+
+                context.player2.claimInitiative();
+                context.player1.passAction();
+                context.player2.clickPrompt('Done');
+                context.player1.clickPrompt('Done');
+
+                expect(context.player1).toHavePrompt('Defeat a resource you control');
+                expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.cunning, context.aggression, context.sparkOfRebellion, context.protector, context.atst]);
+                context.player1.clickCard(context.cunning);
+                expect(context.cunning).toBeInZone('discard', context.player1);
+                expect(context.player1.readyResourceCount).toBe(5);
             });
         });
     });
