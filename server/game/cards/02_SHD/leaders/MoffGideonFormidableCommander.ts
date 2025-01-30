da27f74ea25e97bdb7c716fcbb9b872deeb163f3
aruizc <ing.angelruizcerrillo@gmail.com>
2025-01-30 17:12:37 -0600

    Implementing Moff Gideon leader

---
+++ server/game/cards/02_SHD/leaders/MoffGideonFormidableCommander.ts
@@ -0,0 +1,44 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+import { KeywordName, RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class MoffGideonFormidableCommander extends LeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '2503039837',
+            internalName: 'moff-gideon#formidable-commander',
+        };
+    }
+
+    protected override setupLeaderSideAbilities() {
+        this.addActionAbility({
+            title: 'Attack with a unit that costs 3 or less. If it\'s attacking a unit, it gets +1/+0 for this attack',
+            cost: AbilityHelper.costs.exhaustSelf(),
+            targetResolver: {
+                controller: RelativePlayer.Self,
+                cardTypeFilter: WildcardCardType.Unit,
+                cardCondition: (card) => card.isUnit() && card.cost <= 3,
+                immediateEffect: AbilityHelper.immediateEffects.attack({
+                    attackerLastingEffects: {
+                        condition: (attack) => attack.target.isUnit(),
+                        effect: AbilityHelper.ongoingEffects.modifyStats({ power: 1, hp: 0 })
+                    }
+                })
+            }
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities() {
+        this.addConstantAbility({
+            title: 'Each friendly unit that costs 3 or less gets +1/+0',
+            matchTarget: (card, context) => card.isUnit() && card.controller === context.source.controller && card.cost <= 3,
+            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: 1, hp: 0 }),
+        });
+
+        this.addConstantAbility({
+            title: 'Each friendly unit that costs 3 or less gains Overwhelm while attacking an enemy unit',
+            matchTarget: (card, context) => card.isUnit() && card.isInPlay() && card.isAttacking() && card.controller === context.source.controller && card.cost <= 3 && card.activeAttack?.target?.isUnit(),
+            ongoingEffect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Overwhelm),
+        });
+    }
+}
