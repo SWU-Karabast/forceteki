e7eb3bbb3e6bf86b1fba8178be41e4596efef017
Brian Ries <briantries@gmail.com>
2025-02-02 11:53:44 -0600

    cleanup to isLeavingPlay to use the isArena functions as they accurately capture inplay

--- server/game/core/utils/EnumHelpers.ts
+++ server/game/core/utils/EnumHelpers.ts
@@ -89,9 +89,8 @@ export const isDeckMoveZone = (zoneName: MoveZoneDestination): boolean => {
 };
 
 export const isLeavingPlay = (prevZone: ZoneName, nextZone: MoveZoneDestination): boolean => {
-    const outOfPlayZones = [ZoneName.OutsideTheGame, ZoneName.Hand, ZoneName.Deck, ZoneName.Discard,
-        DeckZoneDestination.DeckBottom, DeckZoneDestination.DeckTop];
-    return !outOfPlayZones.includes(prevZone) && outOfPlayZones.includes(nextZone);
+    const nextZoneName = asConcreteZone(nextZone);
+    return isArena(prevZone) && !isArena(nextZoneName);
 };
 
 export const isUnit = (cardType: CardTypeFilter) => {
