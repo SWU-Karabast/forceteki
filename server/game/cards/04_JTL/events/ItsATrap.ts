9e71c7020b49a51f0068a10887ed19320410e960
aruizc <ing.angelruizcerrillo@gmail.com>
2025-02-04 23:47:58 -0600

    Implementing batch of cards for JTL

---
+++ server/game/cards/04_JTL/events/ItsATrap.ts
@@ -0,0 +1,27 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { ZoneName } from '../../../core/Constants';
+
+export default class ItsATrap extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '0964312065',
+            internalName: 'its-a-trap',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'If an opponent controls more space units than you, ready each space unit you control',
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.controller.getUnitsInPlay(ZoneName.SpaceArena).length < context.player.opponent.getUnitsInPlay(ZoneName.SpaceArena).length,
+                onTrue: AbilityHelper.immediateEffects.ready((context) => {
+                    return { target: context.source.controller.getUnitsInPlay(ZoneName.SpaceArena) };
+                }),
+                onFalse: AbilityHelper.immediateEffects.noAction()
+            })
+        });
+    }
+}
+
+ItsATrap.implemented = true;
