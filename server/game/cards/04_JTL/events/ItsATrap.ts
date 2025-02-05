a53e928511df6dc01a9fd3309d672793c731abbd
aruizc <ing.angelruizcerrillo@gmail.com>
2025-02-05 00:37:27 -0600

    Its a trap refactoring

--- server/game/cards/04_JTL/events/ItsATrap.ts
+++ server/game/cards/04_JTL/events/ItsATrap.ts
@@ -14,7 +14,7 @@ export default class ItsATrap extends EventCard {
         this.setEventAbility({
             title: 'If an opponent controls more space units than you, ready each space unit you control',
             immediateEffect: AbilityHelper.immediateEffects.conditional({
-                condition: (context) => context.source.controller.getUnitsInPlay(ZoneName.SpaceArena).length < context.player.opponent.getUnitsInPlay(ZoneName.SpaceArena).length,
+                condition: (context) => context.player.getUnitsInPlay(ZoneName.SpaceArena).length < context.player.opponent.getUnitsInPlay(ZoneName.SpaceArena).length,
                 onTrue: AbilityHelper.immediateEffects.ready((context) => {
                     return { target: context.player.getUnitsInPlay(ZoneName.SpaceArena) };
                 }),
