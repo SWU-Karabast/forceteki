3d61019e3649e908eaa65fe26109d1639e3b6dff
Tom Wetton <124315575+Twet1@users.noreply.github.com>
2025-02-05 09:10:01 +0000

    Updated unit tests

--- test/server/cards/03_TWI/events/IHaveTheHighGround.spec.ts
+++ test/server/cards/03_TWI/events/IHaveTheHighGround.spec.ts
@@ -5,30 +5,52 @@ describe('I Have The High Ground', function () {
                 phase: 'action',
                 player1: {
                     hand: ['i-have-the-high-ground'],
-                    groundArena: ['battlefield-marine']
+                    groundArena: ['battlefield-marine', 'plo-koon#kohtoyah']
                 },
                 player2: {
-                    groundArena: ['wampa']
+                    groundArena: ['wampa', 'wrecker#boom', 'clone-trooper']
                 }
             });
 
             const { context } = contextRef;
 
+            // Apply event
             context.player1.clickCard(context.iHaveTheHighGround);
-            context.player1.clickCard(context.battlefieldMarine);
+            context.player1.clickCard(context.ploKoon);
+
+            // unit attacked, -4 to attacker
             context.player2.clickCard(context.wampa);
-            context.player2.clickCard(context.battlefieldMarine);
-            expect(context.battlefieldMarine).toBeInZone('groundArena');
-            expect(context.battlefieldMarine.damage).toBe(0);
+            context.player2.clickCard(context.ploKoon);
+            expect(context.ploKoon).toBeInZone('groundArena');
+            expect(context.ploKoon.damage).toBe(0);
             expect(context.wampa.damage).toBe(3);
 
-            context.setDamage(context.wampa,0);
-            context.moveToNextActionPhase();
+            context.player1.passAction();
+
+            // 2nd unit attacked, -4 applied
+            context.player2.clickCard(context.wrecker);
+            context.player2.clickCard(context.ploKoon);
+            expect(context.ploKoon).toBeInZone('groundArena');
+            expect(context.ploKoon.damage).toBe(3);
+            expect(context.wrecker.damage).toBe(3);
 
             context.player1.passAction();
-            context.player2.clickCard(context.wampa);
+
+            // Another unit attacked, to ensure -4 isnt applied globally
+            context.player2.clickCard(context.cloneTrooper);
             context.player2.clickCard(context.battlefieldMarine);
-            expect(context.battlefieldMarine).toBeInZone('discard');
+            expect(context.cloneTrooper).toBeInZone('outsideTheGame');
+            expect(context.battlefieldMarine.damage).toBe(2);
+
+            //  Move to next phase to remove Event effect
+            context.setDamage(context.wampa, 0);
+            context.moveToNextActionPhase();
+            context.player1.passAction();
+
+            // Event removed check
+            context.player2.clickCard(context.wampa);
+            context.player2.clickCard(context.ploKoon);
+            expect(context.ploKoon).toBeInZone('discard');
         });
     });
 });
