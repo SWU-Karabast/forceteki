404443a6dd2b18fd0f83e27e80ee467984dea853
AMMayberry1 <ammayberry1@gmail.com>
2025-02-05 13:36:07 -0500

    Add required catch on server setup

--- server/gamenode/index.ts
+++ server/gamenode/index.ts
@@ -1,4 +1,8 @@
 import { GameServer } from './GameServer.js';
 
 let server;
-GameServer.create().then((createdServer) => server = createdServer);
+GameServer.create()
+    .then((createdServer) => server = createdServer)
+    .catch((error) => {
+        throw error;
+    });
