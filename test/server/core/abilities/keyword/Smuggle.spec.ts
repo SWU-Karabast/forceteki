7bc464436b108aa8fb868423b061b8d28de17908
AMMayberry1 <ammayberry1@gmail.com>
2025-02-09 14:50:37 -0500

    Fix bug with smuggling while deck is empty

--- test/server/core/abilities/keyword/Smuggle.spec.ts
+++ test/server/core/abilities/keyword/Smuggle.spec.ts
@@ -86,6 +86,16 @@ describe('Smuggle keyword', function() {
                 expect(context.player1.readyResourceCount).toBe(7);
             });
 
+            it('can be Smuggled even if the deck is empty', function () {
+                const { context } = contextRef;
+
+                context.player1.setDeck([]);
+
+                context.player1.clickCard(context.collectionsStarhopper);
+                expect(context.collectionsStarhopper).toBeInZone('spaceArena');
+                expect(context.player1.resources.length).toBe(17);
+            });
+
             // TODO: add test confirming if a ready resource is smuggled, its status is swapped with an exhausted resource before moving
         });
     });
