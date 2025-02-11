dd28a7130ab45a3380df38ddc2dfae1432846c25
aruizc <ing.angelruizcerrillo@gmail.com>
2025-02-11 16:03:10 -0600

    Implementing IG2000 and Shuttle Tydirium

---
+++ server/game/cards/04_JTL/units/ShuttleTydiriumFlyCasual.ts
@@ -0,0 +1,33 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class ShuttleTydiriumFlyCasual extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '1990020761',
+            internalName: 'shuttle-tydirium#fly-casual',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addOnAttackAbility({
+            title: 'Discard a card from your deck. If it has an odd cost, you may give an Experience token to another unit',
+            immediateEffect: AbilityHelper.immediateEffects.sequential([
+                AbilityHelper.immediateEffects.discardFromDeck((context) => ({
+                    amount: 1,
+                    target: context.source.controller
+                })),
+                AbilityHelper.immediateEffects.conditional((context) => ({
+                    condition: () => {
+                        const isOdd = context.events[0].card.printedCost % 2 === 1;
+                        return isOdd;
+                    },
+                    onTrue: AbilityHelper.immediateEffects.giveExperience(),
+                    onFalse: AbilityHelper.immediateEffects.noAction()
+                }))
+            ])
+        });
+    }
+}
+
+ShuttleTydiriumFlyCasual.implemented = true;
