7d62dbb2bef0147671d104ca41e18842fdc651f7
Robbie Moyer <rmoyer08@gmail.com>
2025-02-13 09:31:53 -0500

    update display prompt for You're My Only Hope

--- test/server/cards/01_SOR/events/YoureMyOnlyHope.spec.ts
+++ test/server/cards/01_SOR/events/YoureMyOnlyHope.spec.ts
@@ -29,23 +29,27 @@ describe('You\'re My Only Hope', function() {
 
                 // Scenario 1: Do nothing
                 context.player1.clickCard(context.youreMyOnlyHope);
-                // TODO: we need a 'look at' prompt for secretly revealing, currently chat logs go to all players
-                expect(context.getChatLogs(1)).toContain('You\'re My Only Hope sees AT-ST');
-                expect(context.player1).toHavePassAbilityPrompt('Play AT-ST, it costs 5 less');
 
-                context.player1.passAction();
+                expect(context.player1).toHaveExactSelectableDisplayPromptCards([context.atst]);
+                expect(context.player1).toHaveExactDisplayPromptPerCardButtons(['Play for 5 less', 'Leave on top']);
+                expect(context.getChatLogs(1)[0]).not.toContain(context.atst.title);
+
+                context.player1.clickDisplayCardPromptButton(context.atst.uuid, 'leave');
+
                 expect(context.atst).toBeInZone('deck', context.player1);
 
                 reset();
 
                 // Scenario 2: Play the card for 5 less
                 context.player1.clickCard(context.youreMyOnlyHope);
-                // TODO: we need a 'look at' prompt for secretly revealing, currently chat logs go to all players
-                expect(context.getChatLogs(1)).toContain('You\'re My Only Hope sees AT-ST');
-                expect(context.player1).toHavePassAbilityPrompt('Play AT-ST, it costs 5 less');
+
+                expect(context.player1).toHaveExactSelectableDisplayPromptCards([context.atst]);
+                expect(context.player1).toHaveExactDisplayPromptPerCardButtons(['Play for 5 less', 'Leave on top']);
+                expect(context.getChatLogs(1)[0]).not.toContain(context.atst.title);
 
                 let exhaustedResourcesBeforeAction = context.player1.exhaustedResourceCount;
-                context.player1.clickPrompt('Play AT-ST, it costs 5 less');
+                context.player1.clickDisplayCardPromptButton(context.atst.uuid, 'play-discount');
+
                 expect(context.player1.exhaustedResourceCount).toBe(exhaustedResourcesBeforeAction + 3);
                 expect(context.atst).toBeInZone('groundArena', context.player1);
 
@@ -55,11 +59,14 @@ describe('You\'re My Only Hope', function() {
                 context.player1.clickCard(context.isbAgent);
                 context.player2.clickCard(context.blackOne);
                 context.player1.clickCard(context.youreMyOnlyHope);
-                expect(context.getChatLogs(1)).toContain('You\'re My Only Hope sees Wampa');
-                expect(context.player1).toHavePassAbilityPrompt('Play Wampa for free');
+
+                expect(context.player1).toHaveExactSelectableDisplayPromptCards([context.wampa]);
+                expect(context.player1).toHaveExactDisplayPromptPerCardButtons(['Play for free', 'Leave on top']);
+                expect(context.getChatLogs(1)[0]).not.toContain(context.wampa.title);
 
                 exhaustedResourcesBeforeAction = context.player1.exhaustedResourceCount;
-                context.player1.clickPrompt('Play Wampa for free');
+                context.player1.clickDisplayCardPromptButton(context.wampa.uuid, 'play-free');
+
                 expect(context.player1.exhaustedResourceCount).toBe(exhaustedResourcesBeforeAction);
                 expect(context.wampa).toBeInZone('groundArena', context.player1);
 
