68cecf9fb2a79c18cf1cf72820928613aeda11f9
Valentin Jubert <val_jub@live.fr>
2025-02-16 20:58:08 +0100

    fix comment

--- server/game/cards/04_JTL/units/InvincibleNavalAdversary.ts
+++ server/game/cards/04_JTL/units/InvincibleNavalAdversary.ts
@@ -22,7 +22,7 @@ export default class InvincibleNavalAdversary extends NonLeaderUnitCard {
         this.addTriggeredAbility({
             title: 'Return a non-leader unit that costs 3 or less to its owner\'s hand',
             when: {
-                onLeaderDeployed: (event, context) => event.card.controller === context.player,
+                onLeaderDeployed: (event, context) => event.context.player === context.player,
             },
             optional: true,
             targetResolver: {
