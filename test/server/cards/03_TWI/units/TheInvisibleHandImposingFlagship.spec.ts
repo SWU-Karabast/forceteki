018b975ce84ae30a49aaf5f70fad940b2a677fed
Stephane Soulier <stephane.soulier@alan.com>
2025-02-22 09:25:32 +0100

    improve tests

--- test/server/cards/03_TWI/units/TheInvisibleHandImposingFlagship.spec.ts
+++ test/server/cards/03_TWI/units/TheInvisibleHandImposingFlagship.spec.ts
@@ -1,13 +1,16 @@
 describe('The Invisible Hand Imposing Flagship\'s ability', function() {
     integration(function(contextRef) {
-        it('should create 4 battle droid tokens when played and it can exhaust the token on the next attack', async function () {
+        it('should create 4 battle droid tokens when played and it can exhaust separatist units on attack', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
-                    hand: ['the-invisible-hand#imposing-flagship'],
+                    hand: ['the-invisible-hand#imposing-flagship', 'obedient-vanguard'],
+                    groundArena: ['warrior-drone'],
+                    spaceArena: ['vanguard-droid-bomber']
                 },
                 player2: {
-                    groundArena: ['warrior-drone']
+                    groundArena: ['droid-commando'],
+                    spaceArena: ['devastating-gunship']
                 }
             });
 
@@ -20,33 +23,49 @@ describe('The Invisible Hand Imposing Flagship\'s ability', function() {
             expect(battleDroidTokens.every((token) => !token.exhausted)).toBeTrue();
             context.player1.clickCard(context.theInvisibleHand);
             context.player1.clickCard(context.player2.base);
-            context.player1.clickPrompt('Pass'); // It fails because we do not enter the targetResolver
-        });
+            expect(context.player1).toBeAbleToSelectExactly([
+                battleDroidTokens[0],
+                battleDroidTokens[1],
+                battleDroidTokens[2],
+                battleDroidTokens[3],
+                context.warriorDrone,
+                context.vanguardDroidBomber
+            ]);
+            context.player1.clickPrompt('Pass');
+            expect(battleDroidTokens.every((token) => !token.exhausted)).toBeTrue();
+            expect(context.warriorDrone.exhausted).toBe(false);
+            expect(context.vanguardDroidBomber.exhausted).toBe(false);
+            expect(context.droidCommando.exhausted).toBe(false);
+            expect(context.devastatingGunship.exhausted).toBe(false);
+            expect(context.player2.base.damage).toBe(4);
 
-        it('should deal damage to opponent base for each friendly Separarist units exhausted on attack', async function () {
-            await contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    hand: ['providence-destroyer', 'morgan-elsbeth#keeper-of-many-secrets', 'planetary-invasion'],
-                    groundArena: ['warrior-drone', 'droid-commando'],
-                    spaceArena: ['the-invisible-hand#imposing-flagship']
-                }
-            });
-
-            const { context } = contextRef;
+            context.moveToNextActionPhase();
+            context.player1.clickCard(battleDroidTokens[0]);
+            context.player1.clickCard(context.player2.base);
+            expect(battleDroidTokens[0].exhausted).toBe(true);
+            expect(context.player2.base.damage).toBe(5);
+            context.player2.passAction();
 
             context.player1.clickCard(context.theInvisibleHand);
             context.player1.clickCard(context.player2.base);
             expect(context.player1).toBeAbleToSelectExactly([
+                battleDroidTokens[1],
+                battleDroidTokens[2],
+                battleDroidTokens[3],
                 context.warriorDrone,
-                context.droidCommando
+                context.vanguardDroidBomber
             ]);
-            context.player1.clickCard(context.warriorDrone);
-            context.player1.clickCard(context.droidCommando);
+            context.player1.clickCard(battleDroidTokens[1]);
+            context.player1.clickCard(battleDroidTokens[2]);
+            context.player1.clickCard(context.vanguardDroidBomber);
             context.player1.clickPrompt('Done');
-            expect(context.player2.base.damage).toBe(6);
-            expect(context.warriorDrone.exhausted).toBe(true);
-            expect(context.droidCommando.exhausted).toBe(true);
+            expect(battleDroidTokens[1].exhausted).toBe(true);
+            expect(battleDroidTokens[2].exhausted).toBe(true);
+            expect(context.vanguardDroidBomber.exhausted).toBe(true);
+            expect(battleDroidTokens[3].exhausted).toBe(false);
+            expect(context.warriorDrone.exhausted).toBe(false);
+
+            expect(context.player2.base.damage).toBe(12);
         });
     });
 });
