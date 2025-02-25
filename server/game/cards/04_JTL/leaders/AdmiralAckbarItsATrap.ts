237591931953377e80a0688d20eed56ac470d736
Valentin Jubert <val_jub@live.fr>
2025-02-25 13:53:59 +0100

    ackbar

--- server/game/cards/04_JTL/leaders/AdmiralAckbarItsATrap.ts
+++ server/game/cards/04_JTL/leaders/AdmiralAckbarItsATrap.ts
@@ -1,9 +1,6 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
-import { AbilityType, WildcardCardType } from '../../../core/Constants';
-
-// Action [1 resource, Exhaust]: Exhaust a non-leader unit. If you do, its controller creates an X-Wing token.",
-// On Attack: You may exhaust a unit. If you do, its controller creates an X-Wing token.",
+import { WildcardCardType } from '../../../core/Constants';
 
 export default class AdmiralAckbarItsATrap extends LeaderUnitCard {
     protected override getImplementationId() {
@@ -15,7 +12,7 @@ export default class AdmiralAckbarItsATrap extends LeaderUnitCard {
 
     protected override setupLeaderSideAbilities() {
         this.addActionAbility({
-            title: 'Attack with a unit. It gets +1/+0 for this attack',
+            title: 'Exhaust a non-leader unit',
             cost: [AbilityHelper.costs.exhaustSelf(), AbilityHelper.costs.abilityResourceCost(1)],
             targetResolver: {
                 cardTypeFilter: WildcardCardType.NonLeaderUnit,
@@ -29,10 +26,17 @@ export default class AdmiralAckbarItsATrap extends LeaderUnitCard {
     }
 
     protected override setupLeaderUnitSideAbilities() {
-        this.addCoordinateAbility({
-            type: AbilityType.Constant,
-            title: 'This unit gets +2/+0',
-            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 0 })
+        this.addOnAttackAbility({
+            title: 'Exhaust a non-leader unit',
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                immediateEffect: AbilityHelper.immediateEffects.exhaust()
+            },
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'Its controller creates an X-Wing token',
+                immediateEffect: AbilityHelper.immediateEffects.createXWing({ target: ifYouDoContext.target.controller })
+            })
         });
     }
 }
