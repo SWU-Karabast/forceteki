3194f5b6f96ca0857451543c55cf06bc61b1db5e
Caelan Mayberry <gmodman12@gmail.com>
2025-02-26 22:25:47 -0800

    Damage and Printed Power mixins WIP

--- server/game/core/card/propertyMixins/PrintedPower.ts
+++ server/game/core/card/propertyMixins/PrintedPower.ts
@@ -1,27 +1,28 @@
 import * as Contract from '../../utils/Contract';
-import type { Card, CardConstructor } from '../Card';
+import type { Card, CardConstructor, ICardState } from '../Card';
 
 export interface ICardWithPrintedPowerProperty extends Card {
-    readonly printedPower: number;
     getPower(): number;
 }
 
-/** Mixin function that adds the `printedPower` property to a base class. */
-export function WithPrintedPower<TBaseClass extends CardConstructor>(BaseClass: TBaseClass) {
-    return class WithPrintedPower extends BaseClass {
-        public readonly printedPower: number;
+export interface IWithPrintedPowerState extends ICardState {
+    printedPower: number;
+}
 
+/** Mixin function that adds the `printedPower` property to a base class. */
+export function WithPrintedPower<TBaseClass extends CardConstructor<TState>, TState extends ICardState>(BaseClass: TBaseClass) {
+    return class WithPrintedPower extends (BaseClass as TBaseClass & CardConstructor<TState & IWithPrintedPowerState>) {
         // see Card constructor for list of expected args
         public constructor(...args: any[]) {
             super(...args);
             const [Player, cardData] = this.unpackConstructorArgs(...args);
 
             Contract.assertNotNullLike(cardData.power);
-            this.printedPower = cardData.power;
+            this.state.printedPower = cardData.power;
         }
 
         public getPower(): number {
-            return this.printedPower;
+            return this.state.printedPower;
         }
     };
 }
