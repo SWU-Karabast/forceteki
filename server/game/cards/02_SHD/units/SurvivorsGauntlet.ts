aab637f897c0b356f04944e0684ac8cc2be2a8ab
Stephane Soulier <stephane.soulier@alan.com>
2025-03-02 18:52:03 +0100

    fix guardian test

--- server/game/cards/02_SHD/units/SurvivorsGauntlet.ts
+++ server/game/cards/02_SHD/units/SurvivorsGauntlet.ts
@@ -1,4 +1,5 @@
 import AbilityHelper from '../../../AbilityHelper';
+import * as EnumHelpers from '../../../core/utils/EnumHelpers';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { WildcardCardType, WildcardRelativePlayer, WildcardZoneName } from '../../../core/Constants';
 
@@ -28,11 +29,10 @@ export default class SurvivorsGauntlet extends NonLeaderUnitCard {
                     dependsOn: 'chooseUpgrade',
                     cardTypeFilter: WildcardCardType.Unit,
                     zoneFilter: WildcardZoneName.AnyArena,
-                    // controller: (context) => EnumHelpers.asRelativePlayer(context.player, context.targets.chooseUpgrade.parentCard.controller),
+                    controller: (context) => EnumHelpers.asRelativePlayer(context.player, context.targets.chooseUpgrade.parentCard.controller),
                     cardCondition: (card, context) =>
                         context.targets.chooseUpgrade.isUpgrade() &&
                         context.targets.chooseUpgrade.parentCard !== card &&
-                        card.controller === context.targets.chooseUpgrade.parentCard.controller &&
                         context.targets.chooseUpgrade.canAttach(card, context.targets.chooseUpgrade.parentCard.controller),
                     immediateEffect: AbilityHelper.immediateEffects.attachUpgrade((context) => ({
                         upgrade: context.targets.chooseUpgrade,
