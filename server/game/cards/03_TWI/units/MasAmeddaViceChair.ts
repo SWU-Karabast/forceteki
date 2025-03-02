d69bb7d8871375408b0d961fd7f44c17c2f2dea2
Christian Griggs <griggsca91@gmail.com>
2025-03-01 21:35:04 -0500

    MasAmeddaViceChair Card Implementation

---
+++ server/game/cards/03_TWI/units/MasAmeddaViceChair.ts
@@ -0,0 +1,35 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, Trait, WildcardCardType } from '../../../core/Constants';
+
+export default class MasAmeddaViceChair extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '0142631581',
+            internalName: 'mas-amedda#vice-chair',
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.addTriggeredAbility({
+            title: 'Exhaust this unit',
+            when: {
+                onCardPlayed: (event, context) => 
+                    event.card.isUnit() &&
+                    event.card.controller === context.player &&
+                    event.card !== context.source
+            },
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.exhaust(),
+            ifYouDo: {
+                title: 'If you do, search the top 4 cards of your deck for a unit, reveal it, and draw it',
+                immediateEffect: AbilityHelper.immediateEffects.deckSearch({
+                    selectCount: 1,
+                    searchCount: 4,
+                    cardCondition: (card) => card.isUnit(),
+                    selectedCardsImmediateEffect: AbilityHelper.immediateEffects.drawSpecificCard()
+                })
+            }        
+        });
+    }
+}
