3b768fe99cc1622c3319491fb1d7372897efc2de
Anthony Bronico <adbronico92@gmail.com>
2025-03-03 16:34:41 -0500

    Add Asajj tests

--- server/game/cards/04_JTL/leaders/AsajjVentressIWorkAlone.ts
+++ server/game/cards/04_JTL/leaders/AsajjVentressIWorkAlone.ts
@@ -1,6 +1,6 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
-import { AbilityType, DeployType, RelativePlayer, WildcardCardType, WildcardRelativePlayer, WildcardZoneName } from '../../../core/Constants';
+import { AbilityType, KeywordName, RelativePlayer, WildcardCardType } from '../../../core/Constants';
 
 export default class AsajjVentressIWorkAlone extends LeaderUnitCard {
     protected override getImplementationId() {
@@ -37,22 +37,34 @@ export default class AsajjVentressIWorkAlone extends LeaderUnitCard {
     }
 
     protected override setupLeaderUnitSideAbilities() {
-        this.addPilotingAbility({
-            title: 'Deal up to 4 damage divided as you choose among any number of units.',
+        this.addPilotingGainKeywordTargetingAttached({
+            keyword: KeywordName.Grit,
+        });
+
+        this.addPilotingGainAbilityTargetingAttached({
             type: AbilityType.Triggered,
+            title: 'You may deal 1 dmaage to a friendly unit. If you do, deal 1 damage to an enemy unit in the same arena.',
+            optional: true,
             when: {
-                onLeaderDeployed: (event, context) => event.leaderPilotCard === context.source && event.deployType === DeployType.LeaderUpgrade
+                onAttackDeclared: (event, context) => event.attack.attacker === context.source
             },
-            zoneFilter: WildcardZoneName.AnyArena,
-            immediateEffect: AbilityHelper.immediateEffects.distributeDamageAmong((context) => ({
-                amountToDistribute: 4,
-                canChooseNoTargets: true,
-                canDistributeLess: true,
-                controller: WildcardRelativePlayer.Any,
+            targetResolver: {
                 cardTypeFilter: WildcardCardType.Unit,
-                cardCondition: (card) => card.isUnit(),
-                source: context.source
-            }))
+                controller: RelativePlayer.Self,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 })
+            },
+            ifYouDo: (ifYouDoContext) => {
+                const friendlyArena = ifYouDoContext.target.zoneName;
+                return {
+                    title: `Deal 1 damage to an enemy unit in the ${friendlyArena} arena`,
+                    targetResolver: {
+                        cardTypeFilter: WildcardCardType.Unit,
+                        controller: RelativePlayer.Opponent,
+                        zoneFilter: friendlyArena,
+                        immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 })
+                    }
+                };
+            }
         });
     }
 }
--- test/server/cards/04_JTL/leaders/AsajjVentressIWorkAlone.spec.ts
+++ test/server/cards/04_JTL/leaders/AsajjVentressIWorkAlone.spec.ts
@@ -75,109 +75,145 @@ describe('Asajj Ventress, I Work Alone', function() {
 
                 expect(context.player2).toBeActivePlayer();
             });
+        });
 
-            // it('should not allow him to be exhausted when dealing combat damage', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'asajj-ventress#i-work-alone',
-            //             hand: ['daring-raid'],
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             groundArena: ['wampa'],
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+        describe('Asajj Ventress, I Work Alone\'s deployed ability', function() {
+            it('should have grit when deployed as a ground unit', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#i-work-alone',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['daring-raid']
+                    }
+                });
 
-            //     const { context } = contextRef;
+                const { context } = contextRef;
 
-            //     context.player1.clickCard(context.cartelSpacer);
-            //     context.player1.clickCard(context.p2Base);
-            //     expect(context.player1).not.toHavePassAbilityPrompt('Exhaust this leader');
+                context.player1.clickCard(context.asajjVentress);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Asajj Ventress', 'Deploy Asajj Ventress as a Pilot',
+                    'Deal 1 damage to a friendly unit. If you do, deal 1 damage to an enemy unit in the same arena.'
+                ]);
+                context.player1.clickPrompt('Deploy Asajj Ventress');
+                expect(context.asajjVentress.deployed).toBe(true);
+                expect(context.asajjVentress.getPower()).toBe(4);
 
-            //     expect(context.player2).toBeActivePlayer();
-            //     expect(context.bobaFett.exhausted).toBe(false);
-            // });
+                expect(context.player2).toBeActivePlayer();
+                context.player2.clickCard(context.daringRaid);
+                context.player2.clickCard(context.asajjVentress);
+                expect(context.asajjVentress.damage).toBe(2);
+                expect(context.asajjVentress.getPower()).toBe(6);
+            });
 
-            // it('does not deal 4 damage when deployed as a unit', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'boba-fett#any-methods-necessary',
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+            it('should not have an on attack ability when deployed as a unit', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#i-work-alone',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['daring-raid']
+                    }
+                });
 
-            //     const { context } = contextRef;
+                const { context } = contextRef;
 
-            //     context.player1.clickCard(context.bobaFett);
-            //     expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
-            //     context.player1.clickPrompt('Deploy Boba Fett');
-            //     expect(context.bobaFett.deployed).toBe(true);
+                context.player1.clickCard(context.asajjVentress);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Asajj Ventress', 'Deploy Asajj Ventress as a Pilot',
+                    'Deal 1 damage to a friendly unit. If you do, deal 1 damage to an enemy unit in the same arena.'
+                ]);
+                context.player1.clickPrompt('Deploy Asajj Ventress');
+                expect(context.asajjVentress.deployed).toBe(true);
+                expect(context.asajjVentress.getPower()).toBe(4);
 
-            //     expect(context.player2).toBeActivePlayer();
-            // });
+                expect(context.player2).toBeActivePlayer();
+                context.player2.passAction();
 
-            // it('will deal up to 4 damage divided among units when deployed as an upgrade', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'boba-fett#any-methods-necessary',
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             groundArena: ['wampa', 'moisture-farmer'],
-            //             spaceArena: ['concord-dawn-interceptors'],
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+                context.player1.clickCard(context.asajjVentress);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(4);
+                expect(context.player2).toBeActivePlayer();
+            });
 
-            //     const { context } = contextRef;
+            it('should give grit to attached unit when deployed as a pilot', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#i-work-alone',
+                        spaceArena: [{ card: 'cartel-spacer', damage: 2 }],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['daring-raid']
+                    }
+                });
 
-            //     context.player1.clickCard(context.bobaFett);
-            //     expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
-            //     context.player1.clickPrompt('Deploy Boba Fett as a Pilot');
-            //     expect(context.player2).not.toBeActivePlayer();
-            //     expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
-            //     context.player1.clickCard(context.cartelSpacer);
+                const { context } = contextRef;
 
-            //     expect(context.bobaFett.deployed).toBe(true);
-            //     expect(context.bobaFett).toBeInZone('spaceArena');
-            //     expect(context.cartelSpacer.getPower()).toBe(6);
-            //     expect(context.cartelSpacer.getHp()).toBe(7);
+                expect(context.cartelSpacer.damage).toBe(2);
+                context.player1.clickCard(context.asajjVentress);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Asajj Ventress', 'Deploy Asajj Ventress as a Pilot',
+                    'Deal 1 damage to a friendly unit. If you do, deal 1 damage to an enemy unit in the same arena.'
+                ]);
+                context.player1.clickPrompt('Deploy Asajj Ventress as a Pilot');
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickCard(context.cartelSpacer);
+                expect(context.asajjVentress.deployed).toBeTrue();
+                expect(context.cartelSpacer.damage).toBe(2);
+                expect(context.cartelSpacer.getPower()).toBe(7);
+            });
 
-            //     expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer, context.wampa, context.moistureFarmer, context.concordDawnInterceptors]);
-            //     expect(context.player1).toHaveChooseNoTargetButton();
-            //     context.player1.setDistributeDamagePromptState(new Map([
-            //         [context.wampa, 2],
-            //         [context.concordDawnInterceptors, 1],
-            //         [context.cartelSpacer, 1],
-            //     ]));
+            it('should give an on attack ability to attached unit when deployed as a pilot', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#i-work-alone',
+                        spaceArena: ['cartel-spacer'],
+                        groundArena: ['wampa'],
+                        resources: 6
+                    },
+                    player2: {
+                        groundArena: ['pyke-sentinel'],
+                        spaceArena: ['cartel-turncoat']
+                    }
+                });
 
-            //     expect(context.wampa.damage).toBe(2);
-            //     expect(context.moistureFarmer.damage).toBe(0);
-            //     expect(context.concordDawnInterceptors.damage).toBe(1);
-            //     expect(context.cartelSpacer.damage).toBe(1);
+                const { context } = contextRef;
 
-            //     context.player2.clickCard(context.rivalsFall);
-            //     expect(context.player2).toBeAbleToSelectExactly([context.cartelSpacer, context.wampa, context.moistureFarmer, context.concordDawnInterceptors]);
-            //     context.player2.clickCard(context.cartelSpacer);
+                context.player1.clickCard(context.asajjVentress);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Asajj Ventress', 'Deploy Asajj Ventress as a Pilot',
+                    'Deal 1 damage to a friendly unit. If you do, deal 1 damage to an enemy unit in the same arena.'
+                ]);
+                context.player1.clickPrompt('Deploy Asajj Ventress as a Pilot');
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickCard(context.cartelSpacer);
 
-            //     expect(context.cartelSpacer).toBeInZone('discard');
-            //     expect(context.bobaFett).toBeInZone('base');
-            //     expect(context.bobaFett.exhausted).toBe(true);
-            //     expect(context.bobaFett.deployed).toBe(false);
+                context.player2.passAction();
 
-            //     context.moveToNextActionPhase();
-            //     expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
-            // });
+                // Attack base
+                context.player1.clickCard(context.cartelSpacer);
+                context.player1.clickCard(context.p2Base);
+
+                // Deal 1 damage to friendly Wampa
+                expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.cartelSpacer]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickCard(context.wampa);
+                expect(context.wampa.damage).toBe(1);
+
+                // Deal 1 to enemy Pyke Sentinel
+                expect(context.player1).toBeAbleToSelectExactly([context.pykeSentinel]);
+                context.player1.clickCard(context.pykeSentinel);
+                expect(context.pykeSentinel.damage).toBe(1);
+
+                // Resolve attack
+                expect(context.p2Base.damage).toBe(5);
+                expect(context.player2).toBeActivePlayer();
+            });
         });
     });
 });
