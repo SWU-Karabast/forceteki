8f1594ee36294933db438fb3e27a2a61c4a92a30
Anthony Bronico <adbronico92@gmail.com>
2025-03-03 12:46:32 -0500

    Add additional tests and fixes

---
+++ test/server/actions/DeployPilotLeader.spec.ts
@@ -0,0 +1,158 @@
+
+describe('Deploy a Leader as a Pilot', function() {
+    integration(function(contextRef) {
+        describe('Leaders with Pilot deploys', function() {
+            it('can Be deployed as a unit', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['rivals-fall']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.bobaFett);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
+                context.player1.clickPrompt('Deploy Boba Fett');
+                expect(context.bobaFett.deployed).toBe(true);
+                expect(context.bobaFett).toBeInZone('groundArena');
+                expect(context.bobaFett.getPower()).toBe(4);
+                expect(context.bobaFett.getHp()).toBe(7);
+
+                context.player2.clickCard(context.rivalsFall);
+                context.player2.clickCard(context.bobaFett);
+
+                context.moveToNextActionPhase();
+                expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            });
+
+            it('can be deployed as a Pilot upgrade', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        groundArena: ['wampa', 'moisture-farmer'],
+                        spaceArena: ['concord-dawn-interceptors'],
+                        hand: ['rivals-fall']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.bobaFett);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
+                context.player1.clickPrompt('Deploy Boba Fett as a Pilot');
+                expect(context.player2).not.toBeActivePlayer();
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickCard(context.cartelSpacer);
+
+                expect(context.bobaFett.deployed).toBe(true);
+                expect(context.bobaFett).toBeInZone('spaceArena');
+                expect(context.cartelSpacer.getPower()).toBe(6);
+                expect(context.cartelSpacer.getHp()).toBe(7);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer, context.wampa, context.moistureFarmer, context.concordDawnInterceptors]);
+                context.player1.clickPrompt('Choose no targets');
+
+                context.player2.clickCard(context.rivalsFall);
+                expect(context.player2).toBeAbleToSelectExactly([context.cartelSpacer, context.wampa, context.moistureFarmer, context.concordDawnInterceptors]);
+                context.player2.clickCard(context.cartelSpacer);
+
+                expect(context.cartelSpacer).toBeInZone('discard');
+                expect(context.bobaFett).toBeInZone('base');
+                expect(context.bobaFett.exhausted).toBe(true);
+                expect(context.bobaFett.deployed).toBe(false);
+
+                context.moveToNextActionPhase();
+                expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            });
+
+            it('cannot be prevented from deploying as an upgrade by Regional Governor', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['regional-governor']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.passAction();
+                context.player2.clickCard(context.regionalGovernor);
+                context.player2.chooseListOption('Boba Fett');
+
+                context.player1.clickCard(context.bobaFett);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
+                context.player1.clickPrompt('Deploy Boba Fett as a Pilot');
+                expect(context.player2).not.toBeActivePlayer();
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickCard(context.cartelSpacer);
+
+                expect(context.bobaFett.deployed).toBe(true);
+                expect(context.bobaFett).toBeInZone('spaceArena');
+                expect(context.cartelSpacer.getPower()).toBe(6);
+                expect(context.cartelSpacer.getHp()).toBe(7);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer, context.regionalGovernor]);
+                context.player1.clickPrompt('Choose no targets');
+            });
+
+            it('can be defeated by an upgrade removal', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['confiscate']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.bobaFett);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
+                context.player1.clickPrompt('Deploy Boba Fett as a Pilot');
+                expect(context.player2).not.toBeActivePlayer();
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickCard(context.cartelSpacer);
+
+                expect(context.bobaFett.deployed).toBe(true);
+                expect(context.bobaFett).toBeInZone('spaceArena');
+                expect(context.cartelSpacer.getPower()).toBe(6);
+                expect(context.cartelSpacer.getHp()).toBe(7);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.cartelSpacer]);
+                context.player1.clickPrompt('Choose no targets');
+
+                context.player2.clickCard(context.confiscate);
+                expect(context.player2).toBeAbleToSelectExactly([context.bobaFett]);
+                context.player2.clickCard(context.bobaFett);
+
+                expect(context.bobaFett).toBeInZone('base');
+                expect(context.bobaFett.exhausted).toBe(true);
+                expect(context.bobaFett.deployed).toBe(false);
+
+                context.moveToNextActionPhase();
+                expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            });
+        });
+    });
+});
--- test/server/cards/04_JTL/leaders/BobaFettAnyMethodsNecessary.spec.ts
+++ test/server/cards/04_JTL/leaders/BobaFettAnyMethodsNecessary.spec.ts
@@ -2,97 +2,97 @@
 describe('Boba Fett, Any Methods Necessary', function() {
     integration(function(contextRef) {
         describe('Boba Fett, Any Methods Necessary\'s undeployed ability', function() {
-            // it('should allow him to be exhausted when dealing non-combat damage in order to do 1 indirect damage to a player', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'boba-fett#any-methods-necessary',
-            //             hand: ['daring-raid'],
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             groundArena: ['wampa'],
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+            it('should allow him to be exhausted when dealing non-combat damage in order to do 1 indirect damage to a player', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        hand: ['daring-raid'],
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        hand: ['rivals-fall']
+                    }
+                });
 
-            //     const { context } = contextRef;
+                const { context } = contextRef;
 
-            //     context.player1.clickCard(context.daringRaid);
-            //     context.player1.clickCard(context.p2Base);
-            //     expect(context.player1).toHavePassAbilityPrompt('Exhaust this leader');
-            //     expect(context.player1).toHaveExactPromptButtons(['Trigger', 'Pass']);
-            //     context.player1.clickPrompt('Trigger');
-            //     context.player1.clickPrompt('Opponent');
+                context.player1.clickCard(context.daringRaid);
+                context.player1.clickCard(context.p2Base);
+                expect(context.player1).toHavePassAbilityPrompt('Exhaust this leader');
+                expect(context.player1).toHaveExactPromptButtons(['Trigger', 'Pass']);
+                context.player1.clickPrompt('Trigger');
+                context.player1.clickPrompt('Opponent');
 
-            //     expect(context.player2).toHavePrompt('Distribute 1 indirect damage among targets');
+                expect(context.player2).toHavePrompt('Distribute 1 indirect damage among targets');
 
-            //     expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.p2Base]);
-            //     expect(context.player2).not.toHaveChooseNoTargetButton();
-            //     context.player2.setDistributeIndirectDamagePromptState(new Map([
-            //         [context.wampa, 1]
-            //     ]));
+                expect(context.player2).toBeAbleToSelectExactly([context.wampa, context.p2Base]);
+                expect(context.player2).not.toHaveChooseNoTargetButton();
+                context.player2.setDistributeIndirectDamagePromptState(new Map([
+                    [context.wampa, 1]
+                ]));
 
-            //     expect(context.wampa.damage).toBe(1);
-            //     expect(context.p2Base.damage).toBe(2);
-            //     expect(context.bobaFett.exhausted).toBe(true);
-            // });
+                expect(context.wampa.damage).toBe(1);
+                expect(context.p2Base.damage).toBe(2);
+                expect(context.bobaFett.exhausted).toBe(true);
+            });
 
-            // it('should not allow him to be exhausted when dealing combat damage', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'boba-fett#any-methods-necessary',
-            //             hand: ['daring-raid'],
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             groundArena: ['wampa'],
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+            it('should not allow him to be exhausted when dealing combat damage', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        hand: ['daring-raid'],
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        groundArena: ['wampa'],
+                        hand: ['rivals-fall']
+                    }
+                });
 
-            //     const { context } = contextRef;
+                const { context } = contextRef;
 
-            //     context.player1.clickCard(context.cartelSpacer);
-            //     context.player1.clickCard(context.p2Base);
-            //     expect(context.player1).not.toHavePassAbilityPrompt('Exhaust this leader');
+                context.player1.clickCard(context.cartelSpacer);
+                context.player1.clickCard(context.p2Base);
+                expect(context.player1).not.toHavePassAbilityPrompt('Exhaust this leader');
 
-            //     expect(context.player2).toBeActivePlayer();
-            //     expect(context.bobaFett.exhausted).toBe(false);
-            // });
+                expect(context.player2).toBeActivePlayer();
+                expect(context.bobaFett.exhausted).toBe(false);
+            });
 
-            // it('Can Be Deployed As a Unit', async function () {
-            //     await contextRef.setupTestAsync({
-            //         phase: 'action',
-            //         player1: {
-            //             leader: 'boba-fett#any-methods-necessary',
-            //             spaceArena: ['cartel-spacer'],
-            //             resources: 6
-            //         },
-            //         player2: {
-            //             hand: ['rivals-fall']
-            //         }
-            //     });
+            it('Can Be Deployed As a Unit', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'boba-fett#any-methods-necessary',
+                        spaceArena: ['cartel-spacer'],
+                        resources: 6
+                    },
+                    player2: {
+                        hand: ['rivals-fall']
+                    }
+                });
 
-            //     const { context } = contextRef;
+                const { context } = contextRef;
 
-            //     context.player1.clickCard(context.bobaFett);
-            //     expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
-            //     context.player1.clickPrompt('Deploy Boba Fett');
-            //     expect(context.bobaFett.deployed).toBe(true);
-            //     expect(context.bobaFett).toBeInZone('groundArena');
-            //     expect(context.bobaFett.getPower()).toBe(4);
-            //     expect(context.bobaFett.getHp()).toBe(7);
+                context.player1.clickCard(context.bobaFett);
+                expect(context.player1).toHaveExactPromptButtons(['Cancel', 'Deploy Boba Fett', 'Deploy Boba Fett as a Pilot']);
+                context.player1.clickPrompt('Deploy Boba Fett');
+                expect(context.bobaFett.deployed).toBe(true);
+                expect(context.bobaFett).toBeInZone('groundArena');
+                expect(context.bobaFett.getPower()).toBe(4);
+                expect(context.bobaFett.getHp()).toBe(7);
 
-            //     context.player2.clickCard(context.rivalsFall);
-            //     context.player2.clickCard(context.bobaFett);
+                context.player2.clickCard(context.rivalsFall);
+                context.player2.clickCard(context.bobaFett);
 
-            //     context.moveToNextActionPhase();
-            //     expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
-            // });
+                context.moveToNextActionPhase();
+                expect(context.bobaFett).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            });
 
             it('Can Be Deployed As a Pilot Upgrade and deal up to 4 damage divided among units', async function () {
                 await contextRef.setupTestAsync({
@@ -137,7 +137,7 @@ describe('Boba Fett, Any Methods Necessary', function() {
                 expect(context.cartelSpacer.damage).toBe(1);
 
                 context.player2.clickCard(context.rivalsFall);
-                expect(context.player2).toBeAbleToSelectExactly([context.cartelSpacer]);
+                expect(context.player2).toBeAbleToSelectExactly([context.cartelSpacer, context.wampa, context.moistureFarmer, context.concordDawnInterceptors]);
                 context.player2.clickCard(context.cartelSpacer);
 
                 expect(context.cartelSpacer).toBeInZone('discard');
