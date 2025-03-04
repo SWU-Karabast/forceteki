76e1ab14e04471f1ac0f29ac3b24bc8c7edf0475
AMMayberry1 <ammayberry1@gmail.com>
2025-03-04 17:26:35 -0500

    Finish Trench Run tests

--- test/server/cards/04_JTL/events/TrenchRun.spec.ts
+++ test/server/cards/04_JTL/events/TrenchRun.spec.ts
@@ -7,7 +7,7 @@ describe('Trench Run', function() {
                     phase: 'action',
                     player1: {
                         hand: ['trench-run'],
-                        spaceArena: ['headhunter-squadron', 'lurking-tie-phantom']
+                        spaceArena: ['headhunter-squadron', 'lurking-tie-phantom', 'tieln-fighter']
                     },
                     player2: {
                         spaceArena: ['auzituck-liberator-gunship'],
@@ -22,7 +22,7 @@ describe('Trench Run', function() {
 
                 context.player1.clickCard(context.trenchRun);
 
-                expect(context.player1).toBeAbleToSelectExactly([context.headhunterSquadron, context.lurkingTiePhantom]);
+                expect(context.player1).toBeAbleToSelectExactly([context.headhunterSquadron, context.lurkingTiePhantom, context.tielnFighter]);
                 context.player1.clickCard(context.headhunterSquadron);
                 expect(context.player1).toBeAbleToSelectExactly([context.auzituckLiberatorGunship, context.p2Base]);
                 context.player1.clickCard(context.p2Base);
@@ -30,6 +30,36 @@ describe('Trench Run', function() {
                 expect(context.p2Base.damage).toBe(5);
                 expect([context.vanquish, context.takedown]).toAllBeInZone('discard');
                 expect(context.headhunterSquadron.damage).toBe(1);
+            });
+
+            it('Trench Run\'s ability should deal damage even if the card says to prevent it', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.trenchRun);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.headhunterSquadron, context.lurkingTiePhantom, context.tielnFighter]);
+                context.player1.clickCard(context.lurkingTiePhantom);
+                expect(context.player1).toBeAbleToSelectExactly([context.auzituckLiberatorGunship, context.p2Base]);
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.p2Base.damage).toBe(8);
+                expect([context.vanquish, context.takedown]).toAllBeInZone('discard');
+                expect(context.lurkingTiePhantom.damage).toBe(1);
+            });
+
+            it('Trench Run\'s ability should defeat the unit from damage before the combat damage is dealt', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.trenchRun);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.headhunterSquadron, context.lurkingTiePhantom, context.tielnFighter]);
+                context.player1.clickCard(context.tielnFighter);
+                expect(context.player1).toBeAbleToSelectExactly([context.auzituckLiberatorGunship, context.p2Base]);
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.p2Base.damage).toBe(0);
+                expect([context.vanquish, context.takedown]).toAllBeInZone('discard');
+                expect(context.tielnFighter).toBeInZone('discard');
             });
         });
     });
