1e86f0f95700e993c978dbcf35c3b1079d732533
Robbie Moyer <rmoyer08@gmail.com>
2025-03-04 20:34:53 -0500

    wip: granted abilities from events

--- test/scenarios/ability/LoseAllAbilities.spec.ts
+++ test/scenarios/ability/LoseAllAbilities.spec.ts
@@ -13,7 +13,11 @@ describe('Effects that cause a unit to "Lose All Abilities"', function() {
                         hand: [
                             'protector',
                             'vambrace-grappleshot',
-                            'squad-support'
+                            'squad-support',
+                            'strategic-acumen',
+                            'swoop-racer',
+                            'breaking-in',
+                            'heroic-sacrifice'
                         ],
                         groundArena: [
                             'academy-defense-walker',
@@ -27,6 +31,7 @@ describe('Effects that cause a unit to "Lose All Abilities"', function() {
                         ]
                     },
                     player2: {
+                        hand: ['unshakeable-will'],
                         groundArena: [
                             'consular-security-force'
                         ]
@@ -34,143 +39,147 @@ describe('Effects that cause a unit to "Lose All Abilities"', function() {
                 });
             });
 
-            // it('loses keyword abilities until the effect expires', function() {
-            //     const { context } = contextRef;
+            // Abilities printed on units
 
-            //     // Use Kazuda's ability on Academy Defense Walker
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.academyDefenseWalker);
-            //     context.player1.passAction();
+            it('loses keyword abilities until the effect expires', function() {
+                const { context } = contextRef;
 
-            //     // Academy Defense Walker no longer has Sentinel
-            //     context.player2.clickCard(context.consularSecurityForce);
-            //     expect(context.player2).toBeAbleToSelect(context.p1Base);
-            //     context.player2.clickCard(context.p1Base);
+                // Use Kazuda's ability on Academy Defense Walker
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.academyDefenseWalker);
+                context.player1.passAction();
 
-            //     // Effect expires
-            //     context.moveToNextActionPhase();
-            //     context.player1.passAction();
+                // Academy Defense Walker no longer has Sentinel
+                context.player2.clickCard(context.consularSecurityForce);
+                expect(context.player2).toBeAbleToSelect(context.p1Base);
+                context.player2.clickCard(context.p1Base);
 
-            //     // Academy Defense Walker has Sentinel back
-            //     context.player2.clickCard(context.consularSecurityForce);
-            //     expect(context.player2).toBeAbleToSelectExactly([context.academyDefenseWalker]);
-            //     context.player2.clickCard(context.academyDefenseWalker);
-            // });
+                // Effect expires
+                context.moveToNextActionPhase();
+                context.player1.passAction();
 
-            // it('loses triggered abilities until the effect expires', function() {
-            //     const { context } = contextRef;
+                // Academy Defense Walker has Sentinel back
+                context.player2.clickCard(context.consularSecurityForce);
+                expect(context.player2).toBeAbleToSelectExactly([context.academyDefenseWalker]);
+                context.player2.clickCard(context.academyDefenseWalker);
+            });
 
-            //     // Use Kazuda's ability on Contracted Hunter
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.contractedHunter);
+            it('loses triggered abilities until the effect expires', function() {
+                const { context } = contextRef;
 
-            //     context.moveToNextActionPhase();
+                // Use Kazuda's ability on Contracted Hunter
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.contractedHunter);
 
-            //     // Contracted Hunter is still in play because his triggered ability was removed
-            //     expect(context.contractedHunter).toBeInZone('groundArena');
+                context.moveToNextActionPhase();
 
-            //     // Effect expires
-            //     context.moveToNextActionPhase();
+                // Contracted Hunter is still in play because his triggered ability was removed
+                expect(context.contractedHunter).toBeInZone('groundArena');
 
-            //     // Contracted Hunter is defeated because his triggered ability is back
-            //     expect(context.contractedHunter).toBeInZone('discard');
-            // });
+                // Effect expires
+                context.moveToNextActionPhase();
 
-            // it('loses constant abilities until the effect expires', function() {
-            //     const { context } = contextRef;
+                // Contracted Hunter is defeated because his triggered ability is back
+                expect(context.contractedHunter).toBeInZone('discard');
+            });
 
-            //     // Use Kazuda's ability on Strafing Gunship
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.strafingGunship);
+            it('loses constant abilities until the effect expires', function() {
+                const { context } = contextRef;
 
-            //     // Attack with Strafing Gunship, it can only attack the base
-            //     context.player1.clickCard(context.strafingGunship);
-            //     expect(context.player1).toBeAbleToSelectExactly([context.p2Base]);
-            //     context.player1.clickCard(context.p2Base);
+                // Use Kazuda's ability on Strafing Gunship
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.strafingGunship);
 
-            //     // Effect expires
-            //     context.moveToNextActionPhase();
+                // Attack with Strafing Gunship, it can only attack the base
+                context.player1.clickCard(context.strafingGunship);
+                expect(context.player1).toBeAbleToSelectExactly([context.p2Base]);
+                context.player1.clickCard(context.p2Base);
 
-            //     // Strafing Gunship can attack ground units again
-            //     context.player1.clickCard(context.strafingGunship);
-            //     expect(context.player1).toBeAbleToSelectExactly([
-            //         context.p2Base,
-            //         context.consularSecurityForce
-            //     ]);
-            //     context.player1.clickCard(context.consularSecurityForce);
-            // });
+                // Effect expires
+                context.moveToNextActionPhase();
 
-            // it('loses action abilities until the effect expires', function() {
-            //     const { context } = contextRef;
+                // Strafing Gunship can attack ground units again
+                context.player1.clickCard(context.strafingGunship);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.p2Base,
+                    context.consularSecurityForce
+                ]);
+                context.player1.clickCard(context.consularSecurityForce);
+            });
 
-            //     // Use Kazuda's ability on Grogu
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.grogu);
+            it('loses action abilities until the effect expires', function() {
+                const { context } = contextRef;
 
-            //     // Grogu no longer has an action ability
-            //     context.player1.clickCard(context.grogu);
+                // Use Kazuda's ability on Grogu
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.grogu);
 
-            //     expect(context.player1).not.toHaveEnabledPromptButton('Exhaust an enemy unit');
-            //     expect(context.player1).toBeAbleToSelect(context.consularSecurityForce);
+                // Grogu no longer has an action ability
+                context.player1.clickCard(context.grogu);
 
-            //     context.player1.clickCard(context.consularSecurityForce);
-            //     expect(context.consularSecurityForce.exhausted).toBeFalse();
+                expect(context.player1).not.toHaveEnabledPromptButton('Exhaust an enemy unit');
+                expect(context.player1).toBeAbleToSelect(context.consularSecurityForce);
 
-            //     // Effect expires
-            //     context.moveToNextActionPhase();
+                context.player1.clickCard(context.consularSecurityForce);
+                expect(context.consularSecurityForce.exhausted).toBeFalse();
 
-            //     // Grogu has his action ability back
-            //     context.player1.clickCard(context.grogu);
-            //     expect(context.player1).toHaveEnabledPromptButton('Exhaust an enemy unit');
-            //     context.player1.clickPrompt('Exhaust an enemy unit');
-            //     context.player1.clickCard(context.consularSecurityForce);
-            //     expect(context.consularSecurityForce.exhausted).toBeTrue();
-            // });
+                // Effect expires
+                context.moveToNextActionPhase();
 
-            // it('cannot gain new keyword abilities while the effect is active', function() {
-            //     const { context } = contextRef;
+                // Grogu has his action ability back
+                context.player1.clickCard(context.grogu);
+                expect(context.player1).toHaveEnabledPromptButton('Exhaust an enemy unit');
+                context.player1.clickPrompt('Exhaust an enemy unit');
+                context.player1.clickCard(context.consularSecurityForce);
+                expect(context.consularSecurityForce.exhausted).toBeTrue();
+            });
 
-            //     // Use Kazuda's ability on Academy Defense Walker
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.academyDefenseWalker);
+            // Abilities gained from upgrades
 
-            //     // Play Protector on Academy Defense Walker to attempt to give it Sentinel again
-            //     context.player1.clickCard(context.protector);
-            //     context.player1.clickCard(context.academyDefenseWalker);
+            it('cannot gain new keyword abilities from upgrades while the effect is active', function() {
+                const { context } = contextRef;
 
-            //     // Academy Defense Walker does not have Sentinel
-            //     context.player2.clickCard(context.consularSecurityForce);
-            //     expect(context.player2).toBeAbleToSelect(context.p1Base);
-            //     context.player2.clickCard(context.p1Base);
-            // });
+                // Use Kazuda's ability on Academy Defense Walker
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.academyDefenseWalker);
 
-            // it('cannot gain new triggered abilities while the effect is active', function() {
-            //     const { context } = contextRef;
+                // Play Protector on Academy Defense Walker to attempt to give it Sentinel again
+                context.player1.clickCard(context.protector);
+                context.player1.clickCard(context.academyDefenseWalker);
 
-            //     // Use Kazuda's ability on Grogu
-            //     context.player1.clickCard(context.kazudaXiono);
-            //     context.player1.clickPrompt('Select a friendly unit');
-            //     context.player1.clickCard(context.grogu);
+                // Academy Defense Walker does not have Sentinel
+                context.player2.clickCard(context.consularSecurityForce);
+                expect(context.player2).toBeAbleToSelect(context.p1Base);
+                context.player2.clickCard(context.p1Base);
+            });
 
-            //     // Play Vambrace Grappleshot on Grogu to attempt to give it a triggered ability
-            //     context.player1.clickCard(context.vambraceGrappleshot);
-            //     context.player1.clickCard(context.grogu);
+            it('cannot gain new triggered abilities from upgrades while the effect is active', function() {
+                const { context } = contextRef;
 
-            //     context.player2.passAction();
+                // Use Kazuda's ability on Grogu
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.grogu);
 
-            //     // Academy Defense Walker does not have the On Attack ability
-            //     context.player1.clickCard(context.grogu);
-            //     context.player1.clickCard(context.consularSecurityForce);
+                // Play Vambrace Grappleshot on Grogu to attempt to give it a triggered ability
+                context.player1.clickCard(context.vambraceGrappleshot);
+                context.player1.clickCard(context.grogu);
 
-            //     expect(context.consularSecurityForce.exhausted).toBeFalse();
-            // });
+                context.player2.passAction();
 
-            it('cannot gain new constant abilities while the effect is active', function() {
+                // Academy Defense Walker does not have the On Attack ability
+                context.player1.clickCard(context.grogu);
+                context.player1.clickCard(context.consularSecurityForce);
+
+                expect(context.consularSecurityForce.exhausted).toBeFalse();
+            });
+
+            it('cannot gain new constant abilities from upgrades while the effect is active', function() {
                 const { context } = contextRef;
 
                 // Use Kazuda's ability on Grogu
@@ -194,8 +203,82 @@ describe('Effects that cause a unit to "Lose All Abilities"', function() {
                 expect(context.grogu.getHp()).toBe(7);
             });
 
-            // it('cannot gain new action abilities while the effect is active', async function() {
-            // });
+            it('cannot gain new action abilities from upgrades while the effect is active', function() {
+                const { context } = contextRef;
+
+                // Use Kazuda's ability on Academy Defense Walker
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.academyDefenseWalker);
+
+                // Play Strategic Acumen on Academy Defense Walker to attempt to give it an action ability
+                context.player1.clickCard(context.strategicAcumen);
+                context.player1.clickCard(context.academyDefenseWalker);
+
+                context.player2.passAction();
+
+                // Academy Defense Walker does not have an action ability
+                context.player1.clickCard(context.academyDefenseWalker);
+                expect(context.player1).not.toHaveEnabledPromptButton('Play a unit from your hand. It costs 1 less');
+                expect(context.player1).toBeAbleToSelect(context.p2Base);
+                context.player1.clickCard(context.p2Base);
+
+                // Effect expires
+                context.moveToNextActionPhase();
+
+                // Academy Defense Walker has its gained action ability
+                context.player1.clickCard(context.academyDefenseWalker);
+                expect(context.player1).toHaveEnabledPromptButton('Play a unit from your hand. It costs 1 less');
+                context.player1.clickPrompt('Play a unit from your hand. It costs 1 less');
+                expect(context.player1).toBeAbleToSelect(context.swoopRacer);
+                context.player1.clickCard(context.swoopRacer);
+
+                expect(context.academyDefenseWalker.exhausted).toBeTrue();
+                expect(context.swoopRacer).toBeInZone('groundArena');
+            });
+
+            // Abilities gained from events
+
+            it('cannot gain new keyword abilities from events while the effect is active', function() {
+                const { context } = contextRef;
+
+                // Player 2 gives Consular Security Force Sentinel
+                context.player1.passAction();
+                context.player2.clickCard(context.unshakeableWill);
+                context.player2.clickCard(context.consularSecurityForce);
+
+                // Use Kazuda's ability on Academy Defense Walker
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.academyDefenseWalker);
+
+                // Play Breaking In to attempt to give Academy Defense Walker Saboteur
+                context.player1.clickCard(context.breakingIn);
+                context.player1.clickCard(context.academyDefenseWalker);
+
+                // Academy Defense Walker cannot attack the base because it does not have Saboteur
+                expect(context.player1).toBeAbleToSelectExactly([context.consularSecurityForce]);
+            });
+
+            it('cannot gain new triggered abilities from events while the effect is active', function() {
+                const { context } = contextRef;
+
+                // Use Kazuda's ability on Battlefield Marine
+                context.player1.clickCard(context.kazudaXiono);
+                context.player1.clickPrompt('Select a friendly unit');
+                context.player1.clickCard(context.battlefieldMarine);
+
+                // Play Heroic Sacrifice to attempt to give Battlefield Marine a triggered ability
+                context.player1.clickCard(context.heroicSacrifice);
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.consularSecurityForce);
+
+                // Battlefield Marine is not defeated because it does not have the "On Attack" ability from Heroic Sacrifce
+                expect(context.battlefieldMarine).toBeInZone('groundArena');
+
+                expect(context.consularSecurityForce.damage).toBe(5);
+                expect(context.battlefieldMarine.damage).toBe(2);
+            });
         });
     });
 });
