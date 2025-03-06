04b3980ad4274a2c21199022afcd49193fe2f886
Anthony Bronico <adbronico92@gmail.com>
2025-03-06 12:56:32 -0500

    Annihilator tests

--- test/server/cards/04_JTL/units/AnnihilatorTaggesFlagship.spec.ts
+++ test/server/cards/04_JTL/units/AnnihilatorTaggesFlagship.spec.ts
@@ -15,11 +15,30 @@ describe('Annihilator, Tagge\'s Flagship', function() {
             const { context } = contextRef;
 
             context.player1.clickCard(context.annihilator);
-            expect(context.player1).toHavePassAbilityButton();
             expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.battlefieldMarine]);
+            expect(context.player1).toHavePassAbilityButton();
             context.player1.clickCard(context.wampa);
             expect(context.wampa).toBeInZone('discard');
             expect(context.player2).toBeActivePlayer();
+        });
+
+        it('Annihilator can choose to not defeat a unit', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['annihilator#tagges-flagship'],
+                    spaceArena: ['concord-dawn-interceptors']
+                },
+                player2: {
+                    groundArena: ['wampa']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.annihilator);
+            expect(context.player1).toHavePassAbilityButton();
+            context.player1.clickPrompt('Pass');
         });
 
         it('Annihilator should do nothing if there are no enemy units', async function() {
@@ -69,6 +88,45 @@ describe('Annihilator, Tagge\'s Flagship', function() {
             context.player1.clickPrompt('Done');
 
             expect(inHandWampa).toBeInZone('discard');
+
+            expect(context.player2).toBeActivePlayer();
+        });
+
+        it('Annihilator should defeat an enemy unit and discard all cards with the same name from the opponent\'s hand', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['annihilator#tagges-flagship'],
+                    spaceArena: ['concord-dawn-interceptors']
+                },
+                player2: {
+                    leader: { card: 'boba-fett#any-methods-necessary', deployed: true },
+                    groundArena: ['boba-fett#disintegrator', 'wampa'],
+                    hand: ['boba-fett#disintegrator', 'boba-fett#feared-bounty-hunter', 'cartel-spacer']
+                }
+            });
+
+            const { context } = contextRef;
+
+            const inPlayBoba = context.player2.findCardByName('boba-fett#disintegrator', 'groundArena');
+            const inHandBoba = context.player2.findCardByName('boba-fett#disintegrator', 'hand');
+            const inHandPilotBoba = context.player2.findCardByName('boba-fett#feared-bounty-hunter', 'hand');
+
+            context.player1.clickCard(context.annihilator);
+            context.player1.clickCard(inPlayBoba);
+            expect(inPlayBoba).toBeInZone('discard');
+
+            // Player sees the opponent's hand
+            expect(context.player1).toHaveEnabledPromptButton('Done');
+            expect(context.player1).toHaveExactViewableDisplayPromptCards([
+                inHandBoba,
+                inHandPilotBoba,
+                context.cartelSpacer
+            ]);
+            context.player1.clickPrompt('Done');
+
+            expect(inHandBoba).toBeInZone('discard');
+            expect(inHandPilotBoba).toBeInZone('discard');
 
             expect(context.player2).toBeActivePlayer();
         });
