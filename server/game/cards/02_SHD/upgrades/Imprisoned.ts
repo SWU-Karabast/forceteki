2332830a26472c38c9cdc5c4d8c30d0d36ce9cf4
Valentin Jubert <val_jub@live.fr>
2025-03-07 08:43:51 +0100

    imprisoned

---
+++ server/game/cards/02_SHD/upgrades/Imprisoned.ts
@@ -0,0 +1,18 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+
+export default class Imprisoned extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '1368144544',
+            internalName: 'imprisoned',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addConstantAbilityTargetingAttached({
+            title: 'Attached unit loses its current abilities and can\'t gain abilities',
+            ongoingEffect: AbilityHelper.ongoingEffects.loseAllAbilities()
+        });
+    }
+}
