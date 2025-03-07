0f42f01250e244246ca63be844f8ce36a3cd57d3
Christian Griggs <griggsca91@gmail.com>
2025-03-06 21:48:09 -0500

    feedback

--- test/server/cards/04_JTL/upgrades/InDebtToCrimsonDawn.spec.ts
+++ test/server/cards/04_JTL/upgrades/InDebtToCrimsonDawn.spec.ts
@@ -9,21 +9,20 @@ describe('In Debt To Crimson Dawn\'s attached triggered ability', function() {
                 },
                 player2: {
                     groundArena: [{ card: 'frontier-atrt', upgrades: [{ card: 'in-debt-to-crimson-dawn', ownerAndController: 'player1' }] }, 'consular-security-force'],
+                    hand: ['bravado']
                 }
             });
         });
 
-        it('should prompt owner of exhausted unit on regroup to select an option', function () {
+        it('should prompt controller of exhausted unit on regroup to select an option', function () {
             const { context } = contextRef;
 
             context.player1.passAction();
 
-            // CASE 1: Frontier AT-RT attacks and is exhausted
             context.player2.clickCard(context.frontierAtrt);
             context.player2.clickCard(context.p1Base);
             expect(context.frontierAtrt.exhausted).toBeTrue();
 
-            // end phase to start regroup
             context.player1.claimInitiative();
             context.player2.passAction();
 
@@ -31,23 +30,21 @@ describe('In Debt To Crimson Dawn\'s attached triggered ability', function() {
             context.player2.clickPrompt('Done');
 
 
-            expect(context.player2).toHaveEnabledPromptButtons(['Pay 2 resource', 'Exhaust it']);
+            expect(context.player2).toHaveEnabledPromptButtons(['Pay 2 resources', 'Exhaust it']);
             context.player2.clickPrompt('Exhaust it');
 
             expect(context.frontierAtrt.exhausted).toBeTrue();
         });
 
-        it('should unexhaust a unit when the player pays 2 resources', function() {
+        it('should unexhaust a unit when the controller pays 2 resources', function() {
             const { context } = contextRef;
             context.player2.setResourceCount(4);
             context.player1.passAction();
 
-            // CASE 1: Frontier AT-RT attacks and is exhausted
             context.player2.clickCard(context.frontierAtrt);
             context.player2.clickCard(context.p1Base);
             expect(context.frontierAtrt.exhausted).toBeTrue();
 
-            // end phase to start regroup
             context.player1.claimInitiative();
             context.player2.passAction();
 
@@ -55,11 +52,32 @@ describe('In Debt To Crimson Dawn\'s attached triggered ability', function() {
             context.player2.clickPrompt('Done');
 
 
-            expect(context.player2).toHaveEnabledPromptButtons(['Pay 2 resource', 'Exhaust it']);
-            context.player2.clickPrompt('Pay 2 resource');
+            expect(context.player2).toHaveEnabledPromptButtons(['Pay 2 resources', 'Exhaust it']);
+            context.player2.clickPrompt('Pay 2 resources');
 
             expect(context.frontierAtrt.exhausted).toBeFalse();
             expect(context.player2.readyResourceCount).toBe(2);
+        });
+
+        it('should trigger when the unit is readied outside of the regroup phase', function() {
+            const { context } = contextRef;
+            context.player2.setResourceCount(9);
+            context.player1.passAction();
+
+            context.player2.clickCard(context.frontierAtrt);
+            context.player2.clickCard(context.p1Base);
+            expect(context.frontierAtrt.exhausted).toBeTrue();
+
+            context.player1.claimInitiative();
+
+            context.player2.clickCard(context.bravado);
+            context.player2.clickCard(context.frontierAtrt)
+
+            expect(context.player2).toHaveEnabledPromptButtons(['Pay 2 resources', 'Exhaust it']);
+            context.player2.clickPrompt('Pay 2 resources');
+
+            expect(context.frontierAtrt.exhausted).toBeFalse();
+            expect(context.player2.readyResourceCount).toBe(0);
         });
     });
 });
