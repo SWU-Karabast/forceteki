9d13a8aba8157f676abdf5c45d7ef98364b1c5f5
Valentin Jubert <val_jub@live.fr>
2025-03-07 14:19:10 +0100

    fix damage type + damage source and split tests

--- server/game/cards/03_TWI/units/FinnOnTheRun.ts
+++ server/game/cards/03_TWI/units/FinnOnTheRun.ts
@@ -1,6 +1,7 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
-import { AbilityType, WildcardCardType } from '../../../core/Constants';
+import { AbilityType, DamageType, WildcardCardType } from '../../../core/Constants';
+import { DamageSystem } from '../../../gameSystems/DamageSystem';
 
 export default class FinnOnTheRun extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -24,17 +25,23 @@ export default class FinnOnTheRun extends NonLeaderUnitCard {
                         title: 'For this phase, if damage would be dealt to that unit, prevent 1 of that damage',
                         type: AbilityType.ReplacementEffect,
                         when: {
-                            onDamageDealt: (event, context) => event.card === context.source
-                        },
-                        replaceWith: {
-                            replacementImmediateEffect: AbilityHelper.immediateEffects.damage((context) => ({
-                                target: context.source,
-                                amount: () => (Math.max(context.event.amount - 1, 0)),
-                                source: context.event.damageSource.damageDealtBy,
-                            }))
+                            onDamageDealt: (event, context) => {
+                                return event.card === context.source && !event.isIndirect;
+                            }
                         },
                         effect: 'Finn\'s ability prevents 1 damage to {1}',
-                        effectArgs: (context) => [context.source]
+                        effectArgs: (context) => [context.source],
+                        replaceWith: {
+                            replacementImmediateEffect: new DamageSystem((context) => {
+                                return ({
+                                    type: context.event.type,
+                                    target: context.source,
+                                    amount: Math.max(context.event.amount - 1, 0),
+                                    source: context.event.damageSource.type === DamageType.Ability ? context.event.damageSource.card : context.event.damageSource.damageDealtBy,
+                                    sourceAttack: context.event.damageSource.attack,
+                                });
+                            })
+                        },
                     })
                 })
             }
--- test/server/cards/03_TWI/units/FinnOnTheRun.spec.ts
+++ test/server/cards/03_TWI/units/FinnOnTheRun.spec.ts
@@ -1,15 +1,13 @@
 describe('Finn, On the Run', function () {
     integration(function (contextRef) {
-        it('Finn\'s ability should prevent 1 damage from any source to a unique unit', function () {
-            contextRef.setupTest({
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit', async function () {
+            await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
-                    hand: ['moment-of-peace', 'tarfful#kashyyyk-chieftain', 'change-of-heart'],
-                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane', 'syndicate-lackeys']
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane']
                 },
                 player2: {
-                    hand: ['daring-raid'],
-                    groundArena: ['battlefield-marine', 'scout-bike-pursuer', 'maul#shadow-collective-visionary', 'dryden-vos#offering-no-escape'],
+                    groundArena: ['battlefield-marine', 'scout-bike-pursuer'],
                 }
             });
 
@@ -19,7 +17,7 @@ describe('Finn, On the Run', function () {
             context.player1.clickCard(context.finn);
             context.player1.clickCard(context.p2Base);
 
-            expect(context.player1).toBeAbleToSelectExactly([context.finn, context.chewbacca, context.maul, context.drydenVos]);
+            expect(context.player1).toBeAbleToSelectExactly([context.finn, context.chewbacca]);
             context.player1.clickCard(context.chewbacca);
 
             // attack with battlefield marine, should do only 2 damage
@@ -27,32 +25,70 @@ describe('Finn, On the Run', function () {
             context.player2.clickCard(context.chewbacca);
 
             expect(context.chewbacca.damage).toBe(2);
-
-            context.setDamage(context.chewbacca, 0);
             context.player1.passAction();
 
             // attack on another unit, damage is not reduced
             context.player2.clickCard(context.scoutBikePursuer);
             context.player2.clickCard(context.finn);
             expect(context.finn.damage).toBe(1);
+        });
 
-            context.player1.passAction();
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (event + boba fett)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane']
+                },
+                player2: {
+                    hand: ['daring-raid'],
+                    leader: 'boba-fett#any-methods-necessary'
+                }
+            });
 
-            // use daring raid into yularen, should do only 1 damage
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickCard(context.chewbacca);
+
+            // use daring raid into chewbacca, should do only 1 damage
             context.player2.clickCard(context.daringRaid);
             context.player2.clickCard(context.chewbacca);
             expect(context.chewbacca.damage).toBe(1);
 
-            // reset scout bike pursuer
-            context.setDamage(context.chewbacca, 0);
-            context.scoutBikePursuer.exhausted = false;
-            context.setDamage(context.scoutBikePursuer, 0);
+            expect(context.player2).toHavePassAbilityPrompt('Exhaust this leader');
+            context.player2.clickPrompt('Trigger');
+            context.player2.clickPrompt('Opponent');
 
-            // give yularen a shield
-            context.player1.clickCard(context.momentOfPeace);
+            context.player1.setDistributeIndirectDamagePromptState(new Map([
+                [context.chewbacca, 1],
+            ]));
+
+            expect(context.player1).toBeActivePlayer();
+            // indirect damage is unpreventable
+            expect(context.chewbacca.damage).toBe(2);
+        });
+
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (shield not destroyed)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', { card: 'chewbacca#pykesbane', upgrades: ['shield'] }]
+                },
+                player2: {
+                    groundArena: ['scout-bike-pursuer'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
             context.player1.clickCard(context.chewbacca);
 
-            // scout bike pursuer attack yularen, does only 1 damage which is prevented, shield is not defeated
+            // scout bike pursuer attack chewbacca, does only 1 damage which is prevented, shield is not defeated
             context.player2.clickCard(context.scoutBikePursuer);
             context.player2.clickCard(context.chewbacca);
 
@@ -63,33 +99,75 @@ describe('Finn, On the Run', function () {
             context.player1.clickPrompt('For this phase, if damage would be dealt to that unit, prevent 1 of that damage');
             expect(context.chewbacca.damage).toBe(0);
             expect(context.chewbacca).toHaveExactUpgradeNames(['shield']);
+        });
 
-            context.player1.passAction();
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (shield destroyed)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', { card: 'chewbacca#pykesbane', upgrades: ['shield'] }]
+                },
+                player2: {
+                    groundArena: ['dryden-vos#offering-no-escape'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickCard(context.chewbacca);
 
             // attack with dryden vos, shield is defeated after 1 damage was prevented
             context.player2.clickCard(context.drydenVos);
             context.player2.clickCard(context.chewbacca);
+
             context.player1.clickPrompt('For this phase, if damage would be dealt to that unit, prevent 1 of that damage');
             expect(context.chewbacca.damage).toBe(0);
             expect(context.chewbacca.isUpgraded()).toBeFalse();
+        });
 
-            context.player1.moveCard(context.battlefieldMarine, 'groundArena');
-            context.player2.moveCard(context.scoutBikePursuer, 'groundArena');
-            context.setDamage(context.chewbacca, 0);
-            context.setDamage(context.scoutBikePursuer, 0);
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (next round)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane']
+                },
+                player2: {
+                    groundArena: ['battlefield-marine'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickCard(context.chewbacca);
 
             context.moveToNextActionPhase();
 
             context.player1.passAction();
 
-            // next action, ability should be expired
+            // next round, ability should be expired
             context.player2.clickCard(context.battlefieldMarine);
             context.player2.clickCard(context.chewbacca);
-
             expect(context.chewbacca.damage).toBe(3);
+        });
 
-            context.player1.clickCard(context.tarfful);
-            context.player2.passAction();
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (tarfful not triggering)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane', 'tarfful#kashyyyk-chieftain']
+                },
+                player2: {
+                    groundArena: ['scout-bike-pursuer'],
+                }
+            });
+
+            const { context } = contextRef;
 
             // trigger finn ability
             context.player1.clickCard(context.finn);
@@ -102,53 +180,156 @@ describe('Finn, On the Run', function () {
 
             // no combat damage dealt, tarfful does not trigger
             expect(context.player1).toBeActivePlayer();
+        });
 
-            // give a shield to chewbacca
-            context.player1.moveCard(context.momentOfPeace, 'hand');
-            context.player1.clickCard(context.momentOfPeace);
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (tarfful triggering)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane', 'tarfful#kashyyyk-chieftain']
+                },
+                player2: {
+                    groundArena: ['battlefield-marine', 'atst'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
             context.player1.clickCard(context.chewbacca);
-            context.player2.passAction();
 
-            // take maul from player 2
-            context.player1.clickCard(context.changeOfHeart);
-            context.player1.clickCard(context.maul);
+            // attack with 1 damage to chewbacca
+            context.player2.clickCard(context.battlefieldMarine);
+            context.player2.clickCard(context.chewbacca);
 
-            context.player2.moveCard(context.scoutBikePursuer, 'groundArena');
+            expect(context.player1).toBeAbleToSelectExactly([context.atst]);
+            context.player1.clickCard(context.atst);
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.atst.damage).toBe(2);
+        });
+
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (maul redirect while attacking)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', {
+                        card: 'chewbacca#pykesbane',
+                        upgrades: ['shield']
+                    }, 'maul#shadow-collective-visionary']
+                },
+                player2: {
+                    groundArena: ['scout-bike-pursuer'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickCard(context.chewbacca);
+
             context.player2.passAction();
 
             // maul attack scout bike pursuer, should take 1 damage, redirect it to chewbacca and finn ability prevent it
             context.player1.clickCard(context.maul);
             context.player1.clickCard(context.scoutBikePursuer);
+
+            // maul prompt
+            expect(context.player1).toHavePrompt('Choose another friendly Underworld unit. All combat damage that would be dealt to this unit during this attack is dealt to the chosen unit instead.');
             context.player1.clickCard(context.chewbacca);
+
+            // finn replacement prompt
+            expect(context.player1).toHaveExactPromptButtons([
+                'For this phase, if damage would be dealt to that unit, prevent 1 of that damage',
+                'Defeat shield to prevent attached unit from taking damage'
+            ]);
             context.player1.clickPrompt('For this phase, if damage would be dealt to that unit, prevent 1 of that damage');
+            expect(context.chewbacca.damage).toBe(0);
             expect(context.chewbacca).toHaveExactUpgradeNames(['shield']);
+        });
 
-            context.finn.exhausted = false;
-            context.maul.exhausted = false;
-            context.player2.passAction();
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (maul+finn reduce then redirect)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane', 'maul#shadow-collective-visionary']
+                },
+                player2: {
+                    groundArena: ['battlefield-marine'],
+                }
+            });
 
-            // trigger finn ability for maul
+            const { context } = contextRef;
+
+            // trigger finn ability
             context.player1.clickCard(context.finn);
             context.player1.clickCard(context.p2Base);
             context.player1.clickCard(context.maul);
 
             context.player2.passAction();
-            context.player2.moveCard(context.battlefieldMarine, 'groundArena')
 
-            // maul attack battlefield marine, should take 2 damage (3-1) and redirect it to an underworld unit
+            // maul attack scout bike pursuer, should take 1 damage, redirect it to chewbacca and finn ability prevent it
             context.player1.clickCard(context.maul);
             context.player1.clickCard(context.battlefieldMarine);
-            expect(context.player1).toBeAbleToSelectExactly([context.chewbacca, context.syndicateLackeys]);
-            expect(context.player1).toHavePassAbilityButton();
-            context.player1.clickCard(context.syndicateLackeys);
+
+            // maul prompt
+            expect(context.player1).toHavePrompt('Choose another friendly Underworld unit. All combat damage that would be dealt to this unit during this attack is dealt to the chosen unit instead.');
+            context.player1.clickCard(context.chewbacca);
+
             expect(context.player1).toHaveExactPromptButtons([
                 'For this phase, if damage would be dealt to that unit, prevent 1 of that damage',
                 'Redirect combat damage to another Underworld unit',
             ]);
-            // context.player1.clickPrompt('For this phase, if damage would be dealt to that unit, prevent 1 of that damage');
+
+            // reduce damage before redirecting them
+            context.player1.clickPrompt('For this phase, if damage would be dealt to that unit, prevent 1 of that damage');
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.chewbacca.damage).toBe(2);
+        });
+
+        it('Finn\'s ability should prevent 1 damage from any source to a unique unit (maul+finn redirect before reduce)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['finn#on-the-run', 'chewbacca#pykesbane', 'maul#shadow-collective-visionary']
+                },
+                player2: {
+                    groundArena: ['battlefield-marine'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            // trigger finn ability
+            context.player1.clickCard(context.finn);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickCard(context.maul);
+
+            context.player2.passAction();
+
+            // maul attack scout bike pursuer, should take 1 damage, redirect it to chewbacca and finn ability prevent it
+            context.player1.clickCard(context.maul);
+            context.player1.clickCard(context.battlefieldMarine);
+
+            // maul prompt
+            expect(context.player1).toHavePrompt('Choose another friendly Underworld unit. All combat damage that would be dealt to this unit during this attack is dealt to the chosen unit instead.');
+            context.player1.clickCard(context.chewbacca);
+
+            expect(context.player1).toHaveExactPromptButtons([
+                'For this phase, if damage would be dealt to that unit, prevent 1 of that damage',
+                'Redirect combat damage to another Underworld unit',
+            ]);
+
+            // redirect damage, no damage dealt to maul, they're not reduce
             context.player1.clickPrompt('Redirect combat damage to another Underworld unit');
 
-            expect(context.syndicateLackeys.damage).toBe(2);
+            expect(context.player2).toBeActivePlayer();
+            expect(context.chewbacca.damage).toBe(3);
         });
     });
 });
