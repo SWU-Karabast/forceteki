8c3a8783d10fffcd511ce6083d67d534ca16ee49
AMMayberry1 <ammayberry1@gmail.com>
2025-03-08 19:19:15 -0500

    Test opponent Ghost

--- test/server/cards/04_JTL/units/PhantomIiModifiedToDock.spec.ts
+++ test/server/cards/04_JTL/units/PhantomIiModifiedToDock.spec.ts
@@ -5,10 +5,14 @@ describe('Phantom II, Modified to Dock', function() {
                 return contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
-                        spaceArena: ['phantom-ii#modified-to-dock', 'the-ghost#heart-of-the-family'],
+                        spaceArena: [
+                            'phantom-ii#modified-to-dock',
+                            'the-ghost#heart-of-the-family',
+                            'relentless-firespray'
+                        ],
                     },
                     player2: {
-                        spaceArena: ['ruthless-raider'],
+                        spaceArena: ['ruthless-raider', 'the-ghost#spectre-home-base'],
                     }
                 });
             });
@@ -18,23 +22,46 @@ describe('Phantom II, Modified to Dock', function() {
 
                 context.player1.clickCard(context.phantomIi);
                 context.player1.clickPrompt('Attach this as an upgrade to The Ghost');
-                expect(context.player1).toBeAbleToSelectExactly(context.theGhost);
+                expect(context.player1).toBeAbleToSelectExactly([context.theGhostHeartOfTheFamily, context.theGhostSpectreHomeBase]);
 
-                context.player1.clickCard(context.theGhost);
-                expect(context.theGhost).toHaveExactUpgradeNames(['phantom-ii#modified-to-dock']);
-                expect(context.phantomIi.parentCard).toBe(context.theGhost);
+                context.player1.clickCard(context.theGhostHeartOfTheFamily);
+                expect(context.theGhostHeartOfTheFamily).toHaveExactUpgradeNames(['phantom-ii#modified-to-dock']);
+                expect(context.phantomIi.parentCard).toBe(context.theGhostHeartOfTheFamily);
 
-                expect(context.theGhost.getPower()).toBe(5);
-                expect(context.theGhost.getHp()).toBe(6);
+                expect(context.theGhostHeartOfTheFamily.getPower()).toBe(5);
+                expect(context.theGhostHeartOfTheFamily.getHp()).toBe(6);
 
                 context.player2.passAction();
 
                 // do an attack to confirm stats work
-                context.player1.clickCard(context.theGhost);
+                context.player1.clickCard(context.theGhostHeartOfTheFamily);
                 context.player1.clickCard(context.ruthlessRaider);
 
                 expect(context.ruthlessRaider.damage).toBe(5);
-                expect(context.theGhost.damage).toBe(4);
+                expect(context.theGhostHeartOfTheFamily.damage).toBe(4);
+            });
+
+            it('should attach itself to the opponent\'s Ghost as an upgrade', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.phantomIi);
+                context.player1.clickPrompt('Attach this as an upgrade to The Ghost');
+                expect(context.player1).toBeAbleToSelectExactly([context.theGhostHeartOfTheFamily, context.theGhostSpectreHomeBase]);
+
+                context.player1.clickCard(context.theGhostSpectreHomeBase);
+                expect(context.theGhostSpectreHomeBase).toHaveExactUpgradeNames(['phantom-ii#modified-to-dock']);
+                expect(context.phantomIi.parentCard).toBe(context.theGhostSpectreHomeBase);
+
+                expect(context.theGhostSpectreHomeBase.getPower()).toBe(5);
+                expect(context.theGhostSpectreHomeBase.getHp()).toBe(5);
+
+                // do an attack to confirm stats work
+                context.player2.clickCard(context.theGhostSpectreHomeBase);
+                context.player2.clickCard(context.relentlessFirespray);
+                context.player2.clickPrompt('Pass');
+
+                expect(context.relentlessFirespray.damage).toBe(5);
+                expect(context.theGhostSpectreHomeBase.damage).toBe(4);
             });
         });
     });
