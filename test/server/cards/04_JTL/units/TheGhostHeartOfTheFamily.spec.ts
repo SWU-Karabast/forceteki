4fa8f204b07372c86ce7366f59fd402682b2727f
Robbie Moyer <rmoyer08@gmail.com>
2025-03-11 20:19:41 -0400

    Add test cases for gaining keywords

--- test/server/cards/04_JTL/units/TheGhostHeartOfTheFamily.spec.ts
+++ test/server/cards/04_JTL/units/TheGhostHeartOfTheFamily.spec.ts
@@ -1,47 +1,197 @@
 describe('The Ghost, Heart of the Family', () => {
     integration(function(contextRef) {
-        describe('The Ghost\'s constant abilities', function() {
+        it('gains sentinel when it is upgraded', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['academy-training'],
+                    spaceArena: ['the-ghost#heart-of-the-family']
+                },
+                player2: {
+                    spaceArena: ['tieln-fighter'],
+                    hasInitiative: true
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Player 2 can attack base because Ghost is not a Sentinel
+            context.player2.clickCard(context.tielnFighter);
+            expect(context.player2).toBeAbleToSelectExactly([
+                context.theGhost,
+                context.p1Base
+            ]);
+
+            context.player2.clickCard(context.p1Base);
+
+            // Play an upgrade on The Ghost
+            context.player1.clickCard(context.academyTraining);
+            context.player1.clickCard(context.theGhost);
+            context.moveToNextActionPhase();
+
+            // Player 2 can't attack base because Ghost is a Sentinel
+            context.player2.clickCard(context.tielnFighter);
+            expect(context.player2).toBeAbleToSelectExactly([
+                context.theGhost
+            ]);
+
+            context.player2.clickCard(context.theGhost);
+            expect(context.theGhost.damage).toBe(2);
+        });
+
+        describe('When The Ghost gains keywords, it shares them with other Spectre units', function() {
             beforeEach(function () {
                 return contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
-                        hand: ['academy-training'],
-                        spaceArena: ['the-ghost#heart-of-the-family']
+                        leader: 'hera-syndulla#spectre-two',
+                        hand: [
+                            'infiltrators-skill',                     // Saboteur
+                            'red-three#unstoppable',                 // Raid 1
+                            'home-one#alliance-flagship',             // Restore 1
+                            'wedge-antilles#star-of-the-rebellion',  // Ambush
+                        ],
+                        spaceArena: [
+                            'the-ghost#heart-of-the-family',
+                            'phantom-ii#modified-to-dock',            // Grit
+                        ],
+                        groundArena: ['sabine-wren#explosives-artist', 'battlefield-marine'],
                     },
                     player2: {
-                        spaceArena: ['tieln-fighter']
+                        hand: [
+                            'takedown',
+                            'daring-raid',
+                            'top-target',                            // Bounty
+                        ],
+                        groundArena: ['cloud-city-wing-guard']
                     }
                 });
             });
 
-            it('give it sentinel when it is upgraded', function() {
+            it('(Sentinel & Saboteur)', function() {
                 const { context } = contextRef;
 
-                context.player1.passAction();
+                // Sabine must attack the Sentinel
+                context.player1.clickCard(context.sabineWren);
+                expect(context.player1).toBeAbleToSelectExactly([context.cloudCityWingGuard]);
+                context.player1.clickCard(context.cloudCityWingGuard);
+                context.player1.clickCard(context.p2Base); // Resolve Sabine's ability
 
-                // Player 2 can attack base because Ghost is not a Sentinel
-                context.player2.clickCard(context.tielnFighter);
-                expect(context.player2).toBeAbleToSelectExactly([
-                    context.theGhost,
-                    context.p1Base
-                ]);
-
+                // Cloud City Wing Guard can attack base because Sabine is not Sentinel
+                context.player2.clickCard(context.cloudCityWingGuard);
+                expect(context.player2).toBeAbleToSelect(context.p1Base);
                 context.player2.clickCard(context.p1Base);
 
-                // Play an upgrade on The Ghost
-                context.player1.clickCard(context.academyTraining);
-                context.player1.clickCard(context.theGhost);
                 context.moveToNextActionPhase();
+                context.sabineWren.damage = 0;
+                context.cloudCityWingGuard.damage = 0;
+
+                // Play Infiltrator's Skill on The Ghost, giving it Sentinel & Saboteur
+                context.player1.clickCard(context.infiltratorsSkill);
+                context.player1.clickCard(context.theGhost);
+
+                // Cloud City Wing Guard can't attack base because Sabine is Sentinel
+                context.player2.clickCard(context.cloudCityWingGuard);
+                expect(context.player2).toBeAbleToSelectExactly([context.sabineWren]);
+                context.player2.clickCard(context.sabineWren);
+
+                // Sabine can attack the base because she has Saboteur
+                context.player1.clickCard(context.sabineWren);
+                expect(context.player1).toBeAbleToSelectExactly([context.cloudCityWingGuard, context.p2Base]);
+                context.player1.clickCard(context.p2Base);
+                context.player1.clickPrompt('Deal 1 damage to the defender or a base'); // Resolve Sabine's ability
+                context.player1.clickCard(context.p2Base);
+            });
+
+            it('(Raid)', function() {
+                const { context } = contextRef;
+
+                // Play Red Three to give other Heroism units Raid 1
+                context.player1.clickCard(context.redThree);
+                context.player2.passAction();
+
+                // Sabine should have Raid 2, attacking for 4
+                context.player1.clickCard(context.sabineWren);
+                context.player1.clickCard(context.cloudCityWingGuard);
+                context.player1.clickCard(context.p2Base); // Resolve Sabine's ability
+                expect(context.cloudCityWingGuard).toBeInZone('discard');
+            });
+
+            it('(Restore)', function() {
+                const { context } = contextRef;
+
+                // Play Home One to give other Rebel units Restore 1
+                context.player1.clickCard(context.homeOne);
+
+                // Cloud City Wing Guard attacks base, dealing 2 damage
+                context.player2.clickCard(context.cloudCityWingGuard);
+                context.player2.clickCard(context.p1Base);
+
+                // Sabine should have Restore 2, healing for 2
+                context.player1.clickCard(context.sabineWren);
+                context.player1.clickCard(context.cloudCityWingGuard);
+                context.player1.clickPrompt('Deal 1 damage to the defender or a base'); // Resolve Sabine's ability
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.p1Base.damage).toBe(0);
+            });
+
+            it('(Ambush)', function() {
+                const { context } = contextRef;
+
+                // Play Wedge Antilles to give other Vehicle units Ambush
+                context.player1.clickCard(context.wedgeAntilles);
+                context.player2.passAction();
+
+                // Deploy Hera Syndulla, she gains Ambush via The Ghost
+                context.player1.clickCard(context.heraSyndulla);
+                expect(context.player1).toHavePrompt('Trigger the ability \'Ambush\' or pass');
+                context.player1.clickPrompt('Trigger');
+                context.player1.clickCard(context.cloudCityWingGuard);
+                context.player1.clickCard(context.sabineWren); // Resolve Hera's ability
+            });
+
+            it('(Grit)', function() {
+                const { context } = contextRef;
+
+                // Attach Phantom II to give The Ghost Grit
+                context.player1.clickCard(context.phantomIi);
+                context.player1.clickPrompt('Attach this as an upgrade to The Ghost');
+                context.player1.clickCard(context.theGhost);
+
+                // Play Daring Raid to deal damage to Sabine
+                context.player2.clickCard(context.daringRaid);
+                context.player2.clickCard(context.sabineWren);
+
+                // Attack with Sabine, she deals 4 damage due to Grit
+                context.player1.clickCard(context.sabineWren);
+                context.player1.clickCard(context.cloudCityWingGuard);
+                context.player1.clickCard(context.p2Base); // Resolve Sabine's ability
+
+                expect(context.cloudCityWingGuard).toBeInZone('discard');
+            });
+
+            it('(Bounty)', function() {
+                const { context } = contextRef;
+
+                // Attack base with The Ghost
+                context.player1.clickCard(context.theGhost);
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(5);
+
+                // Play Top Target on The Ghost to give it a Bounty
+                context.player2.clickCard(context.topTarget);
+                context.player2.clickCard(context.theGhost);
+
                 context.player1.passAction();
 
-                // Player 2 can't attack base because Ghost is a Sentinel
-                context.player2.clickCard(context.tielnFighter);
-                expect(context.player2).toBeAbleToSelectExactly([
-                    context.theGhost
-                ]);
+                // Play Takedown on Sabine and collect the Bounty to heal the base
+                context.player2.clickCard(context.takedown);
+                context.player2.clickCard(context.sabineWren);
 
-                context.player2.clickCard(context.theGhost);
-                expect(context.theGhost.damage).toBe(2);
+                expect(context.player2).toHavePrompt('Collect Bounty: Heal 4 damage from a unit or base. If the Bounty unit is unique, heal 6 damage instead.');
+                context.player2.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(0);
             });
         });
     });
