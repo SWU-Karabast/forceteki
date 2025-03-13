a9a9c2152868d865b77e180ab588c226660c62f3
Jan Preisig <jan.preisig@intuitive-collaboration.com>
2025-03-13 15:47:30 +0100

    implement FaceOff

---
+++ test/server/cards/04_JTL/events/FaceOff.spec.ts
@@ -0,0 +1,67 @@
+describe('FaceOff', function () {
+    integration(function (contextRef) {
+        describe('FaceOff\'s event ability', function () {
+            describe('when no player has taken the initiative this phase', function () {
+                beforeEach(async function () {
+                    await contextRef.setupTestAsync({
+                        phase: 'action',
+                        player1: {
+                            hand: ['face-off'],
+                            groundArena: [{ card: 'superlaser-technician', exhausted: true }],
+                            leader: { card: 'boba-fett#daimyo', deployed: true },
+                        },
+                        player2: {
+                            groundArena: [{ card: 'seasoned-shoretrooper', exhausted: true }],
+                            leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true },
+                        }
+                    });
+                });
+
+                it('readies an enemy unit and then readies a friendly unit in the same arena', () => {
+                    const { context } = contextRef;
+
+                    context.player1.clickCard(context.faceOff);
+                    expect(context.player1).toHavePrompt('Choose an enemy unit to ready');
+                    expect(context.player1).toBeAbleToSelectExactly(context.seasonedShoretrooper);
+
+                    context.player1.clickCard(context.seasonedShoretrooper);
+                    expect(context.seasonedShoretrooper.exhausted).toBe(false);
+
+                    expect(context.player1).toHavePrompt('Ready a friendly unit in the same arena');
+                    expect(context.player1).toBeAbleToSelectExactly(context.superlaserTechnician);
+
+                    context.player1.clickCard(context.superlaserTechnician);
+                    expect(context.superlaserTechnician.exhausted).toBe(false);
+                });
+            });
+
+            describe('when a player has taken the initiative this phase', function () {
+                beforeEach(async function () {
+                    await contextRef.setupTestAsync({
+                        phase: 'action',
+                        player1: {
+                            groundArena: [{ card: 'seasoned-shoretrooper', exhausted: true }],
+                            leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true },
+                        },
+                        player2: {
+                            hand: ['face-off'],
+                            groundArena: [{ card: 'superlaser-technician', exhausted: true }],
+                            leader: { card: 'boba-fett#daimyo', deployed: true },
+                        },
+                    });
+                });
+
+                it('does nothing', () => {
+                    const { context } = contextRef;
+
+                    context.player1.claimInitiative();
+                    context.player2.clickCard(context.faceOff);
+                    context.player2.clickPrompt('Trigger');
+                    expect(context.player2).not.toHavePrompt('Choose an enemy unit to ready');
+                    expect(context.superlaserTechnician.exhausted).toBe(true);
+                    expect(context.seasonedShoretrooper.exhausted).toBe(true);
+                });
+            });
+        });
+    });
+});
