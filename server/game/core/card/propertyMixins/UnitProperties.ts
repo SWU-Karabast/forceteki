cbfde701ab46c138a2921ec91c4ed0854d9544d0
Robbie Moyer <rmoyer08@gmail.com>
2025-03-14 14:15:07 -0400

    Implement Brain Invaders

--- server/game/core/card/propertyMixins/UnitProperties.ts
+++ server/game/core/card/propertyMixins/UnitProperties.ts
@@ -417,10 +417,6 @@ export function WithUnitProperties<TBaseClass extends InPlayCardConstructor>(Bas
             });
         }
 
-        public override getActionAbilities(): ActionAbility[] {
-            return this.isBlank() ? [] : super.getActionAbilities();
-        }
-
         public override getTriggeredAbilities(): TriggeredAbility[] {
             if (this.isBlank()) {
                 return [];
