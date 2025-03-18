ae612eb25af930d63ee16a59a1385a5377e8cf84
AMMayberry1 <ammayberry1@gmail.com>
2025-03-18 14:33:44 -0400

    Use proper event context, some tests failing still

--- server/game/actions/PlayEventAction.ts
+++ server/game/actions/PlayEventAction.ts
@@ -3,6 +3,7 @@ import * as Contract from '../core/utils/Contract.js';
 import type { PlayCardContext, IPlayCardActionProperties } from '../core/ability/PlayCardAction.js';
 import { PlayCardAction } from '../core/ability/PlayCardAction.js';
 import AbilityResolver from '../core/gameSteps/AbilityResolver.js';
+import type { AbilityContext } from '../core/ability/AbilityContext.js';
 
 export class PlayEventAction extends PlayCardAction {
     private earlyTargetResults?: any = null;
@@ -14,10 +15,7 @@ export class PlayEventAction extends PlayCardAction {
 
         const abilityContext = context.source.getEventAbility().createContext();
         if (this.earlyTargetResults) {
-            abilityContext.target = context.target;
-            abilityContext.targets = context.targets;
-            abilityContext.select = context.select;
-            abilityContext.selects = context.selects;
+            this.copyContextTargets(context, abilityContext);
         }
 
         context.game.queueStep(new AbilityResolver(context.game, abilityContext, false, null, this.earlyTargetResults));
@@ -46,13 +44,30 @@ export class PlayEventAction extends PlayCardAction {
 
         const eventAbility = context.source.getEventAbility();
 
-        // if the opponent will be making any selections, then we need to do play in the correct order (i.e. move to discard first, then select)
-        if (eventAbility.hasTargetsChosenByPlayer(context, context.player.opponent) || eventAbility.playerChoosingOptional === RelativePlayer.Opponent) {
+        // if the opponent will be making any selections or the ability is optional, then we need to do play in the correct order (i.e. move to discard first, then select)
+        if (
+            eventAbility.hasTargetsChosenByPlayer(context, context.player.opponent) ||
+            eventAbility.playerChoosingOptional === RelativePlayer.Opponent ||
+            eventAbility.optional
+        ) {
             return this.getDefaultTargetResults(context);
         }
 
-        this.earlyTargetResults = eventAbility.resolveEarlyTargets(context, passHandler, canCancel);
+        const eventAbilityContext = eventAbility.createContext();
+
+        this.copyContextTargets(context, eventAbilityContext);
+        this.earlyTargetResults = eventAbility.resolveEarlyTargets(eventAbilityContext, passHandler, canCancel);
+
+        this.game.queueSimpleStep(() => this.copyContextTargets(eventAbilityContext, context), 'copy event targets to play context');
+
         return this.earlyTargetResults;
+    }
+
+    private copyContextTargets(from: AbilityContext, to: AbilityContext) {
+        to.target = from.target;
+        to.targets = from.targets;
+        to.select = from.select;
+        to.selects = from.selects;
     }
 
     public moveEventToDiscard(context: PlayCardContext) {
