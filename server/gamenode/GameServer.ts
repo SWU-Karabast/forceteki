1d91156609e561dff093d0f667b7298ee0f9afca
AMMayberry1 <ammayberry1@gmail.com>
2025-03-19 17:27:20 -0400

    Fix boolean condition

--- server/gamenode/GameServer.ts
+++ server/gamenode/GameServer.ts
@@ -175,7 +175,7 @@ export class GameServer {
                 const lobby = this.lobbies.get(gameId);
 
                 // if they are in a game already we give them 403 forbidden
-                if (this.canUserJoinNewLobby(user.id)) {
+                if (!this.canUserJoinNewLobby(user.id)) {
                     return res.status(403).json({
                         success: false,
                         message: 'User is already in a game'
