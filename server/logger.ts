8bf4283fad4fe3730e8bce650086b4d6f14d5538
Brian Ries <briantries@gmail.com>
2025-03-20 14:45:38 -0500

    Added a switch to the logger to do literal newlines for CloudWatch to have a singular stack trace entry

--- server/logger.ts
+++ server/logger.ts
@@ -2,7 +2,14 @@ import * as winston from 'winston';
 
 const logFormatter = winston.format.printf(({ timestamp, level, message, stack }) => {
     const log = `${timestamp} [${level}]: ${message}`;
-    return stack ? log + '\n' + stack : log;
+
+    // Cloud watch cannot handle multi-line logs, so we need to replace newlines with a literal '\n'
+    if (process.env.AWS_EXECUTION_ENV) {
+        return stack ? `${log} ${stack.replace(/\n/g, '\\n')}` : log;
+    }
+
+    // Otherwise, keep multi-line formatting for local readability
+    return stack ? `${log}\n${stack}` : log;
 });
 
 export const logger = winston.createLogger({
