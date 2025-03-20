3a1b82bbf530a64780bc1efe47224b967b0f7df7
Brian Ries <briantries@gmail.com>
2025-03-20 18:44:46 -0500

    Swapping out the \\n and \r for straight JSON logging to CloudWatch instead which fully supports it

--- server/logger.ts
+++ server/logger.ts
@@ -1,15 +1,11 @@
 import * as winston from 'winston';
 
+
+const isRunningInAWS = !!process.env.AWS_EXECUTION_ENV;
+
 const logFormatter = winston.format.printf(({ timestamp, level, message, stack }) => {
     const log = `${timestamp} [${level}]: ${message}`;
-
-    // Cloud watch cannot handle multi-line logs, so we need to replace newlines with a literal '\n'
-    if (process.env.AWS_EXECUTION_ENV) {
-        return stack ? `${log} ${stack.replace(/\n/g, '\\n')}` : log;
-    }
-
-    // Otherwise, keep multi-line formatting for local readability
-    return stack ? `${log}\n${stack}` : log;
+    return stack ? log + '\n' + stack : log;
 });
 
 export const logger = winston.createLogger({
@@ -17,6 +13,6 @@ export const logger = winston.createLogger({
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.errors({ stack: true }),
-        logFormatter
+        isRunningInAWS ? winston.format.json() : logFormatter
     )
 });
