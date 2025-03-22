bf611328ffe4e1d3f01c61d21f420c133cff7db0
AMMayberry1 <ammayberry1@gmail.com>
2025-03-22 14:08:51 -0400

    Add check that cost evaluation works

--- test/server/cards/04_JTL/units/TheStarhawkPrototypeBattleship.spec.ts
+++ test/server/cards/04_JTL/units/TheStarhawkPrototypeBattleship.spec.ts
@@ -6,9 +6,9 @@ describe('The Starhawk, Prototype Battleship', function() {
                     phase: 'action',
                     player1: {
                         leader: 'captain-rex#fighting-for-his-brothers',
-                        hand: ['raddus#holdos-final-command'],
+                        hand: ['raddus#holdos-final-command', 'blue-leader#scarif-air-support'],
                         spaceArena: ['the-starhawk#prototype-battleship'],
-                        groundArena: ['crosshair#following-orders']
+                        groundArena: [{ card: 'crosshair#following-orders', exhausted: true }]
                     },
                     player2: {
                         leader: 'admiral-trench#chkchkchkchk',
@@ -22,24 +22,50 @@ describe('The Starhawk, Prototype Battleship', function() {
             it('should decrease the play cost of a card at pay time by half, rounded up', function() {
                 const { context } = contextRef;
 
+                // exhaust resources down to the expected amount to confirm that cost evaluation before play works and the card shows as selectable
+                const expectedResourceCost = 4;
+                context.player1.exhaustResources(context.player1.readyResourceCount - expectedResourceCost);
+                const exhaustedResourceCountBefore = context.player1.exhaustedResourceCount;
+
+                expect(context.player1).toBeAbleToSelect(context.raddus);
                 context.player1.clickCard(context.raddus);
-                expect(context.player1.exhaustedResourceCount).toBe(4);
+                expect(context.player1.exhaustedResourceCount).toBe(exhaustedResourceCountBefore + expectedResourceCost);
             });
 
             it('should decrease the activation cost of a leader ability', function() {
                 const { context } = contextRef;
 
+                // exhaust resources down to the expected amount to confirm that cost evaluation before play works and the card shows as selectable
+                const expectedResourceCost = 1;
+                context.player1.exhaustResources(context.player1.readyResourceCount - expectedResourceCost);
+                const exhaustedResourceCountBefore = context.player1.exhaustedResourceCount;
+
                 context.player1.clickCard(context.captainRex);
                 context.player1.clickPrompt('If a friendly unit attacked this phase, create a Clone Trooper token.');
-                expect(context.player1.exhaustedResourceCount).toBe(1);
+                expect(context.player1.exhaustedResourceCount).toBe(exhaustedResourceCountBefore + expectedResourceCost);
             });
 
             it('should decrease the activation cost of a unit ability', function() {
                 const { context } = contextRef;
 
+                // exhaust resources down to the expected amount to confirm that cost evaluation before play works and the card shows as selectable
+                const expectedResourceCost = 1;
+                context.player1.exhaustResources(context.player1.readyResourceCount - expectedResourceCost);
+                const exhaustedResourceCountBefore = context.player1.exhaustedResourceCount;
+
+                expect(context.player1).toBeAbleToSelect(context.crosshair);
                 context.player1.clickCard(context.crosshair);
-                context.player1.clickPrompt('Get +1/+0 for this phase');
-                expect(context.player1.exhaustedResourceCount).toBe(1);
+                expect(context.player1.exhaustedResourceCount).toBe(exhaustedResourceCountBefore + expectedResourceCost);
+            });
+
+            it('should not affect costs that are not ability activation costs', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.blueLeader);
+                context.player1.clickPrompt('Pay 2 resources');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.player1.exhaustedResourceCount).toBe(4); // 2 for (adjusted) play cost, 2 for effect payment
             });
 
             it('should not affect opponent\'s play card costs', function() {
