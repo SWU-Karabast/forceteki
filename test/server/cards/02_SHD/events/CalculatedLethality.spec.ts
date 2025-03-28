bd0055e7ee7354e64f92a18372ca7cf20e5a3738
Jan Preisig <jan.preisig@intuitive-collaboration.com>
2025-03-28 13:29:15 +0100

    fix Calculated Lethality test

--- test/server/cards/02_SHD/events/CalculatedLethality.spec.ts
+++ test/server/cards/02_SHD/events/CalculatedLethality.spec.ts
@@ -71,7 +71,7 @@ describe('Calculated Lethality', function () {
                 expect(context.corellianFreighter).toHaveExactUpgradeNames(['experience', 'experience', 'experience']);
             });
         });
-        // TODO ADD A TEST WITH LURKIN TIE PHANTOM
+
         describe('Calculated Lethality\'s ability', function () {
             it('should not be able to defeat lurking tie phantom', async function () {
                 await contextRef.setupTestAsync({
@@ -87,7 +87,6 @@ describe('Calculated Lethality', function () {
                 const { context } = contextRef;
 
                 context.player1.clickCard(context.calculatedLethality);
-                context.player1.clickCard(context.lurkingTiePhantom);
 
                 expect(context.calculatedLethality).toBeInZone('discard', context.player1);
                 expect(context.lurkingTiePhantom).toBeInZone('spaceArena', context.player2);
