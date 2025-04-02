856de929651f3f5d65a241531f1adaffca1b1b50
AMMayberry1 <ammayberry1@gmail.com>
2025-04-02 12:27:55 -0400

    More basic tests

--- server/game/cards/01_SOR/events/ForceLightning.ts
+++ server/game/cards/01_SOR/events/ForceLightning.ts
@@ -27,10 +27,16 @@ export default class ForceLightning extends EventCard {
                 targetResolver: {
                     mode: TargetMode.DropdownList,
                     options: (context) => Array.from({ length: context.player.readyResourceCount + 1 }, (_x, i) => `${i}`),
-                    immediateEffect: AbilityHelper.immediateEffects.damage((context) => ({
-                        amount: parseInt(context.select) * 2,
-                        target: thenContext.target
-                    })),
+                    immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                        AbilityHelper.immediateEffects.payResourceCost((context) => ({
+                            amount: parseInt(context.select),
+                            target: context.player,
+                        })),
+                        AbilityHelper.immediateEffects.damage((context) => ({
+                            amount: parseInt(context.select) * 2,
+                            target: thenContext.target
+                        }))
+                    ]),
                 },
             })
         });
--- test/server/cards/01_SOR/events/ForceLightning.spec.ts
+++ test/server/cards/01_SOR/events/ForceLightning.spec.ts
@@ -13,7 +13,7 @@ describe('Force Lightning', function() {
                     },
                     player2: {
                         base: { card: 'chopper-base', damage: 5 },
-                        groundArena: ['wampa'],
+                        groundArena: ['atst'],
                         spaceArena: ['victor-leader#leading-from-the-front', 'tie-bomber'],
                     }
                 });
@@ -26,7 +26,7 @@ describe('Force Lightning', function() {
                 expect(context.player1).toBeAbleToSelectExactly([
                     context.darthMaul,
                     context.fireball,
-                    context.wampa,
+                    context.atst,
                     context.victorLeader,
                     context.tieBomber
                 ]);
@@ -37,10 +37,100 @@ describe('Force Lightning', function() {
                 expect(context.tieBomber.getHp()).toBe(4);
 
                 // can choose from 0 - 9 resources since 1 was paid for Force Lightning already
-                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (x, i) => `${i}`));
+                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (_x, i) => `${i}`));
                 context.player1.chooseListOption('1');
 
                 expect(context.victorLeader.damage).toBe(2);
+                expect(context.player1.exhaustedResourceCount).toBe(2);
+
+                // ability is un-blanked after the action phase ends
+                context.moveToRegroupPhase();
+                expect(context.tieBomber.getPower()).toBe(1);
+                expect(context.tieBomber.getHp()).toBe(5);
+            });
+
+            it('should always deal damage equal to 2x the resources paid', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.forceLightning);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.darthMaul,
+                    context.fireball,
+                    context.atst,
+                    context.victorLeader,
+                    context.tieBomber
+                ]);
+
+                context.player1.clickCard(context.atst);
+                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (_x, i) => `${i}`));
+                context.player1.chooseListOption('3');
+                expect(context.atst.damage).toBe(6);
+                expect(context.player1.exhaustedResourceCount).toBe(4);
+            });
+
+            it('should allow choosing 0 damage but still blank the target card', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.forceLightning);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.darthMaul,
+                    context.fireball,
+                    context.atst,
+                    context.victorLeader,
+                    context.tieBomber
+                ]);
+
+                // select Victor Leader and confirm that TIE Bomber no longer has the effect
+                context.player1.clickCard(context.victorLeader);
+                expect(context.tieBomber.getPower()).toBe(0);
+                expect(context.tieBomber.getHp()).toBe(4);
+
+                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (_x, i) => `${i}`));
+                context.player1.chooseListOption('0');
+
+                expect(context.victorLeader.damage).toBe(0);
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+            });
+
+            it('should allow exhausting all available resources', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.forceLightning);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.darthMaul,
+                    context.fireball,
+                    context.atst,
+                    context.victorLeader,
+                    context.tieBomber
+                ]);
+
+                context.player1.clickCard(context.atst);
+                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (_x, i) => `${i}`));
+                context.player1.chooseListOption('9');
+                expect(context.atst).toBeInZone('discard');
+                expect(context.player1.exhaustedResourceCount).toBe(10);
+            });
+
+            it('should not prevent triggers that happen at the beginning of the regroup phase', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.forceLightning);
+                expect(context.player1).toBeAbleToSelectExactly([
+                    context.darthMaul,
+                    context.fireball,
+                    context.atst,
+                    context.victorLeader,
+                    context.tieBomber
+                ]);
+
+                context.player1.clickCard(context.fireball);
+                expect(context.player1).toHaveExactDropdownListOptions(Array.from({ length: 10 }, (_x, i) => `${i}`));
+                context.player1.chooseListOption('0');
+                expect(context.fireball.damage).toBe(0);
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+
+                context.moveToNextActionPhase();
+                expect(context.fireball.damage).toBe(1);
             });
         });
     });
