bf6735f4e25cccdf765aafe07573c2e263d50abf
Robbie Moyer <rmoyer08@gmail.com>
2025-04-15 14:17:29 -0400

    Add test suite integration for the Force token

--- test/server/gameSystems/TheForce.spec.ts
+++ test/server/gameSystems/TheForce.spec.ts
@@ -1,65 +1,95 @@
 describe('The Force token', function () {
     integration(function(contextRef) {
-        beforeEach(function () {
-            return contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    base: 'shadowed-undercity',
-                    groundArena: [
-                        'guardian-of-the-whills'
-                    ]
-                },
-                player2: {
-                    base: 'jedi-temple',
-                }
+        describe('The basics of the Force token', function () {
+            beforeEach(function () {
+                return contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        base: 'shadowed-undercity',
+                        groundArena: [
+                            'guardian-of-the-whills'
+                        ]
+                    },
+                    player2: {
+                        base: 'jedi-temple',
+                    }
+                });
+            });
+
+            it('Is initialized for each player at the start of the game', function () {
+                const { context } = contextRef;
+
+                const p1ForceToken = context.player1.findCardByName('force');
+                const p2ForceToken = context.player2.findCardByName('force');
+
+                // Ensure that the force tokens are created and out of play for both players
+                expect(p1ForceToken).toBeInZone('outsideTheGame');
+                expect(p2ForceToken).toBeInZone('outsideTheGame');
+            });
+
+            it('Does not get destoryed at the end of a round with the standar token cleanup', function () {
+                const { context } = contextRef;
+
+                const p1ForceToken = context.player1.findCardByName('force');
+                const p2ForceToken = context.player2.findCardByName('force');
+
+                // Move to the next round
+                context.moveToNextActionPhase();
+
+                // Ensure that the force tokens are still out of play for both players
+                expect(p1ForceToken).toBeInZone('outsideTheGame');
+                expect(p2ForceToken).toBeInZone('outsideTheGame');
+            });
+
+            it('Stays in its current zone across rounds', function () {
+                const { context } = contextRef;
+
+                const p1ForceToken = context.player1.findCardByName('force');
+                const p2ForceToken = context.player2.findCardByName('force');
+
+                // Attack with a Force unit to gain The Force
+                context.player1.clickCard(context.guardianOfTheWhills);
+                context.player1.clickCard(context.p2Base);
+
+                // Ensure Force tokens are in the correct zone
+                expect(p1ForceToken).toBeInZone('base');
+                expect(p2ForceToken).toBeInZone('outsideTheGame');
+
+                // Move to the next round
+                context.moveToNextActionPhase();
+
+                // Ensure that the force tokens are still in the correct zone
+                expect(p1ForceToken).toBeInZone('base');
+                expect(p2ForceToken).toBeInZone('outsideTheGame');
             });
         });
 
-        it('Is initialized for each player at the start of the game', function () {
-            const { context } = contextRef;
+        describe('Test Suite integration', function () {
+            beforeEach(function () {
+                return contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hasForceToken: true
+                    }
+                });
+            });
 
-            const p1ForceToken = context.player1.findCardByName('force');
-            const p2ForceToken = context.player2.findCardByName('force');
+            it('Can be set using test setup options', function () {
+                const { context } = contextRef;
 
-            // Ensure that the force tokens are created and out of play for both players
-            expect(p1ForceToken).toBeInZone('outsideTheGame');
-            expect(p2ForceToken).toBeInZone('outsideTheGame');
-        });
+                expect(context.player1.hasTheForce).toBe(true);
+                expect(context.player2.hasTheForce).toBe(false);
+            });
 
-        it('Does not get destoryed at the end of a round with the standar token cleanup', function () {
-            const { context } = contextRef;
+            it('Can be modified using the player interaction wrapper', function () {
+                const { context } = contextRef;
 
-            const p1ForceToken = context.player1.findCardByName('force');
-            const p2ForceToken = context.player2.findCardByName('force');
+                context.player1.setHasTheForce(false);
+                context.player2.setHasTheForce(true);
 
-            // Move to the next round
-            context.moveToNextActionPhase();
-
-            // Ensure that the force tokens are still out of play for both players
-            expect(p1ForceToken).toBeInZone('outsideTheGame');
-            expect(p2ForceToken).toBeInZone('outsideTheGame');
-        });
-
-        it('Stays in its current zone across rounds', function () {
-            const { context } = contextRef;
-
-            const p1ForceToken = context.player1.findCardByName('force');
-            const p2ForceToken = context.player2.findCardByName('force');
-
-            // Attack with a Force unit to gain The Force
-            context.player1.clickCard(context.guardianOfTheWhills);
-            context.player1.clickCard(context.p2Base);
-
-            // Ensure Force tokens are in the correct zone
-            expect(p1ForceToken).toBeInZone('base');
-            expect(p2ForceToken).toBeInZone('outsideTheGame');
-
-            // Move to the next round
-            context.moveToNextActionPhase();
-
-            // Ensure that the force tokens are still in the correct zone
-            expect(p1ForceToken).toBeInZone('base');
-            expect(p2ForceToken).toBeInZone('outsideTheGame');
+                expect(context.player1.hasTheForce).toBe(false);
+                expect(context.player2.hasTheForce).toBe(true);
+            });
         });
     });
 });
