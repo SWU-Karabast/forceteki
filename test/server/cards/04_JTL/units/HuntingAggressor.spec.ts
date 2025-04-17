a66b065ed4f7b2deabbf42257b12b6c6bfbb67d9
Tommaso Madonia <tommaso@madonia.me>
2025-04-17 14:17:10 +0100

    Hunting Aggressor shouldn't increase indirect damage dealt to self (#1103)
    ![image](https://github.com/user-attachments/assets/804a67c8-9425-4791-a1e8-dbb378818165)
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

--- test/server/cards/04_JTL/units/HuntingAggressor.spec.ts
+++ test/server/cards/04_JTL/units/HuntingAggressor.spec.ts
@@ -138,5 +138,62 @@ describe('Hunting Aggressor', function() {
             expect(context.wampa.damage).toBe(4);
             expect(context.p2Base.damage).toBe(5);
         });
+
+        it('Hunting Aggressor\'s ability should not increase the amount of indirect damage dealt to yourself', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['planetary-bombardment'],
+                    spaceArena: ['hunting-aggressor'],
+                },
+                player2: {
+                    groundArena: ['wampa']
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Planetary Bombardment deals 8 damage
+            context.player1.clickCard(context.planetaryBombardment);
+            context.player1.clickPrompt('You');
+            expect(context.player1).toHavePrompt('Distribute 8 indirect damage among targets');
+
+            context.player1.setDistributeIndirectDamagePromptState(new Map([
+                [context.huntingAggressor, 4],
+                [context.p1Base, 4],
+            ]));
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.huntingAggressor.damage).toBe(4);
+            expect(context.p1Base.damage).toBe(4);
+        });
+
+        it('Hunting Aggressor\'s ability should increase each instance of indirect damage when multiple are triggered', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    spaceArena: ['hunting-aggressor', { card: 'tie-bomber', upgrades: ['dengar#crude-and-slovenly'] }],
+                    leader: 'boba-fett#any-methods-necessary',
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.tieBomber);
+            context.player1.clickCard(context.p2Base);
+
+            // Trigger Tie Bomber, deals 4 indirect damage (3 plus one from Hunting Aggressor)
+            context.player1.clickPrompt('Deal 3 indirect damage to the defending player');
+            expect(context.p2Base.damage).toBe(4);
+
+            // Trigger Boba's ability, deals 2 indirect damage (1 plus one from Hunting Aggressor)
+            context.player1.clickPrompt('Trigger');
+            context.player1.clickPrompt('Opponent');
+            expect(context.p2Base.damage).toBe(6);
+
+            // Trigger Dengar's ability, deals 3 indirect damage (2 plus one from Hunting Aggressor) and 1 combat damage
+            context.player1.clickPrompt('Opponent');
+            expect(context.p2Base.damage).toBe(10);
+        });
     });
 });
