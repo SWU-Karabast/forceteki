1cd948f1532805e9d019295f5e5b7da5160c1ee5
Anthony Bronico <adbronico92@gmail.com>
2025-04-30 09:53:57 -0400

    Fix bugs with Satine and Boba Fett Daimyo

--- test/server/cards/03_TWI/units/SatineKryzeCommittedToPeace.spec.ts
+++ test/server/cards/03_TWI/units/SatineKryzeCommittedToPeace.spec.ts
@@ -48,5 +48,53 @@ describe('Satine Kryze, Committed To Peace', function () {
             expect(context.player2).toBeAbleToSelectExactly([context.battlefieldMarine]);
             context.player2.clickCard(context.battlefieldMarine);
         });
+
+        it('Satine Kryze\'s ability should work with damaged units', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['daring-raid'],
+                    groundArena: [{ card: 'satine-kryze#committed-to-peace', damage: 2 }, { card: 'battlefield-marine', damage: 1 }],
+                    deck: 20
+                },
+                player2: {
+                    hand: ['waylay'],
+                    groundArena: [{ card: 'wampa', upgrades: ['entrenched'], damage: 5 }],
+                    deck: 20
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Check that both players' units have the mill ability
+            context.player1.clickCard(context.satineKryze);
+            expect(context.player1).toHaveExactPromptButtons(['Attack', 'Discard cards from an opponent\'s deck equal to half this unit\'s remaining HP, rounded up', 'Cancel']);
+            context.player1.clickPrompt('Discard cards from an opponent\'s deck equal to half this unit\'s remaining HP, rounded up');
+            expect(context.satineKryze.exhausted).toBe(true);
+            expect(context.player2.deck.length).toBe(18);
+
+            context.player2.clickCard(context.wampa);
+            expect(context.player2).toHaveExactPromptButtons(['Attack', 'Discard cards from an opponent\'s deck equal to half this unit\'s remaining HP, rounded up', 'Cancel']);
+            context.player2.clickPrompt('Discard cards from an opponent\'s deck equal to half this unit\'s remaining HP, rounded up');
+            expect(context.wampa.exhausted).toBe(true);
+            expect(context.player1.deck.length).toBe(18);
+
+            context.moveToNextActionPhase();
+
+            // Check that you cannot trigger the mill ability on opponent's units or on upgrades
+            expect(context.wampa).not.toHaveAvailableActionWhenClickedBy(context.player1);
+            context.player1.passAction();
+            expect(context.satineKryze).not.toHaveAvailableActionWhenClickedBy(context.player2);
+            expect(context.entrenched).not.toHaveAvailableActionWhenClickedBy(context.player2);
+
+            // Check that the mill ability disappears when Satine leaves play
+            context.player2.clickCard(context.waylay);
+            context.player2.clickCard(context.satineKryze);
+            context.player1.passAction();
+
+            context.player2.clickCard(context.wampa);
+            expect(context.player2).toBeAbleToSelectExactly([context.battlefieldMarine]);
+            context.player2.clickCard(context.battlefieldMarine);
+        });
     });
 });
