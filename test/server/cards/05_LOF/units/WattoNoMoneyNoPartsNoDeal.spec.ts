ae7e40bb99922aa319af9bff762f917be666c4f2
Valentin Jubert <val_jub@live.fr>
2025-05-01 07:22:50 +0200

    fix tests

--- test/server/cards/05_LOF/units/WattoNoMoneyNoPartsNoDeal.spec.ts
+++ test/server/cards/05_LOF/units/WattoNoMoneyNoPartsNoDeal.spec.ts
@@ -47,7 +47,7 @@ describe('Watto, No Money No Parts No Deal', function() {
             context.player1.clickCard(context.p2Base);
 
             // Opponent chooses to draw a card
-            context.player2.clickPrompt(`${context.player1.name} draw a card`);
+            context.player2.clickPrompt(`${context.player1.name} draws a card`);
 
             // Check that a card was drawn
             expect(context.player2).toBeActivePlayer();
