c43de0cf8965257a69faa9ebb26b7b5a0b1f5a5f
Robbie Moyer <rmoyer08@gmail.com>
2025-05-02 09:31:53 -0400

    Add implementation for Darth Maul's lightsaber

---
+++ server/game/cards/05_LOF/upgrades/DarthMaulsLightsaber.ts
@@ -0,0 +1,31 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { KeywordName, Trait } from '../../../core/Constants';
+
+export default class DarthMaulsLightsaber extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '2167393423',
+            internalName: 'darth-mauls-lightsaber',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setAttachCondition((card) => !card.hasSomeTrait(Trait.Vehicle));
+
+        this.addWhenPlayedAbility({
+            title: 'Attack with Darth Maul. For this attack, he gains overwhelm and can\'t attack bases.',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.parentCard?.title === 'Darth Maul',
+                onTrue: AbilityHelper.immediateEffects.attack((attackCtx) => ({
+                    target: attackCtx.source.parentCard,
+                    targetCondition: (target) => target.isUnit(),
+                    attackerLastingEffects: {
+                        effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Overwhelm)
+                    }
+                }))
+            })
+        });
+    }
+}
