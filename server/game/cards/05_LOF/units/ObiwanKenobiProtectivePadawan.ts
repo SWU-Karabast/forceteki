60a764696c18157d882310b99e83739d0c0890b3
Tommaso Madonia <tommaso@madonia.me>
2025-05-05 10:14:54 +0100

    Use cardTypeWhenInPlay

--- server/game/cards/05_LOF/units/ObiwanKenobiProtectivePadawan.ts
+++ server/game/cards/05_LOF/units/ObiwanKenobiProtectivePadawan.ts
@@ -1,6 +1,6 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
-import { KeywordName, Trait } from '../../../core/Constants';
+import { CardType, KeywordName, Trait } from '../../../core/Constants';
 
 export default class ObiwanKenobiProtectivePadawan extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -14,13 +14,13 @@ export default class ObiwanKenobiProtectivePadawan extends NonLeaderUnitCard {
         this.addTriggeredAbility({
             title: 'This unit gains Sentinel for this phase',
             when: {
-                onCardPlayed: (event, context) => event.card.isUnit() &&
+                onCardPlayed: (event, context) => event.cardTypeWhenInPlay === CardType.BasicUnit &&
                   event.card.hasSomeTrait(Trait.Force) &&
                   event.player === context.player,
             },
-            immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect((context) => ({
+            immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
                 effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
-            })),
+            }),
         });
     }
 }
