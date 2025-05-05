5196bf7cbc024ab26b66be08da081a683f2e4476
Tommaso Madonia <tommaso@madonia.me>
2025-05-05 14:11:46 +0100

    Add Obi-Wan Kenobi LOF (#1147)
    ![image](https://github.com/user-attachments/assets/a2d881f8-5289-470a-a076-eb647cd9eece)

---
+++ server/game/cards/05_LOF/units/ObiwanKenobiProtectivePadawan.ts
@@ -0,0 +1,26 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { CardType, KeywordName, Trait } from '../../../core/Constants';
+
+export default class ObiwanKenobiProtectivePadawan extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '6059510270',
+            internalName: 'obiwan-kenobi#protective-padawan'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'This unit gains Sentinel for this phase',
+            when: {
+                onCardPlayed: (event, context) => event.cardTypeWhenInPlay === CardType.BasicUnit &&
+                  event.card.hasSomeTrait(Trait.Force) &&
+                  event.player === context.player,
+            },
+            immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
+            }),
+        });
+    }
+}
