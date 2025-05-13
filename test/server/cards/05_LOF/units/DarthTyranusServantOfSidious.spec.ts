433a69fc27fba9438748805371ae7e0ac3c9f64e
Valentin Jubert <val_jub@live.fr>
2025-05-13 18:34:43 +0200

    Anakin Skywakler, Darth Tyranus, Constructed Lightsaber (#1188)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/070.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/231.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/261.png)

---
+++ test/server/cards/05_LOF/units/DarthTyranusServantOfSidious.spec.ts
@@ -0,0 +1,45 @@
+describe('Darth Tyranus, Servant of Sidious', function() {
+    integration(function(contextRef) {
+        it('Darth Tyranus\'s ability should gain Ambush when the Force is with the player', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hasForceToken: true,
+                    hand: ['darth-tyranus#servant-of-sidious'],
+                },
+                player2: {
+                    groundArena: ['sundari-peacekeeper'],
+                },
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard('darth-tyranus#servant-of-sidious');
+
+            expect(context.player1).toHavePassAbilityPrompt('Ambush');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.player1).toBeAbleToSelectExactly([context.sundariPeacekeeper]);
+            context.player1.clickCard(context.sundariPeacekeeper);
+            expect(context.player2).toBeActivePlayer();
+            expect(context.sundariPeacekeeper.damage).toBe(4);
+        });
+
+        it('should lose Ambush when the Force changes sides', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hasForceToken: false,
+                    hand: ['darth-tyranus#servant-of-sidious'],
+                },
+                player2: {
+                    groundArena: ['sundari-peacekeeper'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard('darth-tyranus#servant-of-sidious');
+            expect(context.player2).toBeActivePlayer();
+        });
+    });
+});
