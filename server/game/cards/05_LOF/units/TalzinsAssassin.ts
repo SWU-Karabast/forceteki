92bb1164c4922f9a3a180798db2866eb9a478578
Valentin Jubert <val_jub@live.fr>
2025-05-13 23:26:44 +0200

    Curious Flock, Karis, Talzin's Assassin (#1195)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/031.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/035.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LOF/255.png)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/05_LOF/units/TalzinsAssassin.ts
@@ -0,0 +1,30 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class TalzinsAssassin extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'talzins-assassin-id',
+            internalName: 'talzins-assassin',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addWhenPlayedAbility({
+            title: 'Use the Force',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.useTheForce(),
+            ifYouDo: {
+                title: 'Give a unit -3/-3 for this phase',
+                targetResolver: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                        effect: AbilityHelper.ongoingEffects.modifyStats({ power: -3, hp: -3 })
+                    })
+                }
+            }
+        });
+    }
+}
+
