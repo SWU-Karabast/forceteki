dc539dbed20e8d67f534e882482f3731f102a190
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2025-05-13 16:27:34 -0600

    Implementing Ataru Onslaught, Soresu Stance and Nightsister Warrior (#1197)

---
+++ test/server/cards/05_LOF/events/SoresuStance.spec.ts
@@ -0,0 +1,36 @@
+describe('Soresu Stance ability', function () {
+    integration(function (contextRef) {
+        beforeEach(async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['atst'],
+                    hand: ['soresu-stance', 'yoda#old-master', 'drain-essence', 'anakin-skywalker#ill-try-spinning'],
+                }
+            });
+        });
+
+        it('allows the player to play a Force unit from their hand and gives it a Shield token', function () {
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.soresuStance);
+            expect(context.player1).toBeAbleToSelectExactly([context.yoda, context.anakinSkywalker]);
+            context.player1.clickCard(context.yoda);
+
+            expect(context.yoda).toHaveExactUpgradeNames(['shield']);
+        });
+
+        it('allows the player to play a Force pilot unit from their hand and gives it a Shield token', function () {
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.soresuStance);
+            expect(context.player1).toBeAbleToSelectExactly([context.yoda, context.anakinSkywalker]);
+            context.player1.clickCard(context.anakinSkywalker);
+
+            expect(context.player2).toBeActivePlayer();
+
+            expect(context.anakinSkywalker).toBeInZone('groundArena');
+            expect(context.anakinSkywalker).toHaveExactUpgradeNames(['shield']);
+        });
+    });
+});
