eb071bd7bb9a696aaf28564567edb0193bf13342
AMMayberry1 <ammayberry1@gmail.com>
2025-05-13 15:21:40 -0400

    Remove unneeded mocks and fix keyword parsing issue (#1196)

--- test/server/cards/05_LOF/units/DarthTyranusServantOfSidious.spec.ts
+++ test/server/cards/05_LOF/units/DarthTyranusServantOfSidious.spec.ts
@@ -15,7 +15,8 @@ describe('Darth Tyranus, Servant of Sidious', function() {
 
             context.player1.clickCard('darth-tyranus#servant-of-sidious');
 
-            expect(context.player1).toHavePassAbilityPrompt('Ambush');
+            // resolve Shielded, then Ambush
+            context.player1.clickPrompt('Shielded');
             context.player1.clickPrompt('Trigger');
 
             expect(context.player1).toBeAbleToSelectExactly([context.sundariPeacekeeper]);
