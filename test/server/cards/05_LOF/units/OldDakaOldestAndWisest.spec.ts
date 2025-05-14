f615d3a60fca74b1a7cd105368199d527fee6d9e
AMMayberry1 <ammayberry1@gmail.com>
2025-05-14 12:31:13 -0400

    Finalize tests

--- test/server/cards/05_LOF/units/OldDakaOldestAndWisest.spec.ts
+++ test/server/cards/05_LOF/units/OldDakaOldestAndWisest.spec.ts
@@ -61,6 +61,78 @@ describe('Old Daka, Oldest and Wisest', function() {
                 expect(context.daughterOfDathomir).toBeInZone('discard');
                 expect(context.player2).toBeActivePlayer();
             });
+
+            it('should allow passing at the defeat stage', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.oldDaka);
+                expect(context.player1).toBeAbleToSelectExactly([context.talzinsAssassin, context.asajjVentress]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickPrompt('Pass');
+
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should allow passing at the play stage', function() {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.oldDaka);
+                expect(context.player1).toBeAbleToSelectExactly([context.talzinsAssassin, context.asajjVentress]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickCard(context.talzinsAssassin);
+
+                expect(context.talzinsAssassin).toBeInZone('discard');
+                expect(context.player1).toHavePassAbilityPrompt('Play Talzin\'s Assassin from your discard pile for free');
+                context.player1.clickPrompt('Pass');
+
+                expect(context.talzinsAssassin).toBeInZone('discard');
+            });
+        });
+
+        it('Old Daka\'s ability should allow defeating a friendly Night unit and then playing it for free from discard, then abilities trigger', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['old-daka#oldest-and-wisest'],
+                    hasForceToken: true,
+                    groundArena: [{ card: 'talzins-assassin', upgrades: ['inspiring-mentor'] }, 'wampa'],
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.oldDaka);
+            expect(context.player1).toBeAbleToSelectExactly([context.talzinsAssassin]);
+            expect(context.player1).toHavePassAbilityButton();
+            context.player1.clickCard(context.talzinsAssassin);
+
+            const p1ResourceCount = context.player1.readyResourceCount;
+            expect(context.talzinsAssassin).toBeInZone('discard');
+            expect(context.player1).toHavePassAbilityPrompt('Play Talzin\'s Assassin from your discard pile for free');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.talzinsAssassin).toBeInZone('groundArena');
+            expect(context.player1.readyResourceCount).toBe(p1ResourceCount);
+
+            expect(context.player1).toHavePrompt('Choose an ability to resolve:');
+            expect(context.player1).toHaveExactPromptButtons([
+                'Use the Force',
+                'Give an Experience token to another friendly unit',
+            ]);
+
+            context.player1.clickPrompt('Use the Force');
+            context.player1.clickPrompt('Trigger');
+            expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.talzinsAssassin, context.oldDaka]);
+            context.player1.clickCard(context.wampa);
+            expect(context.wampa.getPower()).toBe(1);
+            expect(context.wampa.getHp()).toBe(2);
+
+            // Inspiring Mentor trigger
+            expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.oldDaka]);
+            context.player1.clickCard(context.oldDaka);
+            expect(context.oldDaka).toHaveExactUpgradeNames(['experience']);
+
+            expect(context.player2).toBeActivePlayer();
         });
     });
 });
