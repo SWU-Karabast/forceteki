98be4e511de850ef87ccc8e308635b482b9d14eb
aruizc <ing.angelruizcerrillo@gmail.com>
2025-05-19 09:50:41 -0600

    Added unit tests for Kit Fisto

--- test/server/cards/05_LOF/leaders/KitFistoFocusedJediMaster.spec.ts
+++ test/server/cards/05_LOF/leaders/KitFistoFocusedJediMaster.spec.ts
@@ -5,14 +5,15 @@ describe('Kit Fisto, Focused Jedi Master', function() {
                 return contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
+                        hand: ['change-of-heart'],
                         groundArena: ['yoda#old-master', 'battlefield-marine', 'jedi-guardian'],
                         spaceArena: ['anakins-interceptor#where-the-fun-begins'],
-                        leader: 'kit-fisto#focused-jedi-master',
-                        resources: 4 // making leader undeployable makes testing the activated ability's condition smoother
+                        leader: 'kit-fisto#focused-jedi-master'
                     },
                     player2: {
                         groundArena: ['count-dooku#fallen-jedi'],
                         spaceArena: ['alliance-xwing', 'padawan-starfighter'],
+                        hand: ['traitorous']
                     }
                 });
             });
@@ -26,8 +27,8 @@ describe('Kit Fisto, Focused Jedi Master', function() {
 
                 // Use Kit Fisto's ability
                 context.player2.passAction();
-                context.player1.clickCard(context.kitFisto); // As resources are not enough to deploy, the ability is activated automatically
-                expect(context.player1).toHavePrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
+                context.player1.clickCard(context.kitFisto);
+                context.player1.clickPrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
 
                 // Only units are selectable, deals 2 damage to the selected unit
                 expect(context.player1).toBeAbleToSelectExactly([context.yoda, context.battlefieldMarine, context.jediGuardian, context.countDooku, context.allianceXwing, context.padawanStarfighter, context.anakinsInterceptor]);
@@ -37,6 +38,33 @@ describe('Kit Fisto, Focused Jedi Master', function() {
                 expect(context.kitFisto.exhausted).toBeTrue();
                 expect(context.countDooku.damage).toBe(2);
                 expect(context.player1.exhaustedResourceCount).toBe(1);
+            });
+
+            it('should be able to deal 2 damage to a unit as stolen Jedi unit attacked this phase', function () {
+                const { context } = contextRef;
+
+                // Attack with a non Jedi unit
+                context.player1.clickCard(context.changeOfHeart);
+                context.player1.clickCard(context.padawanStarfighter);
+
+                const usedResourceCount = context.player1.exhaustedResourceCount;
+
+                context.player2.passAction();
+                context.player1.clickCard(context.padawanStarfighter);
+                context.player1.clickCard(context.p2Base);
+
+                // Use Kit Fisto's ability
+                context.player2.passAction();
+                context.player1.clickCard(context.kitFisto);
+                context.player1.clickPrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
+
+                expect(context.player1).toBeAbleToSelectExactly([context.yoda, context.battlefieldMarine, context.jediGuardian, context.countDooku, context.allianceXwing, context.padawanStarfighter, context.anakinsInterceptor]);
+                expect(context.player1).not.toHavePassAbilityButton();
+                context.player1.clickCard(context.allianceXwing);
+
+                expect(context.kitFisto.exhausted).toBeTrue();
+                expect(context.allianceXwing.damage).toBe(2);
+                expect(context.player1.exhaustedResourceCount).toBe(usedResourceCount + 1);
             });
 
             it('should not be able to deal 2 damage to a unit as the controller hasn\'t attacked with a Jedi this phase', function () {
@@ -49,6 +77,42 @@ describe('Kit Fisto, Focused Jedi Master', function() {
                 // Use Kit Fisto's ability
                 context.player2.passAction();
                 context.player1.clickCard(context.kitFisto); // Ability does not trigger
+                context.player1.clickPrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
+
+                expect(context.kitFisto.exhausted).toBeTrue();
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+            });
+
+            it('should not be able to deal 2 damage to a unit as the controller hasn\'t attacked with a Jedi this phase, only the opponent', function () {
+                const { context } = contextRef;
+
+                // Attack with a non Jedi unit
+                context.player1.passAction();
+                context.player2.clickCard(context.padawanStarfighter);
+                context.player2.clickCard(context.p1Base);
+
+                // Use Kit Fisto's ability
+                context.player1.clickCard(context.kitFisto); // Ability does not trigger
+                context.player1.clickPrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
+
+                expect(context.kitFisto.exhausted).toBeTrue();
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+            });
+
+            it('should not be able to deal 2 damage to a unit as Jedi unit was stolen by the opponent', function () {
+                const { context } = contextRef;
+
+                // Attack with a non Jedi unit
+                context.player1.passAction();
+                context.player2.clickCard(context.traitorous);
+                context.player2.clickCard(context.yoda);
+                context.player1.passAction();
+                context.player2.clickCard(context.yoda);
+                context.player2.clickCard(context.p1Base);
+
+                // Use Kit Fisto's ability
+                context.player1.clickCard(context.kitFisto); // Ability does not trigger
+                context.player1.clickPrompt('If you attacked with a Jedi unit this phase, deal 2 damage to a unit');
 
                 expect(context.kitFisto.exhausted).toBeTrue();
                 expect(context.player1.exhaustedResourceCount).toBe(1);
