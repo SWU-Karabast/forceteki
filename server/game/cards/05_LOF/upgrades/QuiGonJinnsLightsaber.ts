8b51c10f5e7ba4cbf0dad013a26157fc707902b7
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2025-05-19 07:26:37 -0600

    Qui Gon Jinn Lightsaber (#1216)
    ![image](https://github.com/user-attachments/assets/3fe8fc3c-e266-4501-83f2-0598637b098e)

---
+++ server/game/cards/05_LOF/upgrades/QuiGonJinnsLightsaber.ts
@@ -0,0 +1,42 @@
+import AbilityHelper from '../../../AbilityHelper';
+import type { Card } from '../../../core/card/Card';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { TargetMode, Trait } from '../../../core/Constants';
+import * as Contract from '../../../core/utils/Contract';
+
+export default class QuiGonJinnsLightsaber extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '3445044882',
+            internalName: 'quigon-jinns-lightsaber',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setAttachCondition((card: Card) => !card.hasSomeTrait(Trait.Vehicle) && card.controller === this.controller);
+
+        this.addWhenPlayedAbility({
+            title: 'Exhaust any number of units with combined cost 6 or less.',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => context.source.parentCard?.title === 'Qui-Gon Jinn',
+                onTrue: AbilityHelper.immediateEffects.selectCard({
+                    activePromptTitle: 'Exhaust any number of units with combined cost 6 or less',
+                    mode: TargetMode.Unlimited,
+                    multiSelectCardCondition: (card, currentlySelectedCards) => card.isUnit() && this.costSum(currentlySelectedCards.concat(card)) <= 6,
+                    canChooseNoCards: true,
+                    innerSystem: AbilityHelper.immediateEffects.exhaust()
+                })
+            })
+        });
+    }
+
+    private costSum(cards: Card[]): number {
+        let costSum = 0;
+        for (const card of cards) {
+            Contract.assertTrue(card.isUnit());
+            costSum += card.cost;
+        }
+        return costSum;
+    }
+}
