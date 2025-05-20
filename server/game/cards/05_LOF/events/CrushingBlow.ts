e10d1f021a7c8c4e2a9e9286955979e82c6b9abe
Valentin Jubert <val_jub@live.fr>
2025-05-20 18:57:05 +0200

    purge trooper and crushing blow

---
+++ server/game/cards/05_LOF/events/CrushingBlow.ts
@@ -0,0 +1,23 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class CrushingBlow extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: 'crushing-blow-id',
+            internalName: 'crushing-blow',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Defeat a non-leader unit that costs 2 or less',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                cardCondition: (card) => card.isUnit() && card.cost <= 2,
+                immediateEffect: AbilityHelper.immediateEffects.defeat(),
+            }
+        });
+    }
+}
