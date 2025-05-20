4ea4be2d36d744254f16c891282e0d97069acc00
Tommaso Madonia <tommaso@madonia.me>
2025-05-20 21:22:14 +0100

    Add Grand Inquisitor, Stories Travel Quickly

---
+++ server/game/cards/05_LOF/leaders/GrandInquisitorStoriesTravelQuickly.ts
@@ -0,0 +1,36 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+
+export default class GrandInquisitorStoriesTravelQuickly extends LeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'grand-inquisitor#stories-travel-quickly-id',
+            internalName: 'grand-inquisitor#stories-travel-quickly',
+        };
+    }
+
+    protected override setupLeaderSideAbilities() {
+        this.addActionAbility({
+            title: 'Attack with a unit. The defender gets -2/-0 for this attack',
+            cost: [
+                AbilityHelper.costs.exhaustSelf(),
+                AbilityHelper.costs.useTheForce(),
+            ],
+            initiateAttack: {
+                defenderLastingEffects: {
+                    effect: AbilityHelper.ongoingEffects.modifyStats({ power: -2, hp: 0 })
+                }
+            }
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities() {
+        this.addOnAttackAbility({
+            title: 'The defender gets -2/-0 for this attack',
+            immediateEffect: AbilityHelper.immediateEffects.forThisAttackCardEffect((context) => ({
+                target: context.event.attack.getAllTargets(),
+                effect: AbilityHelper.ongoingEffects.modifyStats({ power: -2, hp: 0 })
+            })),
+        });
+    }
+}
