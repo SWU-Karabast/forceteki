c6e6ca3053d3875cb5cd18182a9049d925747113
Tommaso Madonia <tommaso@madonia.me>
2025-05-21 17:44:16 +0100

    Ignore controller requirement when resolving triggered abilities

--- test/server/cards/02_SHD/units/KraytDragon.spec.ts
+++ test/server/cards/02_SHD/units/KraytDragon.spec.ts
@@ -1,8 +1,8 @@
 describe('Krayt Dragon', function () {
     integration(function (contextRef) {
         describe('Krayt Dragon\'s ability', function () {
-            beforeEach(function () {
-                return contextRef.setupTestAsync({
+            it('should deal damage to ground unit or base when enemy play a card', async function () {
+                await contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
                         hand: ['battlefield-marine'],
@@ -13,13 +13,8 @@ describe('Krayt Dragon', function () {
                         groundArena: ['wampa'],
                         resources: ['hotshot-dl44-blaster', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst', 'atst']
                     },
-
-                    // IMPORTANT: this is here for backwards compatibility of older tests, don't use in new code
-                    autoSingleTarget: true
                 });
-            });
 
-            it('should deal damage to ground unit or base when enemy play a card', function () {
                 const { context } = contextRef;
 
                 // play a card, nothing happen
@@ -60,11 +55,135 @@ describe('Krayt Dragon', function () {
                 // enemy kill everyone, krayt ability still activates
                 context.player1.passAction();
                 context.player2.clickCard(context.superlaserBlast);
-                expect(context.player1).toHavePassAbilityPrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
-                context.player1.clickPrompt('Trigger');
+                context.player1.clickCard(context.p2Base);
                 expect(context.p2Base.damage).toBe(8);
             });
-            // TODO test u-wing, vader or endless legion when implemented
+
+            it('should work correctly with Change of Heart', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['battlefield-marine', 'change-of-heart'],
+                    },
+                    player2: {
+                        hand: ['green-squadron-awing'],
+                        groundArena: ['krayt-dragon'],
+                    },
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.changeOfHeart);
+                context.player1.clickCard(context.kraytDragon);
+                expect(context.kraytDragon).toBeInZone('groundArena', context.player1);
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.kraytDragon]);
+
+                context.player2.clickCard(context.p1Base);
+                expect(context.p1Base.damage).toBe(6);
+
+                context.player2.clickCard(context.greenSquadronAwing);
+                expect(context.player1).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player1).toBeAbleToSelectExactly([context.p2Base]);
+
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(2);
+
+                context.player1.clickCard(context.battlefieldMarine);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should work correctly with Choose Sides', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['choose-sides'],
+                        groundArena: ['battlefield-marine'],
+                    },
+                    player2: {
+                        hand: ['green-squadron-awing'],
+                        groundArena: ['krayt-dragon'],
+                    },
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.chooseSides);
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.kraytDragon);
+                expect(context.kraytDragon).toBeInZone('groundArena', context.player1);
+                expect(context.battlefieldMarine).toBeInZone('groundArena', context.player2);
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.kraytDragon]);
+
+                context.player2.clickCard(context.kraytDragon);
+                expect(context.kraytDragon.damage).toBe(7);
+
+                context.player2.clickCard(context.greenSquadronAwing);
+                expect(context.player1).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player1).toBeAbleToSelectExactly([context.p2Base, context.battlefieldMarine]);
+
+                context.player1.clickCard(context.p2Base);
+                expect(context.p2Base.damage).toBe(2);
+            });
+
+            it('should work correctly with U-Wing Reinforcement', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'hera-syndulla#spectre-two',
+                        hand: ['uwing-reinforcement'],
+                        deck: [
+                            'wampa',
+                            'vanguard-infantry',
+                            'battlefield-marine',
+                            'hunting-nexu',
+                            'cartel-turncoat',
+                            'daring-raid',
+                            'protector',
+                            'strike-true',
+                            'atat-suppressor',
+                            'aurra-sing#crackshot-sniper',
+                            'atst'
+                        ],
+                    },
+                    player2: {
+                        groundArena: ['krayt-dragon'],
+                    },
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.uwingReinforcement);
+                context.player1.clickCardInDisplayCardPrompt(context.wampa);
+                context.player1.clickCardInDisplayCardPrompt(context.battlefieldMarine);
+                context.player1.clickCardInDisplayCardPrompt(context.vanguardInfantry);
+                context.player1.clickPrompt('Play cards in selection order');
+
+                // Trigger for Wampa
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.wampa]);
+                context.player2.clickCard(context.wampa);
+                expect(context.wampa.damage).toBe(4);
+
+                // Trigger for Battlefield Marine
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.wampa, context.battlefieldMarine]);
+                context.player2.clickCard(context.battlefieldMarine);
+                expect(context.battlefieldMarine.damage).toBe(2);
+
+                // // Trigger for Vanguard Infantry
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.wampa, context.battlefieldMarine, context.vanguardInfantry]);
+                context.player2.clickCardNonChecking(context.vanguardInfantry);
+                expect(context.vanguardInfantry.damage).toBe(1);
+
+                // Trigger for U-Wing Reinforcement
+                expect(context.player2).toHavePrompt('Deal damage equal to that card’s cost to their base or a ground unit they control');
+                expect(context.player2).toBeAbleToSelectExactly([context.p1Base, context.wampa, context.battlefieldMarine, context.vanguardInfantry]);
+                context.player2.clickCard(context.p1Base);
+                expect(context.p1Base.damage).toBe(7);
+            });
         });
     });
 });
