57c09fd2521dd2ad676088e54f81aae75725d1ee
aruizc <ing.angelruizcerrillo@gmail.com>
2025-05-23 11:59:23 -0600

    Implementing Darth Vader Twilight of the Apprentice

---
+++ server/game/cards/05_LOF/units/DarthVaderTwilightOfTheApprentice.ts
@@ -0,0 +1,39 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer } from '../../../core/Constants';
+
+export default class DarthVaderTwilightOfTheApprentice extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '2277278592',
+            internalName: 'darth-vader#twilight-of-the-apprentice',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addWhenPlayedAbility({
+            title: 'Give a Shield token to a friendly unit and to an enemy unit',
+            targetResolvers: {
+                friendlyUnit: {
+                    controller: RelativePlayer.Self,
+                    immediateEffect: AbilityHelper.immediateEffects.giveShield()
+                },
+                enemyUnit: {
+                    controller: RelativePlayer.Opponent,
+                    immediateEffect: AbilityHelper.immediateEffects.giveShield()
+                }
+            }
+        });
+
+        this.addOnAttackAbility({
+            title: 'Defeat an enemy unit with a Shield token on it',
+            targetResolvers: {
+                enemyUnit: {
+                    controller: RelativePlayer.Opponent,
+                    cardCondition: (card) => card.isUnit() && card.hasShield(),
+                    immediateEffect: AbilityHelper.immediateEffects.defeat()
+                }
+            }
+        });
+    }
+}
