98527190364eac32434f7d769a7927003038870e
Valentin Jubert <val_jub@live.fr>
2025-05-30 00:07:17 +0200

    data & t6 shuttle 1974

---
+++ server/game/cards/05_LOF/units/T6Shuttle1974StayClose.ts
@@ -0,0 +1,22 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class T6Shuttle1974StayClose extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 't6-shuttle-1974#stay-close-id',
+            internalName: 't6-shuttle-1974#stay-close'
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Give an experience token to this unit',
+            when: {
+                onAttackDeclared: (event, context) => event.attack.getAllTargets().includes(context.source),
+            },
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.giveExperience(),
+        });
+    }
+}
