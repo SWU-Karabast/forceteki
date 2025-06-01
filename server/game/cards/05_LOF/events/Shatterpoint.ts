62a90b80b1387396e9af907c2a02ef221716eebc
Valentin Jubert <val_jub@live.fr>
2025-06-01 22:28:28 +0200

    shatterpoint

---
+++ server/game/cards/05_LOF/events/Shatterpoint.ts
@@ -0,0 +1,45 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, TargetMode, WildcardCardType } from '../../../core/Constants';
+import ForceToken from '../tokens/ForceToken';
+
+export default class Shatterpoint extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: 'shatterpoint-id',
+            internalName: 'shatterpoint',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Choose one: Defeat a non-leader unit with 3 or less remaining HP or Use the Force. If you do, defeat a non-leader unit',
+            targetResolver: {
+                mode: TargetMode.Select,
+                choosingPlayer: RelativePlayer.Self,
+                showUnresolvable: true,
+                choices: () => ({
+                    ['Defeat a non-leader unit with 3 or less remaining HP']:
+                        AbilityHelper.immediateEffects.selectCard({
+                            cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                            cardCondition: (card) => card.isUnit() && card.remainingHp <= 3,
+                            innerSystem: AbilityHelper.immediateEffects.defeat(),
+                        }),
+                    ['Use the Force. If you do, defeat a non-leader unit']:
+                        AbilityHelper.immediateEffects.useTheForce()
+                }),
+            },
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'Defeat a non-leader unit',
+                ifYouDoCondition: () => {
+                    const { card, resolutionStatus, player } = ifYouDoContext.events[0];
+                    return player === ifYouDoContext.player && card instanceof ForceToken && resolutionStatus === 'resolved';
+                },
+                targetResolver: {
+                    cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                    immediateEffect: AbilityHelper.immediateEffects.defeat(),
+                }
+            })
+        });
+    }
+}
