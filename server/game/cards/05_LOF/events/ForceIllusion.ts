4d7183bbeab1b1a8ff6edec5db1156f96ee1169e
Valentin Jubert <val_jub@live.fr>
2025-06-06 14:55:38 +0200

    Bendu, Force Illusion, Caretaker Matron (#1346)

---
+++ server/game/cards/05_LOF/events/ForceIllusion.ts
@@ -0,0 +1,34 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { KeywordName, RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class ForceIllusion extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: 'force-illusion-id',
+            internalName: 'force-illusion',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Exhaust an enemy unit. A friendly unit gains Sentinel for this phase',
+            immediateEffect: AbilityHelper.immediateEffects.sequential([
+                AbilityHelper.immediateEffects.selectCard({
+                    activePromptTitle: 'Exhaust an enemy unit',
+                    controller: RelativePlayer.Opponent,
+                    cardTypeFilter: WildcardCardType.Unit,
+                    innerSystem: AbilityHelper.immediateEffects.exhaust()
+                }),
+                AbilityHelper.immediateEffects.selectCard({
+                    activePromptTitle: 'A friendly unit gains Sentinel for this phase',
+                    controller: RelativePlayer.Self,
+                    cardTypeFilter: WildcardCardType.Unit,
+                    innerSystem: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                        effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
+                    })
+                }),
+            ]),
+        });
+    }
+}
