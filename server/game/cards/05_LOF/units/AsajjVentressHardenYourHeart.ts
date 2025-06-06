1b3f4effeb132b0164fb813d9d071ba28b84e65a
Robbie Moyer <moyerr@users.noreply.github.com>
2025-06-06 16:02:51 -0400

    Quinlan Vos & Asajj Ventress (#1357)

---
+++ server/game/cards/05_LOF/units/AsajjVentressHardenYourHeart.ts
@@ -0,0 +1,30 @@
+import { NonLeaderUnitCard } from '../../../../../server/game/core/card/NonLeaderUnitCard';
+import AbilityHelper from '../../../AbilityHelper';
+import { RelativePlayer, Trait, WildcardCardType } from '../../../core/Constants';
+
+export default class AsajjVentressHardenYourHeart extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'asajj-ventress#harden-your-heart-id',
+            internalName: 'asajj-ventress#harden-your-heart',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Give another friendly Force unit +2/+0 for this phase',
+            when: {
+                whenPlayed: true,
+                onAttack: true,
+            },
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                controller: RelativePlayer.Self,
+                cardCondition: (card, context) => card.hasSomeTrait(Trait.Force) && card !== context.source,
+                immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 0 }),
+                })
+            }
+        });
+    }
+}
