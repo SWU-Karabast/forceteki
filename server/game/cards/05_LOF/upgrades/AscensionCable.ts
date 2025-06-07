df100f7c8cb4a08c6ba83d1464ba4e0ced82bbe5
sethhelman <s7helman@gmail.com>
2025-06-07 11:50:22 -0400

    Four LOF Cards

---
+++ server/game/cards/05_LOF/upgrades/AscensionCable.ts
@@ -0,0 +1,20 @@
+import type { Card } from '../../../core/card/Card';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { Trait, KeywordName } from '../../../core/Constants';
+
+export default class AscensionCable extends UpgradeCard {
+    protected override getImplementationId () {
+        return {
+            id: '6128668392',
+            internalName: 'ascension-cable',
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.setAttachCondition((card: Card) => !card.hasSomeTrait(Trait.Vehicle));
+
+        this.addGainKeywordTargetingAttached({
+            keyword: KeywordName.Saboteur
+        });
+    }
+}
