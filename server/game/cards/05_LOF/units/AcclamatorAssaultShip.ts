c942d9ef21a4ce667fb549282a3573fb5ce9b265
Tommaso Madonia <tommaso@madonia.me>
2025-06-07 14:47:09 +0100

    Acclamator Assault Ship, Blockade Runner, Invasion Control Ship, Medical Frigate (#1366)

---
+++ server/game/cards/05_LOF/units/AcclamatorAssaultShip.ts
@@ -0,0 +1,26 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class AcclamatorAssaultShip extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '1072330402',
+            internalName: 'acclamator-assault-ship',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addOnAttackAbility({
+            title: 'Give another unit +5/+5 for this phase',
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                cardCondition: (card, context) => card !== context.source,
+                immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.modifyStats({ power: 5, hp: 5 })
+                })
+            }
+        });
+    }
+}
