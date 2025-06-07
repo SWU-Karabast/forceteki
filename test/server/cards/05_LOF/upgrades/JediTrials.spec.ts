ae616116d0d3449debd00868e76b7a4eda876eaa
Valentin Jubert <val_jub@live.fr>
2025-06-07 12:00:59 +0200

    jedi trials

--- test/server/cards/05_LOF/upgrades/JediTrials.spec.ts
+++ test/server/cards/05_LOF/upgrades/JediTrials.spec.ts
@@ -4,27 +4,31 @@ describe('Jedi Trials', function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
-                    hand: ['knights-saber'],
                     groundArena: [{ card: 'guardian-of-the-whills', upgrades: ['jedi-trials', 'experience', 'experience'] }, 'gungi#finding-himself'],
-                    spaceArena: ['green-squadron-awing'],
                 },
             });
 
             const { context } = contextRef;
-
-            context.player1.clickCard(context.knightsSaber);
-
-            // cannot select guardian of the whills (he is not jedi)
-            expect(context.player1).toBeAbleToSelectExactly([context.gungi]);
-            context.player1.clickPrompt('Cancel');
 
             context.player1.clickCard(context.guardianOfTheWhills);
             context.player1.clickCard(context.p2Base);
 
             // guardian of the whills gains 1 experience
             expect(context.guardianOfTheWhills).toHaveExactUpgradeNames(['jedi-trials', 'experience', 'experience', 'experience']);
+            expect(context.gungi).toHaveExactUpgradeNames([]);
+        });
 
-            context.player2.passAction();
+        it('Jedi Trials\'s ability should give Jedi trait when attached unit have 4 or more upgrades', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['knights-saber'],
+                    groundArena: [{ card: 'guardian-of-the-whills', upgrades: ['jedi-trials', 'experience', 'experience', 'experience'] }, 'gungi#finding-himself'],
+                    spaceArena: ['green-squadron-awing'],
+                },
+            });
+
+            const { context } = contextRef;
 
             context.player1.clickCard(context.knightsSaber);
 
