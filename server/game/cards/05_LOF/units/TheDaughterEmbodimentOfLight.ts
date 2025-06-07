e80bd77c2f8a514cb8928d5b182c5776f12ad8dd
Valentin Jubert <val_jub@live.fr>
2025-06-07 08:34:49 +0200

    babu, the daughter, vanee

---
+++ server/game/cards/05_LOF/units/TheDaughterEmbodimentOfLight.ts
@@ -0,0 +1,29 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class TheDaughterEmbodimentOfLight extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '0662915879',
+            internalName: 'the-daughter#embodiment-of-light',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addTriggeredAbility({
+            title: 'Use the Force to heal 2 damage from your base',
+            when: {
+                onDamageDealt: (event, context) => event.card === context.player.base
+            },
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.useTheForce(),
+            ifYouDo: {
+                title: 'Heal 2 damage from your base',
+                immediateEffect: AbilityHelper.immediateEffects.heal((context) => ({
+                    amount: 2,
+                    target: context.player.base
+                }))
+            }
+        });
+    }
+}
