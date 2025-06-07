0d7b647387953ea03a8c73c3a988d715e602a867
Valentin Jubert <val_jub@live.fr>
2025-06-07 11:04:49 +0200

    zuckuss

---
+++ server/game/cards/05_LOF/units/ZuckussTheFindsman.ts
@@ -0,0 +1,39 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { TargetMode } from '../../../core/Constants';
+
+export default class ZuckussTheFindsman extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '0406487670',
+            internalName: 'zuckuss#the-findsman',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.addOnAttackAbility({
+            title: 'Name a card, then discard the top card of the defending player\'s deck. If a card with that name is discarded, this unit gets +4/+0 for this attack.',
+            targetResolver: {
+                mode: TargetMode.DropdownList,
+                options: this.game.playableCardTitles,
+            },
+            then: (thenContext) => ({
+                title: 'If a card with that name is discarded, this unit gets +4/+0 for this attack.',
+                immediateEffect: AbilityHelper.immediateEffects.sequential([
+                    AbilityHelper.immediateEffects.discardFromDeck((context) => ({
+                        amount: 1,
+                        target: context.source.activeAttack.getDefendingPlayer(),
+                    })),
+                    AbilityHelper.immediateEffects.conditional({
+                        condition: (context) => {
+                            return context.events[0].card.title === thenContext.select;
+                        },
+                        onTrue: AbilityHelper.immediateEffects.forThisAttackCardEffect({
+                            effect: AbilityHelper.ongoingEffects.modifyStats({ power: 4, hp: 0 })
+                        })
+                    })
+                ])
+            })
+        });
+    }
+}
