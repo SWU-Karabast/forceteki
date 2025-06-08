7b66aa28a5bda6ecb7b463a24b7dad21e2b13e89
Robbie Moyer <rmoyer08@gmail.com>
2025-06-08 09:16:28 -0400

    Fix The Son's ability

--- test/server/cards/05_LOF/units/TheSonEmbodimentOfDarkness.spec.ts
+++ test/server/cards/05_LOF/units/TheSonEmbodimentOfDarkness.spec.ts
@@ -1,3 +1,4 @@
+
 describe('The Son, Embodiment of Darkness', function() {
     integration(function(contextRef) {
         describe('The Son\'s ability', function() {
@@ -21,17 +22,22 @@ describe('The Son, Embodiment of Darkness', function() {
             it('should give only friendly units +2/+0 when you have the force', function () {
                 const { context } = contextRef;
 
-                context.player1.clickCard(context.grandInquisitorYoureRightToBeAfraid);
+                context.player1.clickCard(context.grandInquisitor);
                 context.player1.clickCard(context.p2Base);
+                expect(context.grandInquisitor.getPower()).toBe(5);
                 expect(context.p2Base.damage).toBe(9);
 
                 context.player2.clickCard(context.rebelPathfinder);
                 context.player2.clickCard(context.p1Base);
+                expect(context.rebelPathfinder.getPower()).toBe(2);
                 expect(context.p1Base.damage).toBe(5);
+
+                // The Son himself also gets +2/+0
+                expect(context.theSon.getPower()).toBe(8);
             });
         });
 
-        it('The Son, Embodiment of Darkness\'s ability should not be active as player doesn\'t have the Force', async function () {
+        it('The Son, Embodiment of Darkness\'s ability should not be active when player doesn\'t have the Force', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
@@ -48,11 +54,13 @@ describe('The Son, Embodiment of Darkness', function() {
 
             const { context } = contextRef;
 
-            context.player1.clickCard(context.grandInquisitorYoureRightToBeAfraid);
+            context.player1.clickCard(context.grandInquisitor);
             context.player1.clickCard(context.p2Base);
+            expect(context.grandInquisitor.getPower()).toBe(3);
             expect(context.p2Base.damage).toBe(7);
         });
-        it('The Son, Embodiment of Darkness\'s ability should not be active as player doesn\'t have the Force', async function () {
+
+        it('it becomes active when an on-attack trigger gives the player the Force', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
@@ -69,8 +77,16 @@ describe('The Son, Embodiment of Darkness', function() {
 
             const { context } = contextRef;
 
-            context.player1.clickCard(context.grandInquisitorYoureRightToBeAfraid);
+            // Does not have the Force at the start, and The Son's ability is not active
+            expect(context.player1.hasTheForce).toBeFalse();
+            expect(context.grandInquisitor.getPower()).toBe(3);
+
+            context.player1.clickCard(context.grandInquisitor);
             context.player1.clickCard(context.p2Base);
+
+            // Player gains the force on-attack, and The Son's ability give the the buff before combat damage is dealt
+            expect(context.player1.hasTheForce).toBeTrue();
+            expect(context.grandInquisitor.getPower()).toBe(5);
             expect(context.p2Base.damage).toBe(9);
         });
     });
