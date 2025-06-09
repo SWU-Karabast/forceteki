fe0911092af92a1db58fcd175667aa57eb11d990
Valentin Jubert <val_jub@live.fr>
2025-06-09 23:42:20 +0200

    zuckuss tests

--- test/server/cards/05_LOF/units/ZuckussTheFindsman.spec.ts
+++ test/server/cards/05_LOF/units/ZuckussTheFindsman.spec.ts
@@ -48,6 +48,33 @@ describe('Zuckuss, The Findsman', function() {
             expect(context.player2).toBeActivePlayer();
         });
 
+        it('Zuckuss\'s multiple ability should kill defender before naming a card, his ability should name a card, discard the top card of defender\'s deck, get +4/+0 for this attack and use it with Overwhelm', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: [{ card: 'zuckuss#the-findsman', upgrades: ['jedi-lightsaber', 'grievouss-wheel-bike'] }],
+                },
+                player2: {
+                    groundArena: ['jedha-agitator'],
+                    deck: ['fetts-firespray#feared-silhouette']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.zuckuss);
+            context.player1.clickCard(context.jedhaAgitator);
+
+            context.player1.clickPrompt('Give the defender -2/-2 for this phase');
+
+            expect(context.player1).toHaveExactDropdownListOptions(context.getPlayableCardTitles());
+            context.player1.chooseListOption('Fett\'s Firespray');
+
+            expect(context.fettsFirespray).toBeInZone('discard', context.player2);
+            expect(context.player2).toBeActivePlayer();
+            expect(context.p2Base.damage).toBe(14);
+        });
+
         it('Zuckuss\'s ability should name a card (skipped because of empty deck), not discard the top card of defender\'s deck because he is empty', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
