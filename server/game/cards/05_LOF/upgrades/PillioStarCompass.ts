20add2fa07c8c8b6484475626fd0750defcbd6cc
sethhelman <s7helman@gmail.com>
2025-06-09 13:29:17 -0400

    In The Shadows, Saesee Tiin, Pillio Star Compass (#1388)
    ![pillio star
    compass](https://github.com/user-attachments/assets/391bf456-beca-4ba2-a52d-f9a56d62ae7f)
    ![saesee
    tiin](https://github.com/user-attachments/assets/0e25e50a-1e6a-49fa-8d55-b9a581555b59)
    ![in the
    shadows](https://github.com/user-attachments/assets/1a2f823a-b635-41ab-a570-0049a0c3c7f0)
    
    ---------
    
    Co-authored-by: Anthony Bronico <adbronico92@gmail.com>

---
+++ server/game/cards/05_LOF/upgrades/PillioStarCompass.ts
@@ -0,0 +1,26 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { Trait } from '../../../core/Constants';
+
+export default class PillioStarCompass extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: '8580514429',
+            internalName: 'pillio-star-compass',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setAttachCondition((card) => card.isUnit() && !card.hasSomeTrait(Trait.Vehicle));
+
+        this.addWhenPlayedAbility({
+            title: 'Search the top 3 cards of your deck for a unit, reveal it, and draw it',
+            immediateEffect: AbilityHelper.immediateEffects.deckSearch({
+                selectCount: 1,
+                searchCount: 3,
+                cardCondition: (card) => card.isUnit(),
+                selectedCardsImmediateEffect: AbilityHelper.immediateEffects.drawSpecificCard()
+            }),
+        });
+    }
+}
