25caf1c74707b1f2ec0db0059435c3811483d9df
Robbie Moyer <rmoyer08@gmail.com>
2025-06-09 11:23:34 -0400

    Premonition of doom impl

---
+++ server/game/cards/05_LOF/events/PremonitionOfDoom.ts
@@ -0,0 +1,28 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { Duration } from '../../../core/Constants';
+
+export default class PremonitionOfDoom extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '9129337737',
+            internalName: 'premonition-of-doom',
+        };
+    }
+
+    public override setupCardAbilities () {
+        this.setEventAbility({
+            title: 'The next time you take the initiative this phase, exhaust all units',
+            immediateEffect: AbilityHelper.immediateEffects.delayedPlayerEffect({
+                title: 'Exhaust all units',
+                duration: Duration.UntilEndOfPhase,
+                when: {
+                    onClaimInitiative: (event, context) => event.player === context.player
+                },
+                immediateEffect: AbilityHelper.immediateEffects.exhaust((context) => ({
+                    target: context.game.getArenaUnits()
+                }))
+            })
+        });
+    }
+}
