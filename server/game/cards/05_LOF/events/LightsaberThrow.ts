19a8166cc1290aadcf0609c18a037566c66494b1
sethhelman <s7helman@gmail.com>
2025-06-10 09:06:07 -0400

    Lightsaber throw, three lessons, flight of the inquisitor (#1400)
    ![flight of the
    inquisitor](https://github.com/user-attachments/assets/81415ec9-5642-41a9-b091-410c1e220c11)
    ![three
    lessons](https://github.com/user-attachments/assets/1eec3996-e42f-4a3c-bb7e-2e75a7f388ff)
    ![lightsaber
    throw](https://github.com/user-attachments/assets/068485c9-24b7-413d-9a8e-33127a306a5c)

---
+++ server/game/cards/05_LOF/events/LightsaberThrow.ts
@@ -0,0 +1,34 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, Trait, ZoneName } from '../../../core/Constants';
+
+export default class LightsaberThrow extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '0721742014',
+            internalName: 'lightsaber-throw',
+        };
+    }
+
+    public override setupCardAbilities() {
+        this.setEventAbility({
+            title: 'Discard a Lightsaber card from your hand. If you do, deal 4 damage to a ground unit and draw a card.',
+            targetResolver: {
+                controller: RelativePlayer.Self,
+                zoneFilter: ZoneName.Hand,
+                cardCondition: (card) => card.hasSomeTrait(Trait.Lightsaber),
+                immediateEffect: AbilityHelper.immediateEffects.discardSpecificCard(),
+            },
+            ifYouDo: {
+                title: 'Deal 4 damage to a ground unit and draw a card',
+                immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                    AbilityHelper.immediateEffects.selectCard({
+                        zoneFilter: ZoneName.GroundArena,
+                        innerSystem: AbilityHelper.immediateEffects.damage({ amount: 4 })
+                    }),
+                    AbilityHelper.immediateEffects.draw()
+                ])
+            }
+        });
+    }
+}
