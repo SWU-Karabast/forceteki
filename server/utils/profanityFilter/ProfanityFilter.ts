351506ea9b05696b5f4e401b45c613c1c7640465
AMMayberry1 <ammayberry1@gmail.com>
2025-06-17 12:09:12 -0400

    WIP divider update

--- server/utils/profanityFilter/ProfanityFilter.ts
+++ server/utils/profanityFilter/ProfanityFilter.ts
@@ -1,7 +1,6 @@
 import words from 'profane-words';
 import { hateGroupsMatcher, karabastProfanityMatcher } from './KarabastProfanityMatcher';
 
-
 /**
  * Checks if a string contains any profanity from our filter list
  * @param text Text to check for profanity
@@ -23,8 +22,45 @@ export function usernameContainsProfanity(text: string): boolean {
         return true;
     }
 
+    const consolidatedWords = consolidateDividedWords(text);
+    const separatedText = separateWordsWithWhitespace(consolidatedWords);
+    return karabastProfanityMatcher.hasMatch(separatedText);
+}
+
+/**
+ * For cases like usernames that can't have whitespace, separates a string into words based on common separators
+ * such as underscores, periods, and camelCase.
+ *
+ * Example: "helloWorld" -> "hello World", "hello_world" -> "hello world", "hello.world" -> "hello world"
+ */
+function separateWordsWithWhitespace(text: string): string {
     const splitOnNonAlphabetic = text.replace(/[._]+/g, ' ');
     const splitOnWordBoundaries = splitOnNonAlphabetic.replace(/([a-z])([A-Z])/g, '$1 $2');
 
-    return karabastProfanityMatcher.hasMatch(splitOnWordBoundaries);
+    return splitOnWordBoundaries;
+}
+
+function consolidateDividedWords(text: string): string {
+    let newString = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+
+        if (isDividerAt(text, i) && isDividerAt(text, i + 2) && isDividerAt(text, i - 2)) {
+            continue;
+        }
+
+        newString += char;
+    }
+
+    return newString;
+}
+
+function isDividerAt(text: string, index: number): boolean {
+    if (index < 0 || index >= text.length) {
+        return true;
+    }
+
+    const char = text[index];
+    return char === '_' || char === '.';
 }
