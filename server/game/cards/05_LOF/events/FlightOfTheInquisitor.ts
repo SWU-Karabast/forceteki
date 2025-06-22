7bb6eca1557898bf1f94f64ce4582795466be1e4
Robbie Moyer <rmoyer08@gmail.com>
2025-06-22 09:43:22 -0400

    Fix controller issue with Flight of the Inquisitors

--- server/game/cards/05_LOF/events/FlightOfTheInquisitor.ts
+++ server/game/cards/05_LOF/events/FlightOfTheInquisitor.ts
@@ -1,6 +1,6 @@
 import AbilityHelper from '../../../AbilityHelper';
 import { EventCard } from '../../../core/card/EventCard';
-import { Trait, WildcardCardType, ZoneName } from '../../../core/Constants';
+import { RelativePlayer, Trait, WildcardCardType, ZoneName } from '../../../core/Constants';
 
 export default class FlightOfTheInquisitor extends EventCard {
     protected override getImplementationId() {
@@ -18,12 +18,14 @@ export default class FlightOfTheInquisitor extends EventCard {
                 [
                     AbilityHelper.immediateEffects.selectCard({
                         zoneFilter: ZoneName.Discard,
+                        controller: RelativePlayer.Self,
                         cardTypeFilter: WildcardCardType.Unit,
                         cardCondition: (card) => card.hasSomeTrait(Trait.Force),
                         immediateEffect: AbilityHelper.immediateEffects.returnToHand()
                     }),
                     AbilityHelper.immediateEffects.selectCard({
                         zoneFilter: ZoneName.Discard,
+                        controller: RelativePlayer.Self,
                         cardTypeFilter: WildcardCardType.Upgrade,
                         cardCondition: (card) => card.hasSomeTrait(Trait.Lightsaber),
                         immediateEffect: AbilityHelper.immediateEffects.returnToHand()
