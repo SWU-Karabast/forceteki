dfdfed3f701c61922525722ffdce31cc537ac900
Tommaso Madonia <tommaso@madonia.me>
2025-07-14 19:16:31 +0100

    Add test with Superlaser Tech

--- test/server/cards/01_SOR/units/SuperlaserTechnician.spec.ts
+++ test/server/cards/01_SOR/units/SuperlaserTechnician.spec.ts
@@ -26,6 +26,36 @@ describe('Superlaser Technician', function () {
             expect(context.player2).toBeActivePlayer();
         });
 
+        it('Superlaser Technician\'s ability should allow the controller to put the defeated Technician into play as a resource and ready it when it has been cloned with Clone', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['clone'],
+                    groundArena: ['superlaser-technician']
+                },
+                player2: {
+                    groundArena: ['sundari-peacekeeper']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.clone);
+            context.player1.clickCard(context.superlaserTechnician);
+            expect(context.clone).toBeCloneOf(context.superlaserTechnician);
+
+            context.player2.clickCard(context.sundariPeacekeeper);
+            context.player2.clickCard(context.clone);
+
+            const readyResourcesBeforeTrigger = context.player1.readyResourceCount;
+            expect(context.player1).toHavePassAbilityPrompt('Put Superlaser Technician into play as a resource and ready it');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.player1.readyResourceCount).toBe(readyResourcesBeforeTrigger + 1);
+            expect(context.clone).toBeInZone('resource');
+            expect(context.clone.exhausted).toBe(false);
+        });
+
         // TODO FIX MULTIPLE TRIGGER WHICH PLAY THE SAME CARD
         // it('Superlaser Technician\'s ability should allow the controller to put the defeated Technician into play as a resource and ready it', async function () {
         //     await contextRef.setupTestAsync({
