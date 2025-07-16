f3cdb56482c0037475bfe95d2fe7f4271e4bc94f
Caelan Mayberry <gmodman12@gmail.com>
2025-07-15 19:34:07 -0700

    Removed duplicate code

--- test/scenarios/undo/Undo.spec.ts
+++ test/scenarios/undo/Undo.spec.ts
@@ -335,55 +335,6 @@ describe('Undo', function() {
             });
         });
 
-        it('Huyang\'s ability gives another friendly unit +2/+2 until he leaves play', async function () {
-            await contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    hand: ['huyang#enduring-instructor'],
-                    groundArena: ['wampa', 'death-star-stormtrooper']
-                },
-                player2: {
-                    groundArena: ['atst'],
-                }
-            });
-
-            const { context } = contextRef;
-            rollback(context, function leavesPlay() {
-                context.player1.clickCard(context.huyang);
-                expect(context.player1).toBeAbleToSelect(context.wampa);
-                context.player1.clickCard(context.wampa);
-
-                expect(context.wampa.getPower()).toBe(6);
-                expect(context.wampa.getHp()).toBe(7);
-                expect(context.huyang.getPower()).toBe(2);
-                expect(context.huyang.getHp()).toBe(4);
-
-                // Defeat Huyang, effect goes away
-                context.player2.clickCard(context.atst);
-                context.player2.clickCard(context.huyang);
-
-                expect(context.wampa.getPower()).toBe(4);
-                expect(context.wampa.getHp()).toBe(5);
-            }, function checkForLingeringEffects() {
-                // Verify that Wampa is at printed stats
-                expect(context.wampa.getPower()).toBe(4);
-                expect(context.wampa.getHp()).toBe(5);
-
-                // Pick a card other than Wampa
-                context.player1.clickCard(context.huyang);
-                expect(context.player1).toBeAbleToSelect(context.deathStarStormtrooper);
-                context.player1.clickCard(context.deathStarStormtrooper);
-
-                // Defeat Huyang, effect goes away
-                context.player2.clickCard(context.atst);
-                context.player2.clickCard(context.huyang);
-
-                // Ensure that Wampa still remains at same power. Testing that there aren't lingering effects from previous rollback that target this card.
-                expect(context.wampa.getPower()).toBe(4);
-                expect(context.wampa.getHp()).toBe(5);
-            });
-        });
-
         describe('Huyang\'s ability', function () {
             beforeEach(function () {
                 return contextRef.setupTestAsync({
@@ -519,33 +470,6 @@ describe('Undo', function() {
 
                     expect(stolenResource.controller).toBe(context.player2Object);
                     expect(stolenResource.exhausted).toBeTrue();
-                }, function staysInPlay() {
-                    context.player1.clickCard(context.djBlatantThief);
-
-                    expect(context.player1.resources.length).toBe(11);
-                    expect(context.player2.resources.length).toBe(9);
-                    expect(context.player1.readyResourceCount).toBe(4);
-                    expect(context.player1.exhaustedResourceCount).toBe(7);
-                    expect(context.player2.readyResourceCount).toBe(9);
-                    expect(context.player2.exhaustedResourceCount).toBe(0);
-
-                    // check that stolen resource maintained its ready state
-                    const stolenResourceList = context.player1.resources.filter((resource) => resource.owner === context.player2Object);
-                    expect(stolenResourceList.length).toBe(1);
-                    const stolenResource = stolenResourceList[0];
-                    expect(stolenResource.exhausted).toBeFalse();
-
-                    // confirm that player1 can spend with it
-                    context.player2.passAction();
-                    expect(context.player1.readyResourceCount).toBe(4);
-                    context.player1.clickCard(context.strafingGunship);
-                    expect(context.strafingGunship).toBeInZone('spaceArena');
-                    expect(context.player1.exhaustedResourceCount).toBe(11);
-                    expect(stolenResource.exhausted).toBeTrue();
-
-                    // DJ is defeated, resource goes back to owner's resource zone and stays exhausted
-                    context.player2.clickCard(context.atatSuppressor);
-                    context.player2.clickCard(context.dj);
                 });
             });
 
