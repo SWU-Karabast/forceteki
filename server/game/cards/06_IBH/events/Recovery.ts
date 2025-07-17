3abd9a3da4e869c37bc2a003eac110be614b2b68
Valentin Jubert <val_jub@live.fr>
2025-07-17 07:57:41 +0200

    recovery

---
+++ server/game/cards/06_IBH/events/Recovery.ts
@@ -0,0 +1,24 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class Recovery extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '0375794695',
+            internalName: 'recovery',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar) {
+        registrar.setEventAbility({
+            title: 'Heal 5 damage from a unit.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.heal({ amount: 5 }),
+            }
+        });
+    }
+}
+
