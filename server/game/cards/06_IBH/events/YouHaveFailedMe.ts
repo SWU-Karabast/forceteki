5615ac912ae27da7c6a53f63b3b2903f52780162
Valentin Jubert <val_jub@live.fr>
2025-07-18 00:09:41 +0200

    you have failed me

---
+++ server/game/cards/06_IBH/events/YouHaveFailedMe.ts
@@ -0,0 +1,34 @@
+import AbilityHelper from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class YouHaveFailedMe extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '4187779775',
+            internalName: 'you-have-failed-me',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar) {
+        registrar.setEventAbility({
+            title: 'Defeat a friendly unit. If you do, ready a friendly unit with 5 or less power',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                controller: RelativePlayer.Self,
+                immediateEffect: AbilityHelper.immediateEffects.defeat()
+            },
+            ifYouDo: {
+                title: 'Ready a friendly unit with 5 or less power',
+                targetResolver: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    controller: RelativePlayer.Self,
+                    cardCondition: (card) => card.isUnit() && card.getPower() <= 5,
+                    immediateEffect: AbilityHelper.immediateEffects.ready()
+                }
+            }
+        });
+    }
+}
+
