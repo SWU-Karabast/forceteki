d7dd50360db927bcf289d22ca639fb09f52caef7
Robbie Moyer <rmoyer08@gmail.com>
2025-07-28 15:13:26 -0400

    Fix server error with calculated lethality

--- server/game/cards/02_SHD/events/CalculatedLethality.ts
+++ server/game/cards/02_SHD/events/CalculatedLethality.ts
@@ -18,16 +18,21 @@ export default class CalculatedLethality extends EventCard {
                 cardCondition: (card) => card.isNonLeaderUnit() && card.cost <= 3,
                 immediateEffect: AbilityHelper.immediateEffects.defeat(),
             },
-            then: (thenContext) => ({
-                title: 'For each upgrade that was on that unit, give an Experience token to a friendly unit.',
-                thenCondition: () => thenContext.events?.length > 0,
-                immediateEffect: AbilityHelper.immediateEffects.distributeExperienceAmong({
-                    amountToDistribute: thenContext.events[0].lastKnownInformation.upgrades.length,
-                    cardTypeFilter: WildcardCardType.Unit,
-                    controller: RelativePlayer.Self,
-                    canChooseNoTargets: false,
-                })
-            })
+            ifYouDo: (ifYouDoContext) => {
+                const upgradeCount = ifYouDoContext.target.isInPlay()
+                    ? ifYouDoContext.target.upgrades.length
+                    : ifYouDoContext.events[0].lastKnownInformation.upgrades.length;
+
+                return {
+                    title: 'For each upgrade that was on that unit, give an Experience token to a friendly unit.',
+                    immediateEffect: AbilityHelper.immediateEffects.distributeExperienceAmong({
+                        amountToDistribute: upgradeCount,
+                        cardTypeFilter: WildcardCardType.Unit,
+                        controller: RelativePlayer.Self,
+                        canChooseNoTargets: false,
+                    })
+                };
+            }
         });
     }
 }
