25995d32b9c66b24cdfe3ed517400a43239931dc
Robbie Moyer <moyerr@users.noreply.github.com>
2025-07-29 19:18:28 -0400

    Fix server error with Calculated Lethality (#1535)
    Fixes #1534
    
    Calculated Lethality was causing a server error when there were no units
    to target because the `lastKnownInformation` was undefined.
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

--- server/game/cards/02_SHD/events/CalculatedLethality.ts
+++ server/game/cards/02_SHD/events/CalculatedLethality.ts
@@ -18,16 +18,24 @@ export default class CalculatedLethality extends EventCard {
                 cardCondition: (card) => card.isNonLeaderUnit() && card.cost <= 3,
                 immediateEffect: AbilityHelper.immediateEffects.defeat(),
             },
-            then: (thenContext) => ({
-                title: 'For each upgrade that was on that unit, give an Experience token to a friendly unit.',
-                thenCondition: () => thenContext.events?.length > 0,
-                immediateEffect: AbilityHelper.immediateEffects.distributeExperienceAmong({
-                    amountToDistribute: thenContext.events[0].lastKnownInformation.upgrades.length,
-                    cardTypeFilter: WildcardCardType.Unit,
-                    controller: RelativePlayer.Self,
-                    canChooseNoTargets: false,
-                })
-            })
+            then: (thenContext) => {
+                const upgradeCount = thenContext.target
+                    ? (thenContext.target.isInPlay()
+                        ? thenContext.target.upgrades.length
+                        : thenContext.events[0].lastKnownInformation.upgrades.length)
+                    : 0;
+
+                return {
+                    title: 'For each upgrade that was on that unit, give an Experience token to a friendly unit.',
+                    thenCondition: () => thenContext.target,
+                    immediateEffect: AbilityHelper.immediateEffects.distributeExperienceAmong({
+                        amountToDistribute: upgradeCount,
+                        cardTypeFilter: WildcardCardType.Unit,
+                        controller: RelativePlayer.Self,
+                        canChooseNoTargets: false,
+                    })
+                };
+            }
         });
     }
 }
