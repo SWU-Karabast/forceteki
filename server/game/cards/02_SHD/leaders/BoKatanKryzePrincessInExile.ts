87d1b648c8985381fa219bec3e182b1898cd3122
Robbie Moyer <moyerr@users.noreply.github.com>
2025-09-04 16:44:37 -0400

    [Bugfix] Fix issue where Bo-Katan leader's on-attack ability damages herself (#1634)
    Fixes #1631

--- server/game/cards/02_SHD/leaders/BoKatanKryzePrincessInExile.ts
+++ server/game/cards/02_SHD/leaders/BoKatanKryzePrincessInExile.ts
@@ -38,28 +38,24 @@ export default class BoKatanKryzePrincessInExile extends LeaderUnitCard {
 
     protected override setupLeaderUnitSideAbilities(registrar: ILeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
         registrar.addOnAttackAbility({
-            title: 'You may deal 1 damage to a unit. If you attacked with another Mandalorian unit this phase, you may deal 1 damage to a unit',
-            // TODO: correct implementation of the rules for multiple instances of damage in the same ability
-            immediateEffect: AbilityHelper.immediateEffects.sequential([
-                AbilityHelper.immediateEffects.selectCard({
-                    cardTypeFilter: WildcardCardType.Unit,
-                    optional: true,
-                    immediateEffect: AbilityHelper.immediateEffects.damage({
-                        amount: 1
-                    }),
+            title: 'Deal 1 damage to a unit',
+            immediateEffect: AbilityHelper.immediateEffects.selectCard({
+                optional: true,
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 })
+            }),
+            then: {
+                title: 'Deal 1 damage to a unit',
+                thenCondition: (context) => this.attacksThisPhaseWatcher.someUnitAttackedControlledByPlayer({
+                    controller: context.player,
+                    filter: (attack) => context.source !== attack.attacker && attack.attacker.hasSomeTrait(Trait.Mandalorian)
                 }),
-                AbilityHelper.immediateEffects.selectCard({
-                    cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.selectCard({
                     optional: true,
-                    immediateEffect: AbilityHelper.immediateEffects.conditional({
-                        condition: (context) => this.attacksThisPhaseWatcher.someUnitAttackedControlledByPlayer({
-                            controller: context.player,
-                            filter: (attack) => context.source !== attack.attacker && attack.attacker.hasSomeTrait(Trait.Mandalorian)
-                        }),
-                        onTrue: AbilityHelper.immediateEffects.damage({ amount: 1 }),
-                    })
+                    cardTypeFilter: WildcardCardType.Unit,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 })
                 })
-            ])
+            }
         });
     }
 }
