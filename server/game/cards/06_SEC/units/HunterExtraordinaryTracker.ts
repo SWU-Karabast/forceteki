1901679c17f975d1d8cbb511e7c30a52b2f23ec3
Valentin Jubert <val_jub@live.fr>
2025-09-04 07:49:40 +0200

    hunter

---
+++ server/game/cards/06_SEC/units/HunterExtraordinaryTracker.ts
@@ -0,0 +1,28 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class HunterExtraordinaryTracker extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7027608849',
+            internalName: 'hunter#extraordinary-tracker',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addOnAttackAbility({
+            title: 'If the defender is exhausted, it gets –4/–0 for this attack',
+            optional: true,
+            immediateEffect: abilityHelper.immediateEffects.conditional({
+                condition: (context) => {
+                    return context.event.attack.targets.some((x) => x.isUnit() && x.exhausted);
+                },
+                onTrue: abilityHelper.immediateEffects.forThisAttackCardEffect((context) => ({
+                    target: context.event.attack.targets.filter((x) => x.isUnit() && x.exhausted),
+                    effect: abilityHelper.ongoingEffects.modifyStats({ power: -4, hp: -0 })
+                }))
+            }),
+        });
+    }
+}
