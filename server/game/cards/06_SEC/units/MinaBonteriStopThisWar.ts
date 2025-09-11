3195056f7c50ee1b71a345591f27e6a6af56f8c3
Robbie Moyer <moyerr@users.noreply.github.com>
2025-09-11 13:22:03 -0400

    Disclose mechanic and Mina Bonteri (#1640)
    <img
    src="https://github.com/user-attachments/assets/fd3c9cbb-9dfd-4d12-af52-621f29127e50"
    />
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/06_SEC/units/MinaBonteriStopThisWar.ts
@@ -0,0 +1,26 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Aspect } from '../../../core/Constants';
+import * as Helpers from '../../../core/utils/Helpers';
+
+export default class MinaBonteriStopThisWar extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '9378249305',
+            internalName: 'mina-bonteri#stop-this-war',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        const aspects = [Aspect.Command, Aspect.Command, Aspect.Heroism];
+        registrar.addWhenDefeatedAbility({
+            title: `Disclose ${Helpers.aspectString(aspects)} to draw a card`,
+            immediateEffect: AbilityHelper.immediateEffects.disclose({ aspects }),
+            ifYouDo: {
+                title: 'Draw a card',
+                immediateEffect: AbilityHelper.immediateEffects.draw({ amount: 1 })
+            }
+        });
+    }
+}
---
+++ server/game/gameSystems/DiscloseAspectsSystem.ts
@@ -0,0 +1,116 @@
+import type { AbilityContext } from '../core/ability/AbilityContext';
+import type { Card } from '../core/card/Card';
+import type { Aspect, GameStateChangeRequired } from '../core/Constants';
+import { EventName } from '../core/Constants';
+import { RelativePlayer, TargetMode, ZoneName } from '../core/Constants';
+import { RevealSystem } from './RevealSystem';
+import { SelectCardSystem } from './SelectCardSystem';
+import { ViewCardInteractMode } from './ViewCardSystem';
+import * as Helpers from '../core/utils/Helpers';
+import type { GameEvent } from '../core/event/GameEvent';
+import type { IPlayerTargetSystemProperties } from '../core/gameSystem/PlayerTargetSystem';
+import { PlayerTargetSystem } from '../core/gameSystem/PlayerTargetSystem';
+import type { Player } from '../core/Player';
+
+export interface IDiscloseAspectsProperties extends IPlayerTargetSystemProperties {
+    aspects: Aspect[];
+}
+
+export class DiscloseAspectsSystem<TContext extends AbilityContext = AbilityContext> extends PlayerTargetSystem<TContext, IDiscloseAspectsProperties> {
+    public override readonly name = 'aspectsDisclosed';
+    public override readonly eventName = EventName.OnAspectsDisclosed;
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    public eventHandler(): void {}
+
+    public override queueGenerateEventGameSteps(events: GameEvent[], context: TContext, additionalProperties: Partial<IDiscloseAspectsProperties> = {}): void {
+        const generatedEvents = [];
+        for (const target of this.targets(context, additionalProperties)) {
+            if (this.canAffect(target, context, additionalProperties)) {
+                const event = this.generateRetargetedEvent(target, context, additionalProperties);
+                generatedEvents.push(event);
+                events.push(event);
+            }
+        }
+
+        this.generateSelectCardSystem(context, additionalProperties, generatedEvents)
+            .queueGenerateEventGameSteps(events, context);
+    }
+
+    public override defaultTargets(context: TContext): Player[] {
+        return [context.player];
+    }
+
+    public override canAffectInternal(
+        player: Player,
+        context: TContext,
+        additionalProperties?: Partial<IDiscloseAspectsProperties>,
+        mustChangeGameState?: GameStateChangeRequired
+    ): boolean {
+        const newContext = context.copy({ player }) as TContext;
+        const selectCardSystem = this.generateSelectCardSystem(newContext, additionalProperties);
+        return selectCardSystem.hasLegalTarget(newContext, null, mustChangeGameState);
+    }
+
+    protected override addPropertiesToEvent(event: any, player: Player, context: TContext, additionalProperties?: Partial<IDiscloseAspectsProperties>): void {
+        super.addPropertiesToEvent(event, player, context, additionalProperties);
+
+        const { aspects } = this.generatePropertiesFromContext(context, additionalProperties);
+
+        event.aspectsDisclosed = aspects;
+    }
+
+    // Private helpers
+
+    private generateSelectCardSystem(context: TContext, additionalProperties: Partial<IDiscloseAspectsProperties> = {}, cancelEvents = []) {
+        const properties = this.generatePropertiesFromContext(context, additionalProperties);
+
+        return new SelectCardSystem<TContext>({
+            zoneFilter: ZoneName.Hand,
+            controller: RelativePlayer.Self,
+            mode: TargetMode.ExactlyVariable,
+            numCardsFunc: (_context, selectedCards) => this.numberOfCardsToSelect(properties.aspects, selectedCards),
+            cardCondition: (card) => properties.aspects.some((aspect) => card.aspects.includes(aspect)),
+            multiSelectCardCondition: (card, selectedCards, context) =>
+                this.handCanSatisfyAspects(context.player.hand, properties.aspects) &&
+                this.cardContainsMissingAspects(card, selectedCards, properties.aspects),
+            immediateEffect: new RevealSystem<TContext>({
+                activePromptTitle: `Opponent discloses ${Helpers.aspectString(properties.aspects)}`,
+                promptedPlayer: RelativePlayer.Opponent,
+                useDisplayPrompt: true,
+                interactMode: ViewCardInteractMode.ViewOnly
+            }),
+            cancelHandler: cancelEvents ? () => cancelEvents.forEach((event) => event.cancel()) : null,
+            cancelIfNoTargets: true,
+        });
+    }
+
+    private handCanSatisfyAspects(hand: Card[], requiredAspects: Aspect[]): boolean {
+        return this.aspectsMissing(requiredAspects, hand).length === 0;
+    }
+
+    private numberOfCardsToSelect(requiredAspects: Aspect[], selectedCards?: Card[]): number {
+        if (!selectedCards || selectedCards.length === 0) {
+            // Start with arbitrary non-zero number to enable selection
+            return 1;
+        } else if (this.aspectsMissing(requiredAspects, selectedCards).length === 0) {
+            return selectedCards.length;
+        }
+
+        return selectedCards.length + 1;
+    }
+
+    private cardContainsMissingAspects(card: Card, selectedCards: Card[], requiredAspects: Aspect[]): boolean {
+        const missingAspects = this.aspectsMissing(requiredAspects, selectedCards);
+        return card.aspects.some((aspect) => missingAspects.includes(aspect));
+    }
+
+    private aspectsMissing(requiredAspects: Aspect[], cards: Card[]): Aspect[] {
+        const requiredCounts = Helpers.countOccurrences(requiredAspects);
+        const representedCounts = Helpers.countOccurrences(cards.flatMap((card) => card.aspects));
+
+        return Array.from(requiredCounts.entries())
+            .filter(([aspect, requiredCount]) => (representedCounts.get(aspect) || 0) < requiredCount)
+            .map(([aspect]) => aspect);
+    }
+}
