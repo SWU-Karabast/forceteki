e7cefd39508fadfce972b282db6512f82245f596
Tommaso Madonia <tommaso@madonia.me>
2025-09-12 10:23:52 +0100

    Add Mon Mothma

---
+++ server/game/cards/06_SEC/units/MonMothmaClingingToHope.ts
@@ -0,0 +1,43 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { TriggeredAbilityContext } from '../../../core/ability/TriggeredAbilityContext';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import type { Card } from '../../../core/card/Card';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+import type { IThenAbilityPropsWithSystems } from '../../../Interfaces';
+
+export default class MonMothmaClingingToHope extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '1347170274',
+            internalName: 'mon-mothma#clinging-to-hope',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Attack with any number of other units, even if those units are exhausted. They can\'t attack bases for these attacks',
+            ...this.attackWithUnitAbility([], AbilityHelper),
+        });
+    }
+
+    private attackWithUnitAbility(chosenCards: Card[], AbilityHelper: IAbilityHelper): Omit<IThenAbilityPropsWithSystems<TriggeredAbilityContext<NonLeaderUnitCard>>, 'title'> {
+        return {
+            optional: true,
+            targetResolver: {
+                activePromptTitle: 'Attack with a unit even if it is exhausted. It can\'t attack bases for these attacks',
+                controller: RelativePlayer.Self,
+                cardTypeFilter: WildcardCardType.Unit,
+                cardCondition: (card, context) => card !== context.source && !chosenCards.includes(card),
+                immediateEffect: AbilityHelper.immediateEffects.attack({
+                    targetCondition: (card) => !card.isBase(),
+                    allowExhaustedAttacker: true,
+                })
+            },
+            then: (context) => ({
+                title: 'Attack with a unit even if it is exhausted. It can\'t attack bases for these attacks',
+                ...this.attackWithUnitAbility([...chosenCards, context.target], AbilityHelper),
+            }),
+        };
+    }
+}
---
+++ test/server/cards/06_SEC/units/MonMothmaClingingToHope.spec.ts
@@ -0,0 +1,73 @@
+describe('Mon Mothma, Clinging to Hope', function () {
+    integration(function (contextRef) {
+        describe('Mon Mothma\'s ability', function () {
+            it('should allow attacking with any number of exhausted units, one at the time', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['mon-mothma#clinging-to-hope'],
+                        groundArena: ['atst', { card: 'wampa', exhausted: true }],
+                        spaceArena: ['tieln-fighter'],
+                        leader: { card: 'the-mandalorian#sworn-to-the-creed', deployed: true }
+                    },
+                    player2: {
+                        groundArena: ['consular-security-force'],
+                    },
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.monMothma);
+
+                // Attack with Wampa
+                expect(context.player1).toBeAbleToSelectExactly([context.atst, context.theMandalorian, context.wampa]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickCard(context.wampa);
+                expect(context.player1).toBeAbleToSelectExactly([context.consularSecurityForce]);
+                context.player1.clickCard(context.consularSecurityForce);
+                expect(context.wampa.exhausted).toBe(true);
+                expect(context.consularSecurityForce.damage).toBe(4);
+                expect(context.wampa.damage).toBe(3);
+
+                // Attack with The Mandalorian
+                expect(context.player1).toBeAbleToSelectExactly([context.atst, context.theMandalorian]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickCard(context.theMandalorian);
+                expect(context.player1).toBeAbleToSelectExactly([context.consularSecurityForce]);
+                context.player1.clickCard(context.consularSecurityForce);
+                expect(context.theMandalorian.exhausted).toBe(true);
+                expect(context.consularSecurityForce).toBeInZone('discard');
+                expect(context.theMandalorian.damage).toBe(3);
+
+                // AT-ST has no valid target to attack
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('can pass the ability', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['mon-mothma#clinging-to-hope'],
+                        groundArena: ['atst', { card: 'wampa', exhausted: true }],
+                        spaceArena: ['tieln-fighter'],
+                        leader: { card: 'the-mandalorian#sworn-to-the-creed', deployed: true }
+                    },
+                    player2: {
+                        groundArena: ['consular-security-force'],
+                    },
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.monMothma);
+
+                expect(context.player1).toHavePrompt('Attack with a unit even if it is exhausted. It can\'t attack bases for these attacks');
+                expect(context.player1).toBeAbleToSelectExactly([context.atst, context.theMandalorian, context.wampa]);
+                expect(context.player1).toHavePassAbilityButton();
+                context.player1.clickPrompt('Pass');
+
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
