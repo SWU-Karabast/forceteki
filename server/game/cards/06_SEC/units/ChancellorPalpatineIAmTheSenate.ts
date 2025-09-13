0f7eee6352b3e62b7904dd02b6ecef35e875ea0c
Valentin Jubert <val_jub@live.fr>
2025-09-13 17:39:22 +0200

    sly moore, sly moore and chancellor palpatine (#1670)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/SEC/014.png)
    
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/SEC/014-back.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/SEC/033.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/SEC/082.png)

---
+++ server/game/cards/06_SEC/units/ChancellorPalpatineIAmTheSenate.ts
@@ -0,0 +1,32 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { CardType, KeywordName } from '../../../core/Constants';
+
+export default class ChancellorPalpatineIAmTheSenate extends NonLeaderUnitCard {
+    protected override getImplementationId () {
+        return {
+            id: '7936097828',
+            internalName: 'chancellor-palpatine#i-am-the-senate',
+        };
+    }
+
+    public override setupCardAbilities (registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Create 2 Spy tokens and give those tokens Sentinel for this phase',
+            immediateEffect: abilityHelper.immediateEffects.conditional({
+                condition: (context) => context.player.hasSomeArenaCard({ type: CardType.LeaderUnit }),
+                onTrue: abilityHelper.immediateEffects.createSpy({ amount: 2 }),
+            }),
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'Give those tokens Sentinel for this phase',
+                immediateEffect: abilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    target: ifYouDoContext.events[0].generatedTokens,
+                    effect: abilityHelper.ongoingEffects.gainKeyword({
+                        keyword: KeywordName.Sentinel,
+                    })
+                })
+            }),
+        });
+    }
+}
