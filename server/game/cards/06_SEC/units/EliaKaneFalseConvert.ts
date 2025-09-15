458057b41975f1d75eb6e8ef811d9087f0f52a05
Robbie Moyer <rmoyer08@gmail.com>
2025-09-15 16:21:21 -0400

    Improve logging and add test cases for edge cases

--- server/game/cards/06_SEC/units/EliaKaneFalseConvert.ts
+++ server/game/cards/06_SEC/units/EliaKaneFalseConvert.ts
@@ -1,5 +1,7 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { AbilityContext } from '../../../core/ability/AbilityContext';
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import type { IInPlayCard } from '../../../core/card/baseClasses/InPlayCard';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { RelativePlayer } from '../../../core/Constants';
 import * as Helpers from '../../../core/utils/Helpers';
@@ -17,7 +19,7 @@ export default class EliaKaneFalseConvert extends NonLeaderUnitCard {
             title: 'Look at 3 enemy resources',
             immediateEffect: AbilityHelper.immediateEffects.randomSelection((context) => ({
                 target: context.player.opponent.resources,
-                count: 3,
+                count: Math.min(3, context.player.opponent.resources.length),
                 innerSystem: AbilityHelper.immediateEffects.lookAtAndSelectCard((lookAtContext) => ({
                     activePromptTitle: 'Select an enemy resource to defeat. If you do, its controller puts the top card of their deck into play as a resource and readies it.',
                     displayTextByCardUuid: new Map(
@@ -25,15 +27,41 @@ export default class EliaKaneFalseConvert extends NonLeaderUnitCard {
                             .map((card) => [card.uuid, card.exhausted ? 'Exhausted' : 'Ready'])
                     ),
                     immediateEffect: AbilityHelper.immediateEffects.simultaneous([
+                        AbilityHelper.immediateEffects.handler({
+                            handler: (handlerContext) => this.customLogMessage(handlerContext)
+                        }),
                         AbilityHelper.immediateEffects.defeat(),
                         AbilityHelper.immediateEffects.resourceCard({
                             targetPlayer: RelativePlayer.Opponent,
                             target: context.player.opponent.getTopCardOfDeck(),
-                            readyResource: true
-                        })
+                            readyResource: true,
+                        }),
                     ])
                 }))
             }))
         });
+    }
+
+    private customLogMessage(context: AbilityContext) {
+        if (context.selectedPromptCards.length === 0) {
+            return;
+        }
+
+        const selectedCard = context.selectedPromptCards[0] as IInPlayCard;
+
+        context.game.addMessage('{0} uses {1} to defeat {2} {3} from {4}\'s resources.',
+            context.player,
+            context.source,
+            selectedCard.exhausted ? 'an exhausted' : 'a ready',
+            selectedCard,
+            context.player.opponent
+        );
+
+        if (context.player.opponent.getTopCardOfDeck()) {
+            context.game.addMessage('{0} puts the top card of their deck into play as a resource and readies it, due to {1}.',
+                context.player.opponent,
+                context.source
+            );
+        }
     }
 }
--- test/server/cards/06_SEC/units/EliaKaneFalseConvert.spec.ts
+++ test/server/cards/06_SEC/units/EliaKaneFalseConvert.spec.ts
@@ -109,5 +109,85 @@ describe('Elia Kane, False Convert', function() {
                 expect(context.player2.exhaustedResourceCount).toEqual(2);
             });
         });
+
+        describe('Edge cases', function() {
+            it('When the opponent has less than 3 resources, they can only be shown those resources', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['elia-kane#false-convert'],
+                    },
+                    player2: {
+                        deck: ['pyke-sentinel'],
+                        resources: [
+                            'restock',
+                            { card: 'cad-bane#impressed-now', exhausted: true }
+                        ]
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.eliaKane);
+                expect(context.player1).toHaveExactSelectableDisplayPromptCards([
+                    { card: context.restock, displayText: 'Ready' },
+                    { card: context.cadBane, displayText: 'Exhausted' }
+                ]);
+
+                context.player1.clickCardInDisplayCardPrompt(context.restock);
+
+                // Cards were moved to the correct zones
+                expect(context.restock).toBeInZone('discard', context.player2);
+                expect(context.pykeSentinel).toBeInZone('resource', context.player2);
+
+                // Resources are in the correct state
+                expect(context.pykeSentinel.exhausted).toBeFalse();
+                expect(context.player2.readyResourceCount).toEqual(1);
+                expect(context.player2.exhaustedResourceCount).toEqual(1);
+            });
+
+            // TODO: There isn't a good way to test 0 resources since the test setup will prompt the player to
+            // select 2 cards to resource if they have none. This scenario is wildly unlikely to happen in a
+            // real game anyway.
+            // it('When the opponent has 0 resources, Elia Kane\'s ability does nothing', async function() {});
+
+            it('When the opponent has no cards in their deck, they cannot put a card into play as a resource', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['elia-kane#false-convert'],
+                    },
+                    player2: {
+                        deck: [],
+                        resources: [
+                            'atst',
+                            'restock',
+                            { card: 'cad-bane#impressed-now', exhausted: true }
+                        ]
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.eliaKane);
+                expect(context.player1).toHaveExactSelectableDisplayPromptCards([
+                    { card: context.atst, displayText: 'Ready' },
+                    { card: context.restock, displayText: 'Ready' },
+                    { card: context.cadBane, displayText: 'Exhausted' }
+                ]);
+
+                context.player1.clickCardInDisplayCardPrompt(context.atst);
+
+                // Cards were moved to the correct zones
+                expect(context.atst).toBeInZone('discard', context.player2);
+
+                // No resource was put into play
+                expect(context.player2.resources.length).toEqual(2);
+
+                // Resources are in the correct state
+                expect(context.player2.readyResourceCount).toEqual(1);
+                expect(context.player2.exhaustedResourceCount).toEqual(1);
+            });
+        });
     });
 });
