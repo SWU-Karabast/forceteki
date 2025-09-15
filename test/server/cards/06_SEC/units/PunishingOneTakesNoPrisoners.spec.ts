d4053eb2914d67e24295c8b7276a2063ce1ce6f0
Valentin Jubert <val_jub@live.fr>
2025-09-15 22:06:58 +0200

    pr comments

--- test/server/cards/06_SEC/units/PunishingOneTakesNoPrisoners.spec.ts
+++ test/server/cards/06_SEC/units/PunishingOneTakesNoPrisoners.spec.ts
@@ -26,14 +26,37 @@ describe('Punishing One, Takes No Prisoners', function() {
             expect(context.wampa.damage).toBe(1);
         });
 
-        it('Punishing One\'s on attack should optionally deal 1 damage to a unit and gain Raid 1 for each damaged enemy unit', async function () {
+        it('Punishing One\'s on attack should optionally deal 1 damage to a unit', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    spaceArena: ['system-patrol-craft', 'punishing-one#takes-no-prisoners']
+                },
+                player2: {
+                    groundArena: ['wampa'],
+                    spaceArena: ['green-squadron-awing']
+                }
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.punishingOne);
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.player1).toBeAbleToSelectExactly([context.systemPatrolCraft, context.punishingOne, context.wampa, context.greenSquadronAwing]);
+            expect(context.player1).toHavePassAbilityButton();
+
+            context.player1.clickCard(context.wampa);
+            expect(context.wampa.damage).toBe(1);
+        });
+
+        it('Punishing One\'s should gain Raid 1 for each damaged enemy unit', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
                     spaceArena: [{ card: 'system-patrol-craft', damage: 1 }, { card: 'punishing-one#takes-no-prisoners', damage: 1 }]
                 },
                 player2: {
-                    groundArena: ['wampa', 'battlefield-marine'],
+                    groundArena: [{ card: 'wampa', damage: 1 }],
                     spaceArena: [{ card: 'green-squadron-awing', damage: 1 }]
                 }
             });
@@ -42,13 +65,33 @@ describe('Punishing One, Takes No Prisoners', function() {
             context.player1.clickCard(context.punishingOne);
             context.player1.clickCard(context.p2Base);
 
-            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.systemPatrolCraft, context.punishingOne, context.wampa, context.greenSquadronAwing]);
-            expect(context.player1).toHavePassAbilityButton();
-
+            // 1 damage from on attack ability
             context.player1.clickCard(context.wampa);
-            expect(context.wampa.damage).toBe(1);
 
             expect(context.p2Base.damage).toBe(5); // 3 + raid 2
+        });
+
+        it('Punishing One\'s should gain Raid 1 for each damaged enemy unit (with Marchion Ro)', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['marchion-ro#eye-of-the-nihil'],
+                    spaceArena: ['punishing-one#takes-no-prisoners']
+                },
+                player2: {
+                    groundArena: [{ card: 'wampa', damage: 1 }],
+                    spaceArena: [{ card: 'green-squadron-awing', damage: 1 }]
+                }
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.punishingOne);
+            context.player1.clickCard(context.p2Base);
+
+            // 1 damage from on attack ability
+            context.player1.clickCard(context.wampa);
+
+            expect(context.p2Base.damage).toBe(7); // 3 + raid 4
         });
     });
 });
