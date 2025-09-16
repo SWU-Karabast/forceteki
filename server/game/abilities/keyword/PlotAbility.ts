17b3f1cbd2e669d91cf6d7c970dde8f3928a84cd
Anthony Bronico <adbronico92@gmail.com>
2025-09-16 15:23:25 -0400

    Use an ifYouDo

--- server/game/abilities/keyword/PlotAbility.ts
+++ server/game/abilities/keyword/PlotAbility.ts
@@ -3,10 +3,8 @@ import type { Card } from '../../core/card/Card';
 import { KeywordName, PlayType, WildcardCardType, ZoneName } from '../../core/Constants';
 import type Game from '../../core/Game';
 import * as Contract from '../../core/utils/Contract';
-import { ConditionalSystem } from '../../gameSystems/ConditionalSystem';
 import { PlayCardSystem } from '../../gameSystems/PlayCardSystem';
 import { ResourceCardSystem } from '../../gameSystems/ResourceCardSystem';
-import { SequentialSystem } from '../../gameSystems/SequentialSystem';
 import type { ITriggeredAbilityProps } from '../../Interfaces';
 
 export class PlotAbility extends TriggeredAbility {
@@ -20,22 +18,18 @@ export class PlotAbility extends TriggeredAbility {
                 onLeaderDeployed: (event, context) => event.card.owner === context.source.controller && context.source.zoneName === ZoneName.Resource // TODO See if we can remove this zone check once we update Plot registration
             },
             zoneFilter: ZoneName.Resource,
-            immediateEffect: new SequentialSystem((context) => ({
-                gameSystems: [
-                    new PlayCardSystem({
-                        canPlayFromAnyZone: true,
-                        playType: PlayType.Plot,
-                        playAsType: WildcardCardType.Any,
-                        target: context.source
-                    }),
-                    new ConditionalSystem((conditionalContext) => ({
-                        condition: () => context.source.zoneName !== ZoneName.Resource,
-                        onTrue: new ResourceCardSystem({
-                            target: context.player.getTopCardOfDeck()
-                        })
-                    }))
-                ]
-            }))
+            immediateEffect: new PlayCardSystem((context) => ({
+                canPlayFromAnyZone: true,
+                playType: PlayType.Plot,
+                playAsType: WildcardCardType.Any,
+                target: context.source
+            })),
+            ifYouDo: {
+                title: 'Resource the top card of your deck',
+                immediateEffect: new ResourceCardSystem((context) => ({
+                    target: context.player.getTopCardOfDeck()
+                }))
+            }
         };
     }
 
