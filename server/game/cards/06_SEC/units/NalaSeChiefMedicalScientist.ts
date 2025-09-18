f4b1b5eea3c6257b64bb70a68c231ecd33970586
sethhelman <s7helman@gmail.com>
2025-09-18 10:51:36 -0500

    Colonel Yularen, Nala Se, Crosshair Implementations (#1709)
    <img width="300" height="418" alt="crosshair"
    src="https://github.com/user-attachments/assets/2ac80a8d-d377-4fde-a7b7-9f2d85985a2d"
    />
    <img width="1255" height="1749" alt="Nala Se"
    src="https://github.com/user-attachments/assets/39b980bf-e0f3-41ff-a219-8ffd25f601f3"
    />
    <img width="438" height="614" alt="yularen back"
    src="https://github.com/user-attachments/assets/2f739af7-693c-4fff-aebe-192c4cc66d00"
    />
    <img width="535" height="383" alt="Yularen front"
    src="https://github.com/user-attachments/assets/7a2df927-3007-4de5-a336-19f2786f4dcc"
    />

---
+++ server/game/cards/06_SEC/units/NalaSeChiefMedicalScientist.ts
@@ -0,0 +1,32 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Aspect, WildcardRelativePlayer } from '../../../core/Constants';
+import * as Helpers from '../../../core/utils/Helpers';
+
+export default class NalaSeChiefMedicalScientist extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'nala-se#chief-medical-scientist-id',
+            internalName: 'nala-se#chief-medical-scientist',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        const aspects = [Aspect.Vigilance, Aspect.Vigilance];
+        registrar.addOnAttackAbility({
+            title: `Disclose ${Helpers.aspectString(aspects)}. If you do, heal up to 4 damage from among other units`,
+            immediateEffect: AbilityHelper.immediateEffects.disclose({ aspects }),
+            ifYouDo: {
+                title: 'Heal up to 4 damage from among other units',
+                immediateEffect: AbilityHelper.immediateEffects.distributeHealingAmong({
+                    amountToDistribute: 4,
+                    controller: WildcardRelativePlayer.Any,
+                    canChooseNoTargets: true,
+                    canDistributeLess: true,
+                    cardCondition: (card, context) => card.isUnit() && card !== context.source,
+                }),
+            }
+        });
+    }
+}
