2fe82744f5ebcdfe101060504b7a7394f08e00f2
Anthony Bronico <adbronico92@gmail.com>
2025-09-21 11:59:46 -0400

    Fix a bug with Sly Moore's lasting effect (#1722)

--- test/server/cards/06_SEC/units/SlyMooreWitnessToPower.spec.ts
+++ test/server/cards/06_SEC/units/SlyMooreWitnessToPower.spec.ts
@@ -1,6 +1,6 @@
 describe('Sly Moore, Witness to Power', function() {
     integration(function(contextRef) {
-        it('for this phase, each enemy unit gets -2/-0 while it\'s attacking a base', async function () {
+        it('should apply: for this phase, each enemy unit gets -2/-0 while it\'s attacking a base', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
@@ -52,6 +52,76 @@ describe('Sly Moore, Witness to Power', function() {
             context.player2.clickCard(context.p1Base);
 
             expect(context.p1Base.damage).toBe(4);
+        });
+
+        it('should debuff enemy units even if Sly Moore is defeated', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['sly-moore#witness-to-power'],
+                    groundArena: ['consular-security-force']
+                },
+                player2: {
+                    hand: ['vanquish'],
+                    groundArena: ['wampa', 'atst'],
+                    spaceArena: ['green-squadron-awing']
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Play Sly Moore, applying the phase-long effect
+            context.player1.clickCard(context.slyMoore);
+
+            context.player2.clickCard(context.vanquish);
+            context.player2.clickCard(context.slyMoore);
+            context.player1.passAction();
+
+            // Opponent attacks base with Wampa (normally 4 power) => should be reduced by 2
+            context.player2.clickCard(context.wampa);
+            context.player2.clickCard(context.p1Base);
+            expect(context.p1Base.damage).toBe(2);
+
+            // move to next phase, lasting effect should end
+            context.moveToNextActionPhase();
+
+            context.setDamage(context.p1Base, 0);
+
+            context.player1.passAction();
+
+            context.player2.clickCard(context.wampa);
+            context.player2.clickCard(context.p1Base);
+
+            expect(context.p1Base.damage).toBe(4);
+        });
+
+        it('should not affect units played/deployed after Sly Moore', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['sly-moore#witness-to-power'],
+                    groundArena: ['consular-security-force']
+                },
+                player2: {
+                    leader: 'sabine-wren#galvanized-revolutionary',
+                    groundArena: ['wampa', 'atst'],
+                    spaceArena: ['green-squadron-awing']
+                }
+            });
+
+            const { context } = contextRef;
+
+            // Play Sly Moore, applying the phase-long effect
+            context.player1.clickCard(context.slyMoore);
+
+            context.player2.clickCard(context.sabineWren);
+            context.player2.clickPrompt('Deploy Sabine Wren');
+
+            context.player1.passAction();
+
+            context.player2.clickCard(context.sabineWren);
+            context.player2.clickCard(context.p1Base);
+            expect(context.p1Base.damage).toBe(3);
         });
     });
 });
