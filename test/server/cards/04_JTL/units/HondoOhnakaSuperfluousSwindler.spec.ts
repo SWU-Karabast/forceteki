774755ad99b0df8900a98f126c97027294b0f302
Robbie Moyer <rmoyer08@gmail.com>
2025-09-22 23:31:09 -0400

    Add test for upgrade with attach condition that cannot be met

--- test/server/cards/04_JTL/units/HondoOhnakaSuperfluousSwindler.spec.ts
+++ test/server/cards/04_JTL/units/HondoOhnakaSuperfluousSwindler.spec.ts
@@ -114,5 +114,30 @@ describe('Hondo Ohnaka, Superfluous Swindler', function() {
             expect(context.hondoOhnaka).toHaveExactUpgradeNames(['craving-power']);
             expect(context.craftySmuggler).toHaveExactUpgradeNames(['entrenched']);
         });
+
+        it('cannot be used to attach an upgrade to a different unit if no other units are eligible', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['hondo-ohnaka#superfluous-swindler']
+                },
+                player2: {
+                    groundArena: [
+                        {
+                            card: 'secretive-sage',
+                            upgrades: ['constructed-lightsaber']
+                        }
+                    ]
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.hondoOhnaka);
+            context.player1.clickCard(context.secretiveSage);
+
+            // The ability is automatically skipped because there are no other Force units to attach Constructed Lightsaber to
+            expect(context.player2).toBeActivePlayer();
+        });
     });
 });
