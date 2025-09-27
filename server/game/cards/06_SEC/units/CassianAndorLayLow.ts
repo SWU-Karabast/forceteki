72bd6e2e20bdfd8e80aee74f27afd271d784981b
sethhelman <s7helman@gmail.com>
2025-09-26 23:28:25 -0400

    junior senator, corporate war mongering, cassian andor, various mock data fixes

---
+++ server/game/cards/06_SEC/units/CassianAndorLayLow.ts
@@ -0,0 +1,43 @@
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import { DamageType, RelativePlayer } from '../../../core/Constants';
+import { DamageSystem } from '../../../gameSystems/DamageSystem';
+import { DamageSourceType } from '../../../IDamageOrDefeatSource';
+
+export default class CassianAndorLayLow extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'cassian-andor#lay-low-id',
+            internalName: 'cassian-andor#lay-low',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addConstantAbility({
+            title: 'While this unit is defending, the attacker gets -2/-0',
+            targetController: RelativePlayer.Opponent,
+            matchTarget: (card, context) => card.isUnit() && card.isInPlay() && card.isAttacking() && context.source.isDefending(),
+            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: -2, hp: 0 })
+
+        });
+
+        registrar.addReplacementEffectAbility({
+            title: 'If an enemy card ability would do damage to this unit, prevent 2 of that damage',
+            when: { onDamageDealt: (event, context) =>
+                event.card === context.source &&
+                !event.isIndirect && event.damageSource.type !== DamageSourceType.Attack && event.damageSource.player !== context.source.controller },
+
+            replaceWith: {
+                replacementImmediateEffect: new DamageSystem((context) => ({
+                    target: context.source,
+                    amount: Math.max(context.event.amount - 2, 0),
+                    source: context.event.damageSource.type === DamageType.Ability ? context.event.damageSource.card : context.event.damageSource.damageDealtBy.Opponent,
+                    type: context.event.type,
+                }))
+            },
+            effect: 'prevent 2 damage to {1}',
+            effectArgs: (context) => [context.source]
+        });
+    }
+}
---
+++ test/server/cards/06_SEC/units/CassianAndorLayLow.spec.ts
@@ -0,0 +1,78 @@
+describe('Cassian Andor, Lay Low', function() {
+    integration(function(contextRef) {
+        describe('Cassian Andor, Lay Low\'s ability', function() {
+            beforeEach(function () {
+                return contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['blood-sport', 'open-fire'],
+                        groundArena: ['battlefield-marine', 'consular-security-force']
+                    },
+                    player2: {
+                        hand: ['daring-raid'],
+                        groundArena: ['resourceful-pursuers', 'cargo-juggernaut', 'cassian-andor#lay-low']
+                    }
+                });
+            });
+
+            it('should give -2/-0 to the attacker while Cassian is defending', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.cassianAndorLayLow);
+
+                expect(context.cassianAndorLayLow.damage).toBe(1);
+                expect(context.battlefieldMarine.damage).toBe(2);
+
+                // Should not give Cassian or defender -2/-0 when he attacks
+                context.player2.clickCard(context.cassianAndorLayLow);
+                context.player2.clickCard(context.battlefieldMarine);
+
+                expect(context.cassianAndorLayLow).toBeInZone('discard');
+                expect(context.battlefieldMarine).toBeInZone('discard');
+            });
+
+            it('should not give -2/-0 to the attacker while attacking one of Cassian\'s friendly units', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.battlefieldMarine);
+                context.player1.clickCard(context.resourcefulPursuers);
+
+                expect(context.resourcefulPursuers.damage).toBe(3);
+                expect(context.battlefieldMarine).toBeInZone('discard');
+            });
+
+            it('should prevent 2 damage from enemy card abilities', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.bloodSport);
+
+                expect(context.resourcefulPursuers.damage).toBe(2);
+                expect(context.battlefieldMarine.damage).toBe(2);
+                expect(context.consularSecurityForce.damage).toBe(2);
+                expect(context.cargoJuggernaut.isUpgraded()).toBeFalse();
+                expect(context.cassianAndorLayLow.damage).toBe(0);
+            });
+
+            it('should prevent only 2 damage from enemy card abilities', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.openFire);
+                context.player1.clickCard(context.cassianAndorLayLow);
+
+                expect(context.cassianAndor).toBeInZone('discard');
+            });
+
+            it('should not prevent damage from friendly card abilities', function () {
+                const { context } = contextRef;
+
+                context.player1.clickPrompt('Pass');
+
+                context.player2.clickCard(context.daringRaid);
+                context.player2.clickCard(context.cassianAndorLayLow);
+
+                expect(context.cassianAndor).toBeInZone('discard');
+            });
+        });
+    });
+});
