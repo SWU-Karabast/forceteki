386ea0f721d088900dd0bf1ff036eeda66a4fc29
Robbie Moyer <rmoyer08@gmail.com>
2025-10-01 14:08:44 -0400

    Update UnauthorizedInvestigation post merge

--- server/game/cards/06_SEC/events/UnauthorizedInvestigation.ts
+++ server/game/cards/06_SEC/events/UnauthorizedInvestigation.ts
@@ -2,7 +2,7 @@ import type { IAbilityHelper } from '../../../AbilityHelper';
 import { EventCard } from '../../../core/card/EventCard';
 import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { Aspect } from '../../../core/Constants';
-import * as Helpers from '../../../core/utils/Helpers';
+import * as EnumHelpers from '../../../core/utils/EnumHelpers';
 
 export default class UnauthorizedInvestigation extends EventCard {
     protected override getImplementationId () {
@@ -15,10 +15,10 @@ export default class UnauthorizedInvestigation extends EventCard {
     public override setupCardAbilities (registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
         const aspects = [Aspect.Aggression];
         registrar.setEventAbility({
-            title: `Create a Spy token. You may disclose ${Helpers.aspectString(aspects)}. If you do, create another Spy token`,
+            title: `Create a Spy token. You may disclose ${EnumHelpers.aspectString(aspects)}. If you do, create another Spy token`,
             immediateEffect: abilityHelper.immediateEffects.createSpy(),
             then: ({
-                title: `Disclose ${Helpers.aspectString(aspects)} to create another Spy token`,
+                title: `Disclose ${EnumHelpers.aspectString(aspects)} to create another Spy token`,
                 immediateEffect: abilityHelper.immediateEffects.disclose({ aspects }),
                 ifYouDo: {
                     title: 'Create another Spy token',
