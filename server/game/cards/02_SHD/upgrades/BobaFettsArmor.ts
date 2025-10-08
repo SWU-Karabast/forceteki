eb95f04006e2b369fdb989a126b4fda8551d90ac
Robbie Moyer <moyerr@users.noreply.github.com>
2025-10-08 15:30:15 -0400

    Add an IAttachCardContext to pass more information to setAttachCondition() (#1811)
    This fixes a long-standing discrepancy in implementation between cards
    that have "friendly" attach restrictions, and those that do not.
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

--- server/game/cards/02_SHD/upgrades/BobaFettsArmor.ts
+++ server/game/cards/02_SHD/upgrades/BobaFettsArmor.ts
@@ -1,7 +1,6 @@
 import type { IUpgradeAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { UpgradeCard } from '../../../core/card/UpgradeCard';
 import { DamageType, Trait } from '../../../core/Constants';
-import type { Card } from '../../../core/card/Card';
 import { DamageSystem } from '../../../gameSystems/DamageSystem';
 import type { IAbilityHelper } from '../../../AbilityHelper';
 
@@ -14,7 +13,7 @@ export default class BobaFettsArmor extends UpgradeCard {
     }
 
     public override setupCardAbilities(registrar: IUpgradeAbilityRegistrar, AbilityHelper: IAbilityHelper) {
-        registrar.setAttachCondition((card: Card) => !card.hasSomeTrait(Trait.Vehicle));
+        registrar.setAttachCondition((context) => !context.attachTarget.hasSomeTrait(Trait.Vehicle));
 
         registrar.addReplacementEffectAbility({
             title: 'If attached unit is Boba Fett and damage would be dealt to him, prevent 2 of that damage',
--- server/game/core/card/AbilityRegistrationInterfaces.ts
+++ server/game/core/card/AbilityRegistrationInterfaces.ts
@@ -1,4 +1,4 @@
-import type { IAbilityPropsWithType, IActionAbilityPropsWithGainCondition, IConstantAbilityProps, IConstantAbilityPropsWithGainCondition, IEpicActionProps, IEventAbilityProps, IKeywordPropertiesWithGainCondition, IPlayRestrictionAbilityProps, IReplacementEffectAbilityPropsWithGainCondition, ITriggeredAbilityBaseProps, ITriggeredAbilityBasePropsWithGainCondition, ITriggeredAbilityPropsWithGainCondition } from '../../Interfaces';
+import type { IAbilityPropsWithType, IActionAbilityPropsWithGainCondition, IAttachCardContext, IConstantAbilityProps, IConstantAbilityPropsWithGainCondition, IEpicActionProps, IEventAbilityProps, IKeywordPropertiesWithGainCondition, IPlayRestrictionAbilityProps, IReplacementEffectAbilityPropsWithGainCondition, ITriggeredAbilityBaseProps, ITriggeredAbilityBasePropsWithGainCondition, ITriggeredAbilityPropsWithGainCondition } from '../../Interfaces';
 import type { BaseCard } from './BaseCard';
 import type { IDecreaseCostAbilityProps, IIgnoreAllAspectPenaltiesProps, IIgnoreSpecificAspectPenaltyProps } from './baseClasses/PlayableOrDeployableCard';
 import type { Card } from './Card';
@@ -51,7 +51,7 @@ export type IUpgradeAbilityRegistrar = IBasicAbilityRegistrar<UpgradeCard> &
       addGainOnAttackAbilityTargetingAttached(properties: ITriggeredAbilityBasePropsWithGainCondition<UpgradeCard, IUnitCard>): void;
       addGainWhenDefeatedAbilityTargetingAttached(properties: ITriggeredAbilityBasePropsWithGainCondition<UpgradeCard, IUnitCard>): void;
       addGainKeywordTargetingAttached(properties: IKeywordPropertiesWithGainCondition<UpgradeCard>): void;
-      setAttachCondition(attachCondition: (card: Card) => boolean): void;
+      setAttachCondition(attachCondition: (context: IAttachCardContext<UpgradeCard>) => boolean): void;
   };
 
 export type IBaseAbilityRegistrar = IBasicAbilityRegistrar<BaseCard> & {
