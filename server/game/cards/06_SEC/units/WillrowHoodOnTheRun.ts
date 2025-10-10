f6674c62082d2aa600feee13daa456e38e9c8df2
cbybee <cbybee317@gmail.com>
2025-10-10 12:32:16 -0500

    Fix Willrow Hood logic
    now handles cases where there is an enemy upgrade on it correcty. Only checks for friendly upgrades to determine if they should be defeated/bounced

--- server/game/cards/06_SEC/units/WillrowHoodOnTheRun.ts
+++ server/game/cards/06_SEC/units/WillrowHoodOnTheRun.ts
@@ -1,5 +1,6 @@
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { ZoneName } from '../../../core/Constants';
 import { DefeatSourceType } from '../../../IDamageOrDefeatSource';
 
 export default class WillrowHoodOnTheRun extends NonLeaderUnitCard {
@@ -15,14 +16,16 @@ export default class WillrowHoodOnTheRun extends NonLeaderUnitCard {
             title: 'Upgrade cannot be defeated or returned to hand by enemy card abilities',
             when: {
                 onCardDefeated: (event, context) =>
-                    event.card === context.source.upgrades[0] &&
-                    context.source.upgrades.length === 1 &&
+                    context.source.upgrades.filter((upgrade) => upgrade.owner === context.player).length === 1 &&
+                    context.source.upgrades.includes(event.card) &&
+                    event.card.owner === context.player &&
                     event.defeatSource.type === DefeatSourceType.Ability &&
                     event.defeatSource.player !== context.player,
                 onCardMoved: (event, context) =>
-                    event.card === context.source.upgrades[0] &&
-                    context.source.upgrades.length === 1 &&
-                    event.destination === 'hand' &&
+                    context.source.upgrades.filter((upgrade) => upgrade.owner === context.player).length === 1 &&
+                    event.card.owner === context.player &&
+                    context.source.upgrades.includes(event.card) &&
+                    event.destination === ZoneName.Hand &&
                     event.context.player !== context.player
             }
         });
