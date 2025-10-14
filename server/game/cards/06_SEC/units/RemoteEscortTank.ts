732c49d0489d19d8df3bd44c2c2a468496f4ca91
dday512 <80212190+dday512@users.noreply.github.com>
2025-10-14 10:38:34 -0400

    Adding Cards (Remote Escort Tank, Inner Rim Coalition, & Implicate) (#1825)
    <img width="716" height="954" alt="image"
    src="https://github.com/user-attachments/assets/d9201e42-b7f7-4895-8558-9ad94ad1dfd3"
    />
    <img width="716" height="954" alt="image2"
    src="https://github.com/user-attachments/assets/3aa4e54c-83f9-4451-a6de-64a22dceb515"
    />
    <img width="716" height="954" alt="255"
    src="https://github.com/user-attachments/assets/d1659827-7d96-4565-8e99-582c30eacf94"
    />
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/06_SEC/units/RemoteEscortTank.ts
@@ -0,0 +1,25 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { KeywordName, WildcardCardType } from '../../../core/Constants';
+
+export default class RemoteEscortTank extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'remote-escort-tank-id',
+            internalName: 'remote-escort-tank'
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Give a unit Sentinel for this phase',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.gainKeyword(KeywordName.Sentinel)
+                })
+            }
+        });
+    }
+}
