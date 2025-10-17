cec6c088bda4e0bfb9cc33e449a369485ef1b186
Valentin Jubert <val_jub@live.fr>
2025-10-17 07:44:38 +0200

    heroic arc 170 & emissaries from ryloth

---
+++ server/game/cards/06_SEC/units/HeroicARC170.ts
@@ -0,0 +1,27 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class HeroicARC170 extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '3469756679',
+            internalName: 'heroic-arc170',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Ready an exhausted enemy unit',
+            targetResolver: {
+                controller: RelativePlayer.Opponent,
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: abilityHelper.immediateEffects.conditional({
+                    condition: (context) => context.player.hasSomeArenaUnit({ condition: (card) => card.isUnit() && card.damage > 0 }),
+                    onTrue: abilityHelper.immediateEffects.damage({ amount: 2 })
+                })
+            }
+        });
+    }
+}
