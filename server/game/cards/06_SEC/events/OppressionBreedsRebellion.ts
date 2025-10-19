940ed68f11851ff7662aedc4d1c75921657f282e
sethhelman <s7helman@gmail.com>
2025-10-19 17:42:53 -0400

    Create OppressionBreedsRebellion.ts

---
+++ server/game/cards/06_SEC/events/OppressionBreedsRebellion.ts
@@ -0,0 +1,29 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import type { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import type { UnitsDefeatedThisPhaseWatcher } from '../../../stateWatchers/UnitsDefeatedThisPhaseWatcher';
+
+export default class OppressionBreedsRebellion extends EventCard {
+    private unitsDefeatedThisPhaseWatcher: UnitsDefeatedThisPhaseWatcher;
+    protected override getImplementationId() {
+        return {
+            id: '3208071698',
+            internalName: 'oppression-breeds-rebellion',
+        };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar, AbilityHelper: IAbilityHelper): void {
+        this.unitsDefeatedThisPhaseWatcher = AbilityHelper.stateWatchers.unitsDefeatedThisPhase(registrar, this);
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.setEventAbility({
+            title: 'If a friendly unit was defeated while attacking this phase, draw 3 cards',
+            immediateEffect: AbilityHelper.immediateEffects.conditional({
+                condition: (context) => this.unitsDefeatedThisPhaseWatcher.someDefeatedUnitControlledByPlayer(context.player),
+                onTrue: AbilityHelper.immediateEffects.draw({ amount: 3 })
+            })
+        });
+    }
+}
