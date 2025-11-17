5f8b5f6d362e4908997b72715c32b9ab7c4b4d63
AMMayberry1 <ammayberry1@gmail.com>
2025-11-17 17:06:02 -0500

    Finalize VP test cases

--- test/server/cards/06_SEC/units/VuutunPalaaDroidControlShip.spec.ts
+++ test/server/cards/06_SEC/units/VuutunPalaaDroidControlShip.spec.ts
@@ -1,6 +1,48 @@
 describe('Vuutun Palaa, Droid Control Ship', function() {
     integration(function(contextRef) {
-        // TODO THIS PR: test its own cost decrease ability
+        describe('Vuutun Palaa\'s play cost reduction ability', function() {
+            beforeEach(function () {
+                return contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'count-dooku#face-of-the-confederacy',
+                        hand: ['vuutun-palaa#droid-control-ship'],
+                        groundArena: ['battle-droid', 'generals-guardian', 'wampa']
+                    },
+                    player2: {
+                        groundArena: ['separatist-commando']
+                    }
+                });
+            });
+
+            it('should decrease its play cost by 1 for each friendly Droid unit', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.vuutunPalaa);
+
+                expect(context.vuutunPalaa).toBeInZone('spaceArena');
+                expect(context.player1.exhaustedResourceCount).toBe(7);
+                expect(context.wampa.exhausted).toBeFalse();
+            });
+
+            it('should stack with Exploit even if the droid units are exploited', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.countDooku);
+                context.player1.clickPrompt('Play a Separatist card from your hand. It gains Exploit 1.');
+                context.player1.clickCard(context.vuutunPalaa);
+
+                context.player1.clickPrompt('Trigger Exploit');
+                expect(context.player1).toBeAbleToSelectExactly([context.battleDroid, context.generalsGuardian, context.wampa]);
+                context.player1.clickCard(context.battleDroid);
+                context.player1.clickPrompt('Done');
+
+                expect(context.vuutunPalaa).toBeInZone('spaceArena');
+                expect(context.player1.exhaustedResourceCount).toBe(5);
+                expect(context.battleDroid).toBeInZone('outsideTheGame');
+                expect(context.wampa.exhausted).toBeFalse();
+            });
+        });
 
         describe('Vuutun Palaa\'s constant ability', function() {
             beforeEach(function () {
@@ -8,14 +50,17 @@ describe('Vuutun Palaa, Droid Control Ship', function() {
                     phase: 'action',
                     player1: {
                         leader: 'dryden-vos#i-never-ask-twice',
-                        hand: ['atst', 'vanguard-infantry'],
+                        hand: ['atst', 'vanguard-infantry', 'encouraging-leadership', 'generals-blade'],
                         spaceArena: ['vuutun-palaa#droid-control-ship'],
                         groundArena: ['battle-droid', 'generals-guardian', 'wampa']
+                    },
+                    player2: {
+                        groundArena: ['separatist-commando']
                     }
                 });
             });
 
-            it('can reduce the cost by exhausting Droid units', function () {
+            it('can reduce the cost of a unit by exhausting Droid units', function () {
                 const { context } = contextRef;
 
                 context.player1.clickCard(context.atst);
@@ -141,6 +186,98 @@ describe('Vuutun Palaa, Droid Control Ship', function() {
                 context.player1.setExactReadyResources(4);
                 expect(context.player1).not.toBeAbleToSelect(context.atst);
             });
+
+            it('can reduce the cost of an event by exhausting Droid units', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.encouragingLeadership);
+
+                expect(context.player1).toHaveExactPromptButtons(['Pay cost by exhausting units', 'Pay cost normally', 'Cancel']);
+                context.player1.clickPrompt('Pay cost by exhausting units');
+
+                expect(context.player1).toBeAbleToSelectExactly([context.battleDroid, context.generalsGuardian]);
+                expect(context.player1).toHaveDisabledPromptButton('Done');
+                expect(context.player1).toHaveEnabledPromptButton('Cancel');
+
+                context.player1.clickCard(context.generalsGuardian);
+                expect(context.player1).toHaveEnabledPromptButton('Done');
+
+                context.player1.clickCard(context.battleDroid);
+                expect(context.player1).toHaveEnabledPromptButton('Done');
+
+                context.player1.clickPrompt('Done');
+
+                expect(context.encouragingLeadership).toBeInZone('discard');
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+                expect(context.battleDroid.exhausted).toBeTrue();
+                expect(context.generalsGuardian.exhausted).toBeTrue();
+                expect(context.wampa.exhausted).toBeFalse();
+            });
+
+            it('can reduce the cost of an upgrade by exhausting Droid units', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.generalsBlade);
+                context.player1.clickCard(context.generalsGuardian);
+
+                expect(context.player1).toHaveExactPromptButtons(['Pay cost by exhausting units', 'Pay cost normally', 'Cancel']);
+                context.player1.clickPrompt('Pay cost by exhausting units');
+
+                expect(context.player1).toBeAbleToSelectExactly([context.battleDroid, context.generalsGuardian]);
+                expect(context.player1).toHaveDisabledPromptButton('Done');
+                expect(context.player1).toHaveEnabledPromptButton('Cancel');
+
+                context.player1.clickCard(context.generalsGuardian);
+                expect(context.player1).toHaveEnabledPromptButton('Done');
+
+                context.player1.clickCard(context.battleDroid);
+                expect(context.player1).toHaveEnabledPromptButton('Done');
+
+                context.player1.clickPrompt('Done');
+
+                expect(context.generalsGuardian).toHaveExactUpgradeNames(['generals-blade']);
+                expect(context.player1.exhaustedResourceCount).toBe(1);
+                expect(context.battleDroid.exhausted).toBeTrue();
+                expect(context.generalsGuardian.exhausted).toBeTrue();
+                expect(context.wampa.exhausted).toBeFalse();
+            });
+        });
+
+        it('Vuutun Palaa\'s constant ability can reduce the cost of a unit played with Smuggle', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    leader: 'dryden-vos#i-never-ask-twice',
+                    spaceArena: ['vuutun-palaa#droid-control-ship'],
+                    groundArena: ['battle-droid', 'generals-guardian', 'wampa'],
+                    resources: ['collections-starhopper']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.collectionsStarhopper);
+
+            expect(context.player1).toHaveExactPromptButtons(['Pay cost by exhausting units', 'Cancel']);
+            context.player1.clickPrompt('Pay cost by exhausting units');
+
+            expect(context.player1).toBeAbleToSelectExactly([context.battleDroid, context.generalsGuardian]);
+            expect(context.player1).toHaveDisabledPromptButton('Done');
+            expect(context.player1).toHaveEnabledPromptButton('Cancel');
+
+            context.player1.clickCard(context.generalsGuardian);
+            expect(context.player1).toHaveDisabledPromptButton('Done');
+
+            context.player1.clickCard(context.battleDroid);
+            expect(context.player1).toHaveEnabledPromptButton('Done');
+
+            context.player1.clickPrompt('Done');
+
+            expect(context.collectionsStarhopper).toBeInZone('spaceArena');
+            expect(context.player1.exhaustedResourceCount).toBe(1);
+            expect(context.battleDroid.exhausted).toBeTrue();
+            expect(context.generalsGuardian.exhausted).toBeTrue();
+            expect(context.wampa.exhausted).toBeFalse();
         });
     });
 });
