e659883bc75d0e911d398cbbb707aff4315a940b
Valentin Jubert <val_jub@live.fr>
2025-11-27 22:51:05 +0100

    fix zone

--- server/game/core/card/baseClasses/PlayableOrDeployableCard.ts
+++ server/game/core/card/baseClasses/PlayableOrDeployableCard.ts
@@ -290,7 +290,7 @@ export class PlayableOrDeployableCard<T extends IPlayableOrDeployableCardState =
         }
 
         this.controller = newController;
-        if (this.isTokenUpgrade() && moveTo == null) {
+        if (this.isTokenUpgrade() && moveTo !== ZoneName.Resource) {
             this.owner = newController;
         }
 
