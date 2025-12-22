003354d945724de5a240d1acc52f709fca59c181
scapegoat <39223170+scapegoat-code@users.noreply.github.com>
2025-12-22 16:47:23 -0600

    Replace double quotes with single quotes

--- server/game/cards/07_LAW/units/SalaciousCrumbCacklingCompanion.ts
+++ server/game/cards/07_LAW/units/SalaciousCrumbCacklingCompanion.ts
@@ -1,21 +1,21 @@
-import type { IAbilityHelper } from "../../../AbilityHelper";
-import type { INonLeaderUnitAbilityRegistrar } from "../../../core/card/AbilityRegistrationInterfaces";
-import { NonLeaderUnitCard } from "../../../core/card/NonLeaderUnitCard";
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 
 export default class SalaciousCrumbCacklingCompanion extends NonLeaderUnitCard {
     protected override getImplementationId() {
         return {
-            id: "salacious-crumb#cackling-companion-id",
-            internalName: "salacious-crumb#cackling-companion",
+            id: 'salacious-crumb#cackling-companion-id',
+            internalName: 'salacious-crumb#cackling-companion',
         };
     }
 
     public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.addWhenPlayedAbility({
-            title: "If you control Jabba the Hutt (as a leader or unit), this unit enters play ready",
+            title: 'If you control Jabba the Hutt (as a leader or unit), this unit enters play ready',
             immediateEffect: abilityHelper.immediateEffects.conditional({
                 condition: (context) =>
-                    context.player.controlsLeaderUnitOrUpgradeWithTitle("Jabba the Hutt"),
+                    context.player.controlsLeaderUnitOrUpgradeWithTitle('Jabba the Hutt'),
                 onTrue: abilityHelper.immediateEffects.ready()
             })
         });
