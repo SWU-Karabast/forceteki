45d41f416feca83e8f7c5fa81f319fc547af4f70
BWohldmann <brian.wohldmann@gmail.comm>
2025-12-24 14:57:18 -0600

    Re-use dupllicate setup for basic onAttack

--- test/server/cards/07_LAW/units/QuiGonJinnInfluencingChance.spec.ts
+++ test/server/cards/07_LAW/units/QuiGonJinnInfluencingChance.spec.ts
@@ -1,44 +1,71 @@
-
 describe('Qui-Gon Jinn Influencing Chance', function () {
     integration(function (contextRef) {
-        it('on attack, should look at the top 3 cards of your deck, discard 1, put the rest on top in any order', async function () {
-            await contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    groundArena: ['doctor-aphra#digging-for-answers', 'qui-gon-jinn#influencing-chance'],
-                    discard: ['wampa', 'battlefield-marine', 'pirated-starfighter', 'force-throw'],
-                    deck: ['ma-klounkee', 'porg', 'underworld-thug', 'moisture-farmer', 'tieln-fighter'],
-                    resources: ['resupply']
-                },
-                player2: {
-                    groundArena: ['atst', 'consular-security-force'],
-                }
+        describe('Qui-Gon\'s onAttack ability', function () {
+            beforeEach(async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['doctor-aphra#digging-for-answers', 'qui-gon-jinn#influencing-chance'],
+                        discard: ['wampa', 'battlefield-marine', 'pirated-starfighter', 'force-throw'],
+                        deck: ['ma-klounkee', 'porg', 'underworld-thug', 'moisture-farmer', 'tieln-fighter'],
+                        resources: ['resupply']
+                    },
+                    player2: {
+                        groundArena: ['atst', 'consular-security-force'],
+                    }
+                });
             });
 
-            const { context } = contextRef;
+            it('on attack, should look at the top 3 cards of your deck, discard 1, put the rest on top in any order', function () {
+                const { context } = contextRef;
 
-            context.player1.clickCard(context.quiGonJinnInfluencingChance);
-            context.player1.clickCard(context.p2Base);
+                context.player1.clickCard(context.quiGonJinnInfluencingChance);
+                context.player1.clickCard(context.p2Base);
 
-            expect(context.player1).toHavePrompt('Select a card to discard');
-            expect(context.player1).toHaveEnabledPromptButton('Take nothing');
-            expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.porg, context.underworldThug] });
+                expect(context.player1).toHavePrompt('Select a card to discard');
+                expect(context.player1).toHaveEnabledPromptButton('Take nothing');
+                expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.porg, context.underworldThug] });
 
-            context.player1.clickCardInDisplayCardPrompt(context.porg);
+                context.player1.clickCardInDisplayCardPrompt(context.porg);
 
-            expect(context.player1).toHavePrompt('Place cards on top of the deck in any order');
-            expect(context.player1).not.toBeAbleToSelect(context.porg);
+                expect(context.player1).toHavePrompt('Place cards on top of the deck in any order');
+                expect(context.player1).not.toBeAbleToSelect(context.porg);
 
-            expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.underworldThug] });
+                expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.underworldThug] });
 
-            context.player1.clickDisplayCardPromptButton(context.maKlounkee.uuid, 'top');
-            context.player1.clickDisplayCardPromptButton(context.underworldThug.uuid, 'top');
+                context.player1.clickDisplayCardPromptButton(context.maKlounkee.uuid, 'top');
+                context.player1.clickDisplayCardPromptButton(context.underworldThug.uuid, 'top');
 
-            expect(context.porg).toBeInZone('discard');
-            expect(context.maKlounkee).toBeInZone('deck');
-            expect(context.underworldThug).toBeInZone('deck');
+                expect(context.porg).toBeInZone('discard');
+                expect(context.maKlounkee).toBeInZone('deck');
+                expect(context.underworldThug).toBeInZone('deck');
 
-            expect(context.player2).toBeActivePlayer();
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should allow the user to not discard a card, but still make them put all 3 on top of the deck in any order', function () {
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.quiGonJinnInfluencingChance);
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.player1).toHavePrompt('Select a card to discard');
+                expect(context.player1).toHaveEnabledPromptButton('Take nothing');
+                expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.porg, context.underworldThug] });
+
+                context.player1.clickPrompt('Take nothing');
+
+
+                context.player1.clickDisplayCardPromptButton(context.maKlounkee.uuid, 'top');
+                context.player1.clickDisplayCardPromptButton(context.underworldThug.uuid, 'top');
+                context.player1.clickDisplayCardPromptButton(context.porg.uuid, 'top');
+
+                expect(context.porg).toBeInZone('deck');
+                expect(context.maKlounkee).toBeInZone('deck');
+                expect(context.underworldThug).toBeInZone('deck');
+
+                expect(context.player2).toBeActivePlayer();
+            });
         });
 
         it('when played, should look at the top 3 cards of your deck, discard 1, put the rest on top in any order', async function () {
@@ -77,43 +104,6 @@ describe('Qui-Gon Jinn Influencing Chance', function () {
             expect(context.porg).toBeInZone('discard');
             expect(context.maKlounkee).toBeInZone('deck');
             expect(context.forceThrow).toBeInZone('deck');
-
-            expect(context.player2).toBeActivePlayer();
-        });
-
-        it('should allow the user to not discard a card, but still make them put all 3 on top of the deck in any order', async function () {
-            await contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    groundArena: ['doctor-aphra#digging-for-answers', 'qui-gon-jinn#influencing-chance'],
-                    discard: ['wampa', 'battlefield-marine', 'pirated-starfighter', 'force-throw'],
-                    deck: ['ma-klounkee', 'porg', 'underworld-thug', 'moisture-farmer', 'tieln-fighter'],
-                    resources: ['resupply']
-                },
-                player2: {
-                    groundArena: ['atst', 'consular-security-force'],
-                }
-            });
-
-            const { context } = contextRef;
-
-            context.player1.clickCard(context.quiGonJinnInfluencingChance);
-            context.player1.clickCard(context.p2Base);
-
-            expect(context.player1).toHavePrompt('Select a card to discard');
-            expect(context.player1).toHaveEnabledPromptButton('Take nothing');
-            expect(context.player1).toHaveExactDisplayPromptCards({ selectable: [context.maKlounkee, context.porg, context.underworldThug] });
-
-            context.player1.clickPrompt('Take nothing');
-
-
-            context.player1.clickDisplayCardPromptButton(context.maKlounkee.uuid, 'top');
-            context.player1.clickDisplayCardPromptButton(context.underworldThug.uuid, 'top');
-            context.player1.clickDisplayCardPromptButton(context.porg.uuid, 'top');
-
-            expect(context.porg).toBeInZone('deck');
-            expect(context.maKlounkee).toBeInZone('deck');
-            expect(context.underworldThug).toBeInZone('deck');
 
             expect(context.player2).toBeActivePlayer();
         });
