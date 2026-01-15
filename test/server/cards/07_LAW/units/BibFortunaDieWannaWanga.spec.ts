a09cb603ee684d4e10acf838b8a8409ad1ab635f
Valentin Jubert <val_jub@live.fr>
2026-01-15 22:48:01 +0100

    Update BibFortunaDieWannaWanga.spec.ts

--- test/server/cards/07_LAW/units/BibFortunaDieWannaWanga.spec.ts
+++ test/server/cards/07_LAW/units/BibFortunaDieWannaWanga.spec.ts
@@ -17,14 +17,13 @@ describe('Bib Fortuna, Die Wanna Wanga?', function () {
             expect(context.player1.credits).toBe(1);
         });
 
-        it('Bib Fortuna\'s when played ability should create a Credit token (as we do not control another Underworld unit)', async function () {
+        it('Bib Fortuna\'s when played ability should not create a Credit token (as we do not control another Underworld unit)', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
                     hand: ['bib-fortuna#die-wanna-wanga'],
                 },
                 player2: {
-
                     groundArena: ['greedo#slow-on-the-draw']
                 }
             });
