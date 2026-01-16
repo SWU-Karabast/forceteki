ef2defb53b0cd3bb0a9c61c4a91c09c176a6993d
Valentin Jubert <val_jub@live.fr>
2026-01-17 00:24:21 +0100

    R2D2, Finn & The Blade Wing (#2059)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LAW/095.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LAW/145.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/LAW/241.png)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ server/game/cards/07_LAW/units/FinnLookingCloser.ts
@@ -0,0 +1,23 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class FinnLookingCloser extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'finn#looking-closer-id',
+            internalName: 'finn#looking-closer',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addOnAttackAbility({
+            title: 'Give a Shield token to a non-unique unit',
+            optional: true,
+            targetResolver: {
+                cardCondition: (card) => card.isUnit() && !card.unique,
+                immediateEffect: abilityHelper.immediateEffects.giveShield()
+            }
+        });
+    }
+}
