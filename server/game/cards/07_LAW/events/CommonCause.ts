686b86a42ad3513983a733a8d5af3bb3bf0219b6
Valentin Jubert <val_jub@live.fr>
2026-01-23 23:42:59 +0100

    common cause, fear and dead men, you hold this

---
+++ server/game/cards/07_LAW/events/CommonCause.ts
@@ -0,0 +1,29 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { EventCard } from '../../../core/card/EventCard';
+
+export default class CommonCause extends EventCard {
+    protected override getImplementationId () {
+        return {
+            id: '4006503809',
+            internalName: 'common-cause',
+        };
+    }
+
+    public override setupCardAbilities (registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.setEventAbility({
+            title: 'Give a unit +1/+1 for this phase for each different aspect among units you control',
+            targetResolver: {
+                immediateEffect: abilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: abilityHelper.ongoingEffects.modifyStats((_, context) => {
+                        const differentAspectCount = new Set(context.player.getArenaUnits().flatMap((x) => x.aspects)).size;
+                        return {
+                            power: differentAspectCount,
+                            hp: differentAspectCount,
+                        };
+                    })
+                })
+            }
+        });
+    }
+}
