fdede5a7d59863ae6c158e15e45ae59f82a510de
Valentin Jubert <val_jub@live.fr>
2026-01-24 13:54:20 +0100

    secret battle of pretend & chio fain

---
+++ server/game/cards/07_LAW/events/SecretBattleOfPretend.ts
@@ -0,0 +1,39 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { EventCard } from '../../../core/card/EventCard';
+import { RelativePlayer, TargetMode, WildcardCardType } from '../../../core/Constants';
+
+export default class SecretBattleOfPretend extends EventCard {
+    protected override getImplementationId () {
+        return {
+            id: '6215104649',
+            internalName: 'secret-battle-of-pretend',
+        };
+    }
+
+    public override setupCardAbilities (registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.setEventAbility({
+            title: 'Exhaust a friendly unit. If you do, for each different aspect it has, exhaust an enemy unit in the same arena.',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                controller: RelativePlayer.Self,
+                immediateEffect: abilityHelper.immediateEffects.exhaust()
+            },
+            ifYouDo: (ifYouDoContext) => {
+                const differentAspectCount = new Set(ifYouDoContext.target?.aspects).size;
+                return {
+                    title: 'For each different aspect it has, exhaust an enemy unit in the same arena',
+                    targetResolver: {
+                        activePromptTitle: () => `Exhaust ${differentAspectCount} enemy unit in ${ifYouDoContext.target?.zone} arena`,
+                        mode: TargetMode.ExactlyVariable,
+                        numCardsFunc: (context) => Math.min(differentAspectCount, context.player.opponent.getArenaUnits().length),
+                        cardTypeFilter: WildcardCardType.Unit,
+                        controller: RelativePlayer.Opponent,
+                        cardCondition: (card) => card.zone === ifYouDoContext.target?.zone,
+                        immediateEffect: abilityHelper.immediateEffects.exhaust()
+                    }
+                };
+            }
+        });
+    }
+}
