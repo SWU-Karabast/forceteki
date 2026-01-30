1c45cecf8eed580e0335e1babdb97251d12be173
Valentin Jubert <val_jub@live.fr>
2026-01-30 09:16:03 +0100

    fix tests

--- test/server/cards/07_LAW/units/ScavengingSandcrawler.spec.ts
+++ test/server/cards/07_LAW/units/ScavengingSandcrawler.spec.ts
@@ -6,45 +6,43 @@ describe('Scavenging Sandcrawler', function () {
                     phase: 'action',
                     player1: {
                         groundArena: ['scavenging-sandcrawler'],
-                        discard: ['battlefield-marine', 'wampa'],
+                        discard: ['battlefield-marine', 'wampa', 'takedown'],
                         deck: ['superlaser-technician']
                     },
                     player2: {
-                        groundArena: ['death-star-stormtrooper']
+                        groundArena: ['death-star-stormtrooper'],
+                        discard: ['resupply']
                     }
                 });
             });
 
-            it('should allow putting a card from discard pile on bottom of deck to create a Credit token', async function () {
+            it('should allow putting a card from discard pile on bottom of deck to create a Credit token', function () {
                 const { context } = contextRef;
 
                 context.player1.clickCard(context.scavengingSandcrawler);
                 context.player1.clickCard(context.deathStarStormtrooper);
 
                 // Option to use the ability (it's a trigger prompt, so we select the card from discard pile directly)
-                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa]);
+                expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa, context.takedown]);
+                expect(context.player1).toHavePassAbilityButton();
                 context.player1.clickCard(context.battlefieldMarine);
 
-                expect(context.battlefieldMarine).toBeInZone('deck');
-                expect(context.player1.deck[context.player1.deck.length - 1]).toBe(context.battlefieldMarine);
-                
-                // Check if credit token is created
+                expect(context.battlefieldMarine).toBeInBottomOfDeck(context.player1, 1);
+
                 expect(context.player1.credits).toBe(1);
             });
 
-            it('should do nothing if the ability is passed', async function () {
+            it('should do nothing if the ability is passed', function () {
                 const { context } = contextRef;
 
                 context.player1.clickCard(context.scavengingSandcrawler);
                 context.player1.clickCard(context.deathStarStormtrooper);
 
-                // Option to use the ability
-                expect(context.player1).toHaveEnabledPromptButton('Pass');
                 context.player1.clickPrompt('Pass');
 
                 expect(context.battlefieldMarine).toBeInZone('discard');
                 expect(context.wampa).toBeInZone('discard');
-                
+
                 expect(context.player1.credits).toBe(0);
             });
         });
