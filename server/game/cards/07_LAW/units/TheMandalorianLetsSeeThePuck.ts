c20c01a341cb0410544e11f0711bd5e521c4b872
sethhelman <s7helman@gmail.com>
2026-01-30 20:27:23 -0500

    mando, valance, attack from all sides, and detention block rescue title fix

---
+++ server/game/cards/07_LAW/units/TheMandalorianLetsSeeThePuck.ts
@@ -0,0 +1,28 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { PhaseName } from '../../../core/Constants';
+
+export default class TheMandalorianLetsSeeThePuck extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '7505293719',
+            internalName: 'the-mandalorian#lets-see-the-puck',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Draw a card',
+            immediateEffect: AbilityHelper.immediateEffects.draw()
+        });
+        registrar.addTriggeredAbility({
+            title: 'Draw a card',
+            collectiveTrigger: true,
+            when: {
+                onCardsDrawn: (event, context) => event.player === context.player && context.game.currentPhase === PhaseName.Action,
+            },
+            immediateEffect: AbilityHelper.immediateEffects.giveShield()
+        });
+    }
+}
