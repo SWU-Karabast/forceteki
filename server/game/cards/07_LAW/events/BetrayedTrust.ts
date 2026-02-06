f6df8dfc58db289596068e6d332337faab3eeeb1
Robbie Moyer <rmoyer08@gmail.com>
2026-02-06 12:42:36 -0500

    Add implementation for Betrayed Trust

---
+++ server/game/cards/07_LAW/events/BetrayedTrust.ts
@@ -0,0 +1,27 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { EventCard } from '../../../core/card/EventCard';
+import { AbilityRestriction, RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class BetrayedTrust extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: '7069221067',
+            internalName: 'betrayed-trust',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar, AbilityHelper: IAbilityHelper): void {
+        registrar.setEventAbility({
+            title: 'An enemy unit can\'t deal combat damage for this phase',
+            targetResolver: {
+                controller: RelativePlayer.Opponent,
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.cardCannot(AbilityRestriction.DealCombatDamage),
+                    ongoingEffectDescription: 'prevent combat damage from being dealt by'
+                })
+            }
+        });
+    }
+}
