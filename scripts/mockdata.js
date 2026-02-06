52c9e10653e0118525e47c551fc0434714cfe427
sethhelman <s7helman@gmail.com>
2026-02-06 00:31:02 -0500

    benthic, ghost, sabine

--- scripts/mockdata.js
+++ scripts/mockdata.js
@@ -561,25 +561,6 @@ const mockCards = [
         unique: false,
         internalName: 'thermal-detonator',
     }),
-    buildMockCard({
-        title: 'Obi-Wan Kenobi',
-        subtitle: 'Protector of Felucia',
-        power: 7,
-        hp: 7,
-        cost: 7,
-        hasNonKeywordAbility: true,
-        aspects: ['vigilance', 'command', 'heroism'],
-        types: ['unit'],
-        keywords: ['sentinel'],
-        traits: ['force', 'jedi', 'republic'],
-        setId: {
-            set: 'LAW',
-            number: 36,
-        },
-        unique: true,
-        arena: 'ground',
-        internalName: 'obiwan-kenobi#protector-of-felucia',
-    }),
 ];
 
 /** @param {{ title: string, subtitle: string?, hasNonKeywordAbility: boolean, cost: number?, hp: number?, arena?: string, unique: boolean, upgradeHp: number?, upgradePower: number?, aspects: string[]?, traits: string[]?, keywords: string[]?, types: string[], setId: { set: string, number: number }, internalName: string }} cardData */
