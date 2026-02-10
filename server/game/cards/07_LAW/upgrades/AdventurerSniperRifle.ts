b54e394d232792af22acb3ae92deb6c12bb9d703
sethhelman <s7helman@gmail.com>
2026-02-10 18:02:37 -0500

    aurra, rifle

---
+++ server/game/cards/07_LAW/upgrades/AdventurerSniperRifle.ts
@@ -0,0 +1,32 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { IUpgradeAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { UpgradeCard } from '../../../core/card/UpgradeCard';
+import { Trait, WildcardCardType, ZoneName } from '../../../core/Constants';
+
+export default class AdventurerSniperRifle extends UpgradeCard {
+    protected override getImplementationId() {
+        return {
+            id: 'adventurer-sniper-rifle-id',
+            internalName: 'adventurer-sniper-rifle',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IUpgradeAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.setAttachCondition((context) => !context.attachTarget.hasSomeTrait(Trait.Vehicle));
+
+        registrar.addGainActionAbilityTargetingAttached({
+            title: 'Choose an undamaged non-leader ground unit. Its printed HP is considered to be 1 for this phase',
+            cost: AbilityHelper.costs.exhaustSelf(),
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                zoneFilter: ZoneName.GroundArena,
+                cardCondition: (card) => card.isUnit() && card.damage === 0,
+                immediateEffect: AbilityHelper.immediateEffects.forThisPhaseCardEffect({
+                    effect: AbilityHelper.ongoingEffects.overridePrintedAttributes({
+                        printedHp: 1
+                    })
+                })
+            }
+        });
+    }
+}
