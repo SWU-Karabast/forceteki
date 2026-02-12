80f6d146026935ea7cc254b38f9bf65d9c51bf8e
Valentin Jubert <val_jub@live.fr>
2026-02-12 19:54:12 +0100

    pr comments

--- test/server/cards/07_TS26/leaders/AsajjVentressAmbitiousApprentice.spec.ts
+++ test/server/cards/07_TS26/leaders/AsajjVentressAmbitiousApprentice.spec.ts
@@ -1,37 +1,112 @@
 describe('Asajj Ventress, Ambitious Apprentice', function() {
     integration(function(contextRef) {
-        it('Asajj Ventress\'s undeployed ability should allow attacking with a token unit that gets +1/+0 for the attack', async function() {
-            await contextRef.setupTestAsync({
-                phase: 'action',
-                player1: {
-                    leader: 'asajj-ventress#ambitious-apprentice',
-                    hand: ['i-am-the-senate'],
-                    groundArena: ['wampa'],
-                    base: 'echo-base',
-                },
-                player2: {
-                    groundArena: ['battlefield-marine']
-                }
+        describe('Asajj Ventress\'s undeployed ability ', function() {
+            it('should allow attacking with a token unit that gets +1/+0 for the attack', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#ambitious-apprentice',
+                        groundArena: [{ card: 'wampa', upgrades: ['experience', 'shield'] }, 'spy', 'clone-trooper', 'battle-droid'],
+                        spaceArena: ['xwing', 'tie-fighter'],
+                        base: 'echo-base',
+                        credits: 1,
+                        hasForceToken: true,
+                    },
+                    player2: {
+                        groundArena: ['battlefield-marine']
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.asajjVentress);
+                context.player1.clickPrompt('Attack with a token unit. It gets +1/+0 for this attack.');
+                // wampa should not be selectable
+                expect(context.player1).toBeAbleToSelectExactly([context.spy, context.cloneTrooper, context.battleDroid, context.xwing, context.tieFighter]);
+                context.player1.clickCard(context.spy);
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.asajjVentress.exhausted).toBeTrue();
+                expect(context.p2Base.damage).toBe(3);
+                expect(context.spy.getPower()).toBe(0);
+                expect(context.spy.exhausted).toBeTrue();
             });
-            const { context } = contextRef;
 
-            context.player1.clickCard(context.iAmTheSenate);
-            const spies = context.player1.findCardsByName('spy');
+            it('should allow attacking with a token unit that gets +1/+0 for the attack (cannot select enemy token unit)', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#ambitious-apprentice',
+                        groundArena: ['spy'],
+                    },
+                    player2: {
+                        groundArena: ['clone-trooper']
+                    }
+                });
+                const { context } = contextRef;
 
-            context.moveToNextActionPhase();
+                context.player1.clickCard(context.asajjVentress);
+                context.player1.clickPrompt('Attack with a token unit. It gets +1/+0 for this attack.');
+                expect(context.player1).toBeAbleToSelectExactly([context.spy]);
+                context.player1.clickCard(context.spy);
+                context.player1.clickCard(context.p2Base);
 
-            context.player1.clickCard(context.asajjVentress);
-            context.player1.clickPrompt('Attack with a token unit. It gets +1/+0 for this attack.');
-            // wampa should not be selectable
-            expect(context.player1).toBeAbleToSelectExactly([...spies]);
-            context.player1.clickCard(spies[0]);
-            context.player1.clickCard(context.p2Base);
+                expect(context.player2).toBeActivePlayer();
+                expect(context.asajjVentress.exhausted).toBeTrue();
+                expect(context.p2Base.damage).toBe(3);
+                expect(context.spy.getPower()).toBe(0);
+                expect(context.spy.exhausted).toBeTrue();
+            });
 
-            expect(context.player2).toBeActivePlayer();
-            expect(context.asajjVentress.exhausted).toBeTrue();
-            expect(context.p2Base.damage).toBe(3);
-            expect(spies[0].getPower()).toBe(0);
-            expect(spies[0].exhausted).toBeTrue();
+            it('should allow attacking with a token unit that gets +1/+0 for the attack (cannot select exhausted token unit)', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#ambitious-apprentice',
+                        groundArena: [{ card: 'spy', exhausted: true }],
+                        spaceArena: ['xwing'],
+                    },
+                    player2: {
+                        groundArena: ['clone-trooper']
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.asajjVentress);
+                context.player1.clickPrompt('Attack with a token unit. It gets +1/+0 for this attack.');
+                expect(context.player1).toBeAbleToSelectExactly([context.xwing]);
+                context.player1.clickCard(context.xwing);
+                context.player1.clickCard(context.p2Base);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.asajjVentress.exhausted).toBeTrue();
+                expect(context.p2Base.damage).toBe(3);
+                expect(context.xwing.getPower()).toBe(2);
+                expect(context.xwing.exhausted).toBeTrue();
+            });
+
+            it('should notallow attacking with a non-token unit', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'asajj-ventress#ambitious-apprentice',
+                        groundArena: ['wampa'],
+                    },
+                    player2: {
+                        groundArena: ['clone-trooper']
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.asajjVentress);
+                expect(context.player1).toHaveEnabledPromptButton('(No effect) Attack with a token unit. It gets +1/+0 for this attack.');
+                context.player1.clickPrompt('(No Effect) Attack with a token unit. It gets +1/+0 for this attack.');
+                context.player1.clickPrompt('Use it anyway');
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.asajjVentress.exhausted).toBeTrue();
+                expect(context.p2Base.damage).toBe(0);
+            });
         });
 
         describe('Asajj Ventress\'s deployed ability', function() {
