cb485f754cd41e452ed915c188e5c71ec7629e9d
Angel de Jesús Ruiz Cerrillo <ing.angelruizcerrillo@gmail.com>
2026-02-14 16:00:24 -0600

    Fix nit

--- server/game/cards/07_LAW/upgrades/TargetedForRemoval.ts
+++ server/game/cards/07_LAW/upgrades/TargetedForRemoval.ts
@@ -13,7 +13,6 @@ export default class TargetedForRemoval extends UpgradeCard {
     public override setupCardAbilities (registrar: IUpgradeAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.addGainWhenDefeatedAbilityTargetingAttached({
             title: 'An opponent creates Credit tokens equal to this unit\'s cost',
-            optional: false,
             immediateEffect: abilityHelper.immediateEffects.createCreditToken((context) => ({ amount: context.source.printedCost, target: context.player.opponent }))
         });
     }
