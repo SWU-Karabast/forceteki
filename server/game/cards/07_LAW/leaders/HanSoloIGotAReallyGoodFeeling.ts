12f884fd56d28d986327298673e1839222da7c26
Robbie Moyer <rmoyer08@gmail.com>
2026-02-16 20:28:49 -0500

    Add Han Solo impl

---
+++ server/game/cards/07_LAW/leaders/HanSoloIGotAReallyGoodFeeling.ts
@@ -0,0 +1,55 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { ILeaderUnitAbilityRegistrar, ILeaderUnitLeaderSideAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+import { RelativePlayer, TargetMode, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+
+export default class HanSoloIGotAReallyGoodFeeling extends LeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '9738208208',
+            internalName: 'han-solo#i-got-a-really-good-feeling',
+        };
+    }
+
+    protected override setupLeaderSideAbilities(registrar: ILeaderUnitLeaderSideAbilityRegistrar, AbilityHelper: IAbilityHelper): void {
+        registrar.addActionAbility({
+            title: 'Deal 1 damage to a unit',
+            cost: [
+                AbilityHelper.costs.exhaustSelf(),
+                AbilityHelper.costs.defeat({
+                    activePromptTitle: 'Choose a friendly token to defeat',
+                    cardTypeFilter: WildcardCardType.Token,
+                    controller: RelativePlayer.Self
+                })
+            ],
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.damage({ amount: 1 })
+            }
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities(registrar: ILeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper): void {
+        registrar.addOnAttackAbility({
+            title: 'Defeat any number of friendly tokens',
+            targetResolver: {
+                mode: TargetMode.Unlimited,
+                canChooseNoCards: true,
+                cardTypeFilter: WildcardCardType.Token,
+                controller: RelativePlayer.Self,
+                zoneFilter: WildcardZoneName.Any,
+                immediateEffect: AbilityHelper.immediateEffects.defeat()
+            },
+            then: (thenContext) => ({
+                title: `Deal ${thenContext.target.length || 0} damage to a unit`,
+                thenCondition: (_) => thenContext.target && thenContext.target.length > 0,
+                targetResolver: {
+                    cardTypeFilter: WildcardCardType.Unit,
+                    immediateEffect: AbilityHelper.immediateEffects.damage({
+                        amount: thenContext.target.length,
+                    })
+                }
+            })
+        });
+    }
+}
