862719cac37432e56ae69377cbcbef98d4ee0463
Valentin Jubert <val_jub@live.fr>
2026-02-18 21:05:27 +0100

    val & two-faced troig

---
+++ server/game/cards/07_LAW/units/TwoFacedTroig.ts
@@ -0,0 +1,27 @@
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+
+export default class TwoFacedTroig extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'twofaced-troig-id',
+            internalName: 'twofaced-troig',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'An opponent take control of this unit. If you do, create 2 Credit tokens',
+            optional: true,
+            immediateEffect: AbilityHelper.immediateEffects.takeControlOfUnit((context) => ({
+                newController: context.player.opponent,
+                target: context.source
+            })),
+            ifYouDo: {
+                title: 'Create 2 Credit token',
+                immediateEffect: AbilityHelper.immediateEffects.createCreditToken({ amount: 2 })
+            }
+        });
+    }
+}
