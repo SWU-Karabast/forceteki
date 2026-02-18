9cc65ff2f2bf2b3120b969f1c839da69a3b13375
Valentin Jubert <val_jub@live.fr>
2026-02-18 22:48:40 +0100

    bracca shipbreaker, cavern angels xwing, swoop bike marauder

---
+++ server/game/cards/07_LAW/units/CavernAngelsXWing.ts
@@ -0,0 +1,23 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { ZoneName } from '../../../core/Constants';
+
+export default class CavernAngelsXWing extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'cavern-angels-xwing-id',
+            internalName: 'cavern-angels-xwing'
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addWhenDefeatedAbility({
+            title: 'Deal 2 damage to a base',
+            targetResolver: {
+                zoneFilter: ZoneName.Base,
+                immediateEffect: abilityHelper.immediateEffects.damage({ amount: 2 }),
+            }
+        });
+    }
+}
