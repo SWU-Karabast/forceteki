a4b796a0eed09f7a4c37204a023ce29af11aaed2
Robbie Moyer <rmoyer08@gmail.com>
2026-02-19 15:49:19 -0500

    Update LookMoveDeckCardsTopOrBottomSystem for player targeting

--- server/game/cards/07_LAW/upgrades/Watchful.ts
+++ server/game/cards/07_LAW/upgrades/Watchful.ts
@@ -2,6 +2,7 @@ import type { IAbilityHelper } from '../../../AbilityHelper';
 import type { IUpgradeAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { UpgradeCard } from '../../../core/card/UpgradeCard';
 import { TargetMode } from '../../../core/Constants';
+import { asConcretePlayer } from '../../../core/utils/EnumHelpers';
 
 export default class Watchful extends UpgradeCard {
     protected override getImplementationId () {
@@ -13,14 +14,18 @@ export default class Watchful extends UpgradeCard {
 
     public override setupCardAbilities (registrar: IUpgradeAbilityRegistrar, AbilityHelper: IAbilityHelper) {
         registrar.addGainOnAttackAbilityTargetingAttached({
-            title: 'Look at the top card of a deck. You may put it on the bottom of your deck.',
+            title: 'Look at the top card of a deck. You may put it on the bottom of that deck.',
             targetResolver: {
+                activePromptTitle: 'Look at the top card of a deck',
                 mode: TargetMode.Player,
+                effectChoices: (relativePlayer, context) => (asConcretePlayer(relativePlayer, context.player) === context.player
+                    ? 'Your deck'
+                    : 'Opponent\'s deck'),
                 immediateEffect: AbilityHelper.immediateEffects.conditional({
                     condition: (context) => context.player.drawDeck.length > 0 || context.player.opponent.drawDeck.length > 0,
                     onTrue: AbilityHelper.immediateEffects.lookMoveDeckCardsTopOrBottom({ amount: 1 })
                 })
-            },
+            }
         });
     }
 }
