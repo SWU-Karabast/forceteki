23245ba74c804667669863b92def0d159e3776a6
Robbie Moyer <rmoyer08@gmail.com>
2026-02-19 16:47:24 -0500

    Add remaining common LAW bases

--- server/game/cards/07_LAW/bases/ImperialCommandComplex.ts
+++ server/game/cards/07_LAW/bases/ImperialCommandComplex.ts
@@ -1,11 +1,11 @@
 import { LAWCommonBase } from '../common/LAWCommonBase';
 
 // Note: Implementation and test are under the superclass since all common LAW bases share the same text
-export default class CantoBight extends LAWCommonBase {
+export default class ImperialCommandComplex extends LAWCommonBase {
     protected override getImplementationId() {
         return {
-            id: '2937103129',
-            internalName: 'canto-bight',
+            id: '7297371836',
+            internalName: 'imperial-command-complex',
         };
     }
 }
