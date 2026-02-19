f1393714b6f55c140c38bfd320ee3b5fe079b903
Robbie Moyer <rmoyer08@gmail.com>
2026-02-19 11:37:59 -0500

    Update TS Anakin's leader ability

--- server/game/stateWatchers/CardsEnteredPlayThisPhaseWatcher.ts
+++ server/game/stateWatchers/CardsEnteredPlayThisPhaseWatcher.ts
@@ -2,7 +2,6 @@ import { StateWatcher } from '../core/stateWatcher/StateWatcher';
 import { StateWatcherName } from '../core/Constants';
 import type { StateWatcherRegistrar } from '../core/stateWatcher/StateWatcherRegistrar';
 import type { Player } from '../core/Player';
-import type { Card } from '../core/card/Card';
 import type { IInPlayCard } from '../core/card/baseClasses/InPlayCard';
 import type Game from '../core/Game';
 import type { GameObjectRef, UnwrapRef } from '../core/GameObjectBase';
@@ -32,7 +31,7 @@ export class CardsEnteredPlayThisPhaseWatcher extends StateWatcher<EnteredCardEn
     }
 
     /** Filters the list of entered play cards in the state and returns the cards that match */
-    public getCardsEnteredPlay(filter: (entry: UnwrapRef<EnteredCardEntry>) => boolean): Card[] {
+    public getCardsEnteredPlay(filter: (entry: UnwrapRef<EnteredCardEntry>) => boolean): IInPlayCard[] {
         return this.getCurrentValue()
             .filter(filter)
             .map((entry) => entry.card);
