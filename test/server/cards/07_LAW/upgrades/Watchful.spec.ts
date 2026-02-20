8b9aa243ffefa8435b664572db7cd837c278cfa0
Robbie Moyer <rmoyer08@gmail.com>
2026-02-20 10:44:13 -0500

    Update Watchful spec

--- test/server/cards/07_LAW/upgrades/Watchful.spec.ts
+++ test/server/cards/07_LAW/upgrades/Watchful.spec.ts
@@ -19,24 +19,31 @@ describe('Watchful', function() {
 
             it('Attacking lets you look at the top card of the deck and decide whether to put it on the bottom or top of deck, choosing bottom', function () {
                 const { context } = contextRef;
-                const preSwapDeck = context.player1.deck.concat();
 
+                // Play Watchful
                 context.player1.clickCard(context.watchful);
+
+                // All units are valid targets
                 expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.cartelInterceptor, context.imperialDarkTrooper]);
                 context.player1.clickCard(context.battlefieldMarine);
                 context.player2.passAction();
 
+                // Attack with battlefield marine to trigger Watchful's ability
                 context.player1.clickCard(context.battlefieldMarine);
                 context.player1.clickCard(context.p2Base);
+
+                // Ability Prompt
+                expect(context.player1).toHavePrompt('Look at the top card of a deck');
                 expect(context.player1).toHaveEnabledPromptButtons(['Your deck', 'Opponent\'s deck']);
                 context.player1.clickPrompt('Your deck');
+
+                // Player sees Foundling, chooses bottom
                 expect(context.player1).toHaveExactSelectableDisplayPromptCards([context.foundling]);
                 expect(context.player1).toHaveExactDisplayPromptPerCardButtons(['Put on top', 'Put on bottom']);
                 context.player1.clickDisplayCardPromptButton(context.foundling.uuid, 'bottom');
 
-                expect(context.player1.deck.length).toBe(5);
-                expect(context.player1.deck[0]).toBe(preSwapDeck[1]);
-                expect(context.player1.deck[4]).toBe(preSwapDeck[0]);
+                // Foundling is now on the bottom of the deck
+                expect(context.foundling).toBeInBottomOfDeck(context.player1, 1);
                 expect(context.player2).toBeActivePlayer();
             });
 
@@ -58,7 +65,7 @@ describe('Watchful', function() {
                 context.player1.clickDisplayCardPromptButton(context.foundling.uuid, 'top');
 
                 expect(context.player1.deck.length).toBe(5);
-                expect(context.player1.deck).toEqualArray(preSwapDeck);
+                expect(context.player1.deck).toEqualArray(preSwapDeck); // Deck is unchanged
                 expect(context.player2).toBeActivePlayer();
             });
 
@@ -80,13 +87,12 @@ describe('Watchful', function() {
                 context.player1.clickDisplayCardPromptButton(context.daringRaid.uuid, 'top');
 
                 expect(context.player2.deck.length).toBe(5);
-                expect(context.player2.deck).toEqualArray(preSwapDeck);
+                expect(context.player2.deck).toEqualArray(preSwapDeck); // Deck is unchanged
                 expect(context.player2).toBeActivePlayer();
             });
 
             it('Attacking lets you look at the top card of the opponent deck and decide whether to put it on the bottom or top of deck, choosing bottom', function () {
                 const { context } = contextRef;
-                const preSwapDeck = context.player2.deck.concat();
 
                 context.player1.clickCard(context.watchful);
                 expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.cartelInterceptor, context.imperialDarkTrooper]);
@@ -101,9 +107,7 @@ describe('Watchful', function() {
                 expect(context.player1).toHaveExactDisplayPromptPerCardButtons(['Put on top', 'Put on bottom']);
                 context.player1.clickDisplayCardPromptButton(context.daringRaid.uuid, 'bottom');
 
-                expect(context.player2.deck.length).toBe(5);
-                expect(context.player2.deck[0]).toBe(preSwapDeck[1]);
-                expect(context.player2.deck[4]).toBe(preSwapDeck[0]);
+                expect(context.daringRaid).toBeInBottomOfDeck(context.player2, 1);
                 expect(context.player2).toBeActivePlayer();
             });
         });
@@ -113,8 +117,7 @@ describe('Watchful', function() {
                 return contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
-                        hand: ['watchful'],
-                        groundArena: ['imperial-dark-trooper'],
+                        groundArena: [{ card: 'imperial-dark-trooper', upgrades: ['watchful'] }],
                         deck: [],
                     },
                     player2: {
@@ -124,21 +127,16 @@ describe('Watchful', function() {
                 });
             });
 
-            it('while playing shouldn\'t trigger because the deck is empty.', function () {
+            it('has no effect when the decks are empty', function () {
                 const { context } = contextRef;
-
-                context.player1.clickCard(context.watchful);
-                context.player1.clickCard(context.imperialDarkTrooper);
-                context.player2.passAction();
 
                 context.player1.clickCard(context.imperialDarkTrooper);
                 context.player1.clickCard(context.p2Base);
                 context.player1.clickPrompt('Your deck');
 
-                expect(context.player1.deck.length).toBe(0);
+                // No card prompts should be shown since there are no cards in either deck
+                expect(context.player1).not.toHavePrompt('Look at the top card of a deck');
                 expect(context.player2).toBeActivePlayer();
-                expect(context.p1Base.damage).toEqual(0);
-                expect(context.p2Base.damage).toEqual(3);
             });
         });
     });
