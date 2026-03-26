0d9e42cc29f9cfab6fe6e5748adf589283a3ab9e
Valentin Jubert <val_jub@live.fr>
2026-03-26 07:48:52 +0100

    lint

--- server/game/cards/07_TS26/units/MoraloEvalInfamousMurderer.ts
+++ server/game/cards/07_TS26/units/MoraloEvalInfamousMurderer.ts
@@ -15,10 +15,10 @@ export default class MoraloEvalInfamousMurderer extends NonLeaderUnitCard {
         registrar.addTriggeredAbility({
             title: 'Deal 1 damage to a unit',
             when: {
-                onDamageDealt: (event, context) => {
+                onDamageDealt: (event, context) =>
                     // TODO: refactor damage enum types to account for the fact that overwhelm is combat damage
-                    return ((event.type === DamageType.Combat && event.damageSource.attack.getAllTargets().some((target) => target === context.player.base)) || (event.type === DamageType.Overwhelm && event.card === context.player.base));
-                }
+                    (event.type === DamageType.Combat && event.damageSource.attack.getAllTargets().some((target) => target === context.player.base)) ||
+                    (event.type === DamageType.Overwhelm && event.card === context.player.base)
             },
             optional: true,
             targetResolver: {
