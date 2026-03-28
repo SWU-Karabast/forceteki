a45b463f920c676f8c17369128574904a69993f7
Valentin Jubert <val_jub@live.fr>
2026-03-28 16:21:59 +0100

    Coleman Trebor & Mother Talzin (#2317)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/TS26/19.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/TS26/26.png)
    
    Close #2312

---
+++ server/game/cards/07_TS26/units/MotherTalzinStealingTheSpirit.ts
@@ -0,0 +1,41 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class MotherTalzinStealingTheSpirit extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'mother-talzin#stealing-the-spirit-id',
+            internalName: 'mother-talzin#stealing-the-spirit',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        // THIS IMPLEMENTATION IS NOT ACCURATE FOR TWIN SUNS
+        registrar.addWhenDefeatedAbility({
+            title: 'Look at an opponent\'s hand and discard a card from it. If you do, they draw a card. If the discarded card is a unit, for this phase you may play it from their discard pile, ignoring its aspect penalties.',
+            immediateEffect: abilityHelper.immediateEffects.lookAtAndSelectCard((context) => ({
+                target: context.player.opponent.hand,
+                immediateEffect: abilityHelper.immediateEffects.sequential([
+                    abilityHelper.immediateEffects.discardSpecificCard(),
+                    abilityHelper.immediateEffects.draw((context) => ({ target: context.player.opponent })),
+                ])
+            })),
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'For this phase you may play it from their discard pile, ignoring its aspect penalties',
+                ifYouDoCondition: () => ifYouDoContext.selectedPromptCards?.length === 1 && ifYouDoContext.selectedPromptCards[0].isUnit(),
+                immediateEffect: abilityHelper.immediateEffects.simultaneous([
+                    abilityHelper.immediateEffects.forThisPhaseCardEffect({
+                        target: ifYouDoContext.selectedPromptCards[0],
+                        effect: abilityHelper.ongoingEffects.canPlayFromDiscard({ player: ifYouDoContext.player })
+                    }),
+                    abilityHelper.immediateEffects.forThisPhasePlayerEffect({
+                        effect: abilityHelper.ongoingEffects.ignoreAllAspectPenalties({
+                            match: (card) => card === ifYouDoContext.selectedPromptCards[0],
+                        })
+                    })
+                ])
+            })
+        });
+    }
+}
