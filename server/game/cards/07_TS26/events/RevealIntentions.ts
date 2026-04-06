e9756b51dc52c81858d1fda2d91105a8097d2272
Valentin Jubert <val_jub@live.fr>
2026-04-06 09:38:14 +0200

    reveal intentions

---
+++ server/game/cards/07_TS26/events/RevealIntentions.ts
@@ -0,0 +1,33 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { EventCard } from '../../../core/card/EventCard';
+
+export default class RevealIntentions extends EventCard {
+    protected override getImplementationId() {
+        return {
+            id: 'reveal-intentions-id',
+            internalName: 'reveal-intentions',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper): void {
+        registrar.setEventAbility({
+            title: 'Play a non-Vehicle unit from your discard pile and give an Experience token to it',
+            immediateEffect: abilityHelper.immediateEffects.sequential([
+                abilityHelper.immediateEffects.lookAtAndSelectCard((context) => ({
+                    target: context.player.opponent.hand,
+                    canChooseFewer: false,
+                    immediateEffect: abilityHelper.immediateEffects.discardSpecificCard(),
+                })),
+                abilityHelper.immediateEffects.lookAtAndSelectCard((context) => ({
+                    target: context.player.hand,
+                    player: context.player.opponent,
+                    canChooseFewer: false,
+                    immediateEffect: abilityHelper.immediateEffects.discardSpecificCard()
+                })),
+                abilityHelper.immediateEffects.draw((context) => ({ target: context.player })),
+                abilityHelper.immediateEffects.draw((context) => ({ target: context.player.opponent })),
+            ])
+        });
+    }
+}
