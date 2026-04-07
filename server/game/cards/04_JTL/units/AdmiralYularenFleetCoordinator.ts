b980e698e5f61a0df8289e3bafce50299a999d4f
Robbie Moyer <rmoyer08@gmail.com>
2026-04-07 10:39:16 -0400

    WIP: keyword replacement tokens

--- server/game/cards/04_JTL/units/AdmiralYularenFleetCoordinator.ts
+++ server/game/cards/04_JTL/units/AdmiralYularenFleetCoordinator.ts
@@ -1,8 +1,8 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
-import * as KeywordHelpers from '../../../core/ability/KeywordHelpers';
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
 import { AbilityType, KeywordName, TargetMode, Trait, WildcardCardType, WildcardRelativePlayer } from '../../../core/Constants';
+import { TextHelper } from '../../../core/utils/TextHelper';
 import type { KeywordNameOrProperties } from '../../../Interfaces';
 import type { Player } from '../../../core/Player';
 
@@ -15,18 +15,24 @@ export default class AdmiralYularenFleetCoordinator extends NonLeaderUnitCard {
     }
 
     public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        const grit = TextHelper.Grit;
+        const restore1 = TextHelper.Restore(1);
+        const sentinel = TextHelper.Sentinel;
+        const shielded = TextHelper.Shielded;
+
         registrar.addWhenPlayedAbility({
-            title: `Choose Grit, Restore 1, Sentinel, or Shielded. While ${this.title} is in play, each friendly Vehicle unit gains the chosen keyword.`,
+            title: `Choose ${grit}, ${restore1}, ${sentinel}, or ${shielded}. While ${this.title} is in play, each friendly Vehicle unit gains the chosen keyword.`,
+            contextTitle: (context) => `Choose ${grit}, ${restore1}, ${sentinel}, or ${shielded}. While ${context.source.title} is in play, each friendly Vehicle unit gains the chosen keyword.`,
             targetResolver: {
                 mode: TargetMode.Select,
-                activePromptTitle: 'Choose Grit, Restore 1, Sentinel, or Shielded',
+                activePromptTitle: `Choose ${grit}, ${restore1}, ${sentinel}, or ${shielded}`,
                 choices: (context) => {
                     const playedByPlayer = context.player;
                     return {
-                        ['Grit']: this.buildYularenEffect(KeywordName.Grit, AbilityHelper, playedByPlayer),
-                        ['Restore 1']: this.buildYularenEffect({ keyword: KeywordName.Restore, amount: 1 }, AbilityHelper, playedByPlayer),
-                        ['Sentinel']: this.buildYularenEffect(KeywordName.Sentinel, AbilityHelper, playedByPlayer),
-                        ['Shielded']: this.buildYularenEffect(KeywordName.Shielded, AbilityHelper, playedByPlayer)
+                        [grit]: this.buildYularenEffect(KeywordName.Grit, AbilityHelper, playedByPlayer),
+                        [restore1]: this.buildYularenEffect({ keyword: KeywordName.Restore, amount: 1 }, AbilityHelper, playedByPlayer),
+                        [sentinel]: this.buildYularenEffect(KeywordName.Sentinel, AbilityHelper, playedByPlayer),
+                        [shielded]: this.buildYularenEffect(KeywordName.Shielded, AbilityHelper, playedByPlayer)
                     };
                 }
             }
@@ -35,11 +41,11 @@ export default class AdmiralYularenFleetCoordinator extends NonLeaderUnitCard {
 
     private buildYularenEffect(choice: KeywordNameOrProperties, AbilityHelper: IAbilityHelper, playedByPlayer: Player) {
         return AbilityHelper.immediateEffects.whileSourceInPlayCardEffect({
-            ongoingEffectDescription: `give ${KeywordHelpers.keywordDescription(choice)} to`,
+            ongoingEffectDescription: `give ${TextHelper.keyword(choice)} to`,
             ongoingEffectTargetDescription: 'each friendly Vehicle unit',
             effect: AbilityHelper.ongoingEffects.gainAbility({
                 type: AbilityType.Constant,
-                title: `Friendly Vehicle units gains ${choice}`,
+                title: `Friendly Vehicle units gains ${TextHelper.keyword(choice)}`,
                 targetController: WildcardRelativePlayer.Any,
                 targetCardTypeFilter: WildcardCardType.Unit,
                 matchTarget: (card) => card.hasSomeTrait(Trait.Vehicle) && card.controller === playedByPlayer,
