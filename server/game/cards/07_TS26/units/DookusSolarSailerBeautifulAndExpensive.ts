a147116a9881030a62881516ab1dcbe9518a98c8
Valentin Jubert <val_jub@live.fr>
2026-04-21 22:24:09 +0200

    Dooku's Solar Sailer (#2362)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/TS26/38.png)
    
    ---------
    
    Co-authored-by: Robbie Moyer <moyerr@users.noreply.github.com>

---
+++ server/game/cards/07_TS26/units/DookusSolarSailerBeautifulAndExpensive.ts
@@ -0,0 +1,39 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Trait, WildcardCardType } from '../../../core/Constants';
+import type { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import type { BasesHealedThisPhaseWatcher } from '../../../stateWatchers/BasesHealedThisPhaseWatcher';
+
+export default class DookusSolarSailerBeautifulAndExpensive extends NonLeaderUnitCard {
+    private basesHealedThisPhaseWatcher: BasesHealedThisPhaseWatcher;
+
+    protected override getImplementationId() {
+        return {
+            id: '5179344355',
+            internalName: 'dookus-solar-sailer#beautiful-and-expensive',
+        };
+    }
+
+    protected override setupStateWatchers (registrar: StateWatcherRegistrar, AbilityHelper: IAbilityHelper): void {
+        this.basesHealedThisPhaseWatcher = AbilityHelper.stateWatchers.basesHealedThisPhase();
+    }
+
+    public override setupCardAbilities (registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addTriggeredAbility({
+            title: 'Give an Experience token to another Separatist unit',
+            when: {
+                whenPlayed: true,
+                onAttack: true,
+            },
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                cardCondition: (card, context) => card !== context.source && card.hasSomeTrait(Trait.Separatist),
+                immediateEffect: abilityHelper.immediateEffects.conditional({
+                    condition: () => this.basesHealedThisPhaseWatcher.getCurrentValue().length > 0,
+                    onTrue: abilityHelper.immediateEffects.giveExperience()
+                })
+            }
+        });
+    }
+}
