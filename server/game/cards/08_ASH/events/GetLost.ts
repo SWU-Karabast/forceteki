3596f580ab7deffe6ffb05241159b84de341e180
Valentin Jubert <val_jub@live.fr>
2026-05-25 22:43:04 +0200

    get lost & sabine's lightsaber

---
+++ server/game/cards/08_ASH/events/GetLost.ts
@@ -0,0 +1,24 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import { EventCard } from '../../../core/card/EventCard';
+import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { WildcardCardType } from '../../../core/Constants';
+
+export default class GetLost extends EventCard {
+    protected override getImplementationId () {
+        return {
+            id: 'get-lost-id',
+            internalName: 'get-lost',
+        };
+    }
+
+    public override setupCardAbilities(registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.setEventAbility({
+            title: 'Defeat an upgraded non-leader unit',
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.NonLeaderUnit,
+                cardCondition: (card) => card.isNonLeaderUnit() && card.isUpgraded(),
+                immediateEffect: abilityHelper.immediateEffects.defeat()
+            },
+        });
+    }
+}
