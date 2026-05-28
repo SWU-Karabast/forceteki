da90551470924f947507d4016dfd8c6f129e5fc4
Valentin Jubert <val_jub@live.fr>
2026-05-28 08:04:32 +0200

    bothan 5

---
+++ server/game/cards/08_ASH/units/Bothan5NewRepublicPrisonShip.ts
@@ -0,0 +1,38 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { Trait, ZoneName } from '../../../core/Constants';
+import { EnumHelpers } from '../../../core/utils/EnumHelpers';
+
+export default class Bothan5NewRepublicPrisonShip extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'bothan5#new-republic-prison-ship-id',
+            internalName: 'bothan5#new-republic-prison-ship',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addTriggeredAbility({
+            title: 'This unit capture the defeated unit from your discard pile',
+            contextTitle: (context) => `${context.source.title} capture ${context.event.card.title} from your discard pile`,
+            when: {
+                onCardDefeated: (event, context) =>
+                    EnumHelpers.isUnit(event.lastKnownInformation.type) &&
+                    event.lastKnownInformation.controller === context.player &&
+                    event.card !== context.source &&
+                    !event.lastKnownInformation.traits?.has(Trait.Vehicle),
+            },
+            optional: true,
+            limit: abilityHelper.limit.perRound(1),
+            immediateEffect: abilityHelper.immediateEffects.conditional({
+                condition: (context) => context.event.card.zoneName === ZoneName.Discard,
+                onTrue: abilityHelper.immediateEffects.capture((context) => ({
+                    captor: context.source,
+                    target: context.event.card,
+                    fromOutOfPlay: true,
+                }))
+            })
+        });
+    }
+}
---
+++ test/server/cards/08_ASH/units/Bothan5NewRepublicPrisonShip.spec.ts
@@ -0,0 +1,138 @@
+describe('Bothan-5, New Republic Prison Ship', function() {
+    integration(function(contextRef) {
+        it('Bothan-5\'s ability should capture a defeated friendly non-vehicle unit from discard (only 1 time per round)', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['wampa', 'battlefield-marine', 'gungi#finding-himself'],
+                    spaceArena: ['bothan5#new-republic-prison-ship']
+                },
+                player2: {
+                    hasInitiative: true,
+                    groundArena: ['atst'],
+                    hand: ['takedown', 'rivals-fall'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player2.clickCard(context.takedown);
+            context.player2.clickCard(context.wampa);
+
+            expect(context.player1).toHavePassAbilityPrompt('Bothan-5 capture Wampa from your discard pile');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.wampa).toBeCapturedBy(context.bothan5);
+
+            context.player1.passAction();
+
+            context.player2.clickCard(context.rivalsFall);
+            context.player2.clickCard(context.battlefieldMarine);
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.battlefieldMarine).toBeInZone('discard');
+
+            context.moveToNextActionPhase();
+
+            context.player2.clickCard(context.atst);
+            context.player2.clickCard(context.gungi);
+
+            expect(context.player1).toHavePassAbilityPrompt('Bothan-5 capture Gungi from your discard pile');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.gungi).toBeCapturedBy(context.bothan5);
+        });
+
+        it('Bothan-5\'s ability should not capture a defeated friendly Vehicle unit', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['atst'],
+                    spaceArena: ['bothan5#new-republic-prison-ship']
+                },
+                player2: {
+                    hasInitiative: true,
+                    hand: ['rivals-fall'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player2.clickCard(context.rivalsFall);
+            context.player2.clickCard(context.atst);
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.atst).toBeInZone('discard');
+        });
+
+        it('Bothan-5\'s ability should capture a defeated enemy non-vehicle unit from discard', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['rivals-fall'],
+                    spaceArena: ['bothan5#new-republic-prison-ship']
+                },
+                player2: {
+                    groundArena: ['wampa'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.rivalsFall);
+            context.player1.clickCard(context.wampa);
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.wampa).toBeInZone('discard');
+        });
+
+        it('Bothan-5\'s ability should not capture a bounced friendly non-vehicle unit from discard', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['wampa'],
+                    spaceArena: ['bothan5#new-republic-prison-ship']
+                },
+                player2: {
+                    hasInitiative: true,
+                    hand: ['waylay'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player2.clickCard(context.waylay);
+            context.player2.clickCard(context.wampa);
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.wampa).toBeInZone('hand');
+        });
+
+        it('Bothan-5\'s ability should not capture a defeated friendly piloting unit', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['chewbacca#faithful-first-mate'],
+                    spaceArena: ['bothan5#new-republic-prison-ship', 'awing']
+                },
+                player2: {
+                    hand: ['outer-rim-constable'],
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.chewbacca);
+            context.player1.clickPrompt('Play Chewbacca with Piloting');
+            context.player1.clickCard(context.awing);
+
+            context.player2.clickCard(context.outerRimConstable);
+            context.player2.clickCard(context.chewbacca);
+
+            expect(context.player1).toBeActivePlayer();
+            expect(context.chewbacca).toBeInZone('discard');
+        });
+    });
+});
