5664f5abe3a178c4b132e97503aeeb0bf82a147f
Valentin Jubert <val_jub@live.fr>
2026-06-03 18:41:32 +0200

    pr comment (bug)

--- server/game/cards/08_ASH/events/ChooseYourPath.ts
+++ server/game/cards/08_ASH/events/ChooseYourPath.ts
@@ -1,7 +1,7 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
 import { EventCard } from '../../../core/card/EventCard';
 import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
-import { TargetMode, Trait } from '../../../core/Constants';
+import { Trait } from '../../../core/Constants';
 
 export default class ChooseYourPath extends EventCard {
     protected override getImplementationId () {
@@ -14,9 +14,8 @@ export default class ChooseYourPath extends EventCard {
     public override setupCardAbilities(registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.setEventAbility({
             title: 'Choose one: If you control a Force Unit, heal 5 damage from your base. If you control a Mandalorian unit, create a Mandalorian token and give an Advantage token to it.',
-            targetResolver: {
-                mode: TargetMode.Select,
-                showUnresolvable: true,
+            immediateEffect: abilityHelper.immediateEffects.chooseModalEffects({
+                amountOfChoices: 1,
                 choices: {
                     ['If you control a Force unit, heal 5 damage from your base']:
                         abilityHelper.immediateEffects.conditional({
@@ -28,11 +27,14 @@ export default class ChooseYourPath extends EventCard {
                             condition: (c) => c.player.hasSomeArenaUnit({ trait: Trait.Mandalorian }),
                             onTrue: abilityHelper.immediateEffects.sequential([
                                 abilityHelper.immediateEffects.createMandalorian(),
-                                abilityHelper.immediateEffects.giveAdvantage((context) => ({ target: context.events[0]?.generatedTokens[0] }))
+                                abilityHelper.immediateEffects.giveAdvantage((context) => {
+                                    console.log(context.events[0]?.generatedTokens?.length); //TODO GET GENERATION EVENT
+                                    return ({ target: context.events[1]?.generatedTokens[0] });
+                                })
                             ])
                         })
                 }
-            },
+            })
         });
     }
 }
