5267b4543168bc60d994f428f27a8b3f2cf47859
sethhelman <s7helman@gmail.com>
2026-06-03 12:21:17 -0400

    make it work

--- server/game/cards/08_ASH/events/Masterstroke.ts
+++ server/game/cards/08_ASH/events/Masterstroke.ts
@@ -1,7 +1,7 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
 import { EventCard } from '../../../core/card/EventCard';
 import type { IEventAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
-import type { AbilityContext } from '../../../core/ability/AbilityContext';
+import type { ZoneName } from '../../../core/Constants';
 
 export default class Masterstroke extends EventCard {
     protected override getImplementationId () {
@@ -11,20 +11,14 @@ export default class Masterstroke extends EventCard {
         };
     }
 
-    private getPowerFromContext(context: AbilityContext): number {
-        const arenaName = context.target.zoneName;
-        const arena = context.player.opponent.getArenaUnits({ arena: arenaName });
-        return arena.length;
-    }
-
     public override setupCardAbilities (registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.setEventAbility({
             title: 'Attack with a unit. It gets +1/+0 for this attack for each unit the defending player controls in its arena.',
             targetResolver: {
                 immediateEffect: abilityHelper.immediateEffects.attack({
-                    attackerLastingEffects: (context) => ({
+                    attackerLastingEffects: (context, attack) => ({
                         effect: abilityHelper.ongoingEffects.modifyStats({
-                            power: this.getPowerFromContext(context),
+                            power: attack.getDefendingPlayer().getArenaUnits({ arena: attack.attacker.zoneName as ZoneName.GroundArena | ZoneName.SpaceArena }).length,
                             hp: 0,
                         })
                     })
--- test/server/cards/08_ASH/events/Masterstroke.spec.ts
+++ test/server/cards/08_ASH/events/Masterstroke.spec.ts
@@ -1,6 +1,6 @@
 describe('Masterstroke', function() {
     integration(function(contextRef) {
-        it('Masterstroke\'s ability should give a unit +1/+0 for each unit the defending player controls in its arena', async function () {
+        it('Masterstroke\'s ability should give a unit +1/+0 for each unit the defending player controls in its arena for ground', async function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
@@ -25,6 +25,35 @@ describe('Masterstroke', function() {
 
             expect(context.p2Base.damage).toBe(7);
             expect(context.battlefieldMarine.getPower()).toBe(3);
+
+            expect(context.player2).toBeActivePlayer();
+        });
+
+        it('Masterstroke\'s ability should give a unit +1/+0 for each unit the defending player controls in its arena for space', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['masterstroke'],
+                    groundArena: ['battlefield-marine'],
+                    spaceArena: [{ card: 'awing', upgrades: ['shield'] }]
+                },
+                player2: {
+                    groundArena: [{ card: 'atst', upgrades: ['pointless-to-resist'] }, 'death-star-stormtrooper', 'guavian-antagonizer'],
+                    spaceArena: ['phoenix-squadron-awing'],
+                    leader: { card: 'sabine-wren#galvanized-revolutionary', deployed: true, upgrades: ['fulcrum'] }
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.masterstroke);
+            expect(context.player1).toBeAbleToSelectExactly([context.awing, context.battlefieldMarine]);
+            context.player1.clickCard(context.awing);
+
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.p2Base.damage).toBe(3);
+            expect(context.awing.getPower()).toBe(1);
 
             expect(context.player2).toBeActivePlayer();
         });
