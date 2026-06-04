b96bd5b6964a460474e3bee7167257e7c0913e9f
sethhelman <s7helman@gmail.com>
2026-06-04 11:20:50 -0400

    Update VaneQuarrelsomePirate.spec.ts

--- test/server/cards/08_ASH/leaders/VaneQuarrelsomePirate.spec.ts
+++ test/server/cards/08_ASH/leaders/VaneQuarrelsomePirate.spec.ts
@@ -76,6 +76,7 @@ describe('Vane, Quarrelsome Pirate', function() {
                 expect(context.p1Base.damage).toBe(0);
 
                 expect(context.player2).toBeActivePlayer();
+                expect(context.battlefieldMarine).toHaveExactUpgradeNames(['nimble-prowess']);
             });
 
             it('should deal 2 damage to friendly base after defeating a friendly upgrade', async function() {
@@ -147,6 +148,91 @@ describe('Vane, Quarrelsome Pirate', function() {
                 expect(context.player1).toHavePrompt('Choose a friendly upgrade to defeat');
                 expect(context.player1).toBeAbleToSelectExactly([context.p1Tokens.advantage, context.nimbleProwess]);
                 context.player1.clickCard(context.p1Tokens.advantage);
+
+                expect(context.p1Base.damage).toBe(2);
+                expect(context.p2Base.damage).toBe(0);
+
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not be able to select an upgrade that was played on a friendly unit by the enemy', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'vane#quarrelsome-pirate',
+                        groundArena: [{ card: 'battlefield-marine', upgrades: ['advantage', 'nimble-prowess'] }]
+                    },
+                    player2: {
+                        hand: ['shadow-of-stygeon-prime'],
+                        groundArena: ['atst'],
+                        spaceArena: [{ card: 'wing-leader', upgrades: ['experience'] }],
+                        leader: { card: 'grand-inquisitor#hunting-the-jedi', deployed: true },
+                        hasInitiative: true
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.p1Tokens = {
+                    advantage: context.player1.findCardByName('advantage'),
+                };
+                context.p2Tokens = {
+                    experience: context.player2.findCardByName('experience'),
+                };
+
+                context.player2.clickCard(context.shadowOfStygeonPrime);
+                context.player2.clickCard(context.battlefieldMarine);
+
+                context.player1.clickCard(context.vane);
+                context.player1.clickPrompt('Deal 2 damage to a base');
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                context.player1.clickCard(context.p1Base);
+
+                expect(context.player1).toHavePrompt('Choose a friendly upgrade to defeat');
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Tokens.advantage, context.nimbleProwess]);
+                context.player1.clickCard(context.p1Tokens.advantage);
+
+                expect(context.p1Base.damage).toBe(2);
+                expect(context.p2Base.damage).toBe(0);
+
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should be able to select a token upgrade on a unit that changed control', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        leader: 'vane#quarrelsome-pirate',
+                        hand: ['change-of-heart']
+                    },
+                    player2: {
+                        hand: ['shadow-of-stygeon-prime'],
+                        groundArena: ['atst', { card: 'battlefield-marine', upgrades: ['advantage', 'nimble-prowess'] }],
+                        spaceArena: [{ card: 'wing-leader', upgrades: ['experience'] }],
+                        leader: { card: 'grand-inquisitor#hunting-the-jedi', deployed: true },
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.p2Tokens = {
+                    experience: context.player2.findCardByName('experience'),
+                    advantage: context.player2.findCardByName('advantage')
+                };
+
+                context.player1.clickCard(context.changeOfHeart);
+                context.player1.clickCard(context.battlefieldMarine);
+
+                context.player2.passAction();
+
+                context.player1.clickCard(context.vane);
+                context.player1.clickPrompt('Deal 2 damage to a base');
+                expect(context.player1).toBeAbleToSelectExactly([context.p1Base, context.p2Base]);
+                context.player1.clickCard(context.p1Base);
+
+                expect(context.player1).toHavePrompt('Choose a friendly upgrade to defeat');
+                expect(context.player1).toBeAbleToSelectExactly([context.p2Tokens.advantage]);
+                context.player1.clickCard(context.p2Tokens.advantage);
 
                 expect(context.p1Base.damage).toBe(2);
                 expect(context.p2Base.damage).toBe(0);
