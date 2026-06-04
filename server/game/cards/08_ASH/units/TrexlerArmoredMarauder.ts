5870f60c752d36411c830ea2b2f4b4f8b8dfd29e
sethhelman <s7helman@gmail.com>
2026-06-04 16:00:07 -0400

    covert believers, trexler armored marauders, nebulon frigate

---
+++ server/game/cards/08_ASH/units/TrexlerArmoredMarauder.ts
@@ -0,0 +1,23 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class TrexlerArmoredMarauder extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '6339984693',
+            internalName: 'trexler-armored-marauder',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addWhenPlayedAbility({
+            title: 'Give a shield token to a unit that costs 3 or less',
+            optional: true,
+            targetResolver: {
+                cardCondition: (card) => card.isUnit() && card.cost <= 3,
+                immediateEffect: AbilityHelper.immediateEffects.giveShield()
+            }
+        });
+    }
+}
