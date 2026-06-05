c1a52d48fee65643c80d77ae135035451caf9991
Valentin Jubert <val_jub@live.fr>
2026-06-05 15:48:28 +0200

    Bokken Saber, Camtono, DDC Defender (#2523)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/180.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/210.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/229.png)

--- server/game/cards/08_ASH/upgrades/WhistlingBirds.ts
+++ server/game/cards/08_ASH/upgrades/WhistlingBirds.ts
@@ -24,12 +24,9 @@ export default class WhistlingBirds extends UpgradeCard {
     public override setupCardAbilities(registrar: IUpgradeAbilityRegistrar, AbilityHelper: IAbilityHelper) {
         registrar.setAttachCondition((context) => !context.attachTarget.hasSomeTrait(Trait.Vehicle));
 
-        registrar.addGainTriggeredAbilityTargetingAttached({
+        registrar.addGainWhenAttackEndsAbilityTargetingAttached({
             title: 'Deal 2 damage to each unit that opponent controls in this unit\'s arena',
             contextTitle: (context) => this.makeAbilityTitle(context),
-            when: {
-                onAttackEnd: (event, context) => event.attack.attacker === context.source
-            },
             immediateEffect: AbilityHelper.immediateEffects.conditional({
                 condition: (context) =>
                     this.damageDealtThisPhaseWatcher.unitHasDealtCombatDamageToBaseThisAttack(context.source, context),
