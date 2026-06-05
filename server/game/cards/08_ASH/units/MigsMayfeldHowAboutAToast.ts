aaa342eb6afc694054a75a4539c5ca13451db1e9
sethhelman <s7helman@gmail.com>
2026-06-05 13:25:28 -0400

    perseverance, danger squadron wingmen, Migs mayfeld

---
+++ server/game/cards/08_ASH/units/MigsMayfeldHowAboutAToast.ts
@@ -0,0 +1,37 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import type { IAttackableCard } from '../../../core/card/CardInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+
+export default class MigsMayfeldHowAboutAToast extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '2696522642',
+            internalName: 'migs-mayfeld#how-about-a-toast',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addOnAttackAbility({
+            title: 'Deal 1 damage to the defending unit. If this unit is upgraded, deal 2 damage to the defending unit instead',
+            immediateEffect: AbilityHelper.immediateEffects.conditional((context) => ({
+                condition: (context) => context.source.isUpgraded(),
+                onTrue: AbilityHelper.immediateEffects.conditional({
+                    condition: () => this.checkMigsCondition(context.source.activeAttack?.getSingleTarget()),
+                    onTrue: AbilityHelper.immediateEffects.damage({ target: context.source.activeAttack?.getSingleTarget(), amount: 2 })
+                }),
+                onFalse: AbilityHelper.immediateEffects.conditional({
+                    condition: () => this.checkMigsCondition(context.source.activeAttack?.getSingleTarget()),
+                    onTrue: AbilityHelper.immediateEffects.damage({ target: context.source.activeAttack?.getSingleTarget(), amount: 1 })
+                }),
+            }))
+        });
+    }
+
+    private checkMigsCondition(defender: IAttackableCard): boolean {
+        if (defender.isBase()) {
+            return false;
+        }
+        return true;
+    }
+}
