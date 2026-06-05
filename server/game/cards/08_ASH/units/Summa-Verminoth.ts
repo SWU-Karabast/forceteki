bbd221046583df7a8256c7836c4ba9fbe1ad521f
sethhelman <s7helman@gmail.com>
2026-06-05 14:04:40 -0400

    buy time, atst raider, summa verminoth

---
+++ server/game/cards/08_ASH/units/Summa-Verminoth.ts
@@ -0,0 +1,23 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { ZoneName } from '../../../core/Constants';
+
+export default class Summaverminoth extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: '8724833287',
+            internalName: 'summaverminoth',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addOnAttackAbility({
+            title: 'Defeat all other space units',
+            immediateEffect: AbilityHelper.immediateEffects.defeat((context) => {
+                const allSpaceUnits = context.game.getArenaUnits({ otherThan: context.source, arena: ZoneName.SpaceArena });
+                return { target: allSpaceUnits };
+            })
+        });
+    }
+}
