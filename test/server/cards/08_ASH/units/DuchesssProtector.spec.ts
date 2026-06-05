3ef286ca032d8ab8cfa1dfd9d70401863c19c6d9
Santiago Vázquez <santiagovazq@gmail.com>
2026-06-04 21:38:58 -0300

    Add NGOR test

--- test/server/cards/08_ASH/units/DuchesssProtector.spec.ts
+++ test/server/cards/08_ASH/units/DuchesssProtector.spec.ts
@@ -27,5 +27,32 @@ describe('Duchess\'s Protector', function() {
 
             expect(context.player1).toBeActivePlayer();
         });
+
+        it('should create an exhausted Mandalorian token for the opponent when defeated with No Glory Only Results', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['duchesss-protector'],
+                },
+                player2: {
+                    hand: ['no-glory-only-results'],
+                    hasInitiative: true
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player2.clickCard(context.noGloryOnlyResults);
+            context.player2.clickCard(context.duchesssProtector);
+
+            expect(context.duchesssProtector).toBeInZone('discard', context.player1);
+
+            const mandalorians = context.player2.findCardsByName('mandalorian');
+            expect(mandalorians.length).toBe(1);
+            expect(mandalorians[0]).toBeInZone('groundArena', context.player2);
+            expect(mandalorians[0].exhausted).toBeTrue();
+
+            expect(context.player1).toBeActivePlayer();
+        });
     });
 });
