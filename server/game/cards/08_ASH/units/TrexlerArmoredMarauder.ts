8e789c2a8737961ab42cc3cafe246c3c0516567a
sethhelman <s7helman@gmail.com>
2026-06-04 21:51:54 -0400

    Update TrexlerArmoredMarauder.ts

--- server/game/cards/08_ASH/units/TrexlerArmoredMarauder.ts
+++ server/game/cards/08_ASH/units/TrexlerArmoredMarauder.ts
@@ -1,6 +1,7 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { WildcardCardType } from '../../../core/Constants';
 
 export default class TrexlerArmoredMarauder extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -15,6 +16,7 @@ export default class TrexlerArmoredMarauder extends NonLeaderUnitCard {
             title: 'Give a shield token to a unit that costs 3 or less',
             optional: true,
             targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
                 cardCondition: (card) => card.isUnit() && card.cost <= 3,
                 immediateEffect: AbilityHelper.immediateEffects.giveShield()
             }
