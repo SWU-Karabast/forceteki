aaf7eaf97e69b2a5b0633157e3d362e0d4fe8194
Valentin Jubert <val_jub@live.fr>
2026-06-07 23:00:14 +0200

    some tests

--- test/server/cards/08_ASH/upgrades/ArcanaStarMap.spec.ts
+++ test/server/cards/08_ASH/upgrades/ArcanaStarMap.spec.ts
@@ -159,6 +159,194 @@ describe('Arcana Star Map', function () {
                 });
                 context.player2.clickPrompt('Take nothing');
             });
+
+            it('', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['search-your-feelings'],
+                        groundArena: [{ card: 'battlefield-marine', upgrades: ['arcana-star-map#path-to-peridea'] }],
+                        deck: ['wampa', 'atst', 'yoda#old-master']
+                    },
+                });
+
+                const buildCardName = (card) => `${card.title}${card.subtitle ? ', ' + card.subtitle : ''}`;
+
+                const { context } = contextRef;
+
+                // TODO REFACTOR THIS WHEN ENABLING FULL DECK SEARCH
+
+                context.player1.clickCard(context.searchYourFeelings);
+                expect(context.player1).toHaveExactDropdownListOptions([context.wampa, context.atst, context.yoda].map((card) => buildCardName(card)));
+
+                context.player1.chooseListOption('Wampa');
+
+                expect(context.getChatLogs(3).join('\n')).not.toContain('Wampa');
+                expect(context.player2).toBeActivePlayer();
+                expect(context.searchYourFeelings).toBeInZone('discard');
+                expect(context.wampa).toBeInZone('hand');
+                expect(context.player1.deck.length).toBe(2);
+            });
+
+            it('', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['annihilator#tagges-flagship'],
+                        groundArena: [{ card: 'battlefield-marine', upgrades: ['arcana-star-map#path-to-peridea'] }],
+                    },
+                    player2: {
+                        groundArena: ['boba-fett#disintegrator', 'wampa'],
+                        deck: ['boba-fett#disintegrator', 'boba-fett#feared-bounty-hunter', 'cartel-spacer', 'elite-p38-starfighter', 'crafty-smuggler']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                const inPlayBoba = context.player2.findCardByName('boba-fett#disintegrator', 'groundArena');
+                const inDeckBoba = context.player2.findCardByName('boba-fett#disintegrator', 'deck');
+                const inDeckPilotBoba = context.player2.findCardByName('boba-fett#feared-bounty-hunter', 'deck');
+
+                context.player1.clickCard(context.annihilator);
+                context.player1.clickCard(inPlayBoba);
+                expect(inPlayBoba).toBeInZone('discard');
+
+                // Player sees the opponent's deck
+                expect(context.player1).toHaveExactDisplayPromptCards({
+                    selectable: [inDeckBoba, inDeckPilotBoba],
+                    // invalid: [context.cartelSpacer]      // TODO: uncomment when we re-enable full deck search
+                });
+
+                context.player1.clickCardInDisplayCardPrompt(inDeckBoba);
+                context.player1.clickCardInDisplayCardPrompt(inDeckPilotBoba);
+                context.player1.clickDone();
+
+                expect(context.player2).toBeActivePlayer();
+                expect(inDeckBoba).toBeInZone('discard');
+                expect(inDeckPilotBoba).toBeInZone('discard');
+            });
+
+            it('', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [{ card: 'wampa', upgrades: ['arcana-star-map#path-to-peridea'] }],
+                        hand: ['darth-vader#commanding-the-first-legion'],
+                        deck: [
+                            'battlefield-marine', // 1
+                            'vanguard-infantry', // 2
+                            'scout-bike-pursuer', // 3
+                            'hunting-nexu', // 4
+                            'tieln-fighter', // 5
+                            'daring-raid', // 6
+                            'protector', // 7
+                            'isb-agent', // 8
+                            'death-star-stormtrooper', // 9
+                            'superlaser-technician', // 10
+                            'atst', // 11
+                            'awing', // 12
+                            'yoda#old-master', // 13
+                            'gungi#finding-himself', // 14
+                            'takedown', // 15
+                            'rivals-fall', // 16
+                            'avenger#hunting-star-destroyer', // 17
+                            'tie-striker', // 18
+                            'peridea-bandit', // 19
+                            'mouse-droid', // 20
+                            'lothwolf'// 21
+                        ],
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.darthVader);
+                context.player1.clickPrompt('(No effect) Ambush');
+                expect(context.player1).toHaveExactDisplayPromptCards({
+                    selectable: [
+                        context.scoutBikePursuer, context.tielnFighter, context.isbAgent, context.deathStarStormtrooper,
+                        context.superlaserTechnician, context.tieStriker, context.perideaBandit, context.mouseDroid
+                    ],
+                    invalid: [
+                        context.vanguardInfantry, context.huntingNexu, context.daringRaid, context.protector,
+                        context.battlefieldMarine, context.atst, context.awing, context.yoda, context.takedown,
+                        context.gungi, context.rivalsFall, context.avenger
+                    ]
+                });
+
+                context.player1.clickCardInDisplayCardPrompt(context.perideaBandit);
+                context.player1.clickCardInDisplayCardPrompt(context.mouseDroid);
+                context.player1.clickDone();
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.perideaBandit).toBeInZone('groundArena');
+                expect(context.mouseDroid).toBeInZone('groundArena');
+
+                expect([
+                    context.vanguardInfantry, context.huntingNexu, context.daringRaid, context.protector,
+                    context.battlefieldMarine, context.atst, context.awing, context.yoda, context.takedown,
+                    context.gungi, context.rivalsFall, context.avenger, context.scoutBikePursuer, context.tielnFighter,
+                    context.isbAgent, context.deathStarStormtrooper, context.superlaserTechnician, context.tieStriker,
+                ]).toAllBeInBottomOfDeck(context.player1, 18);
+            });
+
+            it('', async function () {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: [{ card: 'wampa', upgrades: ['arcana-star-map#path-to-peridea'] }],
+                        hand: ['prepare-for-takeoff'],
+                        deck: [
+                            'battlefield-marine', // 1
+                            'vanguard-infantry', // 2
+                            'scout-bike-pursuer', // 3
+                            'hunting-nexu', // 4
+                            'tieln-fighter', // 5
+                            'daring-raid', // 6
+                            'protector', // 7
+                            'isb-agent', // 8
+                            'death-star-stormtrooper', // 9
+                            'superlaser-technician', // 10
+                            'atst', // 11
+                            'awing', // 12
+                            'yoda#old-master', // 13
+                            'gungi#finding-himself', // 14
+                            'raddus#holdos-final-command', // 15
+                            'avenger#hunting-star-destroyer', // 16,
+                            'mouse-droid'
+                        ],
+                    }
+                });
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.prepareForTakeoff);
+
+                expect(context.player1).toHaveExactDisplayPromptCards({
+                    selectable: [
+                        context.tielnFighter, context.awing, context.atst, context.avenger, context.raddus
+                    ],
+                    invalid: [
+                        context.isbAgent, context.deathStarStormtrooper, context.superlaserTechnician,
+                        context.vanguardInfantry, context.huntingNexu, context.daringRaid, context.protector,
+                        context.battlefieldMarine, context.yoda,
+                        context.gungi
+                    ]
+                });
+
+                context.player1.clickCardInDisplayCardPrompt(context.raddus);
+                context.player1.clickCardInDisplayCardPrompt(context.avenger);
+                context.player1.clickDone();
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.raddus).toBeInZone('groundArena');
+                expect(context.avenger).toBeInZone('groundArena');
+
+                expect([
+                    context.isbAgent, context.deathStarStormtrooper, context.superlaserTechnician,
+                    context.vanguardInfantry, context.huntingNexu, context.daringRaid, context.protector,
+                    context.battlefieldMarine, context.yoda,
+                    context.gungi, context.tielnFighter, context.awing, context.atst,
+                ]).toAllBeInBottomOfDeck(context.player1, 14);
+            });
         });
     });
 });
