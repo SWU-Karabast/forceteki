6a505755e0d0dbf7bae0b28d11de9e0c86b26d38
Valentin Jubert <val_jub@live.fr>
2026-06-07 20:13:32 +0200

    Mark My Words, Preparation & Unfettered Ambition (#2517)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/181.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/182.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/ASH/228.png)
    
    ---------
    
    Co-authored-by: AMMayberry1 <ammayberry1@gmail.com>

---
+++ test/server/cards/08_ASH/units/EvisceratorBurnThemAway.spec.ts
@@ -0,0 +1,91 @@
+describe('Eviscerator, Burn Them Away', function() {
+    integration(function(contextRef) {
+        it('Eviscerator\'s when played ability should give 2 Advantage token to other friendly units', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['eviscerator#burn-them-away'],
+                    groundArena: ['wampa', 'porg'],
+                    spaceArena: ['awing']
+                },
+                player2: {
+                    groundArena: ['sundari-peacekeeper'],
+                }
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.eviscerator);
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.wampa).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.awing).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.porg).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.sundariPeacekeeper).toHaveExactUpgradeNames([]);
+            expect(context.eviscerator).toHaveExactUpgradeNames([]);
+        });
+
+        it('Eviscerator\'s on attack ability should give 2 Advantage token to other friendly units', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: [],
+                    groundArena: ['wampa', 'porg'],
+                    spaceArena: ['awing', 'eviscerator#burn-them-away']
+                },
+                player2: {
+                    groundArena: ['sundari-peacekeeper'],
+                }
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.eviscerator);
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.wampa).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.awing).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.porg).toHaveExactUpgradeNames(['advantage', 'advantage']);
+            expect(context.sundariPeacekeeper).toHaveExactUpgradeNames([]);
+            expect(context.eviscerator).toHaveExactUpgradeNames([]);
+        });
+
+        it('Eviscerator\'s constant ability should make friendly Advantage tokens lose all abilities', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['eviscerator#burn-them-away'],
+                    groundArena: ['wampa', 'porg'],
+                    spaceArena: [{ card: 'awing', upgrades: ['advantage', 'advantage'] }]
+                },
+                player2: {
+                    hand: ['rivals-fall'],
+                    groundArena: [{ card: 'sundari-peacekeeper', upgrades: ['advantage'] }],
+                }
+            });
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.eviscerator);
+
+            context.player2.clickCard(context.sundariPeacekeeper);
+            context.player2.clickCard(context.p1Base);
+            expect(context.sundariPeacekeeper).toHaveExactUpgradeNames([]);
+
+            context.player1.clickCard(context.awing);
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.awing).toHaveExactUpgradeNames(['advantage', 'advantage', 'advantage', 'advantage']);
+
+            context.player2.clickCard(context.rivalsFall);
+            context.player2.clickCard(context.eviscerator);
+
+            expect(context.wampa).toHaveExactUpgradeNames(['advantage', 'advantage']);
+
+            context.player1.clickCard(context.wampa);
+            context.player1.clickCard(context.p2Base);
+            context.player1.clickPrompt('Defeat Advantage token');
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.wampa).toHaveExactUpgradeNames([]);
+        });
+    });
+});
