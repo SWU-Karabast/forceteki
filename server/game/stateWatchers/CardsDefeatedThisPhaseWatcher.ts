15341a33748948ba692fe2700ba887f57a28afcc
Robbie Moyer <rmoyer08@gmail.com>
2026-06-10 12:58:31 -0400

    Update current value to include units and upgrades

--- server/game/stateWatchers/CardsDefeatedThisPhaseWatcher.ts
+++ server/game/stateWatchers/CardsDefeatedThisPhaseWatcher.ts
@@ -63,22 +63,18 @@ export class CardsDefeatedThisPhaseWatcher extends StateWatcher<DefeatedCardEntr
         }));
     }
 
-    /**
-     * Returns an array of {@link DefeatedCardEntry} objects representing every unit defeated
-     * this phase so far, as well as the controlling and defeating player.
-     */
-    public override getCurrentValue() {
-        return super.getCurrentValue().filter((entry) => EnumHelpers.isUnit(entry.lastKnownInformation.type));
-    }
-
     /** Get the list of the units that were defeated this phase */
     public someUnitDefeatedThisPhase(filter: (entry: UnwrapRef<DefeatedCardEntry>) => boolean): boolean {
-        return this.getCurrentValue().filter(filter).length > 0;
+        return this.getCurrentValue()
+            .filter((entry) => EnumHelpers.isUnit(entry.lastKnownInformation.type))
+            .filter(filter)
+            .length > 0;
     }
 
     /** Get the list of the specified player's units that were defeated */
     public getDefeatedUnitsControlledByPlayer(controller: Player): InPlayUnit[] {
         return this.getCurrentValue()
+            .filter((entry) => EnumHelpers.isUnit(entry.lastKnownInformation.type))
             .filter((entry) => entry.controlledBy === controller)
             .map((entry) => ({ unit: entry.card as IUnitCard, inPlayId: entry.inPlayId }));
     }
@@ -94,12 +90,21 @@ export class CardsDefeatedThisPhaseWatcher extends StateWatcher<DefeatedCardEntr
 
     /** Check if there is some units controlled by player that was defeated this phase */
     public someDefeatedUnitControlledByPlayer(controller: Player): boolean {
-        return this.getCurrentValue().filter((entry) => entry.controlledBy === controller).length > 0;
+        return this.getCurrentValue()
+            .filter((entry) =>
+                EnumHelpers.isUnit(entry.lastKnownInformation.type) &&
+                entry.controlledBy === controller
+            ).length > 0;
     }
 
     /** Check if there is some units controlled by player that was defeated this phase */
     public someDefeatedWhileAttackingUnitControlledByPlayer(controller: Player): boolean {
-        return this.getCurrentValue().filter((entry) => entry.controlledBy === controller && entry.wasDefeatedWhileAttacking).length > 0;
+        return this.getCurrentValue()
+            .filter((entry) =>
+                EnumHelpers.isUnit(entry.lastKnownInformation.type) &&
+                entry.controlledBy === controller &&
+                entry.wasDefeatedWhileAttacking
+            ).length > 0;
     }
 
     /** Check if the given player defeated an enemy unit */
@@ -112,7 +117,7 @@ export class CardsDefeatedThisPhaseWatcher extends StateWatcher<DefeatedCardEntr
         controller?: Player;
         filter?: (entry: UnwrapRef<DefeatedCardEntry>) => boolean;
     } = {}): boolean {
-        return super.getCurrentValue()
+        return this.getCurrentValue()
             .filter((entry) => EnumHelpers.isUpgrade(entry.lastKnownInformation.type))
             .filter((entry) => controller == null || entry.controlledBy === controller)
             .filter((entry) => filter == null || filter(entry))
