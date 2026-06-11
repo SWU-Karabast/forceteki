4865fc02bc61238064180c677954c47fa3c352d5
Anthony Bronico <adbronico92@gmail.com>
2026-06-11 18:34:42 -0400

    Card Clarification Changes / Tests (#2151)
    Made changes to support a few broken interactions:
    - Mon Mothma letting a unit attack, returning it to hand, and attacking
    again
    - JTL Yularen changing control now always gives his keywords to owner's
    units
    - Survivors Gauntlet + stolen pilot
    - Hondo Ohnaka + Timely Intervention
    - Updated an Exploit test to properly order the Bossk trigger last
    
    ---------
    
    Co-authored-by: sethhelman <s7helman@gmail.com>

--- server/game/cards/04_JTL/units/AdmiralYularenFleetCoordinator.ts
+++ server/game/cards/04_JTL/units/AdmiralYularenFleetCoordinator.ts
@@ -2,8 +2,9 @@ import type { IAbilityHelper } from '../../../AbilityHelper';
 import * as KeywordHelpers from '../../../core/ability/KeywordHelpers';
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
-import { AbilityType, KeywordName, RelativePlayer, TargetMode, Trait, WildcardCardType } from '../../../core/Constants';
+import { AbilityType, KeywordName, TargetMode, Trait, WildcardCardType, WildcardRelativePlayer } from '../../../core/Constants';
 import type { KeywordNameOrProperties } from '../../../Interfaces';
+import type { Player } from '../../../core/Player';
 
 export default class AdmiralYularenFleetCoordinator extends NonLeaderUnitCard {
     protected override getImplementationId() {
@@ -19,26 +20,29 @@ export default class AdmiralYularenFleetCoordinator extends NonLeaderUnitCard {
             targetResolver: {
                 mode: TargetMode.Select,
                 activePromptTitle: 'Choose Grit, Restore 1, Sentinel, or Shielded',
-                choices: {
-                    ['Grit']: this.buildYularenEffect(KeywordName.Grit, AbilityHelper),
-                    ['Restore 1']: this.buildYularenEffect({ keyword: KeywordName.Restore, amount: 1 }, AbilityHelper),
-                    ['Sentinel']: this.buildYularenEffect(KeywordName.Sentinel, AbilityHelper),
-                    ['Shielded']: this.buildYularenEffect(KeywordName.Shielded, AbilityHelper)
+                choices: (context) => {
+                    const playedByPlayer = context.player;
+                    return {
+                        ['Grit']: this.buildYularenEffect(KeywordName.Grit, AbilityHelper, playedByPlayer),
+                        ['Restore 1']: this.buildYularenEffect({ keyword: KeywordName.Restore, amount: 1 }, AbilityHelper, playedByPlayer),
+                        ['Sentinel']: this.buildYularenEffect(KeywordName.Sentinel, AbilityHelper, playedByPlayer),
+                        ['Shielded']: this.buildYularenEffect(KeywordName.Shielded, AbilityHelper, playedByPlayer)
+                    };
                 }
             }
         });
     }
 
-    private buildYularenEffect(choice: KeywordNameOrProperties, AbilityHelper: IAbilityHelper) {
+    private buildYularenEffect(choice: KeywordNameOrProperties, AbilityHelper: IAbilityHelper, playedByPlayer: Player) {
         return AbilityHelper.immediateEffects.whileSourceInPlayCardEffect({
             ongoingEffectDescription: `give ${KeywordHelpers.keywordDescription(choice)} to`,
             ongoingEffectTargetDescription: 'each friendly Vehicle unit',
             effect: AbilityHelper.ongoingEffects.gainAbility({
                 type: AbilityType.Constant,
                 title: `Friendly Vehicle units gains ${choice}`,
-                targetController: RelativePlayer.Self,
+                targetController: WildcardRelativePlayer.Any,
                 targetCardTypeFilter: WildcardCardType.Unit,
-                matchTarget: (card) => card.hasSomeTrait(Trait.Vehicle),
+                matchTarget: (card) => card.hasSomeTrait(Trait.Vehicle) && card.controller === playedByPlayer,
                 ongoingEffect: AbilityHelper.ongoingEffects.gainKeyword(choice),
             })
         });
