2d1983fa22ee04024ca7d404bb21aa85e3bf206e
Valentin Jubert <val_jub@live.fr>
2026-07-04 21:10:36 +0200

    pr comments

--- server/game/cards/09_IC27/units/JarJarBinksBumblingRepresentative.ts
+++ server/game/cards/09_IC27/units/JarJarBinksBumblingRepresentative.ts
@@ -13,18 +13,17 @@ export default class JarJarBinksBumblingRepresentative extends NonLeaderUnitCard
     public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.addOnAttackAbility({
             title: 'Discard a card from your deck. If it costs 6 or more, this unit gets +4/+0 for this attack',
-            immediateEffect: abilityHelper.immediateEffects.sequential([
-                abilityHelper.immediateEffects.discardFromDeck((c) => ({
-                    amount: 1,
-                    target: c.player,
-                })),
-                abilityHelper.immediateEffects.conditional({
-                    condition: (c) => (c.events.length < 2 ? false : c.events[0].card.cost >= 6),
-                    onTrue: abilityHelper.immediateEffects.forThisAttackCardEffect({
-                        effect: abilityHelper.ongoingEffects.modifyStats({ power: 4, hp: 0 })
-                    })
+            immediateEffect: abilityHelper.immediateEffects.discardFromDeck((c) => ({
+                amount: 1,
+                target: c.player,
+            })),
+            ifYouDo: (ifYouDoContext) => ({
+                title: 'This unit gets +4/+0 for this attack',
+                ifYouDoCondition: () => (ifYouDoContext.events.length < 2 ? false : ifYouDoContext.events[0].card.cost >= 6),
+                immediateEffect: abilityHelper.immediateEffects.forThisAttackCardEffect({
+                    effect: abilityHelper.ongoingEffects.modifyStats({ power: 4, hp: 0 })
                 })
-            ]),
+            })
         });
     }
 }
