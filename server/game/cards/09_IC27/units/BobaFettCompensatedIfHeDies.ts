87c428ed5e973ef5645c6243f02a0ddf3301ab47
Valentin Jubert <val_jub@live.fr>
2026-07-05 10:06:44 +0200

    Boba Fett

---
+++ server/game/cards/09_IC27/units/BobaFettCompensatedIfHeDies.ts
@@ -0,0 +1,33 @@
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import { TextHelper } from '../../../core/utils/TextHelper';
+import * as AttackHelpers from '../../../core/attack/AttackHelpers';
+import type { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import type { CardsDefeatedThisPhaseWatcher } from '../../../stateWatchers/CardsDefeatedThisPhaseWatcher';
+
+export default class BobaFettCompensatedIfHeDies extends NonLeaderUnitCard {
+    private cardsDefeatedThisPhaseWatcher: CardsDefeatedThisPhaseWatcher;
+
+    protected override getImplementationId() {
+        return {
+            id: 'boba-fett#compensated-if-he-dies-id',
+            internalName: 'boba-fett#compensated-if-he-dies',
+        };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar, AbilityHelper: IAbilityHelper): void {
+        this.cardsDefeatedThisPhaseWatcher = AbilityHelper.stateWatchers.cardsDefeatedThisPhase();
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addWhenAttackEndsAbility({
+            title: `Ready ${TextHelper.resource(2)}`,
+            optional: true,
+            immediateEffect: abilityHelper.immediateEffects.conditional({
+                condition: (context) => AttackHelpers.defenderWasDefeated(context.event.attack, this.cardsDefeatedThisPhaseWatcher),
+                onTrue: abilityHelper.immediateEffects.readyResources({ amount: 2 }),
+            })
+        });
+    }
+}
