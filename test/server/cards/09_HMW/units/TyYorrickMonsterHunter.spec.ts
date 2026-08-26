f34706f772dd352e756ae715e0cbd739c012e388
Valentin Jubert <val_jub@live.fr>
2026-08-26 23:15:50 +0200

    Luke Skywalker & Ty Yorrick

---
+++ test/server/cards/09_HMW/units/TyYorrickMonsterHunter.spec.ts
@@ -0,0 +1,232 @@
+describe('Ty Yorrick, Monster Hunter', function() {
+    integration(function(contextRef) {
+        it('Ty Yorrick, Monster Hunter\'s on-attack ability should deal 1 damage to a Creature unit when attacking', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['ty-yorrick#monster-hunter', 'porg'],
+                },
+                player2: {
+                    groundArena: ['wampa', 'atst'],
+                    spaceArena: ['mynock']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.tyYorrickMonsterHunter);
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.player1).toBeAbleToSelectExactly([context.wampa, context.porg, context.mynock]);
+            expect(context.player1).toHavePassAbilityButton();
+            context.player1.clickCard(context.wampa);
+
+            // Constant ability triggering, pass it, will be tested in the damage modification test
+            context.player1.clickPrompt('Pass');
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.wampa.damage).toBe(1);
+        });
+
+        describe('Ty Yorrick, Monster Hunter\'s damage modification', function() {
+            it('should optionally increase friendly event ability damage by 1', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                        hand: ['daring-raid'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.daringRaid);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.wampa.damage).toBe(3);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not increase enemy event ability dealing damage', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                        hand: ['daring-raid'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.daringRaid);
+                context.player1.clickCard(context.tyYorrick);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.tyYorrick.damage).toBe(3);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should optionally increase friendly event ability damage by 1 (even if targeting friendly unit)', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                    },
+                    player2: {
+                        hand: ['daring-raid'],
+                        groundArena: ['wampa'],
+                        hasInitiative: true,
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player2.clickCard(context.daringRaid);
+                context.player2.clickCard(context.tyYorrick);
+
+                expect(context.player1).toBeActivePlayer();
+                expect(context.tyYorrick.damage).toBe(2);
+            });
+
+            it('should optionally increase friendly unit ability damage by 1', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.tyYorrick);
+                context.player1.clickCard(context.p2Base);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.wampa.damage).toBe(2);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should optionally increase friendly ability dealing indirect damage by 1', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['devastator#hunting-the-rebellion'],
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.devastator);
+                context.player1.setDistributeIndirectDamagePromptState(new Map([
+                    [context.p2Base, 4],
+                ]));
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.p2Base.damage).toBe(5);
+            });
+
+            it('should not increase friendly Overwhelm damage', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter', 'wampa'],
+                    },
+                    player2: {
+                        groundArena: ['porg']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wampa);
+                context.player1.clickCard(context.porg);
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.p2Base.damage).toBe(3);
+            });
+
+            it('should optionally increase friendly ability damage by 1 (multiple time on a turn)', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                        hand: ['daring-raid']
+                    },
+                    player2: {
+                        groundArena: ['mythosaur#folklore-awakened']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.daringRaid);
+                context.player1.clickCard(context.mythosaur);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.mythosaur.damage).toBe(3);
+
+                context.player2.passAction();
+
+                context.player1.clickCard(context.tyYorrick);
+                context.player1.clickCard(context.p2Base);
+                context.player1.clickCard(context.mythosaur);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Trigger');
+
+                expect(context.player2).toBeActivePlayer();
+                expect(context.mythosaur.damage).toBe(5);
+            });
+
+            it('may pass the damage increase', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        groundArena: ['ty-yorrick#monster-hunter'],
+                        hand: ['daring-raid'],
+                    },
+                    player2: {
+                        groundArena: ['wampa']
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.daringRaid);
+                context.player1.clickCard(context.wampa);
+
+                expect(context.player1).toHavePassAbilityPrompt('If a friendly ability would deal damage, you may have that ability deal that much damage plus 1 instead');
+                context.player1.clickPrompt('Pass');
+
+                expect(context.wampa.damage).toBe(2);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
