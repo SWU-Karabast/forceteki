9c1f5202c5165ad806141e6d26e38697e6e17075
Robbie Moyer <moyerr@users.noreply.github.com>
2026-08-31 04:37:21 +0000

    Fix zone filter for Ritual Dragon ability (#2745)
    This was causing its constant ability to be active even when out of
    play.

--- server/game/cards/09_HMW/units/RitualDragon.ts
+++ server/game/cards/09_HMW/units/RitualDragon.ts
@@ -1,7 +1,8 @@
 import type { IAbilityHelper } from '../../../AbilityHelper';
 import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
 import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
-import { EntryType, RelativePlayer, Trait, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+import { EffectName, EntryType, RelativePlayer, Trait, WildcardCardType, WildcardZoneName } from '../../../core/Constants';
+import { OngoingEffectBuilder } from '../../../core/ongoingEffect/OngoingEffectBuilder';
 import { TextHelper } from '../../../core/utils/TextHelper';
 
 export default class RitualDragon extends NonLeaderUnitCard {
@@ -14,9 +15,15 @@ export default class RitualDragon extends NonLeaderUnitCard {
 
     public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.addConstantAbility({
+            title: 'This unit enters play ready',
+            sourceZoneFilter: WildcardZoneName.Any,
+            condition: (context) => context.player.base.hasSomeTrait(Trait.Tatooine),
+            ongoingEffect: OngoingEffectBuilder.card.static(EffectName.EntersPlayReady)
+        });
+
+        registrar.addConstantAbility({
             title: `While you control a ${TextHelper.Trait.Tatooine} base, friendly units enter play ready`,
             condition: (context) => context.player.base.hasSomeTrait(Trait.Tatooine),
-            sourceZoneFilter: WildcardZoneName.Any,
             targetController: RelativePlayer.Self,
             ongoingEffect: abilityHelper.ongoingEffects.unitsEnterPlayReady({
                 entryType: new Set<EntryType>([EntryType.Played, EntryType.Created, EntryType.Rescued, EntryType.Deployed]),
