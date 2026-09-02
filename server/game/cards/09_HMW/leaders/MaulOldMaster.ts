b8cc4a9f0fc04f4cb7601fcf683d897423d4083a
Valentin Jubert <val_jub@live.fr>
2026-09-02 07:28:23 +0200

    maul

---
+++ server/game/cards/09_HMW/leaders/MaulOldMaster.ts
@@ -0,0 +1,67 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type {
+    ILeaderUnitAbilityRegistrar,
+    ILeaderUnitLeaderSideAbilityRegistrar
+} from '../../../core/card/AbilityRegistrationInterfaces';
+import { LeaderUnitCard } from '../../../core/card/LeaderUnitCard';
+import { RelativePlayer, WildcardCardType, ZoneName } from '../../../core/Constants';
+import { CostAdjustType } from '../../../core/cost/CostAdjuster';
+import type { StateWatcherRegistrar } from '../../../core/stateWatcher/StateWatcherRegistrar';
+import { TextHelper } from '../../../core/utils/TextHelper';
+import type { CardsDefeatedThisPhaseWatcher } from '../../../stateWatchers/CardsDefeatedThisPhaseWatcher';
+
+export default class MaulOldMaster extends LeaderUnitCard {
+    private cardsDefeatedThisPhaseWatcher: CardsDefeatedThisPhaseWatcher;
+
+    protected override getImplementationId() {
+        return {
+            id: 'maul#old-master-id',
+            internalName: 'maul#old-master',
+        };
+    }
+
+    protected override setupStateWatchers(registrar: StateWatcherRegistrar, abilityHelper: IAbilityHelper): void {
+        this.cardsDefeatedThisPhaseWatcher = abilityHelper.stateWatchers.cardsDefeatedThisPhase();
+    }
+
+    protected override setupLeaderSideAbilities(registrar: ILeaderUnitLeaderSideAbilityRegistrar, abilityHelper: IAbilityHelper): void {
+        registrar.addActionAbility({
+            title: `Play a unit from your hand. It costs ${TextHelper.resource(1)} less. Then, defeat it.`,
+            cost: abilityHelper.costs.exhaustSelf(),
+
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                zoneFilter: ZoneName.Hand,
+                controller: RelativePlayer.Self,
+                immediateEffect:
+                    abilityHelper.immediateEffects.sequential([
+                        abilityHelper.immediateEffects.playCardFromHand({
+                            adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 1 },
+                            playAsType: WildcardCardType.Unit,
+                        }),
+                        abilityHelper.immediateEffects.defeat()
+                    ])
+            }
+        });
+    }
+
+    protected override setupLeaderUnitSideAbilities(registrar: ILeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper): void {
+        registrar.addTriggeredAbility({
+            title: `Play a unit that was defeated this phase from your discard pile. It costs ${TextHelper.resource(5)} less.`,
+            optional: true,
+            when: {
+                onLeaderDeployed: (event, context) => event.card === context.source
+            },
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                zoneFilter: ZoneName.Discard,
+                controller: RelativePlayer.Self,
+                cardCondition: (card) => card.isUnit() && this.cardsDefeatedThisPhaseWatcher.wasDefeatedThisPhase(card),
+                immediateEffect: abilityHelper.immediateEffects.playCardFromOutOfPlay({
+                    adjustCost: { costAdjustType: CostAdjustType.Decrease, amount: 5 },
+                    playAsType: WildcardCardType.Unit
+                })
+            }
+        });
+    }
+}
