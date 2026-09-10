2e32f5ebe81292bf3453a7a1b80b0e63f9ff2707
Valentin Jubert <val_jub@live.fr>
2026-09-10 07:57:20 +0200

    pr comments

--- test/server/cards/09_HMW/units/KeeperOfSkaraNalAwoken.spec.ts
+++ test/server/cards/09_HMW/units/KeeperOfSkaraNalAwoken.spec.ts
@@ -16,21 +16,64 @@ describe('Keeper of Skara Nal, Awoken', function() {
             const handKeepers = context.player1.findCardsByName('keeper-of-skara-nal#awoken', 'hand');
             const groundKeeper = context.player1.findCardByName('keeper-of-skara-nal#awoken', 'groundArena');
 
-            // Attack with Keeper of Skara Nal
             context.player1.clickCard(groundKeeper);
             context.player1.clickCard(context.wampa);
 
-            // Trigger the On Attack ability and discard 2 named cards
             context.player1.clickPrompt('Discard 2 cards named Keeper of Skara Nal from your hand. If you do, this unit gets +15/+0 and gains Overwhelm for this attack.');
+
             expect(context.player1).toBeAbleToSelectExactly([handKeepers[0], handKeepers[1]]);
+            expect(context.player1).toHaveChooseNothingButton();
+            expect(context.player1).toHavePassAbilityButton();
+
             context.player1.clickCard(handKeepers[0]);
+            expect(context.player1).not.toHaveEnabledPromptButton('Done');
+            expect(context.player1).toHavePassAbilityButton();
+
             context.player1.clickCard(handKeepers[1]);
             context.player1.clickDone();
+
+            expect(context.player2).toBeActivePlayer();
 
             // Keeper gets +15/+0 (5 -> 20) and Overwhelm, defeating Wampa and dealing 15 excess to base
             expect(context.wampa).toBeInZone('discard');
             expect(context.p2Base.damage).toBe(15);
+
+            expect(handKeepers[0]).toBeInZone('discard', context.player1);
+            expect(handKeepers[1]).toBeInZone('discard', context.player1);
+        });
+
+        it('should not discard Keeper of Skara Nal cards if selecting one and pass (and should not have +15 and Overwhelm)', async function() {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['keeper-of-skara-nal#awoken', 'keeper-of-skara-nal#awoken', 'atst'],
+                    groundArena: ['keeper-of-skara-nal#awoken'],
+                },
+                player2: {
+                    groundArena: ['wampa']
+                }
+            });
+
+            const { context } = contextRef;
+            const handKeepers = context.player1.findCardsByName('keeper-of-skara-nal#awoken', 'hand');
+            const groundKeeper = context.player1.findCardByName('keeper-of-skara-nal#awoken', 'groundArena');
+
+            context.player1.clickCard(groundKeeper);
+            context.player1.clickCard(context.wampa);
+
+            context.player1.clickPrompt('Discard 2 cards named Keeper of Skara Nal from your hand. If you do, this unit gets +15/+0 and gains Overwhelm for this attack.');
+            expect(context.player1).toBeAbleToSelectExactly([handKeepers[0], handKeepers[1]]);
+            context.player1.clickCard(handKeepers[0]);
+            context.player1.clickPrompt('Pass');
+
             expect(context.player2).toBeActivePlayer();
+
+            // Keeper gets +15/+0 (5 -> 20) and Overwhelm, defeating Wampa and dealing 15 excess to base
+            expect(context.wampa).toBeInZone('discard');
+            expect(context.p2Base.damage).toBe(0);
+
+            expect(handKeepers[0]).toBeInZone('hand', context.player1);
+            expect(handKeepers[1]).toBeInZone('hand', context.player1);
         });
 
         it('should not trigger if player cannot discard 2 copy of Keeper of Skara Nal', async function() {
