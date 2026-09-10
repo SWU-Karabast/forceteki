b7afe2d8b67e3919b6500f3fd7a694b4d0b77b1b
Valentin Jubert <val_jub@live.fr>
2026-09-10 13:48:12 +0000

    Keeper of Skara Nal, Mother Talzin, Talzin's Shuttle (#2787)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/HMW/039.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/HMW/040.png)
    ![](https://swudb.com/cdn-cgi/image/width=300/images/cards/HMW/041.png)

---
+++ server/game/cards/09_HMW/units/KeeperOfSkaraNalAwoken.ts
@@ -0,0 +1,39 @@
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import { KeywordName, RelativePlayer, TargetMode, ZoneName } from '../../../core/Constants';
+import { TextHelper } from '../../../core/utils/TextHelper';
+
+export default class KeeperOfSkaraNalAwoken extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'keeper-of-skara-nal#awoken-id',
+            internalName: 'keeper-of-skara-nal#awoken',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, abilityHelper: IAbilityHelper) {
+        registrar.addOnAttackAbility({
+            title: `Discard 2 cards named Keeper of Skara Nal from your hand. If you do, this unit gets +15/+0 and gains ${TextHelper.Overwhelm} for this attack.`,
+            optional: true,
+            targetResolver: {
+                activePromptTitle: 'Choose 2 cards named Keeper of Skara Nal from your hand to discard',
+                mode: TargetMode.Exactly,
+                numCards: 2,
+                controller: RelativePlayer.Self,
+                zoneFilter: ZoneName.Hand,
+                cardCondition: (card) => card.title === 'Keeper of Skara Nal',
+                immediateEffect: abilityHelper.immediateEffects.discardSpecificCard()
+            },
+            ifYouDo: {
+                title: `This unit gets +15/+0 and gains ${TextHelper.Overwhelm} for this attack`,
+                immediateEffect: abilityHelper.immediateEffects.forThisAttackCardEffect({
+                    effect: [
+                        abilityHelper.ongoingEffects.modifyStats({ power: 15, hp: 0 }),
+                        abilityHelper.ongoingEffects.gainKeyword(KeywordName.Overwhelm)
+                    ]
+                })
+            }
+        });
+    }
+}
