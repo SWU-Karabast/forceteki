7a189389d6d1c9cca24078d8bc42a5aeb196291e
Valentin Jubert <val_jub@live.fr>
2026-09-10 11:49:04 +0000

    Fix Hidden deletion (#2799)
    Close #2791

--- test/server/cards/05_LOF/units/TuskenTracker.spec.ts
+++ test/server/cards/05_LOF/units/TuskenTracker.spec.ts
@@ -1,48 +1,60 @@
 describe('Tusken Tracker', function() {
     integration(function(contextRef) {
-        describe('Tusken Tracker\'s when played ability', function() {
-            it('should remove Hidden from all enemy units for the phase', async function () {
-                await contextRef.setupTestAsync({
-                    phase: 'action',
-                    player1: {
-                        hand: ['tusken-tracker'],
-                        groundArena: ['village-tender', 'witch-of-the-mist'],
-                    },
-                    player2: {
-                        groundArena: ['vulptex'],
-                        hand: ['banking-clan-shuttle', 'charging-phillak'],
-                    }
-                });
-
-                const { context } = contextRef;
-                context.player1.clickPrompt('pass');
-                context.player2.clickCard(context.bankingClanShuttle);
-
-
-                expect(context.villageTender.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.witchOfTheMist.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.bankingClanShuttle.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.vulptex.hasSomeKeyword('hidden')).toBeTrue();
-
-                context.player1.clickCard(context.tuskenTracker);
-
-                expect(context.villageTender.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.witchOfTheMist.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.bankingClanShuttle.hasSomeKeyword('hidden')).toBeFalse();
-                expect(context.vulptex.hasSomeKeyword('hidden')).toBeFalse();
-
-                context.player2.clickCard(context.chargingPhillak);
-                context.player2.clickPrompt('pass');
-                expect(context.chargingPhillak.hasSomeKeyword('hidden')).toBeTrue();
-
-                context.moveToRegroupPhase();
-
-                expect(context.villageTender.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.witchOfTheMist.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.bankingClanShuttle.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.vulptex.hasSomeKeyword('hidden')).toBeTrue();
-                expect(context.chargingPhillak.hasSomeKeyword('hidden')).toBeTrue();
+        it('Tusken Tracker\'s when played ability should remove Hidden from all enemy units for the phase', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['tusken-tracker', 'ewok-warrior'],
+                    groundArena: ['atst', 'rey#skywalker', 'porg']
+                },
+                player2: {
+                    hand: ['village-tender', 'vulptex', 'dooku#it-is-too-late'],
+                    groundArena: ['battlefield-marine', 'wampa']
+                }
             });
+
+            const { context } = contextRef;
+
+            // play hidden unit
+            context.player1.clickCard(context.ewokWarrior);
+            context.player2.clickCard(context.villageTender);
+
+            // hidden unit cannot be attacked
+            context.player1.clickCard(context.porg);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
+
+            context.player2.clickCard(context.wampa);
+            expect(context.player2).toBeAbleToSelectExactly([context.atst, context.rey, context.porg, context.p1Base]);
+            context.player2.clickCard(context.p1Base);
+
+            // play tusken tracker, enemy unit loose Hidden
+            context.player1.clickCard(context.tuskenTracker);
+
+            // friendly units do not loose Hidden
+            context.player2.clickCard(context.battlefieldMarine);
+            expect(context.player2).toBeAbleToSelectExactly([context.atst, context.rey, context.porg, context.tuskenTracker, context.p1Base]);
+            context.player2.clickCard(context.p1Base);
+
+            // Village Tender can be attacked
+            context.player1.clickCard(context.atst);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa, context.villageTender, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
+
+            context.moveToNextActionPhase();
+
+            // Hidden has no more effect
+            context.player1.clickCard(context.porg);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa, context.villageTender, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
+
+            // Dooku "hide" friendly units with Hidden
+            context.player2.clickCard(context.dooku);
+
+            // Village Tender cannot be attacked
+            context.player1.clickCard(context.rey);
+            expect(context.player1).toBeAbleToSelectExactly([context.battlefieldMarine, context.wampa, context.p2Base]);
+            context.player1.clickCard(context.p2Base);
         });
     });
 });
