0d9f163321934f5d54c2b3853c6c525ac5ff3ea7
Valentin Jubert <val_jub@live.fr>
2026-09-10 07:08:48 +0200

    moff jerjerrod case

--- test/server/cards/09_HMW/units/EravanaHaulingRathtars.spec.ts
+++ test/server/cards/09_HMW/units/EravanaHaulingRathtars.spec.ts
@@ -4,26 +4,48 @@ describe('Eravana, Hauling Rathtars', function () {
             await contextRef.setupTestAsync({
                 phase: 'action',
                 player1: {
-                    leader: 'sly-moore#cipher-in-the-dark',
-                    base: 'data-vault',
-                    resources: 8,
                     spaceArena: ['eravana#hauling-rathtars']
                 },
-                player2: {
-                    base: 'data-vault'
-                }
             });
 
             const { context } = contextRef;
-            const eravana = context.player1.findCardByName('eravana#hauling-rathtars');
 
-            context.player1.clickCard(eravana);
+            context.player1.clickCard(context.eravana);
             context.player1.clickCard(context.p2Base);
 
             const beast = context.player1.findCardByName('beast');
             expect(beast).toBeInZone('groundArena', context.player1);
             expect(beast.exhausted).toBeFalse();
             expect(context.player2).toBeActivePlayer();
+        });
+
+        it('Eravana\'s on attack ability should create a Beast token and readies it', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    groundArena: ['moff-jerjerrod#we-shall-redouble-our-efforts'],
+                    spaceArena: ['eravana#hauling-rathtars']
+                },
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.eravana);
+            context.player1.clickCard(context.p2Base);
+
+            expect(context.player1).toHavePassAbilityPrompt('Defeat Moff Jerjerrod to create 2 Beast tokens instead');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.player2).toBeActivePlayer();
+            expect(context.moffJerjerrod).toBeInZone('discard');
+
+            const beasts = context.player1.findCardsByName('beast', 'groundArena');
+            expect(beasts.length).toBe(2);
+
+            for (const beast of beasts) {
+                expect(beast).toBeInZone('groundArena', context.player1);
+                expect(beast.exhausted).toBeFalse();
+            }
         });
     });
 });
