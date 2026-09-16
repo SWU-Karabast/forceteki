39aebd50da362f86cac631babe072904b33afd42
Valentin Jubert <val_jub@live.fr>
2026-09-16 21:10:59 +0200

    Clone of the Zillo Beast

---
+++ server/game/cards/09_HMW/units/CloneOfTheZilloBeastEmperorsExperiment.ts
@@ -0,0 +1,31 @@
+import type { IAbilityHelper } from '../../../AbilityHelper';
+import type { INonLeaderUnitAbilityRegistrar } from '../../../core/card/AbilityRegistrationInterfaces';
+import { NonLeaderUnitCard } from '../../../core/card/NonLeaderUnitCard';
+import { RelativePlayer, WildcardCardType } from '../../../core/Constants';
+
+export default class CloneOfTheZilloBeastEmperorsExperiment extends NonLeaderUnitCard {
+    protected override getImplementationId() {
+        return {
+            id: 'clone-of-the-zillo-beast#emperors-experiment-id',
+            internalName: 'clone-of-the-zillo-beast#emperors-experiment',
+        };
+    }
+
+    public override setupCardAbilities(registrar: INonLeaderUnitAbilityRegistrar, AbilityHelper: IAbilityHelper) {
+        registrar.addConstantAbility({
+            title: 'Other friendly units get -2/-2',
+            targetController: RelativePlayer.Self,
+            matchTarget: (card, context) => card.isUnit() && card !== context.source,
+            ongoingEffect: AbilityHelper.ongoingEffects.modifyStats({ power: -2, hp: -2 })
+        });
+
+        registrar.addOnAttackAbility({
+            title: 'Give a Weakness token to a unit',
+            optional: true,
+            targetResolver: {
+                cardTypeFilter: WildcardCardType.Unit,
+                immediateEffect: AbilityHelper.immediateEffects.giveWeakness()
+            }
+        });
+    }
+}
