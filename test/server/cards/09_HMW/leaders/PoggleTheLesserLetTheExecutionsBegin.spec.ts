ba183bd6cb9325c998514cdd7ce3a10228557a8b
Robin Müller <robin.hsi@fhnw.ch>
2026-09-17 15:51:57 +0200

    adjusted pass

--- test/server/cards/09_HMW/leaders/PoggleTheLesserLetTheExecutionsBegin.spec.ts
+++ test/server/cards/09_HMW/leaders/PoggleTheLesserLetTheExecutionsBegin.spec.ts
@@ -177,9 +177,8 @@ describe('Poggle the Lesser, Let the Executions Begin', function () {
                 context.player1.clickCard(context.poggleTheLesser);
                 context.player1.clickCard(context.p2Base);
 
-                // Selection should allow Pass
-                expect(context.player1).toHaveEnabledPromptButton('Pass');
-                context.player1.clickPrompt('Pass');
+                // Should allow Pass
+                context.player1.passAction();
 
                 expect(context.huntingNexu.exhausted).toBeTrue();
                 expect(context.poggleTheLesser.exhausted).toBeTrue();
