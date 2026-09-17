43efaea21b12eb6937d7a7106ec0d6ffc352cee0
Valentin Jubert <val_jub@live.fr>
2026-09-17 17:47:17 +0200

    clean test

--- test/server/cards/09_HMW/units/MylayaRider.spec.ts
+++ test/server/cards/09_HMW/units/MylayaRider.spec.ts
@@ -16,8 +16,6 @@ describe('Mylaya Rider', function() {
 
                 const beast = context.player1.findCardByName('beast');
                 expect(beast).toBeInZone('groundArena', context.player1);
-                expect(beast.getPower()).toBe(3);
-                expect(beast.getHp()).toBe(3);
                 expect(context.p1Base.damage).toBe(3);
                 expect(context.player2).toBeActivePlayer();
             });
@@ -26,25 +24,6 @@ describe('Mylaya Rider', function() {
                 await contextRef.setupTestAsync({
                     phase: 'action',
                     player1: {
-                        hand: ['mylaya-rider']
-                    }
-                });
-
-                const { context } = contextRef;
-
-                context.player1.clickCard(context.mylayaRider);
-
-                const beast = context.player1.findCardByName('beast');
-                expect(beast).toBeInZone('groundArena', context.player1);
-                expect(context.p1Base.damage).toBe(0);
-                expect(context.player2).toBeActivePlayer();
-            });
-
-            it('should only heal up to the amount of damage on the base', async function() {
-                await contextRef.setupTestAsync({
-                    phase: 'action',
-                    player1: {
-                        base: { card: 'kestro-city', damage: 1 },
                         hand: ['mylaya-rider']
                     }
                 });
