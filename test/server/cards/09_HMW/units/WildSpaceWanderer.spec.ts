75227cf7288debd3c9f29c7f2f140d0b0f554c17
Valentin Jubert <val_jub@live.fr>
2026-09-17 13:39:05 +0200

    wild space wanderer

---
+++ test/server/cards/09_HMW/units/WildSpaceWanderer.spec.ts
@@ -0,0 +1,96 @@
+describe('Wild Space Wanderer', function() {
+    integration(function(contextRef) {
+        describe('Wild Space Wanderer\'s when played ability', function() {
+            it('should defeat an upgrade on a base', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wild-space-wanderer'],
+                        base: { card: 'kestro-city', upgrades: ['alliance-shield-generator'] }
+                    },
+                    player2: {
+                        base: { card: 'colossus', upgrades: ['trap-field'] }
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wildSpaceWanderer);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.allianceShieldGenerator, context.trapField]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                context.player1.clickCard(context.trapField);
+
+                expect(context.trapField).toBeInZone('discard', context.player2);
+                expect(context.allianceShieldGenerator).toBeAttachedTo(context.p1Base);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not be able to target upgrades on units', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wild-space-wanderer'],
+                        base: { card: 'kestro-city', upgrades: ['alliance-shield-generator'] }
+                    },
+                    player2: {
+                        groundArena: [{ card: 'wampa', upgrades: ['resilient'] }]
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wildSpaceWanderer);
+
+                expect(context.player1).toBeAbleToSelectExactly([context.allianceShieldGenerator]);
+                expect(context.player1).toHavePassAbilityButton();
+
+                context.player1.clickCard(context.allianceShieldGenerator);
+
+                expect(context.allianceShieldGenerator).toBeInZone('discard', context.player1);
+                expect(context.wampa).toHaveExactUpgradeNames(['resilient']);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should allow the player to decline defeating an upgrade', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wild-space-wanderer']
+                    },
+                    player2: {
+                        base: { card: 'colossus', upgrades: ['trap-field'] }
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wildSpaceWanderer);
+                context.player1.clickPrompt('Pass');
+
+                expect(context.trapField).toBeAttachedTo(context.p2Base);
+                expect(context.player2).toBeActivePlayer();
+            });
+
+            it('should not trigger when no base has an upgrade', async function() {
+                await contextRef.setupTestAsync({
+                    phase: 'action',
+                    player1: {
+                        hand: ['wild-space-wanderer']
+                    },
+                    player2: {
+                        groundArena: [{ card: 'wampa', upgrades: ['resilient'] }]
+                    }
+                });
+
+                const { context } = contextRef;
+
+                context.player1.clickCard(context.wildSpaceWanderer);
+
+                expect(context.wampa).toHaveExactUpgradeNames(['resilient']);
+                expect(context.player2).toBeActivePlayer();
+            });
+        });
+    });
+});
