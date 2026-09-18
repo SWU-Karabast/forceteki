bfdad0c46e27ef568fd614dc67030888cc94e9e9
sethhelman <s7helman@gmail.com>
2026-09-18 09:52:43 -0400

    Apply suggestion from @VJubert
    Co-authored-by: Valentin Jubert <val_jub@live.fr>

--- server/game/cards/09_HMW/events/FamiliarStrategem.ts
+++ server/game/cards/09_HMW/events/FamiliarStrategem.ts
@@ -13,7 +13,7 @@ export default class FamiliarStrategem extends EventCard {
 
     public override setupCardAbilities (registrar: IEventAbilityRegistrar, abilityHelper: IAbilityHelper) {
         registrar.setEventAbility({
-            title: 'Attack with a unit. If it shares a trait with another friendly unit, it gets +2/+0 for this attack.',
+            title: 'Attack with a unit. If it shares a Trait with another friendly unit, it gets +2/+0 for this attack.',
             initiateAttack: {
                 attackerLastingEffects: {
                     effect: abilityHelper.ongoingEffects.modifyStats({ power: 2, hp: 0 }),
