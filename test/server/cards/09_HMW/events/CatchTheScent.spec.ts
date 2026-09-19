b4194f0ba6f73a4b74070dc4436f3c8404dc6079
sethhelman <s7helman@gmail.com>
2026-09-18 22:53:52 -0400

    Update CatchTheScent.spec.ts

--- test/server/cards/09_HMW/events/CatchTheScent.spec.ts
+++ test/server/cards/09_HMW/events/CatchTheScent.spec.ts
@@ -46,5 +46,33 @@ describe('Catch the Scent', function() {
 
             expect(context.player2).toBeActivePlayer();
         });
+
+        it('Catch the Scent\'s ability should only ready 1 Beast with Jerjerrod', async function () {
+            await contextRef.setupTestAsync({
+                phase: 'action',
+                player1: {
+                    hand: ['catch-the-scent'],
+                    groundArena: ['moff-jerjerrod#we-shall-redouble-our-efforts']
+                }
+            });
+
+            const { context } = contextRef;
+
+            context.player1.clickCard(context.catchTheScent);
+            expect(context.player1).toHavePassAbilityPrompt('Defeat Moff Jerjerrod to create 4 Beast tokens instead');
+            context.player1.clickPrompt('Trigger');
+
+            expect(context.moffJerjerrod).toBeInZone('discard');
+
+            const p1Beasts = context.player1.findCardsByName('beast', 'groundArena');
+            expect(p1Beasts.length).toBe(4);
+            expect(p1Beasts.filter((beast) => beast.exhausted).length).toBe(3);
+            expect(p1Beasts.filter((beast) => !beast.exhausted).length).toBe(1);
+
+            const p2Beast = context.player2.findCardsByName('beast', 'groundArena');
+            expect(p2Beast.length).toBe(0);
+
+            expect(context.player2).toBeActivePlayer();
+        });
     });
 });
